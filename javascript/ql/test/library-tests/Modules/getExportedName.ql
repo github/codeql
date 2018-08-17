a9f1e21363d648256eb49998a0b7c0ea323da655
Max Schaefer <max@semmle.com>
2018-08-17 12:14:41 +0100

    JavaScript: Fix exported name of default re-exports.
    A default re-export (not part of the standard yet) looks like this:
    
    ```
    export f from 'mod';
    ```
    
    What this means is that the default export of `mod` is re-exported under the name `f`.
    
    Default re-export specifiers (like `f` in this example) are modelled as a kind of default export specifier in our library, but unlike normal default export specifiers they do not export the name `default`.
    
    This was previously not modelled correctly, leading to surprising errors down the line, for example in type inference where we suddenly would no longer be able to resolve an import that otherwise looked resolvable.

---
+++ javascript/ql/test/library-tests/Modules/getExportedName.ql
@@ -0,0 +1,4 @@
+import javascript
+
+from ExportSpecifier es
+select es, es.getLocalName()
--- javascript/ql/test/library-tests/Modules/m/c.js
+++ javascript/ql/test/library-tests/Modules/m/c.js
@@ -1,3 +1,7 @@
 import * as b from '../b';
 
 b.g();
+
+export { g as h } from '../b';
+
+// semmle-extractor-options: --experimental
