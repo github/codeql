003b600e2478fb0aaf4db19b61c782d30cae5156
Asger F <asger@semmle.com>
2018-09-04 13:11:52 +0100

    TypeScript: disable queries that rely on token information

---
+++ javascript/ql/test/query-tests/LanguageFeatures/SemicolonInsertion/template_literal.ts
@@ -0,0 +1,12 @@
+function foo(arg) {
+  console.log(arg);
+  console.log(arg);
+  console.log(arg);
+  console.log(arg);
+  console.log(arg);
+  console.log(arg);
+  console.log(arg);
+  console.log(arg);
+  console.log(arg);
+  console.log(`Unknown option '${arg}'.`);
+}
