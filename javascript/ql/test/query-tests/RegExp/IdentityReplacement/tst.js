0e63ea1b511a0a4017fc0dfd77df79978ccac935
Max Schaefer <max@semmle.com>
2018-09-25 11:03:15 +0100

    JavaScript: Update tests.

--- javascript/ql/test/query-tests/RegExp/IdentityReplacement/tst.js
+++ javascript/ql/test/query-tests/RegExp/IdentityReplacement/tst.js
@@ -2,3 +2,15 @@ raw.replace("\\", "\\"); // NOT OK
 raw.replace(/(\\)/, "\\"); // NOT OK
 raw.replace(/["]/, "\""); // NOT OK
 raw.replace("\\", "\\\\"); // OK
+
+raw.replace(/foo/g, 'foo'); // NOT OK
+raw.replace(/foo/gi, 'foo'); // OK
+
+raw.replace(/^\\/, "\\"); // NOT OK
+raw.replace(/\\$/, "\\"); // NOT OK
+raw.replace(/\b\\/, "\\"); // NOT OK
+raw.replace(/\B\\/, "\\"); // NOT OK
+raw.replace(/\\(?!\\)/, "\\"); // NOT OK
+raw.replace(/(?<!\\)\\/, "\\"); // NOT OK
+
+raw.replace(/^/, ""); // NOT OK
