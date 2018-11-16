9f688eb7e252fee3cfc045d6b0b3f1ea82ee5998
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2018-11-16 22:48:30 +0000

    CPP: Exclude variables that have non-function accesses.

--- cpp/ql/test/query-tests/JPL_C/LOC-3/Rule 13/LimitedScopeFunction/test.c
+++ cpp/ql/test/query-tests/JPL_C/LOC-3/Rule 13/LimitedScopeFunction/test.c
@@ -11,7 +11,7 @@ int globalInt6; // BAD [only used in func1]
 int globalInt7; // GOOD [not used, should be reported by another query]
 int globalInt8; // GOOD [used at file level]
 int *addrGlobalInt8 = &globalInt8; // GOOD [used in func1, func2]
-int globalInt9; // GOOD [used at file level and in func1] [FALSE POSITIVE]
+int globalInt9; // GOOD [used at file level and in func1]
 int *addrGlobalInt9 = &globalInt9; // GOOD [used in func1, func2]
 
 int externInt; // GOOD [extern'd so could be part of an interface]
