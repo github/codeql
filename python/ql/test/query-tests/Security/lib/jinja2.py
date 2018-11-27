1080525d7dd8509e693c364622d264b2ca72da39
Mark Shannon <mark@hotpy.org>
2018-11-27 14:14:32 +0000

    Python: Add missing test stub.

---
+++ python/ql/test/query-tests/Security/lib/jinja2.py
@@ -0,0 +1,15 @@
+
+class Environment(object):
+
+    def __init__(self, loader, autoescape):
+        pass
+
+def select_autoescape(files=[]):
+    def autoescape(template_name):
+        pass
+    return autoescape
+
+class FileSystemLoader(object):
+
+    def __init__(self, searchpath):
+        pass
