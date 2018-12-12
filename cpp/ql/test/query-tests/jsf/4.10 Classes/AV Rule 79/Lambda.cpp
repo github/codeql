8e2459a6b7605035362c0f75208d9bd02fea43f2
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2018-12-12 10:06:06 +0000

    CPP: Add similar test cases with function pointers.

--- cpp/ql/test/query-tests/jsf/4.10 Classes/AV Rule 79/Lambda.cpp
+++ cpp/ql/test/query-tests/jsf/4.10 Classes/AV Rule 79/Lambda.cpp
@@ -22,15 +22,35 @@ public:
 		deleter3(); 
 
 		r4 = new char[4096]; // BAD
+
+		r5 = new char[4096]; // GOOD [FALSE POSITIVE]
+		deleter5 = &deleter_for_r5;
+
+		r6 = new char[4096]; // GOOD [FALSE POSITIVE]
+		deleter6 = &testLambda::deleter_for_r6;
+	}
+
+	static void deleter_for_r5(char *r)
+	{
+		delete [] r;
+	}
+
+	void deleter_for_r6()
+	{
+		delete [] r6;
 	}
 
 	~testLambda()
 	{
 		deleter1(r1);
+		deleter5(r5);
+		((*this).*deleter6)();
 	}
 
 private:
-	char *r1, *r2, *r3, *r4;
+	char *r1, *r2, *r3, *r4, *r5, *r6;
 
-	void (*deleter1)(char *r); 
+	void (*deleter1)(char *r);
+	void (*deleter5)(char *r);
+	void (testLambda::*deleter6)();
 };
