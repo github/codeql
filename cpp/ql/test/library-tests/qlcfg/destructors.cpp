9146b8e32e38a4d0fa4c45b42959605d5592d644
Jonas Jensen <jonas@semmle.com>
2019-01-09 11:33:46 +0100

    C++: Add example of conditional destruction
    The QL CFG and extractor CFG are the same, so the test passes. Neither
    of them model that `ref` may or may not be destructed.

--- cpp/ql/test/library-tests/qlcfg/destructors.cpp
+++ cpp/ql/test/library-tests/qlcfg/destructors.cpp
@@ -36,3 +36,23 @@ void destructor_catch() {
     HasDtor d2 = { 0 };
   }
 }
+
+namespace cond_destruct {
+  struct C {
+      C();
+      C(const C&) = delete;
+      ~C();
+      int getInt() const;
+      void *data;
+  };
+
+  int f(int x) {
+      C local;
+      const C &ref = x ? (const C&)C() : (const C&)local;
+      return ref.getInt();
+      // If `x` was true, `ref` refers to a temporary object whose lifetime was
+      // extended to coincide with `ref`. Before the function returns, it
+      // should destruct `ref` if and only if the first branch was taken in the
+      // ?: expression.
+  }
+}
