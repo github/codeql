fa02042fdaab92628b39f6da7017e07020233793
Robert Marsh <rdmarsh@semmle.com>
2019-01-23 11:42:44 -0800

    C++: add more tests and rename test functions

--- cpp/ql/test/query-tests/Likely Bugs/Likely Typos/FutileParams/test.c
+++ cpp/ql/test/query-tests/Likely Bugs/Likely Typos/FutileParams/test.c
@@ -1,18 +1,29 @@
-void foo();
-void bar(void);
-void baz(int);
+void declared_empty();
+void declared_void(void);
+void declared_with(int);
+void declared_empty_defined_with();
 
 void test() {
-  foo(); // GOOD
-  foo(1); // BAD
-  bar(); // GOOD
-  baz(1); // GOOD
+  declared_empty(); // GOOD
+  declared_empty(1); // BAD
+  declared_void(); // GOOD
+  declared_with(1); // GOOD
   
   undeclared(1); // GOOD
   
   not_yet_declared1(1); // BAD
   not_yet_declared2(1); // GOOD
+
+  declared_empty_defined_with(); // BAD
+  declared_empty_defined_with(1); // GOOD
+
+  int x;
+  declared_empty_defined_with(&x); // BAD
+  declared_empty_defined_with(x, x); // BAD
 }
 
 void not_yet_declared1();
 void not_yet_declared2(int);
+void declared_empty_defined_with(int x) {
+  // do nothing
+}
