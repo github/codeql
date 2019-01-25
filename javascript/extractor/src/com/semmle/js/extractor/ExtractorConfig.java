72fe63074a30857d76bd140817d734a0554250b2
Asger F <asger@semmle.com>
2019-01-25 11:26:27 +0000

    JS: convert tabs to spaces

--- javascript/extractor/src/com/semmle/js/extractor/ExtractorConfig.java
+++ javascript/extractor/src/com/semmle/js/extractor/ExtractorConfig.java
@@ -52,25 +52,25 @@ public class ExtractorConfig {
     };
 
     /**
-	 * The type of a source file, which together with the {@link Platform}
-	 * determines how the top-level scope of the file behaves, and whether ES2015
-	 * module syntax should be allowed.
-	 * <p>
-	 * Note that the names of these enum members are depended on by {@link Main},
-	 * {@link AutoBuild}, and {@link JcornWrapper}.
-	 */
+     * The type of a source file, which together with the {@link Platform}
+     * determines how the top-level scope of the file behaves, and whether ES2015
+     * module syntax should be allowed.
+     * <p>
+     * Note that the names of these enum members are depended on by {@link Main},
+     * {@link AutoBuild}, and {@link JcornWrapper}.
+     */
     public static enum SourceType {
-		/** A script executed in the global scope. */
-		SCRIPT,
+        /** A script executed in the global scope. */
+        SCRIPT,
 
-		/** An ES2015 module. */
-		MODULE,
+        /** An ES2015 module. */
+        MODULE,
 
-		/** A Closure-Library module, defined using `goog.module()`. */
-		CLOSURE_MODULE,
+        /** A Closure-Library module, defined using `goog.module()`. */
+        CLOSURE_MODULE,
 
-		/** Automatically determined source type. */
-		AUTO;
+        /** Automatically determined source type. */
+        AUTO;
 
         @Override
         public String toString() {
