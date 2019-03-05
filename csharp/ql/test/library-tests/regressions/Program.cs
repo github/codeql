58fdcd8703fa12e43da138a2b483b4db739f7618
calum <calum@semmle.com>
2019-03-05 18:24:01 +0000

    C#: Regression test case demonstrating extractor error.

--- csharp/ql/test/library-tests/regressions/Program.cs
+++ csharp/ql/test/library-tests/regressions/Program.cs
@@ -147,4 +147,78 @@ partial class C1<T> where T: DynamicType
 {
 }
 
+namespace NoPia
+{
+    class CommonEmbeddedTypesManager { }
+    class CommonPEModuleBuilder { }
+    class SyntaxNode {  }
+    class CommonCompilationState { }
+    class CommonModuleCompilationState { }
+    class AttributeData { }
+
+    namespace Cci
+    {
+        interface IGenericMethodParameterReference { }
+        interface ICustomAttribute { }
+        interface INamespaceTypeReference { }
+        interface IFieldReference { }
+        interface ITypeMemberReference { }
+        interface IMethodReference { }
+        interface IParameterListEntry { }
+        interface INamedEntity { }
+    }
+
+    internal abstract partial class EmbeddedTypesManager<
+        TPEModuleBuilder,
+        TModuleCompilationState,
+        TEmbeddedTypesManager,
+        TSyntaxNode,
+        TAttributeData,
+        TSymbol,
+        TAssemblySymbol,
+        TNamedTypeSymbol,
+        TFieldSymbol,
+        TMethodSymbol,
+        TEventSymbol,
+        TPropertySymbol,
+        TParameterSymbol,
+        TTypeParameterSymbol,
+        TEmbeddedType,
+        TEmbeddedField,
+        TEmbeddedMethod,
+        TEmbeddedEvent,
+        TEmbeddedProperty,
+        TEmbeddedParameter,
+        TEmbeddedTypeParameter> : CommonEmbeddedTypesManager
+        where TPEModuleBuilder : CommonPEModuleBuilder
+        where TModuleCompilationState : CommonModuleCompilationState
+        where TEmbeddedTypesManager : EmbeddedTypesManager<TPEModuleBuilder, TModuleCompilationState, TEmbeddedTypesManager, TSyntaxNode, TAttributeData, TSymbol, TAssemblySymbol, TNamedTypeSymbol, TFieldSymbol, TMethodSymbol, TEventSymbol, TPropertySymbol, TParameterSymbol, TTypeParameterSymbol, TEmbeddedType, TEmbeddedField, TEmbeddedMethod, TEmbeddedEvent, TEmbeddedProperty, TEmbeddedParameter, TEmbeddedTypeParameter>
+        where TSyntaxNode : SyntaxNode
+        where TAttributeData : AttributeData, Cci.ICustomAttribute
+        where TAssemblySymbol : class, TSymbol
+        where TNamedTypeSymbol : class, TSymbol, Cci.INamespaceTypeReference
+        where TFieldSymbol : class, TSymbol, Cci.IFieldReference
+        where TMethodSymbol : class, TSymbol, Cci.IMethodReference
+        where TEventSymbol : class, TSymbol, Cci.ITypeMemberReference
+        where TPropertySymbol : class, TSymbol, Cci.ITypeMemberReference
+        where TParameterSymbol : class, TSymbol, Cci.IParameterListEntry, Cci.INamedEntity
+        where TTypeParameterSymbol : class, TSymbol, Cci.IGenericMethodParameterReference
+        where TEmbeddedType : EmbeddedTypesManager<TPEModuleBuilder, TModuleCompilationState, TEmbeddedTypesManager, TSyntaxNode, TAttributeData, TSymbol, TAssemblySymbol, TNamedTypeSymbol, TFieldSymbol, TMethodSymbol, TEventSymbol, TPropertySymbol, TParameterSymbol, TTypeParameterSymbol, TEmbeddedType, TEmbeddedField, TEmbeddedMethod, TEmbeddedEvent, TEmbeddedProperty, TEmbeddedParameter, TEmbeddedTypeParameter>.CommonEmbeddedType
+        where TEmbeddedField : EmbeddedTypesManager<TPEModuleBuilder, TModuleCompilationState, TEmbeddedTypesManager, TSyntaxNode, TAttributeData, TSymbol, TAssemblySymbol, TNamedTypeSymbol, TFieldSymbol, TMethodSymbol, TEventSymbol, TPropertySymbol, TParameterSymbol, TTypeParameterSymbol, TEmbeddedType, TEmbeddedField, TEmbeddedMethod, TEmbeddedEvent, TEmbeddedProperty, TEmbeddedParameter, TEmbeddedTypeParameter>.CommonEmbeddedField
+        where TEmbeddedMethod : EmbeddedTypesManager<TPEModuleBuilder, TModuleCompilationState, TEmbeddedTypesManager, TSyntaxNode, TAttributeData, TSymbol, TAssemblySymbol, TNamedTypeSymbol, TFieldSymbol, TMethodSymbol, TEventSymbol, TPropertySymbol, TParameterSymbol, TTypeParameterSymbol, TEmbeddedType, TEmbeddedField, TEmbeddedMethod, TEmbeddedEvent, TEmbeddedProperty, TEmbeddedParameter, TEmbeddedTypeParameter>.CommonEmbeddedMethod
+        where TEmbeddedEvent : EmbeddedTypesManager<TPEModuleBuilder, TModuleCompilationState, TEmbeddedTypesManager, TSyntaxNode, TAttributeData, TSymbol, TAssemblySymbol, TNamedTypeSymbol, TFieldSymbol, TMethodSymbol, TEventSymbol, TPropertySymbol, TParameterSymbol, TTypeParameterSymbol, TEmbeddedType, TEmbeddedField, TEmbeddedMethod, TEmbeddedEvent, TEmbeddedProperty, TEmbeddedParameter, TEmbeddedTypeParameter>.CommonEmbeddedEvent
+        where TEmbeddedProperty : EmbeddedTypesManager<TPEModuleBuilder, TModuleCompilationState, TEmbeddedTypesManager, TSyntaxNode, TAttributeData, TSymbol, TAssemblySymbol, TNamedTypeSymbol, TFieldSymbol, TMethodSymbol, TEventSymbol, TPropertySymbol, TParameterSymbol, TTypeParameterSymbol, TEmbeddedType, TEmbeddedField, TEmbeddedMethod, TEmbeddedEvent, TEmbeddedProperty, TEmbeddedParameter, TEmbeddedTypeParameter>.CommonEmbeddedProperty
+        where TEmbeddedParameter : EmbeddedTypesManager<TPEModuleBuilder, TModuleCompilationState, TEmbeddedTypesManager, TSyntaxNode, TAttributeData, TSymbol, TAssemblySymbol, TNamedTypeSymbol, TFieldSymbol, TMethodSymbol, TEventSymbol, TPropertySymbol, TParameterSymbol, TTypeParameterSymbol, TEmbeddedType, TEmbeddedField, TEmbeddedMethod, TEmbeddedEvent, TEmbeddedProperty, TEmbeddedParameter, TEmbeddedTypeParameter>.CommonEmbeddedParameter
+        where TEmbeddedTypeParameter : EmbeddedTypesManager<TPEModuleBuilder, TModuleCompilationState, TEmbeddedTypesManager, TSyntaxNode, TAttributeData, TSymbol, TAssemblySymbol, TNamedTypeSymbol, TFieldSymbol, TMethodSymbol, TEventSymbol, TPropertySymbol, TParameterSymbol, TTypeParameterSymbol, TEmbeddedType, TEmbeddedField, TEmbeddedMethod, TEmbeddedEvent, TEmbeddedProperty, TEmbeddedParameter, TEmbeddedTypeParameter>.CommonEmbeddedTypeParameter
+    {
+        public class CommonEmbeddedType {  }
+        public class CommonEmbeddedField { }
+        public class CommonEmbeddedMethod { }
+        public class CommonEmbeddedEvent { }
+        public class CommonEmbeddedProperty { }
+        public class CommonEmbeddedParameter { }
+        public class CommonEmbeddedTypeParameter { }
+    }
+}
+
 // semmle-extractor-options: /r:System.Dynamic.Runtime.dll
