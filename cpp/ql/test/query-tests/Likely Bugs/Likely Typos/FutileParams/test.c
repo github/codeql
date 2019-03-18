0c350dc50476f22200c98a8c3815acaac7403b50
Ziemowit Laski <zlaski@semmle.com>
2019-03-18 16:10:35 -0700

    [CPP-340] Create QL query for function call argument count mismatches.           Update QHELP file, test and test results.

--- cpp/ql/test/query-tests/Likely Bugs/Likely Typos/FutileParams/test.c
+++ cpp/ql/test/query-tests/Likely Bugs/Likely Typos/FutileParams/test.c
@@ -1,7 +1,6 @@
 void declared_empty();
 void declared_void(void);
 void declared_with(int);
-void declared_empty_defined_with();
 
 void test() {
   declared_empty(); // GOOD
@@ -9,17 +8,20 @@ void test() {
   declared_void(); // GOOD
   declared_with(1); // GOOD
   
+  declared_ellipsis(); // GOOD
+  declared_ellipsis(2);  // GOOD
+
   undeclared(1); // GOOD
   
   not_yet_declared1(1); // BAD
   not_yet_declared2(1); // GOOD
 
-  declared_empty_defined_with(); // BAD [NOT DETECTED]
+  declared_empty_defined_with(); // BAD
   declared_empty_defined_with(1); // GOOD
 
   int x;
-  declared_empty_defined_with(&x); // BAD [NOT DETECTED]
-  declared_empty_defined_with(x, x); // BAD [NOT DETECTED]
+  declared_empty_defined_with(&x); // GOOD
+  declared_empty_defined_with(x, x); // BAD
 }
 
 void not_yet_declared1();
