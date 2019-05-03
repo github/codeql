f3a4acf0b24e8a501e47608e397f8e41d9b99285
Asger F <asger@semmle.com>
2019-05-03 13:41:04 +0100

    JS: Add async functions to test

--- javascript/ql/test/library-tests/TaintTracking/exceptions.js
+++ javascript/ql/test/library-tests/TaintTracking/exceptions.js
@@ -23,7 +23,7 @@ function test(unsafe, safe) {
     sink(e); // NOT OK
     sink(e.toString()); // NOT OK
     sink(e.message); // NOT OK
-    sink(e.fileName); // OK
+    sink(e.fileName); // OK - but flagged anyway
   }
 
   try {
@@ -32,7 +32,7 @@ function test(unsafe, safe) {
     sink(e); // NOT OK
     sink(e.toString()); // NOT OK
     sink(e.message); // NOT OK
-    sink(e.fileName); // OK
+    sink(e.fileName); // OK - but flagged anyway
   }
 
   try {
@@ -41,7 +41,25 @@ function test(unsafe, safe) {
     sink(e); // NOT OK
     sink(e.toString()); // NOT OK
     sink(e.message); // NOT OK
-    sink(e.fileName); // OK
+    sink(e.fileName); // OK - but flagged anyway
+  }
+
+  try {
+    throwAsync(source());
+  } catch (e) {
+    sink(e); // OK - but flagged anyway
+  }
+
+  throwAsync(source()).catch(e => {
+    sink(e); // NOT OK - but not flagged
+  });
+
+  async function asyncTester() {
+    try {
+      await throwAsync(source());
+    } catch (e) {
+      sink(e); // NOT OK
+    }
   }
 }
 
@@ -63,5 +81,11 @@ function throwError1(x) {
   throw new Error(x);
 }
 
+async function throwAsync(x) {
+  throw x; // doesn't actually throw - returns failed promise
+}
+
 test(source(), "hello");
 test("hey", "hello"); // no single-call inlining
+
+// semmle-extractor-options: --experimental
