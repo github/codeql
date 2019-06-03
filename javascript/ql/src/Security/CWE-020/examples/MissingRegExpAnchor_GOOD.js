04868e5b97b4abfaf19755854ca932f33b69d96f
Esben Sparre Andreasen <esben@semmle.com>
2019-06-03 17:05:19 +0200

    JS: format qhelp examples

--- javascript/ql/src/Security/CWE-020/examples/MissingRegExpAnchor_GOOD.js
+++ javascript/ql/src/Security/CWE-020/examples/MissingRegExpAnchor_GOOD.js
@@ -1,7 +1,7 @@
-app.get('/some/path', function(req, res) {
+app.get("/some/path", function(req, res) {
     let url = req.param("url");
     // GOOD: the host of `url` can not be controlled by an attacker
-	if (url.match(/^https?:\/\/www\.example\.com\//)) {
+    if (url.match(/^https?:\/\/www\.example\.com\//)) {
         res.redirect(url);
     }
 });
