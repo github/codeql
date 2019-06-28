4da7a17f4b5ec90440ca031a1f8b56bcfdd88865
Tom Hvitved <tom@semmle.com>
2019-06-28 11:25:18 +0200

    C#: Add more tests for `cs/constant-condition`

--- csharp/ql/test/query-tests/Bad Practices/Control-Flow/ConstantCondition/ConstantCondition.cs
+++ csharp/ql/test/query-tests/Bad Practices/Control-Flow/ConstantCondition/ConstantCondition.cs
@@ -1,4 +1,4 @@
-// semmle-extractor-options: /r:System.Threading.Thread.dll /r:System.Diagnostics.Debug.dll
+// semmle-extractor-options: /r:System.Threading.Thread.dll /r:System.Diagnostics.Debug.dll /langversion:preview
 
 using System;
 using System.Collections;
@@ -88,6 +88,23 @@ class ConstantMatching
             case IList _ : // GOOD
               break;
         }
+    }
+
+    string M4(object o)
+    {
+        return o switch
+        {
+            _ => o.ToString() // BAD
+        };
+    }
+
+    string M5(object o)
+    {
+        return o switch
+        {
+            "" => " ",
+            _ => o.ToString() // GOOD
+        };
     }
 }
 
