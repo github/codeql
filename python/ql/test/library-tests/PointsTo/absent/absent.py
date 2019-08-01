ebd5829bfbe4d59b8aa3a34b4829b2df536d38c1
Mark Shannon <mark@hotpy.org>
2019-08-01 10:37:41 +0100

    Python: Treat the result of calling a missing module member as 'unknown'.

--- python/ql/test/library-tests/PointsTo/absent/absent.py
+++ python/ql/test/library-tests/PointsTo/absent/absent.py
@@ -12,3 +12,26 @@ open()
 import module
 
 module
+
+from xxxx import deco
+
+@deco
+def func1():
+    pass
+
+@deco(0)
+def func2():
+    pass
+
+@deco(undefined)
+def func3():
+    pass
+
+@deco(open)
+def func4():
+    pass
+
+func1
+func2
+func3
+func4
