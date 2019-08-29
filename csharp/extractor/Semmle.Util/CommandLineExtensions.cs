8f3f9406e27ae9c3bbc648f170359edc31272270
Tom Hvitved <tom@semmle.com>
2019-08-29 14:50:38 +0200

    C#: Early identification of duplicate extraction

--- csharp/extractor/Semmle.Util/CommandLineExtensions.cs
+++ csharp/extractor/Semmle.Util/CommandLineExtensions.cs
@@ -11,13 +11,16 @@ namespace Semmle.Util
         /// Subsequent "@" arguments are ignored.
         /// </summary>
         /// <param name="commandLineArguments">The raw command line arguments.</param>
-        /// <param name="filename">The full filename to write to.</param>
+        /// <param name="textWriter">The writer to archive to.</param>
         /// <returns>True iff the file was written.</returns>
-        public static bool ArchiveCommandLine(this IEnumerable<string> commandLineArguments, string filename)
+        public static bool WriteCommandLine(this IEnumerable<string> commandLineArguments, TextWriter textWriter)
         {
             foreach (var arg in commandLineArguments.Where(arg => arg[0] == '@').Select(arg => arg.Substring(1)))
             {
-                File.Copy(arg, filename, true);
+                string line;
+                using (StreamReader file = new StreamReader(arg))
+                    while ((line = file.ReadLine()) != null)
+                        textWriter.WriteLine(line);
                 return true;
             }
             return false;
