fd3615d1201809027f2e4bdf336f466aa7188b29
Jonas Jensen <jonas@semmle.com>
2019-09-06 10:52:25 +0200

    C++: Show that there are too many partial defs

--- cpp/ql/test/library-tests/dataflow/partialdefinitions/partialdefinitions.cpp
+++ cpp/ql/test/library-tests/dataflow/partialdefinitions/partialdefinitions.cpp
@@ -14,3 +14,18 @@ void test()
 	s.x = 1;
 	s.y.z = 1;
 }
+
+struct Int {
+  int data;
+  Int(int value) : data(value) { } // Not a partial definition but a `PostUpdateNode`
+  int getData() const { return data; }
+  void setData(int value) { data = value; }
+};
+
+int getSet() {
+  Int myInt(1);
+  myInt.setData(
+      myInt.getData() + 1 // should not be a partial def
+  );
+  return myInt.getData(); // should not be a partial def
+}
