ccdc821c5d187cdf66de811e69bf319033dbc9a8
Erik Krogh Kristensen <erik@semmle.com>
2019-09-06 14:32:58 +0100

    add xlink:href as xss target when using setAttribute

--- javascript/ql/test/query-tests/Security/CWE-079/tst3.js
+++ javascript/ql/test/query-tests/Security/CWE-079/tst3.js
@@ -4,6 +4,7 @@ var data = JSON.parse(decodeURIComponent(window.location.search.substr(1)));
 foo.setAttribute("src", data.src); // NOT OK
 foo.setAttribute("HREF", data.p);  // NOT OK
 foo.setAttribute("width", data.w); // OK
+foo.setAttribute("xlink:href", data.p) // NOT OK
 
 for (var p in data)
   foo.setAttribute(p, data[p]); // not flagged since attribute name is unknown
