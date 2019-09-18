cafa9edf691ec223bbfb4d2f6eae140ddb6020ca
Asger F <asger@semmle.com>
2019-09-18 15:42:42 +0100

    JS: upgrade script, stats, version string

--- javascript/extractor/src/com/semmle/js/extractor/Main.java
+++ javascript/extractor/src/com/semmle/js/extractor/Main.java
@@ -1,5 +1,13 @@
 package com.semmle.js.extractor;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+
 import com.semmle.js.extractor.ExtractorConfig.HTMLHandling;
 import com.semmle.js.extractor.ExtractorConfig.Platform;
 import com.semmle.js.extractor.ExtractorConfig.SourceType;
@@ -23,13 +31,6 @@ import com.semmle.util.language.LegacyLanguage;
 import com.semmle.util.process.ArgsParser;
 import com.semmle.util.process.ArgsParser.FileMode;
 import com.semmle.util.trap.TrapWriter;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.regex.Pattern;
 
 /** The main entry point of the JavaScript extractor. */
 public class Main {
@@ -37,7 +38,7 @@ public class Main {
    * A version identifier that should be updated every time the extractor changes in such a way that
    * it may produce different tuples for the same file under the same {@link ExtractorConfig}.
    */
-  public static final String EXTRACTOR_VERSION = "2019-09-13";
+  public static final String EXTRACTOR_VERSION = "2019-09-18";
 
   public static final Pattern NEWLINE = Pattern.compile("\n");
 
