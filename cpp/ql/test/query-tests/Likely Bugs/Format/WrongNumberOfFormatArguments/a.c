f7607313e7366eed95fb117532e294be4ab10ce6
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2019-09-20 15:12:55 +0100

    CPP: Fix FPs.

--- cpp/ql/test/query-tests/Likely Bugs/Format/WrongNumberOfFormatArguments/a.c
+++ cpp/ql/test/query-tests/Likely Bugs/Format/WrongNumberOfFormatArguments/a.c
@@ -12,7 +12,7 @@ void test_custom_printf1()
   myMultiplyDefinedPrintf("%i", 0); // BAD (too few format arguments)
   myMultiplyDefinedPrintf("%i", 0, 1); // GOOD
   myMultiplyDefinedPrintf("%i", 0, 1, 2); // BAD (too many format arguments)
-  myMultiplyDefinedPrintf2("%i", 0); // GOOD (we can't tell which definition is correct so we have to assume this is OK) [FALSE POSITIVE]
+  myMultiplyDefinedPrintf2("%i", 0); // GOOD (we can't tell which definition is correct so we have to assume this is OK)
   myMultiplyDefinedPrintf2("%i", 0, 1); // GOOD (we can't tell which definition is correct so we have to assume this is OK)
-  myMultiplyDefinedPrintf2("%i", 0, 1, 2); // GOOD (we can't tell which definition is correct so we have to assume this is OK) [FALSE POSITIVE]
+  myMultiplyDefinedPrintf2("%i", 0, 1, 2); // GOOD (we can't tell which definition is correct so we have to assume this is OK)
 }
