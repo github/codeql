2d8e4b3176f8bd53424f05581909eb4385bc0062
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2019-09-23 11:28:50 +0100

    CPP: Additional cases resembling the ticket.

--- cpp/ql/test/query-tests/Likely Bugs/Format/WrongNumberOfFormatArguments/a.c
+++ cpp/ql/test/query-tests/Likely Bugs/Format/WrongNumberOfFormatArguments/a.c
@@ -4,8 +4,12 @@ void myMultiplyDefinedPrintf(const char *format, int extraArg, ...)
 {
 	// ...
 }
+
 __attribute__((format(printf, 1, 3)))
 void myMultiplyDefinedPrintf2(const char *format, int extraArg, ...);
+
+__attribute__((format(printf, 2, 3)))
+void myMultiplyDefinedPrintf3(int extraArg, const char *format, ...);
 
 void test_custom_printf1()
 {
@@ -15,4 +19,7 @@ void test_custom_printf1()
   myMultiplyDefinedPrintf2("%i", 0); // GOOD (we can't tell which definition is correct so we have to assume this is OK)
   myMultiplyDefinedPrintf2("%i", 0, 1); // GOOD (we can't tell which definition is correct so we have to assume this is OK)
   myMultiplyDefinedPrintf2("%i", 0, 1, 2); // BAD (too many format arguments regardless of which definition is correct) [NOT DETECTED]
+  myMultiplyDefinedPrintf3("%s", "%s"); // GOOD (we can't tell which definition is correct so we have to assume this is OK)
+  myMultiplyDefinedPrintf3("%s", "%s", "%s"); // GOOD (we can't tell which definition is correct so we have to assume this is OK)
+  myMultiplyDefinedPrintf3("%s", "%s", "%s", "%s"); // BAD (too many format arguments regardless of which definition is correct) [NOT DETECTED]
 }
--- cpp/ql/test/query-tests/Likely Bugs/Format/WrongNumberOfFormatArguments/c.c
+++ cpp/ql/test/query-tests/Likely Bugs/Format/WrongNumberOfFormatArguments/c.c
@@ -8,4 +8,7 @@ void test_custom_printf2()
   myMultiplyDefinedPrintf2("%i", 0); // GOOD (we can't tell which definition is correct so we have to assume this is OK)
   myMultiplyDefinedPrintf2("%i", 0, 1); // GOOD (we can't tell which definition is correct so we have to assume this is OK)
   myMultiplyDefinedPrintf2("%i", 0, 1, 2); // BAD (too many format arguments regardless of which definition is correct) [NOT DETECTED]
+  myMultiplyDefinedPrintf3("%s", "%s"); // GOOD (we can't tell which definition is correct so we have to assume this is OK)
+  myMultiplyDefinedPrintf3("%s", "%s", "%s"); // GOOD (we can't tell which definition is correct so we have to assume this is OK)
+  myMultiplyDefinedPrintf3("%s", "%s", "%s", "%s"); // BAD (too many format arguments regardless of which definition is correct) [NOT DETECTED]
 }
