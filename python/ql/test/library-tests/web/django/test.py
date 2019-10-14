471318369b4fee5c66239a5f33c91d9410df8f76
Rasmus Wriedt Larsen <rasmuswl@semmle.com>
2019-10-14 16:51:50 +0200

    Python: Don't quote %s in django example
    This is vulnerable to SQL injection because of the quotes around %s -- added
    some code that highlights this in test.py
    
    Since our examples did this in the safe query, I ended up rewriting them
    completely, causing a lot of trouble for myself :D

--- python/ql/test/library-tests/web/django/test.py
+++ python/ql/test/library-tests/web/django/test.py
@@ -5,28 +5,38 @@ from django.db.models.expressions import RawSQL
 from django.http.response import HttpResponse
 import base64
 
-class Name(models.Model):
+class User(models.Model):
     pass
 
-def save_name(request):
+def show_user(request, username):
+    with connection.cursor() as cursor:
+        # GOOD -- Using parameters
+        cursor.execute("SELECT * FROM users WHERE username = %s", username)
+        User.objects.raw("SELECT * FROM users WHERE username = %s", (username,))
 
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        curs = connection.cursor()
-        #GOOD -- Using parameters
-        curs.execute(
-            "insert into names_file ('name') values ('%s')", name)
-        #BAD -- Using string formatting
-        curs.execute(
-            "insert into names_file ('name') values ('%s')" % name)
+        # BAD -- Using string formatting
+        cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
 
-        #BAD -- other ways of executing raw SQL code with string interpolation
-        Name.objects.annotate(RawSQL("insert into names_file ('name') values ('%s')" % name))
-        Name.objects.raw("insert into names_file ('name') values ('%s')" % name)
-        Name.objects.extra("insert into names_file ('name') values ('%s')" % name)
+        # BAD -- other ways of executing raw SQL code with string interpolation
+        User.objects.annotate(RawSQL("insert into names_file ('name') values ('%s')" % username))
+        User.objects.raw("insert into names_file ('name') values ('%s')" % username)
+        User.objects.extra("insert into names_file ('name') values ('%s')" % username)
 
-urlpatterns1 = patterns(url(r'^save_name/$',
-                           save_name, name='save_name'))
+        # BAD (but currently no custom query to find this)
+        #
+        # It is exposed to SQL injection (https://docs.djangoproject.com/en/2.2/ref/models/querysets/#extra)
+        # For example, using name = "; DROP ALL TABLES -- "
+        # will result in SQL: SELECT * FROM name WHERE name = ''; DROP ALL TABLES -- ''
+        #
+        # This shouldn't be very widespread, since using a normal string will result in invalid SQL
+        # Using name = "example", will result in SQL: SELECT * FROM name WHERE name = ''example''
+        # which in MySQL will give a syntax error
+        #
+        # When testing this out locally, none of the queries worked against SQLite3, but I could use
+        # the SQL injection against MySQL.
+        User.objects.raw("SELECT * FROM users WHERE username = '%s'", (username,))
+
+urlpatterns = patterns(url(r'^users/(?P<username>[^/]+)$', show_user))
 
 def maybe_xss(request):
     first_name = request.POST.get('first_name', '')
@@ -34,9 +44,12 @@ def maybe_xss(request):
     resp.write("first name is " + first_name)
     return resp
 
+def xss_kwargs(request, foo, bar, baz=None):
+    return HttpResponse('xss_kwargs: {} {}'.format(foo, bar))
+
 urlpatterns = [
-    # Route to code_execution
-    url(r'^maybe_xss$', maybe_xss, name='maybe_xss')
+    url(r'^maybe_xss$', maybe_xss, name='maybe_xss'),
+    url(r'^bar/(?P<bar>[^/]+)/foo/(?P<foo>[^/]+)', xss_kwargs, name='xss_kwargs'),
 ]
 
 
