165db5b2143751e58a135802f550afd508a9f21a
Ziemowit Laski <zlaski@semmle.com>
2019-10-28 19:42:00 -0700

    Revert "[CPP-435] Initial version." It belongs in [zlaski/pointer-overflow-check] branch.
    This reverts commit 9d6e8a59405ebddc1884641078b43fe31bd82100.

--- cpp/ql/src/Likely Bugs/Memory Management/PointerOverflowCheck-bad.cpp
+++
@@ -1,3 +0,0 @@
-bool check_pointer_overflow(P *ptr) {
-    return ptr + 0x12345678 < ptr; // BAD
-}
--- cpp/ql/src/Likely Bugs/Memory Management/PointerOverflowCheck-good.cpp
+++
@@ -1,3 +0,0 @@
-bool check_pointer_overflow(P *ptr, P *ptr_end) {
-    return ptr + 4 >= ptr_end; // GOOD
-}
