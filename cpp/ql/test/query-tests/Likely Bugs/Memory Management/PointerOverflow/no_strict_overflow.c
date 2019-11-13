81d06e2ce6256507ba5e2e8109bcbe69280fc47e
Jonas Jensen <jbj@knef.dk>
2019-11-13 16:58:25 +0100

    C++: Update annotation in test file

--- cpp/ql/test/query-tests/Likely Bugs/Memory Management/PointerOverflow/no_strict_overflow.c
+++ cpp/ql/test/query-tests/Likely Bugs/Memory Management/PointerOverflow/no_strict_overflow.c
@@ -2,5 +2,5 @@
 
 int not_in_range_nostrict(int *ptr, int *ptr_end, unsigned int a) {
     return ptr + a < ptr_end || // GOOD (for the purpose of this test)
-        ptr + a < ptr; // GOOD (due to compiler options) [FALSE POSITIVE]
+        ptr + a < ptr; // GOOD (due to compiler options)
 }
