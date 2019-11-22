e274e01432e7f893d7c4cff7e6421344a9015f9d
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2019-11-22 16:01:18 +0000

    CPP: More consistency.

--- cpp/ql/test/query-tests/Likely Bugs/Protocols/test3.cpp
+++ cpp/ql/test/query-tests/Likely Bugs/Protocols/test3.cpp
@@ -5,7 +5,7 @@
 void useTLS_bad()
 {
 	boost::asio::ssl::context ctx(boost::asio::ssl::context::tls);
-	ctx.set_options(boost::asio::ssl::context::no_tlsv1); // BAD: missing SSL_OP_NO_TLSv1_1
+	ctx.set_options(boost::asio::ssl::context::no_tlsv1); // BAD: missing no_tlsv1_1
 
 	// ...
 }
