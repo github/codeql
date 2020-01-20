fa00e96ba834e109c90b88e6271c44c813254751
Jonas Jensen <jbj@github.com>
2020-01-20 13:46:14 +0100

    C++: Test IR taint through library functions

---
+++ cpp/ql/test/library-tests/dataflow/DefaultTaintTracking/defaulttainttracking.cpp
@@ -0,0 +1,28 @@
+int atoi(const char *nptr);
+char *getenv(const char *name);
+char *strcat(char * s1, const char * s2);
+
+char *strdup(const char *);
+char *_strdup(const char *);
+char *unmodeled_function(const char *);
+
+void sink(const char *);
+void sink(int);
+
+int main(int argc, char *argv[]) {
+  int taintedInt = atoi(getenv("VAR"));
+  taintedInt++; // BUG: `taintedInt` isn't marked as tainted. Only `++` is.
+
+  sink(_strdup(getenv("VAR"))); // BUG: no taint
+  sink(strdup(getenv("VAR")));
+  sink(unmodeled_function(getenv("VAR")));
+
+  char untainted_buf[100] = "";
+  char buf[100] = "VAR = ";
+  sink(strcat(buf, getenv("VAR"))); // BUG: no taint
+
+  sink(buf); // BUG: no taint
+  sink(untainted_buf); // the two buffers would be conflated if we added flow through partial chi inputs
+
+  return 0;
+}
