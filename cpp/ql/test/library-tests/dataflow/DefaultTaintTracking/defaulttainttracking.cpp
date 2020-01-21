6d46e4d946ed2f30bb4e96f4a5b6a4876e1fb223
Jonas Jensen <jbj@github.com>
2020-01-21 12:04:45 +0100

    C++: Wire up models to DefaultTaintTracking
    This adds support for arg-to-arg and arg-to-return taint.

--- cpp/ql/test/library-tests/dataflow/DefaultTaintTracking/defaulttainttracking.cpp
+++ cpp/ql/test/library-tests/dataflow/DefaultTaintTracking/defaulttainttracking.cpp
@@ -13,13 +13,13 @@ int main(int argc, char *argv[]) {
   int taintedInt = atoi(getenv("VAR"));
   taintedInt++; // BUG: `taintedInt` isn't marked as tainted. Only `++` is.
 
-  sink(_strdup(getenv("VAR"))); // BUG: no taint
+  sink(_strdup(getenv("VAR")));
   sink(strdup(getenv("VAR")));
   sink(unmodeled_function(getenv("VAR")));
 
   char untainted_buf[100] = "";
   char buf[100] = "VAR = ";
-  sink(strcat(buf, getenv("VAR"))); // BUG: no taint
+  sink(strcat(buf, getenv("VAR")));
 
   sink(buf); // BUG: no taint
   sink(untainted_buf); // the two buffers would be conflated if we added flow through partial chi inputs
