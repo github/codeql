0e3ed2dfa6f684022f33aec1d19413b0cfc86a8c
Jonas Jensen <jbj@github.com>
2020-01-27 14:25:28 +0100

    C++: Remove test for unrelated issue
    The issue for that test is being tested and fixed on PR #2686. Adding a
    test here will cause a semantic merge conflict.

--- cpp/ql/test/library-tests/dataflow/DefaultTaintTracking/defaulttainttracking.cpp
+++ cpp/ql/test/library-tests/dataflow/DefaultTaintTracking/defaulttainttracking.cpp
@@ -10,8 +10,8 @@ void sink(const char *);
 void sink(int);
 
 int main(int argc, char *argv[]) {
-  int taintedInt = atoi(getenv("VAR"));
-  taintedInt++; // BUG: `taintedInt` isn't marked as tainted. Only `++` is.
+
+
 
   sink(_strdup(getenv("VAR")));
   sink(strdup(getenv("VAR")));
