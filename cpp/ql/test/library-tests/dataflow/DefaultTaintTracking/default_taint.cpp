0aa0349d2850c74c870b64220fc954eefd20bf9e
Mathias Vorreiter Pedersen <mathiasvp@github.com>
2020-01-27 16:52:03 +0100

    C++: Modify test to exercise virtual dispatch analysis

--- cpp/ql/test/library-tests/dataflow/DefaultTaintTracking/default_taint.cpp
+++ cpp/ql/test/library-tests/dataflow/DefaultTaintTracking/default_taint.cpp
@@ -1,62 +1,129 @@
-void alias_taint(const char *a, const char** pa, const char* alias_a)
-{
-    pa = &a; // IR, AST: a -> pa
-    alias_a = *pa; //IR, AST: a -> alias_a
-                   //IR, AST: pa -> alias_a
-}
-
-int global;
-
-void global_taint(int n, int* pn)
-{
-    pn = &global;
-    *pn = n; // AST: n -> *pn = n
-}
-
-void side_effect_taint(int a, int b, int c, int d, int d1, int d2)
-{
-    a = (b = c); // IR: c -> (b = c)
-    a = d1++; // IR: d1 -> d1
-    d = ++d2; // IR: d2 -> d2
-}
-
-void unary_taint(int a, int d) {
-    a = -d;
-    a = +d;
-}
-
-extern bool b;
-
-void conditional_taint(int a, int b1, int b2) {
-    a = b ? b1 : b2;
-
-    a = *(b ? &b1 : &b2); // IR: (b1 -> b ? &b1 : &b2)
-                          // IR: (b2 -> b ? &b1 : &b2)
-    a = b / 0;
-}
+void sink(const char *);
+void sink(int);
 
 struct S {
-    int s;
+    void(*f)(const char*);
 
-    void set(int p) {
-        s = p; // AST: p -> s = p
-               // AST: p -> b (in 'void struct_taint(int a, int b, S s)')
-               // AST: p -> get (in 'void struct_taint(int a, int b, S s)')
+    void apply(char* p) {
+        f(p);
     }
 
-    int get() {
-        return s;
+    void (*get())(const char*) {
+        return f;
     }
 };
 
-void struct_taint(int a, int b, S s) {
-    s.set(a); // AST: s -> set
-              // AST: s -> s
-              // AST: a -> s = p (in 'void set(int p)')
-              // AST: a -> p (in 'void set(int p)')
-              // AST: a -> s (in 'int get()')
-              // AST: a -> b = s.get()
-              // AST: a -> get
-    b = s.get(); // AST: s -> get
-                 // AST: s -> s (in 'int get()')
+void calls_sink_with_argv(const char* a) {
+    sink(a);
+}
+
+extern int i;
+
+class BaseWithPureVirtual {
+public:
+    virtual void f(const char*) = 0;
+};
+
+class DerivedCallsSink : public BaseWithPureVirtual {
+public:
+    void f(const char* p) override {
+        sink(p);
+    }
+};
+
+class DerivedDoesNotCallSink : public BaseWithPureVirtual {
+public:
+    void f(const char* p) override {}
+};
+
+class DerivedCallsSinkDiamond1 : virtual public BaseWithPureVirtual {
+public:
+    void f(const char* p) override {
+        sink(p);
+    }
+};
+
+class DerivedDoesNotCallSinkDiamond2 : virtual public BaseWithPureVirtual {
+public:
+    void f(const char* p) override {}
+};
+
+class DerivesMultiple : public DerivedCallsSinkDiamond1, public DerivedDoesNotCallSinkDiamond2 {
+    void f(const char* p) override {
+        DerivedCallsSinkDiamond1::f(p);
+    }
+};
+
+template<typename T>
+class CRTP {
+public:
+    void f(const char* p) {
+        static_cast<T*>(this)->g(p);
+    }
+};
+
+class CRTPCallsSink : public CRTP<CRTPCallsSink> {
+    public:
+    void g(const char* p) {
+        sink(p);
+    }
+};
+
+class Derived1 : public BaseWithPureVirtual {};
+
+class Derived2 : public Derived1 {
+    public:
+    void f(const char* p) override {}
+};
+
+class Derived3 : public Derived2 {
+    public:
+    void f(const char* p) override {
+        sink(p);
+    }
+};
+
+class CRTPDoesNotCallSink : public CRTP<CRTPDoesNotCallSink> {
+    public:
+    void g(const char* p) {}
+};
+
+int main(int argc, char *argv[]) {
+    sink(argv[0]);
+
+    sink(reinterpret_cast<int>(argv));
+
+    calls_sink_with_argv(argv[1]);
+
+    char*** p = &argv;
+
+    sink(*p[0]);
+
+    calls_sink_with_argv(*p[i]);
+
+    sink(*(argv + 1)); // flow [NOT DECTED by AST]
+
+    BaseWithPureVirtual* b = new DerivedCallsSink;
+
+    b->f(argv[1]); // flow [NOT DETECTED by IR]
+
+    b = new DerivedDoesNotCallSink;
+    b->f(argv[0]); // no flow [FALSE POSITIVE by AST]
+
+    BaseWithPureVirtual* b2 = new DerivesMultiple;
+
+    b2->f(argv[i]); // flow [NOT DETECTED]
+    
+    CRTP<CRTPDoesNotCallSink> crtp_not_call_sink;
+    crtp_not_call_sink.f(argv[0]);
+
+    CRTP<CRTPCallsSink> crtp_calls_sink;
+    crtp_calls_sink.f(argv[0]); // flow [NOT DETECTED]
+
+    Derived1* calls_sink = new Derived3;
+    calls_sink->f(argv[1]); // flow [NOT DETECTED by AST]
+
+    static_cast<Derived2*>(calls_sink)->f(argv[1]); // flow [NOT DETECTED]
+
+    dynamic_cast<Derived2*>(calls_sink)->f(argv[1]); // flow [NOT DETECTED by IR]
 }
