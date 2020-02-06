28657230590823a8143aa8566dea202c70950a1b
Erik Krogh Kristensen <erik-krogh@github.com>
2020-02-06 15:44:33 +0100

    add test for new barrier

--- javascript/ql/test/library-tests/TaintTracking/sanitizer-guards.js
+++ javascript/ql/test/library-tests/TaintTracking/sanitizer-guards.js
@@ -74,3 +74,15 @@ function phi2() {
   }
   sink(x); // NOT OK
 }
+
+function falsy() {
+  let x = source();
+  
+  sink(x); // NOT OK
+  
+  if (x) {
+    sink(x); // NOT OK
+  } else {
+	sink(x); // OK
+  }
+}
