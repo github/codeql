e747add485d68811fd89aa80b1f3881738e3fd28
Rasmus Wriedt Larsen <rasmuswl@github.com>
2020-02-12 16:32:50 +0100

    Python: Descriptor tests fixup (3/3)
    Better tests for properties

--- python/ql/test/library-tests/descriptors/test.py
+++ python/ql/test/library-tests/descriptors/test.py
@@ -1,12 +1,54 @@
-class C(object):
+class WithDecorator(object):
+    def __init__(self):
+        self._x = None
 
     @property
-    def f(self):
-        return self._f
+    def x(self):
+        """I'm the 'x' property."""
+        return self._x
 
-    @f.setter
-    def f(self, value):
-        self._f = value
+    @x.setter
+    def x(self, value):
+        self._x = value
+
+    @x.deleter
+    def x(self):
+        del self._x
+
+class WithDecoratorOnlyGetter(object):
+
+    @property
+    def x(self):
+        return 42
+
+class WithoutDecorator(object):
+    def __init__(self):
+        self._x = None
+
+    def getx(self):
+        return self._x
+
+    def setx(self, value):
+        self._x = value
+
+    def delx(self):
+        del self._x
+
+    x = property(getx, setx, delx, "I'm the 'x' property.")
+
+class WithoutDecoratorOnlyGetter(object):
+
+    def getx(self):
+        return 42
+
+    x = property(getx)
+
+class WithoutDecoratorOnlySetter(object):
+
+    def setx(self, value):
+        self._x = value
+
+    x = property(fset=setx) # TODO: Not handled
 
 class D(object):
 
