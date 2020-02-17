362e7aebbb1e2bccea3f91ea75728024c7d9ffeb
Rasmus Wriedt Larsen <rasmuswl@github.com>
2020-02-17 16:49:05 +0100

    Python: Add HttpRedirectSinks test for django

---
+++ python/ql/test/query-tests/Security/lib/django/shortcuts.py
@@ -0,0 +1,7 @@
+# see https://docs.djangoproject.com/en/1.11/_modules/django/shortcuts/#redirect
+
+def render(request, template_name, context=None, content_type=None, status=None, using=None):
+    pass
+
+def redirect(to, *args, **kwargs):
+    pass
