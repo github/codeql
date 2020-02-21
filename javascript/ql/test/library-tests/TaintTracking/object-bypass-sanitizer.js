a1954294716dfec6c6edaf71b1b38d647a4cef30
Asger Feldthaus <asgerf@github.com>
2020-02-21 12:56:00 +0000

    JS: Add test with non-guard sanitizer

--- javascript/ql/test/library-tests/TaintTracking/object-bypass-sanitizer.js
+++ javascript/ql/test/library-tests/TaintTracking/object-bypass-sanitizer.js
@@ -1,10 +1,20 @@
 import * as dummy from 'dummy';
 
+function sanitizer_id(x) {
+    if (really_complicated_reason(x))
+        return x;
+    return null;
+}
+
 function f(x) {
     if (isSafe(x)) {
         sink(x);
         sink(x.foo); // NOT OK
     }
+
+    sink(sanitizer_id(x)); // OK
+    sink(sanitizer_id(x.foo)); // OK
+    sink(sanitizer_id(x).foo); // NOT OK
 }
 
 function g() {
