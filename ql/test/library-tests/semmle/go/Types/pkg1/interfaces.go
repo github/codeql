6dfd5fd934fefda8fbbd148f7d98c08c02bad3c9
Max Schaefer <max@semmle.com>
2020-02-27 09:31:52 +0000

    Extend `Types` tests to cover interfaces.

---
+++ ql/test/library-tests/semmle/go/Types/pkg1/interfaces.go
@@ -0,0 +1,29 @@
+package pkg1
+
+type A interface {
+	m()
+}
+
+type B interface {
+	m()
+	n()
+}
+
+type C interface {
+	n()
+	o()
+}
+
+type AEmbedded interface {
+	A
+}
+
+type AC interface {
+	A
+	C
+}
+
+type AExtended interface {
+	A
+	n()
+}
