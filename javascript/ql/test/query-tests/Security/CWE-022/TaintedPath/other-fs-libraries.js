5a3a1c480d6de7b0c3405831d4859b0551752286
Esben Sparre Andreasen <esbena@github.com>
2020-02-28 10:48:24 +0100

    JS: add tests for the fs-module and friends

---
+++ javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/other-fs-libraries.js
@@ -0,0 +1,26 @@
+var http = require("http"),
+  url = require("url"),
+  fs = require("fs"),
+  gracefulFs = require("graceful-fs"),
+  fsExtra = require("fs-extra"),
+  originalFs = require("original-fs");
+
+var server = http.createServer(function(req, res) {
+  var path = url.parse(req.url, true).query.path;
+
+  fs.readFileSync(path); // NOT OK
+  gracefulFs.readFileSync(path); // NOT OK
+  fsExtra.readFileSync(path); // NOT OK
+  originalFs.readFileSync(path); // NOT OK
+
+  getFsModule(true).readFileSync(path); // NOT OK
+  getFsModule(false).readFileSync(path); // NOT OK
+});
+
+function getFsModule(special) {
+  if (special) {
+    return require("fs");
+  } else {
+    return require("original-fs");
+  }
+}
