8e3cf5c9c8150af7b9cdf6bbbe3019fa26fe9610
Erik Krogh Kristensen <erik-krogh@github.com>
2020-03-09 09:25:17 +0100

    add test for data-flow on arrays

---
+++ javascript/ql/test/library-tests/Arrays/arrays.js
@@ -0,0 +1,42 @@
+(function () {
+  let source = "source";
+
+  var obj = { foo: source };
+  sink(obj.foo); // NOT OK
+
+  var arr = [];
+  arr.push(source);
+
+  for (var i = 0; i < arr.length; i++) {
+    sink(arr[i]); // NOT OK
+  }
+
+
+  arr.forEach((e) => sink(e)); // NOT OK
+  arr.map((e) => sink(e)); // NOT OK
+
+  [1, 2, 3].map(i => "source").forEach(e => sink(e)); // NOT OK.
+
+  sink(arr.pop()); // NOT OK
+
+  var arr2 = Array.from("source");
+  sink(arr2.pop()); // NOT OK
+
+  var arr3 = ["source"];
+  sink(arr3.pop()); // NOT OK
+
+  var arr4 = [];
+  arr4.splice(0, 0, "source");
+  sink(arr4.pop()); // NOT OK
+
+  var arr5 = [].concat(arr4);
+  sink(arr5.pop()); // NOT OK
+
+  sink(arr5.slice(2).pop()); // NOT OK
+
+  var arr6 = [];
+  for (var i = 0; i < arr5.length; i++) {
+    arr6[i] = arr5[i];
+  }
+  sink(arr6.pop()); // NOT OK
+});
