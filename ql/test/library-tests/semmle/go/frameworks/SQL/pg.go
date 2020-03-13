ad1324d2dd49c113058e7f842d1e4c1698d7100d
Max Schaefer <max@semmle.com>
2020-03-13 12:59:38 +0000

    Add test.

---
+++ ql/test/library-tests/semmle/go/frameworks/SQL/pg.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/go-pg/pg"
+	"github.com/go-pg/pg/orm"
+	newpg "github.com/go-pg/pg/v9"
+)
+
+func pgtest(query string, conn pg.Conn, db pg.DB, tx pg.Tx) {
+	pg.Q(query)
+	var dst []byte
+	conn.FormatQuery(dst, query)
+	conn.Prepare(query)
+	db.FormatQuery(dst, query)
+	db.Prepare(query)
+	tx.FormatQuery(dst, query)
+	tx.Prepare(query)
+}
+
+// go-pg v9 dropped support for `FormatQuery`
+func newpgtest(query string, conn newpg.Conn, db newpg.DB, tx newpg.Tx) {
+	newpg.Q(query)
+	conn.Prepare(query)
+	db.Prepare(query)
+	tx.Prepare(query)
+}
+func pgormtest(query string, q orm.Query) {
+	orm.Q(query)
+	q.ColumnExpr(query)
+	q.For(query)
+	var b []byte
+	q.FormatQuery(b, query)
+	q.Having(query)
+	q.Where(query)
+	q.WhereInMulti(query)
+	q.WhereOr(query)
+}
