d9327705d200c22ed1863e00141c942a8a6304d0
luchua-bc <shengxin.canada@gmail.com>
2020-03-18 16:48:31 -0400

    Fix the issue of mixed tabs and spaces

--- java/ql/src/experimental/CWE-532/SensitiveInfoLog.java
+++ java/ql/src/experimental/CWE-532/SensitiveInfoLog.java
@@ -1,18 +1,18 @@
 public static void main(String[] args) {
-	{
-		private static final Logger logger = LogManager.getLogger(SensitiveInfoLog.class);
-  
-    		String password = "Pass@0rd";
+    {
+        private static final Logger logger = LogManager.getLogger(SensitiveInfoLog.class);
 
-		// BAD: user password is written to debug log
-		logger.debug("User password is "+password);
-	}
+        String password = "Pass@0rd";
+
+        // BAD: user password is written to debug log
+        logger.debug("User password is "+password);
+    }
 	
-	{
-		private static final Logger logger = LogManager.getLogger(SensitiveInfoLog.class);
+    {
+        private static final Logger logger = LogManager.getLogger(SensitiveInfoLog.class);
   
-    		String password = "Pass@0rd";
+        String password = "Pass@0rd";
 
-		// GOOD: user password is never written to debug log
-	}
+        // GOOD: user password is never written to debug log
+    }
 }
