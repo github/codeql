9a18dc32c144ce9e2b76c34fbf1ad0777473c588
Erik Krogh Kristensen <erik-krogh@github.com>
2020-03-23 23:47:28 +0100

    autoformat WebSocket tests

--- javascript/ql/test/library-tests/frameworks/WebSocket/browser.js
+++ javascript/ql/test/library-tests/frameworks/WebSocket/browser.js
@@ -2,14 +2,14 @@
 	const socket = new WebSocket('ws://localhost:8080');
 
 	socket.addEventListener('open', function (event) {
-    	socket.send('Hi from browser!');
+		socket.send('Hi from browser!');
 	});
 
 	socket.addEventListener('message', function (event) {
-	    console.log('Message from server ', event.data);
+		console.log('Message from server ', event.data);
 	});
-	
-	socket.onmessage = function(event) {
-      console.log("Message from server 2", event.data)
-    };
+
+	socket.onmessage = function (event) {
+		console.log("Message from server 2", event.data)
+	};
 })();
