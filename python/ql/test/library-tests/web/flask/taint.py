d95cd9ae4b315b9e291dc1b9862af30fdd2c80c8
Rasmus Wriedt Larsen <rasmuswl@github.com>
2020-05-12 18:26:32 +0200

    Python: Add explicit test of taint for flask requests

---
+++ python/ql/test/library-tests/web/flask/taint.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/test_taint/<name>/<int:number>')
+def test_taint(name = "World!", number="0", foo="foo"):
+    ensure_tainted(name, number)
+    ensure_not_tainted(foo)
+
+    ensure_tainted(
+        request.path,
+        request.full_path,
+        request.base_url,
+        request.url,
+
+        request.args,
+        request.form,
+        request.values,
+        request.files,
+        request.headers,
+        request.json,
+    )
