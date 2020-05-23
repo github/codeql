9d82483d0c8f1563aaf528dc6939b79bd71e1d5a
Sreejith Krishnan R <skr.io7803@gmail.com>
2020-05-24 04:07:18 +0530

    Switch to spring app like test

--- java/ql/test/experimental/query-tests/security/CWE-918/SpringRestTemplateBad.java
+++ java/ql/test/experimental/query-tests/security/CWE-918/SpringRestTemplateBad.java
@@ -4,24 +4,18 @@
 
 package test.cwe918.cwe.examples;
 
-import java.io.IOException;
 import java.net.URI;
 import java.util.HashMap;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RequestParam;
 
-public class SpringRestTemplateBad extends HttpServlet {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	throws ServletException, IOException {
+public class SpringRestTemplateBad {
+	protected void handleRequest(@RequestParam String userInput) {
         RestTemplate template = new RestTemplate();
-        String userInput = request.getParameter("webhook");
        
         template.getForObject(userInput, Entity.class, "webhook");
         template.getForObject(userInput, Entity.class, new HashMap<>());
