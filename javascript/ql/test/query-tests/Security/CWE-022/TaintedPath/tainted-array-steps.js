60320a9d78f6eccee329f901ee524ffc271192eb
Erik Krogh Kristensen <erik-krogh@github.com>
2020-06-04 11:00:15 +0200

    update TaintedPath to use new consistency checking

--- javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/tainted-array-steps.js
+++ javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/tainted-array-steps.js
@@ -7,11 +7,11 @@ var fs = require('fs'),
 
 var server = http.createServer(function(req, res) {
   let path = url.parse(req.url, true).query.path;
-  res.write(fs.readFileSync(['public', path].join('/'))); // BAD: taint is preserved [INCONSISTENCY]
+  res.write(fs.readFileSync(['public', path].join('/'))); // BAD: taint is preserved
 
   let parts = ['public', path];
   parts = parts.map(x => x.toLowerCase());
-  res.write(fs.readFileSync(parts.join('/'))); // BAD: taint is preserved [INCONSISTENCY]
+  res.write(fs.readFileSync(parts.join('/'))); // BAD: taint is preserved
 });
 
 server.listen();
