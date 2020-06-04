5ce2987cb210ea6cfb539444552c04b611243448
Erik Krogh Kristensen <erik-krogh@github.com>
2020-06-04 16:15:37 +0200

    adjust comments to reflect that tainted-path have no array-steps

--- javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/tainted-array-steps.js
+++ javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/tainted-array-steps.js
@@ -7,11 +7,11 @@ var fs = require('fs'),
 
 var server = http.createServer(function(req, res) {
   let path = url.parse(req.url, true).query.path;
-  res.write(fs.readFileSync(['public', path].join('/'))); // BAD: taint is preserved
+  res.write(fs.readFileSync(['public', path].join('/'))); // BAD - but not flagged because we have no array-steps [INCONSISTENCY]
 
   let parts = ['public', path];
   parts = parts.map(x => x.toLowerCase());
-  res.write(fs.readFileSync(parts.join('/'))); // BAD: taint is preserved
+  res.write(fs.readFileSync(parts.join('/'))); // BAD - but not flagged because we have no array-steps [INCONSISTENCY]
 });
 
 server.listen();
