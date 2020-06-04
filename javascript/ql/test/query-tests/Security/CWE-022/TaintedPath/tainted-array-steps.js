d513e6c5b5911dbf344d6b18115f15f4d3a7b52b
Erik Krogh Kristensen <erik-krogh@github.com>
2020-06-04 10:40:14 +0200

    update comments in TaintedPath tests

--- javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/tainted-array-steps.js
+++ javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/tainted-array-steps.js
@@ -7,12 +7,11 @@ var fs = require('fs'),
 
 var server = http.createServer(function(req, res) {
   let path = url.parse(req.url, true).query.path;
-  // BAD: taint is preserved
-  res.write(fs.readFileSync(['public', path].join('/')));
-  // BAD: taint is preserved
+  res.write(fs.readFileSync(['public', path].join('/'))); // BAD: taint is preserved [INCONSISTENCY]
+
   let parts = ['public', path];
   parts = parts.map(x => x.toLowerCase());
-  res.write(fs.readFileSync(parts.join('/')));
+  res.write(fs.readFileSync(parts.join('/'))); // BAD: taint is preserved [INCONSISTENCY]
 });
 
 server.listen();
