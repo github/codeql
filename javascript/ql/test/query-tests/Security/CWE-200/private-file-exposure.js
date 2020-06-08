167239e74558271d867aaf1f42f2bcf8302d0265
Erik Krogh Kristensen <erik-krogh@github.com>
2020-06-08 19:54:23 +0200

    add query to detect accidential leak of private files

---
+++ javascript/ql/test/query-tests/Security/CWE-200/private-file-exposure.js
@@ -0,0 +1,37 @@
+
+var express = require('express');
+var path = require("path");
+
+var app = express();
+
+// Not good. 
+app.use(express.static('./node_modules/angular'));
+app.use('/angular', express.static('node_modules/angular'));
+app.use('/animate', express.static('node_modules/angular-animate'));
+app.use('/js', express.static(__dirname + '/node_modules/angular'));
+app.use('/router', express.static(__dirname + '/node_modules/angular-route/'));
+app.use(express.static('/node_modules/angular'));
+app.use('/node_modules', express.static(path.resolve(__dirname, '../node_modules')));
+app.use('/js',express.static('./'));
+app.use('/angular', express.static("./node_modules" + '/angular/'));
+app.use('/angular', express.static(path.join("./node_modules" + '/angular/')));
+app.use('/angular', express.static(path.join(__dirname, "/node_modules")));
+app.use('/angular', express.static(path.join(__dirname, "/node_modules") + '/angular/'));
+const rootDir = __dirname;
+const nodeDir = path.join(rootDir + "/node_modules");
+app.use('/angular', express.static(nodeDir + '/angular/'));
+
+
+// Good
+app.use(express.static('./node_modules/jquery/dist'));
+app.use(express.static('./node_modules/bootstrap/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/html5sortable/dist'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/favicon.ico', express.static(__dirname + '/favicon.ico'));
+app.use(express.static(__dirname + "/static"));
+app.use(express.static(__dirname + "/static/js"));
+app.use('/docs/api', express.static('docs/api'));
+app.use('/js/', express.static('node_modules/bootstrap/dist/js'))
+app.use('/css/', express.static('node_modules/font-awesome/css'));
+app.use('basedir', express.static(__dirname)); // GOOD, because there is no package.json in the same folder.
+app.use('/monthly', express.static(__dirname + '/')); // GOOD, because there is no package.json in the same folder.
