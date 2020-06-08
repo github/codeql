5acfc52087963607749b65dbf64f405e9d644f72
luchua-bc <shengxin.canada@gmail.com>
2020-06-08 16:17:40 +0000

    Add dependent stub classes for the test case

--- java/ql/test/experimental/query-tests/security/CWE-297/InsecureJavaMail.java
+++ java/ql/test/experimental/query-tests/security/CWE-297/InsecureJavaMail.java
@@ -9,28 +9,27 @@ import javax.mail.Session;
 
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
-import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
 
 import java.util.Properties;
 
 class InsecureJavaMail {
 	public void testJavaMail() {
-			final Properties properties = new Properties();
-			properties.put("mail.transport.protocol", "protocol");
-			properties.put("mail.smtp.host", "hostname");
-			properties.put("mail.smtp.socketFactory.class", "classname");
+		final Properties properties = new Properties();
+		properties.put("mail.transport.protocol", "protocol");
+		properties.put("mail.smtp.host", "hostname");
+		properties.put("mail.smtp.socketFactory.class", "classname");
 
-			final javax.mail.Authenticator authenticator = new javax.mail.Authenticator() {
-					protected PasswordAuthentication getPasswordAuthentication() {
-							return new PasswordAuthentication("username", "password");
-					}
-			};
-			if (null != authenticator) {
-					properties.put("mail.smtp.auth", "true");
-					// properties.put("mail.smtp.ssl.checkserveridentity", "true");
+		final javax.mail.Authenticator authenticator = new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("username", "password");
 			}
-			final Session session = Session.getInstance(properties, authenticator);
+		};
+		if (null != authenticator) {
+			properties.put("mail.smtp.auth", "true");
+			// properties.put("mail.smtp.ssl.checkserveridentity", "true");
+		}
+		final Session session = Session.getInstance(properties, authenticator);
 	}
 
 	public void testSimpleMail() {
