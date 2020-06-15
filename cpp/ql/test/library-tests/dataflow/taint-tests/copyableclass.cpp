c196ea24b29937fff58d1bf55cba1a9ccac6decf
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2020-06-15 23:05:07 +0100

    C++: Add taint tests of class constructors and assignment.

---
+++ cpp/ql/test/library-tests/dataflow/taint-tests/copyableclass.cpp
@@ -0,0 +1,68 @@
+
+int source();
+void sink(...) {};
+
+class MyCopyableClass {
+public:
+	MyCopyableClass(int _v = 0) : v(_v) {} // Constructor
+	MyCopyableClass(const MyCopyableClass &other) : v(other.v) {} // CopyConstructor
+	MyCopyableClass &operator=(const MyCopyableClass &other) { // CopyAssignmentOperator
+		v = other.v;
+		return *this;
+	}
+
+	int v;
+};
+
+void test_copyableclass()
+{
+	{
+		MyCopyableClass s1(1);
+		MyCopyableClass s2 = 1;
+		MyCopyableClass s3(s1);
+		MyCopyableClass s4;
+		s4 = 1;
+
+		sink(s1);
+		sink(s2);
+		sink(s3);
+		sink(s4);
+	}
+
+	{
+		MyCopyableClass s1(source());
+		MyCopyableClass s2 = source();
+		MyCopyableClass s3(s1);
+		MyCopyableClass s4;
+		s4 = source();
+
+		sink(s1); // tainted [NOT DETECTED]
+		sink(s2); // tainted [NOT DETECTED]
+		sink(s3); // tainted [NOT DETECTED]
+		sink(s4); // tainted [NOT DETECTED]
+	}
+
+	{
+		MyCopyableClass s1;
+		MyCopyableClass s2 = s1;
+		MyCopyableClass s3(s1);
+		MyCopyableClass s4;
+		s4 = s1;
+
+		sink(s1);
+		sink(s2);
+		sink(s3);
+		sink(s4);
+	}
+
+	{
+		MyCopyableClass s1 = MyCopyableClass(source());
+		MyCopyableClass s2;
+		MyCopyableClass s3;
+		s2 = MyCopyableClass(source());
+
+		sink(s1); // tainted [NOT DETECTED]
+		sink(s2); // tainted [NOT DETECTED]
+		sink(s3 = source()); // tainted [NOT DETECTED]
+	}
+}
---
+++ cpp/ql/test/library-tests/dataflow/taint-tests/movableclass.cpp
@@ -0,0 +1,66 @@
+
+int source();
+void sink(...) {};
+
+class MyMovableClass {
+public:
+	MyMovableClass(int _v = 0) : v(_v) {} // Constructor
+	MyMovableClass(MyMovableClass &&other) noexcept { // ConversionConstructor, MoveConstructor
+		v = other.v;
+		other.v = 0;
+	}
+	MyMovableClass &operator=(MyMovableClass &&other) noexcept { // MoveAssignmentOperator
+		v = other.v;
+		other.v = 0;
+		return *this;
+	}
+
+	int v;
+};
+
+MyMovableClass &&getUnTainted() { return MyMovableClass(1); }
+MyMovableClass &&getTainted() { return MyMovableClass(source()); }
+
+void test_copyableclass()
+{
+	{
+		MyMovableClass s1(1);
+		MyMovableClass s2 = 1;
+		MyMovableClass s3;
+		s3 = 1;
+
+		sink(s1);
+		sink(s2);
+		sink(s3);
+	}
+
+	{
+		MyMovableClass s1(source());
+		MyMovableClass s2 = source();
+		MyMovableClass s3;
+		s3 = source();
+
+		sink(s1); // tainted [NOT DETECTED]
+		sink(s2); // tainted [NOT DETECTED]
+		sink(s3); // tainted [NOT DETECTED]
+	}
+
+	{
+		MyMovableClass s1 = MyMovableClass(source());
+		MyMovableClass s2;
+		s2 = MyMovableClass(source());
+
+		sink(s1); // tainted [NOT DETECTED]
+		sink(s2); // tainted [NOT DETECTED]
+	}
+
+	{
+		MyMovableClass s1(getUnTainted());
+		MyMovableClass s2(getTainted());
+		MyMovableClass s3;
+
+		sink(s1);
+		sink(s2); // tainted [NOT DETECTED]
+		sink(s3 = source()); // tainted [NOT DETECTED]
+	}
+}
