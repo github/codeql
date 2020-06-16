d811518a2eb0f5a659ca944416d7215614b4f612
Erik Krogh Kristensen <erik-krogh@github.com>
2020-06-16 23:26:22 +0200

    fixed from doc review, and add fixed example for js/biased-cryptographic-random using a secure library

--- javascript/ql/src/Security/CWE-327/examples/bad-random-fixed.js
+++ javascript/ql/src/Security/CWE-327/examples/bad-random-fixed.js
@@ -1,10 +1,3 @@
-const crypto = require('crypto');
+const cryptoRandomString = require('crypto-random-string');
 
-const digits = [];
-while (digits.length < 10) {
-    const byte = crypto.randomBytes(1)[0];
-    if (byte >= 250) {
-        continue;
-    }
-    digits.push(byte % 10); // OK
-}
+const digits = cryptoRandomString({length: 10, type: 'numeric'});
