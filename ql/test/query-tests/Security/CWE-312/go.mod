df4265e31bbd56b17b7a89c78434c24360fe922d
Max Schaefer <max-schaefer@github.com>
2020-06-24 07:33:17 +0100

    Add more tests for clear-text logging.

--- ql/test/query-tests/Security/CWE-312/go.mod
+++ ql/test/query-tests/Security/CWE-312/go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/sirupsen/logrus v1.5.0
+	k8s.io/klog v1.0.0
 )
---
+++ ql/test/query-tests/Security/CWE-312/klog.go
@@ -0,0 +1,28 @@
+package main
+
+//go:generate depstubber -vendor k8s.io/klog "" Info
+
+import (
+	"k8s.io/klog"
+	"net/http"
+	"strings"
+)
+
+func mask(key, value string) string {
+	if strings.EqualFold(key, "Authorization") {
+		return "<masked>"
+	}
+	return value
+}
+
+func klogTest() {
+	http.HandleFunc("/klog", func(w http.ResponseWriter, r *http.Request) {
+		for name, headers := range r.Header {
+			for _, header := range headers {
+				klog.Info(header)             // NOT OK
+				klog.Info(mask(name, header)) // OK
+			}
+		}
+	})
+	http.ListenAndServe(":80", nil)
+}
