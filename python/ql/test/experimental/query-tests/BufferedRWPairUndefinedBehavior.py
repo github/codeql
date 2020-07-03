78c10b95bcf7145b6bbf975b7c3cc755c5fe52f0
dilanbhalla <dbhalla21@berkeley.edu>
2020-07-02 19:24:25 -0700

    added to test file

--- python/ql/test/experimental/query-tests/BufferedRWPairUndefinedBehavior.py
+++ python/ql/test/experimental/query-tests/BufferedRWPairUndefinedBehavior.py
@@ -4,8 +4,13 @@ def buffer_rw_pair_test():
 
     reader = io.BufferedReader(io.RawIOBase)
     # BAD, uses reader for both parameters
-    io.BufferedRWPair(reader, reader)
+    pair1 = io.BufferedRWPair(reader, reader)
 
     writer = io.BufferedWriter(io.RawIOBase)
     # GOOD, uses different reader/writer
-    io.BufferedRWPair(reader, writer)
+    pair2 = io.BufferedRWPair(reader, writer)
+
+    # GOOD, uses reader variable as both reader and writer for random access
+    random = io.BufferedRandom(reader)
+
+    return (pair1, pair2, random)
