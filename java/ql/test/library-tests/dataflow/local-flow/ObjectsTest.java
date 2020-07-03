9cf6601d02adf1e3c9567a07164112e8ddbee881
Arthur Baars <arthur@semmle.com>
2020-07-03 20:27:50 +0200

    Java: Data flow for `java.util.Objects`

---
+++ java/ql/test/library-tests/dataflow/local-flow/ObjectsTest.java
@@ -0,0 +1,15 @@
+import java.util.Objects;
+
+class ObjectsTest {
+	public static void taintSteps() {
+		sink(Objects.requireNonNull(source()));
+		sink(Objects.requireNonNull(source(), "message"));
+		sink(Objects.requireNonNull(source(), () -> "value1"));
+		sink(Objects.requireNonNullElse(source(), source()));
+		sink(Objects.requireNonNullElseGet(source(), () -> "value2"));
+		sink(Objects.toString(null, source()));
+	}
+	private static <T> T source() { return null; }
+	private static void sink(Object o) {}
+}
+
