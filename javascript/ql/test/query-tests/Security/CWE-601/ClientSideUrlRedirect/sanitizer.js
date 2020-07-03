b5104ae42d545bc75927fcb0453a1c85420cd89f
Asger Feldthaus <asgerf@github.com>
2020-07-03 14:34:59 +0100

    JS: Add StartsWith sanitizer

--- javascript/ql/test/query-tests/Security/CWE-601/ClientSideUrlRedirect/sanitizer.js
+++ javascript/ql/test/query-tests/Security/CWE-601/ClientSideUrlRedirect/sanitizer.js
@@ -4,13 +4,13 @@ function f() {
         window.location = url; // NOT OK - can be example.com.evil.com
     }
     if (url.startsWith('https://example.com/')) {
-        window.location = url; // OK - but flagged anyway
+        window.location = url; // OK
     }
     if (url.startsWith('https://example.com//')) {
-        window.location = url; // OK - but flagged anyway
+        window.location = url; // OK
     }
     if (url.startsWith('https://example.com/foo')) {
-        window.location = url; // OK - but flagged anyway 
+        window.location = url; // OK 
     }
     if (url.startsWith('https://')) {
         window.location = url; // NOT OK - does not restrict hostname
@@ -31,9 +31,13 @@ function f() {
         window.location = url; // NOT OK - can be //example.com.evil.com
     }
     if (url.startsWith('//example.com/')) {
-        window.location = url; // OK - but flagged anyway
+        window.location = url; // OK
     }
     if (url.endsWith('https://example.com/')) {
         window.location = url; // NOT OK - could be evil.com?x=https://example.com/
+    }
+    let basedir = whatever() ? 'foo' : 'bar';
+    if (url.startsWith('https://example.com/' + basedir)) {
+        window.location = url; // OK - the whole prefix is not known, but enough to restrict hostname
     }
 }
