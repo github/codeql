dbeef312cac2615d59ddbefc85bacdfac792a744
Erik Krogh Kristensen <erik-krogh@github.com>
2020-07-09 10:05:23 +0200

    add some TypeScript tests to DeadStoreOfProperty

--- javascript/ql/test/query-tests/Declarations/DeadStoreOfProperty/fieldInit.ts
+++ javascript/ql/test/query-tests/Declarations/DeadStoreOfProperty/fieldInit.ts
@@ -13,3 +13,19 @@ class D {
     this.f = 5;
   }
 }
+
+class G {
+  constructor(public h: string) { // NOT OK
+    this.h = h;
+  }
+}
+
+class Foo {
+  static static() {}
+
+  static *foo() {}
+
+  static set() {}
+
+  static static() {}
+}
