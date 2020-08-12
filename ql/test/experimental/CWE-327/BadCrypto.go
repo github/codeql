37eca95d440b37dc7615674ce1c3230eac31354d
dilanbhalla <dbhalla21@berkeley.edu>
2020-08-11 23:53:50 -0700

    restructured library

--- ql/test/experimental/CWE-327/BadCrypto.go
+++ ql/test/experimental/CWE-327/BadCrypto.go
@@ -3,24 +3,29 @@ package main
 import (
 	"crypto/des"
 	"crypto/md5"
-	"fmt"
+	"crypto/rc4"
+	"crypto/sha1"
+	"crypto/sha256"
 )
 
 func main() {
 
 	password := []byte("password")
-
-	var tripleDESKey []byte
-	tripleDESKey = append(tripleDESKey, password[:16]...)
-	tripleDESKey = append(tripleDESKey, password[:8]...)
-
 	// BAD, des is a weak crypto algorithm
-	_, err := des.NewTripleDESCipher(tripleDESKey)
-	if err != nil {
-		panic(err)
-	}
+	des.NewTripleDESCipher(password)
 
 	// BAD, md5 is a weak crypto algorithm
-	fmt.Printf("%x", md5.Sum(password))
+	md5.Sum(password)
 
+	key := []byte{ 1, 2, 3, 4, 5, 6, 7 }
+	// BAD, rc4 is a weak crypto algorithm
+	rc4.NewCipher(key)
+
+	data := []byte("this page intentionally left blank.")
+	// BAD, sha1 is a weak crypto algorithm
+	sha1.Sum(data)
+
+	sha256key := []byte("hello world")
+    // GOOD, sha256 is a strong crypto algorithm
+    sha256.Sum256([]byte(sha256key))
 }
