f5070c7ff4552108e434696a72a9c2a203971d81
Justin Hutchings <jhutchings1@users.noreply.github.com>
2020-08-13 12:50:59 -0700

    Tweak search paths

--- misc/scripts/resolve-code-scanning-query-packs.py
+++ misc/scripts/resolve-code-scanning-query-packs.py
@@ -25,7 +25,7 @@ packs = [ "code-scanning", "security-and-quality", "security-extended" ]
 #  - the 'codeql' subdirectory of the cwd
 #
 # (and assumes the codeql-go repo is in a similar location)
-codeql_search_path = ".:../codeql-go:codeql:codeql-go"   # will be extended further down
+codeql_search_path = ".:../codeql-go:./codeql:./codeql-go:codeql:codeql-go"   # will be extended further down
 
 
 def prefix_repo_nwo(filename):
