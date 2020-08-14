f7273b86657db896223569b52d4e39713f00748b
Jonas Jensen <jbj@github.com>
2020-08-14 12:15:08 +0200

    C++: Add custom modeling to extensibility.ql

--- cpp/ql/test/experimental/library-tests/rangeanalysis/extensibility/extensibility.c
+++ cpp/ql/test/experimental/library-tests/rangeanalysis/extensibility/extensibility.c
@@ -1,9 +1,9 @@
-/// Adds its arguments (requires custom modeling in QL)
+/// Adds its arguments (has custom modeling in QL)
 int custom_add_function(int a, int b);
 
 int test_extensibility_add(int x) {
   if (x >= -10 && x <= 10) {
     int result = custom_add_function(x, 100);
-    return result; // should be 90 .. 110
+    return result; // 90 .. 110
   }
 }
