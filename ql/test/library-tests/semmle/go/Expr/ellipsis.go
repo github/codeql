a0a8a584a4560d919026c0cd87096d3dfc3d5668
Max Schaefer <max-schaefer@github.com>
2020-08-24 17:01:16 +0100

    Fix error in `ellipsis.go`.

--- ql/test/library-tests/semmle/go/Expr/ellipsis.go
+++ ql/test/library-tests/semmle/go/Expr/ellipsis.go
@@ -1,11 +1,11 @@
 package main
 
-func f(x int, ys ...int) {
+func fn(x int, ys ...int) {
 }
 
 func ellipsisTest() {
-	f(1)
-	f(1, 2)
-	f(1, 2, 3)
-	f(1, []int{2, 3}...)
+	fn(1)
+	fn(1, 2)
+	fn(1, 2, 3)
+	fn(1, []int{2, 3}...)
 }
