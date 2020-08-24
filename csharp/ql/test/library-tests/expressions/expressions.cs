3dea6b32182c0dce8a7c91d0df7be3ff3b3e40e9
Tamas Vajk <tamasvajk@github.com>
2020-08-24 16:10:42 +0200

    C#: Change implicitly sized array test input

--- csharp/ql/test/library-tests/expressions/expressions.cs
+++ csharp/ql/test/library-tests/expressions/expressions.cs
@@ -441,7 +441,7 @@ namespace Expressions
                 { new int[,] { {1,3}, {5,7} }, new int[,] { {0,2}, {4,6}, {8,10} }, new int[,] { {11,22}, {99,88}, {0,9} } },
                 { new int[,] { {1,3}, {5,7} }, new int[,] { {0,2}, {4,6}, {8,10} }, new int[,] { {11,22}, {99,88}, {0,9} } }
             };
-            o = new int[][,] { new int[,] { { 1, 2 } }, new int[,] { { 1, 2 } } };
+            o = new int[][,] { new int[,] { { 1, 2 } }, new int[,] { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } } };
         }
 
         void MainAnonymousFunctions()
