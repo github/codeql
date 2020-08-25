6a07e1e82b41da91ee3235ef3195e9f0ca76b3f7
Erik Krogh Kristensen <erik-krogh@github.com>
2020-08-25 14:17:05 +0200

    add more passing tests

--- javascript/ql/test/library-tests/Generators/generators.js
+++ javascript/ql/test/library-tests/Generators/generators.js
@@ -14,4 +14,20 @@
     return source;
   }
   sink(gen2()); // OK
+
+  Array.from(gen1()).forEach(x => sink(x)); // NOT OK
+
+  function gen3() {
+    yield source;
+  }
+  Array.from(gen3()).forEach(x => sink(x)); // NOT OK
+
+  function *gen4() {
+    throw source;
+  }
+  try {
+    Array.from(gen4());
+  } catch (e) {
+    sink(e); // NOT OK
+  }
 });
