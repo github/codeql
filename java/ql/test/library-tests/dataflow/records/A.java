82692876d8f9805bbf83530a5e25b9cbb4116db1
Anders Schack-Mulligen <aschackmull@github.com>
2020-09-01 11:24:30 +0200

    Java: Add some test cases.

--- java/ql/test/library-tests/dataflow/records/A.java
+++ java/ql/test/library-tests/dataflow/records/A.java
@@ -1,7 +1,7 @@
 public class A {
   record Pair(Object x, Object y) { }
 
-  Object source() { return null; }
+  static Object source() { return null; }
 
   void sink(Object o) { }
 
@@ -24,5 +24,42 @@ public class A {
     sink(p1y);
     sink(p2x);
     sink(p2y);
+  }
+
+  record RecWithGetter(Object f) {
+    public Object f() {
+      return this.f;
+    }
+  }
+
+  record RecWithWeirdGetter1(Object f) {
+    public Object f() {
+      return new Object();
+    }
+  }
+
+  record RecWithWeirdGetter2(Object f) {
+    public Object f() {
+      return source();
+    }
+  }
+
+  void testExplicitGetter1() {
+    RecWithGetter r1 = new RecWithGetter(source());
+    RecWithWeirdGetter1 r2 = new RecWithWeirdGetter1(source());
+    RecWithWeirdGetter2 r3 = new RecWithWeirdGetter2(source());
+    testExplicitGetter2(r1, r2, r3);
+  }
+
+  void testExplicitGetter2(RecWithGetter r1, RecWithWeirdGetter1 r2, RecWithWeirdGetter2 r3) {
+    sink(r1.f);
+    sink(r2.f);
+    sink(r3.f);
+    Object r1f = r1.f();
+    Object r2f = r2.f();
+    Object r3f = r3.f();
+    sink(r1f);
+    sink(r2f);
+    sink(r3f);
   }
 }
