bf3a266f581ccbe14497dc50b7ecd85a243c1890
Rasmus Wriedt Larsen <rasmuswl@github.com>
2020-09-02 13:51:00 +0200

    Python: dataflow regression tests: remove taint tracking tests
    they will be reintroduced in an other PR

--- python/ql/test/experimental/dataflow/regression/test.py
+++ python/ql/test/experimental/dataflow/regression/test.py
@@ -212,22 +212,3 @@ def flow_in_generator():
 def flow_from_generator():
     for x in flow_in_generator():
         SINK(x)  # Flow not found
-
-def const_eq_clears_taint():
-    tainted = SOURCE
-    if tainted == "safe":
-        SINK(tainted) # safe  # FP
-    SINK(tainted) # unsafe
-
-def const_eq_clears_taint2():
-    tainted = SOURCE
-    if tainted != "safe":
-        return
-    SINK(tainted) # safe  # FP
-
-def non_const_eq_preserves_taint(x):
-    tainted = SOURCE
-    if tainted == tainted:
-        SINK(tainted) # unsafe
-    if tainted == x:
-        SINK(tainted) # unsafe
