dafcd59148272620a569255a2c482b4f0b12eb74
Erik Krogh Kristensen <erik-krogh@github.com>
2020-09-16 09:53:30 +0200

    add another indirect route-handler test

--- javascript/ql/test/query-tests/Security/CWE-352/MissingCsrfMiddlewareBad.js
+++ javascript/ql/test/query-tests/Security/CWE-352/MissingCsrfMiddlewareBad.js
@@ -26,3 +26,19 @@ app.post('/changeEmail', function (req, res) {
         let newEmail = req.cookies["newEmail"];
     }));
 })
+
+(function () {
+    var app = express();
+
+    app.use(cookieParser());
+    app.use(passport.authorize({ session: true }));
+
+    const errorCatch = (fn) =>
+        (req, res, next) => {
+            fn.call(this, req, res, next).catch((e) => console.log("Caught " + e));
+        };
+
+    app.post('/changeEmail', errorCatch(async function (req, res) {
+        let newEmail = req.cookies["newEmail"];
+    }));
+})
