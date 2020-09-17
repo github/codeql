797ac23db770f7f97c00b0d5a76dd454a8df9c47
Taus Brock-Nannestad <tausbn@github.com>
2020-09-17 13:20:58 +0200

    Python: Clean up global flow test

--- python/ql/test/experimental/dataflow/tainttracking/unwanted-global-flow/test.py
+++ python/ql/test/experimental/dataflow/tainttracking/unwanted-global-flow/test.py
@@ -3,21 +3,42 @@ from taintlib import *
 
 # Various instances where flow is undesirable
 
-tainted = NOT_TAINTED
-ensure_not_tainted(tainted)
+
+# A global variable that starts out being not tainted, but gets tainted through a later assignment.
+# In this case, we do not want flow from the tainting assignment back to the place where the value
+# was used in a potentially unsafe manner.
+
+tainted_later = NOT_TAINTED
+ensure_not_tainted(tainted_later)
 
 def write_global():
-    global tainted
-    tainted = TAINTED_STRING
+    global tainted_later
+    tainted_later = TAINTED_STRING
 
-tainted2 = TAINTED_STRING
-len(tainted2)
-tainted2 = NOT_TAINTED
-ensure_not_tainted(tainted2)
 
-def use_of_tainted2():
-    global tainted2
-    tainted2 = NOT_TAINTED
+# A global variable that starts out tainted, and is subsequently reassigned to be untainted.
+# In this case we don't want flow from the first assignment to any of its uses.
+
+initially_tainted = TAINTED_STRING
+len(initially_tainted) # Some call that _could_ potentially modify `initially_tainted`
+initially_tainted = NOT_TAINTED
+ensure_not_tainted(initially_tainted)
+
+def use_of_initially_tainted():
+    ensure_not_tainted(initially_tainted) # FP
+
+
+# A very similar case to the above, but here we _do_ want taint flow, because the initially tainted
+# value is actually used before it gets reassigned to an untainted value. 
+
+def use_of_initially_tainted2():
+    ensure_tainted(initially_tainted)
+
+initially_tainted2 = TAINTED_STRING
+use_of_initially_tainted2()
+initially_tainted2 = NOT_TAINTED
+ensure_not_tainted(initially_tainted2)
+
 
 # Flow via global assigment
 
@@ -28,5 +49,6 @@ def write_tainted():
 def sink_global():
     ensure_tainted(g)
 
+write_global()
 write_tainted()
 sink_global()
