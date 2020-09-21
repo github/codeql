2bdd0284dc0a152a677fc72ec79532bf7c6a5ab7
Rasmus Wriedt Larsen <rasmuswl@github.com>
2020-09-21 14:44:09 +0200

    Python: Port py-command-line-injection with new dataflow

---
+++ python/ql/test/experimental/query-tests/Security-new-dataflow/CWE-078/command_injection.py
@@ -0,0 +1,35 @@
+
+import os
+import subprocess
+
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/command1")
+def command_injection1():
+    files = request.args.get('files', '')
+    # Don't let files be `; rm -rf /`
+    os.system("ls " + files)
+
+
+@app.route("/command2")
+def command_injection2():
+    files = request.args.get('files', '')
+    # Don't let files be `; rm -rf /`
+    subprocess.Popen(["ls", files], shell = True)
+
+
+@app.route("/command3")
+def first_arg_injection():
+    cmd = request.args.get('cmd', '')
+    subprocess.Popen([cmd, "param1"])
+
+
+@app.route("/other_cases")
+def others():
+    files = request.args.get('files', '')
+    # Don't let files be `; rm -rf /`
+    os.popen("ls " + files)
+
+# TODO: popen2 module for Python 2 only https://devdocs.io/python~2.7/library/popen2
+# (deprecated since Python 2.6, but still functional in Python 2.7.17)
