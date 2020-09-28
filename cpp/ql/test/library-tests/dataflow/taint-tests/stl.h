773bc48a914e77d1db257d2c72e07f8a419e5b45
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2020-09-28 16:37:29 +0100

    C++: Use a more modern make_pair.

--- cpp/ql/test/library-tests/dataflow/taint-tests/stl.h
+++ cpp/ql/test/library-tests/dataflow/taint-tests/stl.h
@@ -325,7 +325,7 @@ namespace std {
 		void swap(pair& p) /*noexcept(...)*/;
 	};
 
-	template <class T1, class T2> pair<T1,T2> make_pair(T1, T2);
+	template<class T1, class T2> constexpr pair<T1, T2> make_pair(T1&&, T2&&);
 }
 
 // --- map ---
