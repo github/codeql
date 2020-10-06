98ab38a630f51486a2fcc04337bbb36eb2b2cf7a
Max Schaefer <max-schaefer@github.com>
2020-10-06 15:32:21 +0100

    JavaScript: Add yet another API-graph test with re-exports.

--- javascript/ql/test/ApiGraphs/reexport/index.js
+++ javascript/ql/test/ApiGraphs/reexport/index.js
@@ -3,5 +3,6 @@ const impl = require("./lib/impl.js");
 module.exports = {
     impl,
     util: require("./lib/utils"),
-    other: require("./lib/stuff")
+    other: require("./lib/stuff"),
+    util2: require("./lib/utils2")
 };
