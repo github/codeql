c71c41b75931def6a2b607074cdcfe1ad474ebae
Rasmus Wriedt Larsen <rasmuswl@github.com>
2020-10-08 12:41:40 +0200

    Python: Django test: Disable DB (for now)

--- python/ql/test/experimental/library-tests/frameworks/django-v2-v3/testproj/settings.py
+++ python/ql/test/experimental/library-tests/frameworks/django-v2-v3/testproj/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = 'testproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
