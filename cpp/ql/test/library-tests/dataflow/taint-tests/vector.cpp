126615a7ee7a79d014c1a36908fe43acda0ac13d
Robert Marsh <rdmarsh@semmle.com>
2020-10-14 13:33:50 -0700

    C++: add prefix operator++ in inserter tests

--- cpp/ql/test/library-tests/dataflow/taint-tests/vector.cpp
+++ cpp/ql/test/library-tests/dataflow/taint-tests/vector.cpp
@@ -435,4 +435,18 @@ void test_vector_inserter(char *source_string) {
 		*it++ = source();
 		sink(out); // tainted [NOT DETECTED]
 	}
+
+	{
+		std::vector<std::string> out;
+		auto it = std::back_inserter(out);
+		*++it = std::string(source_string);
+		sink(out); // tainted [NOT DETECTED]
+	}
+
+	{
+		std::vector<int> out;
+		auto it = std::back_inserter(out);
+		*++it = source();
+		sink(out); // tainted [NOT DETECTED]
+	}
 }
