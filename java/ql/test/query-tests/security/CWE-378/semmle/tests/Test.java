545eb2c4b6de5f5c574c1d08775726c515fe9c8c
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2020-10-15 16:10:06 -0400

    Fix inverted predicate logic and add additional test cases

--- java/ql/test/query-tests/security/CWE-378/semmle/tests/Test.java
+++ java/ql/test/query-tests/security/CWE-378/semmle/tests/Test.java
@@ -35,5 +35,33 @@ public class Test {
             throw new RuntimeException("Failed to create directory");
         }
     }
+
+    static File safe3() {
+        File temp = File.createTempFile("test", "directory");
+        temp.delete();
+        if (!(temp.mkdirs()))) {
+            throw new RuntimeException("Failed to create directory");
+        }
+        return temp;
+    }
+
+    static File safe4() {
+        boolean success = true;
+        File temp = File.createTempFile("test", "directory");
+        success &= temp.delete();
+        success &= f.mkdir();
+        if (!success) {
+            throw new RuntimeException("Failed to create directory");
+        }
+    }
+
+    static File safe5() {
+        File temp = File.createTempFile("test", "directory");
+        if (temp.delete() && temp.mkdir()) {
+            return temp;
+        } else {
+            throw new RuntimeException("Failed to create directory");
+        }
+    }
     
 }
