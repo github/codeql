d89e985246d0d1513f6b935beb3d0d57c2433adb
Rasmus Lerchedahl Petersen <yoff@github.com>
2020-10-24 09:20:30 +0200

    Python: Test showing chaining FP

---
+++ python/ql/test/experimental/query-tests/Security-new-dataflow/CWE-022/test_chaining.py
@@ -0,0 +1,32 @@
+import os.path
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+def source():
+    return request.args.get("path", "")
+
+
+# Wrap normalization, so we can fool the chained configurations.
+# (Call context is lost at cross-over nodes.)
+def normalize(x):
+    return os.path.normpath(x)
+
+
+@app.route("/path")
+def normalize_then_check():
+    x = source()
+    y = normalize(x)  # <--- this call...
+    if y.startswith("subfolder/"):
+        open(y)  # OK
+
+
+@app.route("/path")
+def normalize_check_normalize():
+    x = source()
+    y = normalize(x)  # (...or this call...)
+    if y.startswith("subfolder/"):
+        z = normalize(y)  # <--- ...can jump to here, resulting in FP
+        open(z)  # OK
