0b35b34288ec253dd117fe6fcd94b9af6805dc78
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2020-10-27 09:04:27 +0000

    C++: Add pointer tests.

--- cpp/ql/test/library-tests/dataflow/DefaultTaintTracking/shared.h
+++ cpp/ql/test/library-tests/dataflow/DefaultTaintTracking/shared.h
@@ -12,3 +12,6 @@ char *strcat(char * s1, const char * s2);
 char *strdup(const char *string);
 char *_strdup(const char *string);
 char *unmodeled_function(const char *const_string);
+
+typedef unsigned long size_t;
+void *memcpy(void *s1, const void *s2, size_t n);
