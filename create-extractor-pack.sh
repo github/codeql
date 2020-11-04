a83ac246523d1edd26889792dc9585a6bd0e6965
Nick Rolfe <nickrolfe@github.com>
2020-11-04 11:59:17 +0000

    Add bash script to create extractor pack locally

---
+++ create-extractor-pack.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eux
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  platform="linux64"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  platform="osx64"
+else
+  echo "Unknown OS"
+  exit 1
+fi
+
+cargo build --release
+
+cargo run --release -p ruby-generator
+codeql query format -i ql/src/codeql_ruby/ast.qll
+
+rm -rf extractor-pack
+mkdir -p extractor-pack
+cp -r codeql-extractor.yml tools ql/src/ruby.dbscheme ql/src/ruby.dbscheme.stats extractor-pack/
+mkdir -p extractor-pack/tools/${platform}
+cp target/release/ruby-extractor extractor-pack/tools/${platform}/extractor
