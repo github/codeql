b16588f0580557c03ee94c6e757af4f326766504
Nick Rolfe <nickrolfe@github.com>
2020-11-04 12:09:52 +0000

    Add powershell script to create extractor pack locally

---
+++ create-extractor-pack.ps1
@@ -0,0 +1,11 @@
+cargo build --release
+
+cargo run --release -p ruby-generator
+codeql query format -i ql\src\codeql_ruby\ast.qll
+
+rm -Recurse -Force extractor-pack
+mkdir extractor-pack | Out-Null
+cp codeql-extractor.yml, ql\src\ruby.dbscheme, ql\src\ruby.dbscheme.stats extractor-pack
+cp -Recurse tools extractor-pack
+mkdir extractor-pack\tools\win64 | Out-Null
+cp target\release\ruby-extractor.exe extractor-pack\tools\win64\extractor.exe
