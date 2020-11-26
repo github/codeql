548f276e1f4d568c545496adc6dacd4b4a4ee538
Tamas Vajk <tamasvajk@github.com>
2020-11-26 12:15:21 +0100

    Add more tests

--- csharp/ql/test/library-tests/csharp9/TargetType.cs
+++ csharp/ql/test/library-tests/csharp9/TargetType.cs
@@ -9,12 +9,33 @@ public class TargetType
         var rand = new Random();
         var condition = rand.NextDouble() > 0.5;
 
-        int? x = condition
+        int? x0 = 12;
+        x0 = 13;
+        int? x1 = null;
+
+        int? x2 = condition
             ? 12
             : null;
 
-        IEnumerable<int> xs = x is null
+        int? x3 = condition
+            ? (int?)12
+            : null;
+
+        int? x4 = condition
+            ? 12
+            : (int?)null;
+
+        IEnumerable<int> xs0 = new List<int>() { 0, 1 };
+        IEnumerable<int> xs1 = new int[] { 2, 3 };
+
+        IEnumerable<int> xs2 = x2 is null
             ? new List<int>() { 0, 1 }
             : new int[] { 2, 3 };
+
+        int? c = condition
+            ? new TargetType()
+            : 12;
     }
+
+    public static implicit operator int(TargetType d) => 0;
 }
