4ead118a312bdf43e41ace8eccb65992bc9c9719
Rasmus Wriedt Larsen <rasmuswl@github.com>
2020-12-04 13:27:01 +0100

    Python: Add class based route handler in django tests
    Disabled CSRF middleware for now, since it blocked my debugging curl POST requests :(

--- python/ql/test/experimental/library-tests/frameworks/django-v2-v3/testapp/views.py
+++ python/ql/test/experimental/library-tests/frameworks/django-v2-v3/testapp/views.py
@@ -1,10 +1,33 @@
 from django.http import HttpRequest, HttpResponse
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
 
 def foo(request: HttpRequest):  # $routeHandler
     return HttpResponse("foo")  # $HttpResponse
 
+
 def bar_baz(request: HttpRequest):  # $routeHandler
     return HttpResponse("bar_baz")  # $HttpResponse
 
+
 def deprecated(request: HttpRequest):  # $routeHandler
     return HttpResponse("deprecated")  # $HttpResponse
+
+
+class MyBasicViewHandler(View):
+    def get(self, request: HttpRequest): # $ MISSING: routeHandler
+        return HttpResponse("MyViewHandler: GET") # $ HttpResponse
+
+    def post(self, request: HttpRequest): # $ MISSING: routeHandler
+        return HttpResponse("MyViewHandler: POST") # $ HttpResponse
+
+
+class MyCustomViewBaseClass(View):
+    def post(self, request: HttpRequest): # $ MISSING: routeHandler
+        return HttpResponse("MyCustomViewBaseClass: POST") # $ HttpResponse
+
+
+class MyViewHandlerWithCustomInheritance(MyCustomViewBaseClass):
+    def get(self, request: HttpRequest): # $ MISSING: routeHandler
+        return HttpResponse("MyViewHandlerWithCustomInheritance: GET") # $ HttpResponse
