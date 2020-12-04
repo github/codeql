d55fbc8a0541b6dac566acd6784c8538584543d0
Tamas Vajk <tamasvajk@github.com>
2020-12-04 10:38:45 +0100

    Add test cases for safe API calls

--- csharp/ql/test/query-tests/Security Features/CWE-020/UntrustedData.cs
+++ csharp/ql/test/query-tests/Security Features/CWE-020/UntrustedData.cs
@@ -9,6 +9,9 @@ public class UntrustedData : IHttpHandler
     public void ProcessRequest(HttpContext ctx)
     {
         var name = ctx.Request.QueryString["name"];
+        var len = name.Length;
+
+        var myEncodedString = HttpUtility.HtmlEncode(name);
         ctx.Response.Write(name);
     }
 
