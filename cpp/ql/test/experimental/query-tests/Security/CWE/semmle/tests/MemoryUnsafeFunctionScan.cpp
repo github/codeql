209191bb2464b935af5046d82aeca0e469c22dd6
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2020-12-09 16:46:33 +0000

    C++: Add another good example.

--- cpp/ql/test/experimental/query-tests/Security/CWE/semmle/tests/MemoryUnsafeFunctionScan.cpp
+++ cpp/ql/test/experimental/query-tests/Security/CWE/semmle/tests/MemoryUnsafeFunctionScan.cpp
@@ -1,5 +1,10 @@
 ///// Library routines /////
 
+typedef unsigned long size_t;
+void *malloc(size_t size);
+
+size_t strlen(const char *s);
+
 int scanf(const char *format, ...);
 int sscanf(const char *str, const char *format, ...);
 int fscanf(const char *str, const char *format, ...);
@@ -21,6 +26,15 @@ int main(int argc, char **argv)
     // BAD, do not use scanf without specifying a length first
     char file[10];
     fscanf(file, "%s", buf2);
+
+    // GOOD, with 'sscanf' the input can be checked first and enough room allocated [FALSE POSITIVE]
+    if (argc >= 1)
+    {
+		char *src = argv[0];
+		char *dest = (char *)malloc(strlen(src) + 1);
+
+		sscanf(src, "%s", dest);
+	}
 
     return 0;
 }
