6bf93452584835faea1371e8309b90507a1d8667
Asger Feldthaus <asgerf@github.com>
2020-12-12 20:27:03 +0000

    JS: Add test for class with locally-unused field

--- javascript/ql/test/library-tests/frameworks/Angular2/sink.component.ts
+++ javascript/ql/test/library-tests/frameworks/Angular2/sink.component.ts
@@ -11,6 +11,7 @@ export class SinkComponent {
     sink3: string;
     sink4: string;
     sink5: string;
+    sink6: string;
 
     constructor(private sanitizer: DomSanitizer) {}
 
@@ -20,5 +21,6 @@ export class SinkComponent {
         this.sanitizer.bypassSecurityTrustHtml(this.sink3);
         this.sanitizer.bypassSecurityTrustHtml(this.sink4);
         this.sanitizer.bypassSecurityTrustHtml(this.sink5);
+        this.sanitizer.bypassSecurityTrustHtml(this.sink6);
     }
 }
