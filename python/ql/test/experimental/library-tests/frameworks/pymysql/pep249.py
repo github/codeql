31d4ea77cbce6b801420ff39a5a492f1b4dd17a7
Rasmus Wriedt Larsen <rasmuswl@github.com>
2020-12-14 10:56:33 +0100

    Python: Add modeling of PyMySQL

--- python/ql/test/experimental/library-tests/frameworks/pymysql/pep249.py
+++ python/ql/test/experimental/library-tests/frameworks/pymysql/pep249.py
@@ -2,4 +2,4 @@ import pymysql
 connection = pymysql.connect(host="localhost", user="user", password="passwd")
 
 cursor = connection.cursor()
-cursor.execute("some sql", (42,))  # $ MISSING: getSql="some sql"
+cursor.execute("some sql", (42,))  # $ getSql="some sql"
