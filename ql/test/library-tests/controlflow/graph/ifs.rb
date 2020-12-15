bc47338b52f8420ddadcd2f38b2147a5372e63eb
Arthur Baars <aibaars@github.com>
2020-12-15 18:05:13 +0100

    CFG: add test-case for conditional method declarations

--- ql/test/library-tests/controlflow/graph/ifs.rb
+++ ql/test/library-tests/controlflow/graph/ifs.rb
@@ -32,3 +32,7 @@ end
 def m5 (b1, b2, b3, b4, b5)
   if (if b1 then b2 elsif b3 then b4 else b5 end) then "b2 || b4 || b5" else "!b2 || !b4 || !b5" end
 end
+
+def conditional_method_def()
+  puts "bla"
+end unless 1 == 2
