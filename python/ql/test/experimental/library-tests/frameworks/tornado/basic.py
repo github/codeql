57d08a85235129f0d999a61acec60b032d77ab0c
Rasmus Wriedt Larsen <rasmuswl@github.com>
2020-12-16 10:37:18 +0100

    Python: Rewrite old Tornado tests
    Now you can run them, and the examples have been adjusted so they actually work!

---
+++ python/ql/test/experimental/library-tests/frameworks/tornado/basic.py
@@ -0,0 +1,54 @@
+import tornado.web
+
+
+class BasicHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("BasicHandler " + self.get_argument("xss"))
+
+    def post(self):
+        self.write("BasicHandler (POST)")
+
+
+class DeepInheritance(BasicHandler):
+    def get(self):
+        self.write("DeepInheritance" + self.get_argument("also_xss"))
+
+
+class FormHandler(tornado.web.RequestHandler):
+    def post(self):
+        name = self.get_body_argument("name")
+        self.write(name)
+
+
+class RedirectHandler(tornado.web.RequestHandler):
+    def get(self):
+        req = self.request
+        h = req.headers
+        url = h["url"]
+        self.redirect(url)
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/basic", BasicHandler),
+        (r"/deep", DeepInheritance),
+        (r"/form", FormHandler),
+        (r"/redirect", RedirectHandler),
+    ])
+
+
+if __name__ == "__main__":
+    import tornado.ioloop
+
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
+
+    # http://localhost:8888/basic?xss=foo
+    # http://localhost:8888/deep?also_xss=foo
+
+    # curl -X POST http://localhost:8888/basic
+    # curl -X POST http://localhost:8888/deep
+
+    # curl -X POST -F "name=foo" http://localhost:8888/form
+    # curl -v -H 'url: http://example.com' http://localhost:8888/redirect
