01b204ea30b880e388364b74be9cc319ced31423
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2021-01-04 15:33:10 +0000

    C++: Add a test case with a tainted integer.

--- cpp/ql/test/query-tests/Security/CWE/CWE-089/SqlTainted/test.c
+++ cpp/ql/test/query-tests/Security/CWE/CWE-089/SqlTainted/test.c
@@ -7,11 +7,13 @@ typedef unsigned long size_t;
 int snprintf(char *s, size_t n, const char *format, ...);
 void sanitizeString(char *stringOut, size_t len, const char *strIn);
 int mysql_query(int arg1, const char *sqlArg);
+int atoi(const char *nptr);
 
 ///// Test code /////
 
 int main(int argc, char** argv) {
   char *userName = argv[2];
+  int userNumber = atoi(argv[3]);
 
   // a string from the user is injected directly into an SQL query.
   char query1[1000] = {0};
@@ -24,4 +26,9 @@ int main(int argc, char** argv) {
   char query2[1000] = {0};
   snprintf(query2, 1000, "SELECT UID FROM USERS where name = \"%s\"", userNameSanitized);
   mysql_query(0, query2); // GOOD
+
+  // an integer from the user is injected into an SQL query.
+  char query3[1000] = {0};
+  snprintf(query3, 1000, "SELECT UID FROM USERS where number = \"%i\"", userNumber);
+  mysql_query(0, query3); // BAD [FALSE POSITIVE]
 }
