potar-vietcombank/https-github.com

support nested stars in js/ReDoS

javascript/ql/test/query-tests/Performance/ReDoS/tst.js
@@ -56,7 +56,7 @@ var good6 = /(a|.)*/;
 
 // Testing the NFA - only some of the below are detected.
 var bad7 = /^([a-z]+)+$/;
-var bad8 = /^([a-z]*)*$/; // NOT detected
+var bad8 = /^([a-z]*)*$/;
 var bad9 = /^([a-zA-Z0-9])(([\\-.]|[_]+)?([a-zA-Z0-9]+))*(@){1}[a-z0-9]+[.]{1}(([a-z]{2,3})|([a-z]{2,3}[.]{1}[a-z]{2,3}))$/;
 var bad10 = /^(([a-z])+.)+[A-Z]([a-z])+$/;
 
@@ -310,13 +310,13 @@ var good36 = /(([^/]|X)+)(\/[^]*)*$/;
 // GOOD - but we spuriously conclude that a rejecting suffix exists. 
 var good37 = /^((x([^Y]+)?)*(Y|$))/;
 
-// NOT GOOD - but not detected
+// NOT GOOD
 var bad68 = /(a*)+b/;
 
-// NOT GOOD - but not detected
+// NOT GOOD
 var bad69 = /foo([\w-]*)+bar/;
 
-// NOT GOOD - but not detected
+// NOT GOOD
 var bad70 = /((ab)*)+c/;
 
 // NOT GOOD
@@ -344,7 +344,7 @@ var bad76 = /^(b+.)+$/;
 var good39 = /a*b/;
 
 // All 4 bad combinations of nested * and +
-var bad77 = /(a*)*b/; // not detected
+var bad77 = /(a*)*b/;
 var bad78 = /(a+)*b/;
-var bad79 = /(a*)+b/; // not detected
+var bad79 = /(a*)+b/;
 var bad80 = /(a+)+b/;

github/codeql

javascript/ql/test/query-tests/Performance/ReDoS/tst.js
@@ -56,7 +56,7 @@ var good6 = /(a|.)*/;
 
 // Testing the NFA - only some of the below are detected.
 var bad7 = /^([a-z]+)+$/;
-var bad8 = /^([a-z]*)*$/; // NOT detected
+var bad8 = /^([a-z]*)*$/;
 var bad9 = /^([a-zA-Z0-9])(([\\-.]|[_]+)?([a-zA-Z0-9]+))*(@){1}[a-z0-9]+[.]{1}(([a-z]{2,3})|([a-z]{2,3}[.]{1}[a-z]{2,3}))$/;
 var bad10 = /^(([a-z])+.)+[A-Z]([a-z])+$/;
 
@@ -310,13 +310,13 @@ var good36 = /(([^/]|X)+)(\/[^]*)*$/;
 // GOOD - but we spuriously conclude that a rejecting suffix exists. 
 var good37 = /^((x([^Y]+)?)*(Y|$))/;
 
-// NOT GOOD - but not detected
+// NOT GOOD
 var bad68 = /(a*)+b/;
 
-// NOT GOOD - but not detected
+// NOT GOOD
 var bad69 = /foo([\w-]*)+bar/;
 
-// NOT GOOD - but not detected
+// NOT GOOD
 var bad70 = /((ab)*)+c/;
 
 // NOT GOOD
@@ -347,7 +347,7 @@ var bad78 = /^(b+.)+$/;
 var good39 = /a*b/;
 
 // All 4 bad combinations of nested * and +
-var bad79 = /(a*)*b/; // not detected
+var bad79 = /(a*)*b/;
 var bad80 = /(a+)*b/;
-var bad81 = /(a*)+b/; // not detected
+var bad81 = /(a*)+b/;
 var bad82 = /(a+)+b/;