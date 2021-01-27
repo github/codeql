bec00643968bad1ae39d53e0256111187569b907
ihsinme <61293369+ihsinme@users.noreply.github.com>
2021-01-27 14:54:47 +0300

    Update test.cpp

--- cpp/ql/test/experimental/query-tests/Security/CWE/CWE-570/semmle/tests/test.cpp
+++ cpp/ql/test/experimental/query-tests/Security/CWE/CWE-570/semmle/tests/test.cpp
@@ -20,14 +20,14 @@ void* operator new[]( std::size_t count, const std::nothrow_t& tag );
 void badNew_0_0()
 {
     while (true) {
-        new int[100];
-        if(!(new int[100]))
+        new int[100]; // BAD [NOT DETECTED]
+        if(!(new int[100])) // BAD [NOT DETECTED]
             return;
     }
 }
 void badNew_0_1()
 {
-    int * i = new int[100];
+    int * i = new int[100]; // BAD
     if(i == 0)
         return;
     if(!i)
@@ -35,7 +35,7 @@ void badNew_0_1()
     if(i == NULL)
         return;
     int * j;
-    j = new int[100];
+    j = new int[100]; // BAD
     if(j == 0)
         return;
     if(!j)
@@ -47,10 +47,10 @@ void badNew_1_0()
 {
     try {
         while (true) {
-            new(std::nothrow) int[100];
-            int* p = new(std::nothrow) int[100];
+            new(std::nothrow) int[100]; // BAD
+            int* p = new(std::nothrow) int[100]; // BAD
             int* p1;
-            p1 = new(std::nothrow) int[100];
+            p1 = new(std::nothrow) int[100]; // BAD
         }
     } catch (const exception &){//const std::bad_alloc& e) {
 //        std::cout << e.what() << '\n';
@@ -59,8 +59,8 @@ void badNew_1_0()
 void badNew_1_1()
 {
     while (true) {
-        int* p = new(std::nothrow) int[100];
-        new(std::nothrow) int[100];
+        int* p = new(std::nothrow) int[100]; // BAD [NOT DETECTED]
+        new(std::nothrow) int[100]; // BAD [NOT DETECTED]
     }
 }
 
@@ -68,7 +68,7 @@ void goodNew_0_0()
 {
     try {
         while (true) {
-            new int[100];
+            new int[100]; // GOOD
         }
     } catch (const exception &){//const std::bad_alloc& e) {
 //        std::cout << e.what() << '\n';
@@ -78,18 +78,18 @@ void goodNew_0_0()
 void goodNew_1_0()
 {
     while (true) {
-        int* p = new(std::nothrow) int[100];
+        int* p = new(std::nothrow) int[100]; // GOOD
         if (p == nullptr) {
 //            std::cout << "Allocation returned nullptr\n";
             break;
         }
         int* p1;
-        p1 = new(std::nothrow) int[100];
+        p1 = new(std::nothrow) int[100]; // GOOD
         if (p1 == nullptr) {
 //            std::cout << "Allocation returned nullptr\n";
             break;
         }
-        if (new(std::nothrow) int[100] == nullptr) {
+        if (new(std::nothrow) int[100] == nullptr) { // GOOD
 //            std::cout << "Allocation returned nullptr\n";
             break;
         }
