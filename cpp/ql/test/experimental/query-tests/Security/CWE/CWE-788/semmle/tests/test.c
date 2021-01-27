aebf7bdff412b6a49441a223eb1535af162fb721
ihsinme <61293369+ihsinme@users.noreply.github.com>
2021-01-27 13:12:48 +0300

    Update test.c

--- cpp/ql/test/experimental/query-tests/Security/CWE/CWE-788/semmle/tests/test.c
+++ cpp/ql/test/experimental/query-tests/Security/CWE/CWE-788/semmle/tests/test.c
@@ -2,14 +2,14 @@ void workFunction_0(char *s) {
   char buf[80];
   strncat(buf, s, sizeof(buf)-strlen(buf)-1); // GOOD
   strncat(buf, s, sizeof(buf)-strlen(buf));  // BAD
-  strncat(buf, "fix", sizeof(buf)-strlen(buf)); // BAD but usually the size of the buffer is calculated manually. 
+  strncat(buf, "fix", sizeof(buf)-strlen(buf)); // BAD [NOT DETECTED] 
 }
 void workFunction_1(char *s) {
 #define MAX_SIZE 80
   char buf[MAX_SIZE];
   strncat(buf, s, MAX_SIZE-strlen(buf)-1); // GOOD
   strncat(buf, s, MAX_SIZE-strlen(buf));  // BAD
-  strncat(buf, "fix", MAX_SIZE-strlen(buf)); // BAD but usually the size of the buffer is calculated manually. 
+  strncat(buf, "fix", MAX_SIZE-strlen(buf)); // BAD [NOT DETECTED]
 }
 void workFunction_2_0(char *s) {
   char * buf;
@@ -17,8 +17,7 @@ void workFunction_2_0(char *s) {
   buf = (char *) malloc(len);
   strncat(buf, s, len-strlen(buf)-1); // GOOD
   strncat(buf, s, len-strlen(buf));  // BAD
-  strncat(buf, "fix", len-strlen(buf)); // BAD but usually the size of the buffer is calculated manually. 
-}
+  strncat(buf, "fix", len-strlen(buf)); // BAD [NOT DETECTED]
 void workFunction_2_1(char *s) {
   char * buf;
   int len=80;
