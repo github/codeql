683233333cf7dc328a1a6fb7726be200ff40ad8f
Francis Alexander <helofrancis@gmail.com>
2021-02-04 22:28:10 +0530

    test case return statements and feedback

--- java/ql/test/stubs/playframework-2.6.x/play/libs/F.java
+++ java/ql/test/stubs/playframework-2.6.x/play/libs/F.java
@@ -6,11 +6,14 @@ package play.libs;
 
 import java.util.*;
 import java.util.concurrent.*;
-import java.util.function.Supplier;
 
-//import scala.concurrent.ExecutionContext;
+// import scala.concurrent.ExecutionContext;
 
 /** Defines a set of functional programming style helpers. */
 public class F {
-    public static class Promise<A> { } // this is needed for play.libs.F for Play 2.3.x
+  public static class Promise<A> { // this is needed for play.libs.F for Play 2.3.x
+    public static <A> Promise<A> pure(final A a) {
+      return null;
+    }
+  }
 }
