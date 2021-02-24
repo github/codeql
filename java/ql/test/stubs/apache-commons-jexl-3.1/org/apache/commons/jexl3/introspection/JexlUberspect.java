34b6ed0a05d2bdd2c1f789882d121f011899b0b2
Artem Smotrakov <artem.smotrakov@gmail.com>
2021-02-24 22:31:03 +0100

    Removed commented code from JexlUberspect

--- java/ql/test/stubs/apache-commons-jexl-3.1/org/apache/commons/jexl3/introspection/JexlUberspect.java
+++ java/ql/test/stubs/apache-commons-jexl-3.1/org/apache/commons/jexl3/introspection/JexlUberspect.java
@@ -6,32 +6,4 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-public interface JexlUberspect {
-    /*
-    interface PropertyResolver {}
-
-    List<PropertyResolver> getResolvers(JexlOperator op, Object obj);
-
-    void setClassLoader(ClassLoader loader);
-
-    ClassLoader getClassLoader();
-
-    int getVersion();
-
-    JexlMethod getConstructor(Object ctorHandle, Object... args);
-
-    JexlMethod getMethod(Object obj, String method, Object... args);
-
-    JexlPropertyGet getPropertyGet(Object obj, Object identifier);
-
-    JexlPropertyGet getPropertyGet(List<PropertyResolver> resolvers, Object obj, Object identifier);
-
-    JexlPropertySet getPropertySet(Object obj, Object identifier, Object arg);
-
-    JexlPropertySet getPropertySet(List<PropertyResolver> resolvers, Object obj, Object identifier, Object arg);
-
-    Iterator<?> getIterator(Object obj);
-
-    JexlArithmetic.Uberspect getArithmetic(JexlArithmetic arithmetic);
-    */
-}
+public interface JexlUberspect {}
