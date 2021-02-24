d05a733109f3c634ace0c96366cdf7cbfeeb27b5
Tamas Vajk <tamasvajk@github.com>
2021-02-24 12:29:05 +0100

    Add more test cases

--- csharp/ql/test/library-tests/dataflow/tuples/Tuples.cs
+++ csharp/ql/test/library-tests/dataflow/tuples/Tuples.cs
@@ -1,3 +1,5 @@
+using System;
+
 class Tuples
 {
     static void M1()
@@ -24,5 +26,63 @@ class Tuples
         Sink(x.Item2.Item2);    // Tainted
     }
 
+    static void M2()
+    {
+        var x = ("taint source", 2, 3, 4, 5, 6, 7, 8, 9, "taint source");
+        Sink(x.Item1);          // Tainted
+        Sink(x.Item2);
+        Sink(x.Item10);         // Tainted
+    }
+
+    static void M3()
+    {
+        var x = (ValueTuple<string, int, int>)("taint source", 2, 3);
+        Sink(x.Item1);          // Tainted
+        Sink(x.Item2);
+
+        var y = (ValueTuple<object, int, int>)("taint source", 2, 3);
+        Sink(y.Item1);          // Tainted, not found
+        Sink(y.Item2);
+    }
+
+    static void M4()
+    {
+        var x = ("taint source", 2, 3);
+        switch (x)
+        {
+            case ValueTuple<string, int, int> t when t.Item2 > 10:
+                break;
+            case var (a, b, _):
+                Sink(a);        // Tainted, not found
+                Sink(b);
+                break;
+        }
+    }
+
+    record R1(string i, int j) { };
+
+    static void M5()
+    {
+        var r = new R1("taint source", 1);
+        Sink(r.i);          // Tainted
+
+        var (a, b) = r;
+        Sink(a);            // Tainted, not found
+        Sink(b);
+
+        switch (r)
+        {
+            case var (x, y):
+                Sink(x);        // Tainted, not found
+                Sink(y);
+                break;
+        }
+    }
+
     static void Sink<T>(T x) { }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit { }
 }
