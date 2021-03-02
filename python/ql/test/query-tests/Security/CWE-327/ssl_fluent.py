7a1d953fcac8d159440345dd26dd13b4136f1481
Rasmus Lerchedahl Petersen <yoff@github.com>
2021-03-02 22:46:01 +0100

    Python: More tests

--- python/ql/test/query-tests/Security/CWE-327/ssl_fluent.py
+++ python/ql/test/query-tests/Security/CWE-327/ssl_fluent.py
@@ -66,6 +66,102 @@ def test_fluent_ssl_safe_combined():
         with context.wrap_socket(sock, server_hostname=hostname) as ssock:
             print(ssock.version())
 
+def test_fluent_ssl_unsafe_combined_wrongly():
+    hostname = 'www.python.org'
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    context.options |= ssl.OP_NO_TLSv1 & ssl.OP_NO_TLSv1_1
+
+    with socket.create_connection((hostname, 443)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+            print(ssock.version())
+
+def test_fluent_ssl_safe_combined_multiple():
+    hostname = 'www.python.org'
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2
+
+    with socket.create_connection((hostname, 443)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+            print(ssock.version())
+
+
+def create_relaxed_context():
+    return ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+
+def create_secure_context():
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
+    return context
+
+def create_connection(context):
+    with socket.create_connection(('www.python.org', 443)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+            print(ssock.version())
+
+def test_delegated_context_unsafe():
+    context = create_relaxed_context()
+    with socket.create_connection(('www.python.org', 443)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+            print(ssock.version())
+
+def test_delegated_context_safe():
+    context = create_secure_context()
+    with socket.create_connection(('www.python.org', 443)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+            print(ssock.version())
+
+def test_delegated_context_made_safe():
+    context = create_relaxed_context()
+    context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
+    with socket.create_connection(('www.python.org', 443)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+            print(ssock.version())
+
+def test_delegated_context_made_unsafe():
+    context = create_secure_context()
+    context.options &= ~ssl.OP_NO_TLSv1_1
+    with socket.create_connection(('www.python.org', 443)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+            print(ssock.version())
+
+def test_delegated_connection_unsafe():
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    create_connection(context)
+
+def test_delegated_connection_safe():
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
+    create_connection(context)
+
+def test_delegated_connection_made_safe():
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
+    create_connection(context)
+
+def test_delegated_connection_made_unsafe():
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
+    context.options &= ~ssl.OP_NO_TLSv1_1
+    create_connection(context)
+
+def test_delegated_unsafe():
+    context = create_relaxed_context()
+    create_connection(context)
+
+def test_delegated_safe():
+    context = create_secure_context()
+    create_connection(context)
+
+def test_delegated_made_safe():
+    context = create_relaxed_context()
+    context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
+    create_connection(context)
+
+def test_delegated_made_unsafe():
+    context = create_secure_context()
+    context.options &= ~ssl.OP_NO_TLSv1_1
+    create_connection(context)
+
 # From Python 3.7
 # see https://docs.python.org/3/library/ssl.html#ssl.SSLContext.minimum_version
 def test_fluent_ssl_unsafe_version():
