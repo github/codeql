7ffbfa80438731956f999832c2fbc356eec357c2
Rasmus Wriedt Larsen <rasmuswl@github.com>
2021-03-02 16:45:09 +0100

    Python: Expand stdlib md5 tests with keyword-arguments

--- python/ql/test/experimental/library-tests/frameworks/stdlib/test_md5.py
+++ python/ql/test/experimental/library-tests/frameworks/stdlib/test_md5.py
@@ -5,6 +5,10 @@ hasher = hashlib.md5(b"secret message") # $ MISSING: CryptographicOperation Cryp
 print(hasher.hexdigest())
 
 
+hasher = hashlib.md5(string=b"secret message") # $ MISSING: CryptographicOperation CryptographicOperationInput=b"secret message" CryptographicOperationAlgorithm=MD5
+print(hasher.hexdigest())
+
+
 hasher = hashlib.md5()
 hasher.update(b"secret") # $ MISSING: CryptographicOperation CryptographicOperationInput=b"secret" CryptographicOperationAlgorithm=MD5
 hasher.update(b" message") # $ MISSING: CryptographicOperation CryptographicOperationInput=b" message" CryptographicOperationAlgorithm=MD5
@@ -12,6 +16,10 @@ print(hasher.hexdigest())
 
 
 hasher = hashlib.new('md5', b"secret message") # $ MISSING: CryptographicOperation CryptographicOperationInput=b"secret message" CryptographicOperationAlgorithm=MD5
+print(hasher.hexdigest())
+
+
+hasher = hashlib.new('md5', data=b"secret message") # $ MISSING: CryptographicOperation CryptographicOperationInput=b"secret message" CryptographicOperationAlgorithm=MD5
 print(hasher.hexdigest())
 
 
