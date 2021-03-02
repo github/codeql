bf6f5074c24ff8064a3609b3016b23d75a41def7
Rasmus Wriedt Larsen <rasmuswl@github.com>
2021-03-02 10:06:06 +0100

    Python: Port cryptodome tests to crypto
    I don't know if this is really a smart test-setup... I feel a bit stupid when
    doing this xD

--- python/ql/test/experimental/library-tests/frameworks/crypto/test_rc4.py
+++ python/ql/test/experimental/library-tests/frameworks/crypto/test_rc4.py
@@ -17,14 +17,14 @@ print("encrypt/decrypt")
 secret_message = b"secret message"
 
 cipher = ARC4.new(key)
-encrypted = cipher.encrypt(secret_message)
+encrypted = cipher.encrypt(secret_message) # $ CryptographicOperation CryptographicOperationAlgorithm=ARC4 CryptographicOperationInput=secret_message
 
 print("encrypted={}".format(encrypted))
 
 print()
 
 cipher = ARC4.new(key)
-decrypted = cipher.decrypt(encrypted)
+decrypted = cipher.decrypt(encrypted) # $ CryptographicOperation CryptographicOperationAlgorithm=ARC4 CryptographicOperationInput=encrypted
 
 print("decrypted={}".format(decrypted))
 assert decrypted == secret_message
