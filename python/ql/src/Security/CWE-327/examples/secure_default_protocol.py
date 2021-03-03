dcf8c881ff61b75bfce18c3abfd9c7f44e1768c8
Rasmus Lerchedahl Petersen <yoff@github.com>
2021-03-03 16:54:36 +0100

    Python: correct mistake in example

--- python/ql/src/Security/CWE-327/examples/secure_default_protocol.py
+++ python/ql/src/Security/CWE-327/examples/secure_default_protocol.py
@@ -6,4 +6,4 @@ context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
 
 # Declaring a minimum version to restrict the protocol
 context = ssl.create_default_context()
-context.minimum_version(ssl.TLSVersion.TLSv1_2)
+context.minimum_version = ssl.TLSVersion.TLSv1_2
