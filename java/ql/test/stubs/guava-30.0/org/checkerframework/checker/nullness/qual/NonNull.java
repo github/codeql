1e3c4d0eb18672f3eef3a05dd7b382bad24b0643
Joe Farebrother <joefarebrother@github.com>
2021-03-09 14:10:08 +0000

    Add stubs to fix broken test case

--- java/ql/test/stubs/guava-30.0/org/checkerframework/checker/nullness/qual/NonNull.java
+++ java/ql/test/stubs/guava-30.0/org/checkerframework/checker/nullness/qual/NonNull.java
@@ -3,4 +3,4 @@ import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-public @interface Nullable {}
+public @interface NonNull {}
