4094b184074ca1c4b20e4ad599bef6d536f518c3
Rasmus Lerchedahl Petersen <yoff@github.com>
2021-03-15 16:28:08 +0100

    Python: Clean up tests

--- python/ql/test/query-tests/Security/CWE-327/InsecureProtocol.py
+++ python/ql/test/query-tests/Security/CWE-327/InsecureProtocol.py
@@ -2,7 +2,7 @@ import ssl
 from OpenSSL import SSL
 from ssl import SSLContext
 
-# true positives
+# insecure versions specified
 ssl.wrap_socket(ssl_version=ssl.PROTOCOL_SSLv2)
 ssl.wrap_socket(ssl_version=ssl.PROTOCOL_SSLv3)
 ssl.wrap_socket(ssl_version=ssl.PROTOCOL_TLSv1)
@@ -12,41 +12,26 @@ SSLContext(protocol=ssl.PROTOCOL_SSLv3)
 SSLContext(protocol=ssl.PROTOCOL_TLSv1)
 
 SSL.Context(SSL.SSLv2_METHOD)
-SSL.Context(SSL.SSLv23_METHOD)
 SSL.Context(SSL.SSLv3_METHOD)
 SSL.Context(SSL.TLSv1_METHOD)
-
-# not relevant
-wrap_socket(ssl_version=ssl.PROTOCOL_SSLv3)
-wrap_socket(ssl_version=ssl.PROTOCOL_TLSv1)
-wrap_socket(ssl_version=ssl.PROTOCOL_SSLv2)
-
-Context(SSL.SSLv3_METHOD)
-Context(SSL.TLSv1_METHOD)
-Context(SSL.SSLv2_METHOD)
-Context(SSL.SSLv23_METHOD)
-
-# true positive using flow
 
 METHOD = SSL.SSLv2_METHOD
 SSL.Context(METHOD)
 
-# secure versions
+# importing the protocol constant directly
+from ssl import PROTOCOL_SSLv2
+ssl.wrap_socket(ssl_version=PROTOCOL_SSLv2)
+SSLContext(protocol=PROTOCOL_SSLv2)
 
+# secure versions specified
 ssl.wrap_socket(ssl_version=ssl.PROTOCOL_TLSv1_2)
 SSLContext(protocol=ssl.PROTOCOL_TLSv1_2)
 SSL.Context(SSL.TLSv1_2_METHOD)
 
-# possibly insecure default
-ssl.wrap_socket()
-context = SSLContext()
+# possibly secure versions specified
+SSLContext(protocol=ssl.PROTOCOL_SSLv23)
+SSLContext(protocol=ssl.PROTOCOL_TLS)
+SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
+SSLContext(protocol=ssl.PROTOCOL_TLS_SERVER)
 
-# importing the protocol constant directly
-
-from ssl import PROTOCOL_SSLv2
-
-ssl.wrap_socket(ssl_version=PROTOCOL_SSLv2)
-SSLContext(protocol=PROTOCOL_SSLv2)
-
-# FP for insecure default
-ssl.SSLContext(ssl.SSLv23_METHOD)
+SSL.Context(SSL.SSLv23_METHOD)
