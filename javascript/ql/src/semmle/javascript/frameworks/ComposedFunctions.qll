asger-semmle/ql

JS: ComposedFunctions

javascript/ql/src/semmle/javascript/frameworks/ComposedFunctions.qll
@@ -28,21 +28,16 @@ private class ComposedFunction extends DataFlow::CallNode {
 /**
  * A taint step for a composed function.
  */
-private class ComposedFunctionTaintStep extends TaintTracking::AdditionalTaintStep {
-  ComposedFunction composed;
-  DataFlow::CallNode call;
-
-  ComposedFunctionTaintStep() {
-    call = composed.getACall() and
-    this = call
-  }
-
+private class ComposedFunctionTaintStep extends TaintTracking::SharedTaintStep {
   override predicate step(DataFlow::Node pred, DataFlow::Node succ) {
-    exists(int fnIndex, DataFlow::FunctionNode fn | fn = composed.getFunction(fnIndex) |
+    exists(int fnIndex, DataFlow::FunctionNode fn, ComposedFunction composed, DataFlow::CallNode call |
+      fn = composed.getFunction(fnIndex) and
+      call = composed.getACall()
+    |
       // flow out of the composed call
       fnIndex = composed.getNumArgument() - 1 and
       pred = fn.getAReturn() and
-      succ = this
+      succ = call
       or
       if fnIndex = 0
       then

github/codeql

javascript/ql/src/semmle/javascript/frameworks/ComposedFunctions.qll
@@ -110,20 +110,12 @@ module FunctionCompositionCall {
   }
 }
 
-/**
- * A taint step for a composed function.
- */
-private class ComposedFunctionTaintStep extends TaintTracking::AdditionalTaintStep {
-  FunctionCompositionCall composed;
-  DataFlow::CallNode call;
-
-  ComposedFunctionTaintStep() {
-    call = composed.getACall() and
-    this = call
-  }
-
+private class ComposedFunctionTaintStep extends TaintTracking::SharedTaintStep {
   override predicate step(DataFlow::Node pred, DataFlow::Node succ) {
-    exists(int fnIndex, DataFlow::FunctionNode fn | fn = composed.getOperandFunction(fnIndex) |
+    exists(int fnIndex, DataFlow::FunctionNode fn, FunctionCompositionCall composed, DataFlow::CallNode call |
+      fn = composed.getOperandFunction(fnIndex) and
+      call = composed.getACall()
+    |
       // flow into the first function
       fnIndex = composed.getNumOperand() - 1 and
       exists(int callArgIndex |
@@ -140,7 +132,7 @@ private class ComposedFunctionTaintStep extends TaintTracking::AdditionalTaintSt
       // flow out of the composed call
       fnIndex = 0 and
       pred = fn.getReturnNode() and
-      succ = this
+      succ = call
     )
   }
 }