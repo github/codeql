c127b109d0ccf29db088f2f2f0e4d58107444df1
jorgectf <jorgectf@protonmail.com>
2021-03-26 16:21:34 +0100

    Create re.compile().ReMethod test

--- python/ql/src/experimental/Security/CWE-730/unit_tests/re_bad.py
+++ python/ql/src/experimental/Security/CWE-730/unit_tests/re_bad.py
@@ -1,3 +1,5 @@
+# move outside test folder
+
 from flask import request, Flask
 import re
 
@@ -7,16 +9,18 @@ app = Flask(__name__)
 @app.route("/direct")
 def direct():
     pattern = request.args['pattern']
-
     re.search(pattern, "")
 
 
 @app.route("/compile")
 def compile():
     pattern = re.compile(request.args['pattern'])
-
     pattern.search("")
 
+
+@app.route("/compile_direct")
+def compile_direct():
+    re.compile(request.args['pattern']).search("")
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
--- python/ql/src/experimental/Security/CWE-730/unit_tests/re_good.py
+++ python/ql/src/experimental/Security/CWE-730/unit_tests/re_good.py
@@ -1,3 +1,5 @@
+# move outside test folder
+
 from flask import request, Flask
 import re
 
@@ -7,15 +9,18 @@ app = Flask(__name__)
 @app.route("/direct")
 def direct():
     pattern = re.escape(request.args['pattern'])
-
     re.search(pattern, "")
 
 
 @app.route("/compile")
 def compile():
     pattern = re.compile(re.escape(request.args['pattern']))
-
     pattern.search("")
+
+
+@app.route("/compile_direct")
+def compile_direct():
+    re.compile(re.escape(request.args['pattern'])).search("")
 
 
 # if __name__ == "__main__":
