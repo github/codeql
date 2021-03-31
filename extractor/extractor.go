4451920adafe0ce15d2fdbb74874955e7610d056
Sauyon Lee <sauyon@github.com>
2021-03-31 08:56:34 -0700

    fixup windows

--- extractor/extractor.go
+++ extractor/extractor.go
@@ -614,12 +614,12 @@ func (extraction *Extraction) extractFileInfo(tw *trap.Writer, file string) {
 		}
 		if i == len(components)-1 {
 			stem, ext := stemAndExt(component)
-			lbl := tw.Labeler.FileLabelFor(path)
+			lbl := tw.Labeler.FileLabelFor(file)
 			dbscheme.FilesTable.Emit(tw, lbl, path, stem, ext, 0)
 			dbscheme.ContainerParentTable.Emit(tw, parentLbl, lbl)
 			dbscheme.HasLocationTable.Emit(tw, lbl, emitLocation(tw, lbl, 0, 0, 0, 0))
 			extraction.Lock.Lock()
-			slbl := extraction.StatWriter.Labeler.FileLabelFor(path)
+			slbl := extraction.StatWriter.Labeler.FileLabelFor(file)
 			dbscheme.CompilationCompilingFilesTable.Emit(extraction.StatWriter, extraction.Label, extraction.GetFileIdx(path), slbl)
 			extraction.Lock.Unlock()
 			break
