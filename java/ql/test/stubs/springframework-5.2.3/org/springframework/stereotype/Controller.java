86ef2588f1599cbf0c1ed578676b0e51728823d6
haby0 <ha_yub@163.com>
2021-04-08 17:55:29 +0800

    Restore @Component annotation

--- java/ql/test/stubs/springframework-5.2.3/org/springframework/stereotype/Controller.java
+++ java/ql/test/stubs/springframework-5.2.3/org/springframework/stereotype/Controller.java
@@ -9,6 +9,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Component
 public @interface Controller {
     String value() default "";
 }
