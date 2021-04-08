a8cbdc92b94f36df5c4d3487f072be75fd5e2720
Tamas Vajk <tamasvajk@github.com>
2021-04-08 12:17:19 +0200

    Add more test cases

--- csharp/ql/test/library-tests/parameters/Parameters.cs
+++ csharp/ql/test/library-tests/parameters/Parameters.cs
@@ -8,6 +8,10 @@ public class Parameters
     public void M6(MyStruct s1, MyStruct s2 = default(MyStruct), MyStruct s3 = new MyStruct()) => throw null;
     public void M7(MyEnum e1, MyEnum e2 = default(MyEnum), MyEnum e3 = new MyEnum(), MyEnum e4 = MyEnum.A, MyEnum e5 = (MyEnum)5) => throw null;
 
+    public void M8<T>(T t = default) => throw null;
+    public void M9<T>(T t = default) where T : struct => throw null;
+    public void M10<T>(T t = default) where T : class => throw null;
+
     public struct MyStruct { }
     public enum MyEnum { A = 1, B = 2 }
 }
