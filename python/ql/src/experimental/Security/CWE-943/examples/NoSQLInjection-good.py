983af32ab525b909ee6f9bf668c007f71af8c7ed
jorgectf <jorgectf@protonmail.com>
2021-04-09 21:36:11 +0200

    Polish qhelp examples

--- python/ql/src/experimental/Security/CWE-943/examples/NoSQLInjection-good.py
+++ python/ql/src/experimental/Security/CWE-943/examples/NoSQLInjection-good.py
@@ -1,8 +1,15 @@
-# Annotated version
+from flask import Flask, request
+from flask_pymongo import PyMongo
 from mongosanitizer.sanitizer import sanitize
+import json
 
-unsanitized_search = json.loads(request.args['search'])
+mongo = PyMongo(app)
 
-sanitize(unsanitized_search)
 
-db_results = mongo.db.user.find({'name': unsanitized_search})
+@app.route("/")
+def home_page():
+    unsafe_search = request.args['search']
+    json_search = json.loads(unsafe_search)
+    safe_search = sanitize(unsanitized_search)
+
+    result = client.db.collection.find_one({'data': safe_search})
