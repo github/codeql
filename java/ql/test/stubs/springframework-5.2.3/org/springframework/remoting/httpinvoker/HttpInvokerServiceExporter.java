254de76078cbc7825565b4e75481b0cf22c1fa8f
Chris Smowton <smowton@github.com>
2021-04-15 16:20:27 +0100

    Remove unnecessary stubs

--- java/ql/test/stubs/springframework-5.2.3/org/springframework/remoting/httpinvoker/HttpInvokerServiceExporter.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.remoting.httpinvoker;
-
-public class HttpInvokerServiceExporter extends org.springframework.remoting.rmi.RemoteInvocationSerializingExporter {
-
-    public void setService(Object service) {}
-
-    public void setServiceInterface(Class clazz) {}
-}
--- java/ql/test/stubs/springframework-5.2.3/org/springframework/remoting/rmi/RemoteInvocationSerializingExporter.java
+++
@@ -1,3 +0,0 @@
-package org.springframework.remoting.rmi;
-
-public abstract class RemoteInvocationSerializingExporter {}
