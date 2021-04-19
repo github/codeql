7d258ae72299e4410dfd0dc295432d837739a8cb
Chris Smowton <smowton@github.com>
2021-04-19 16:05:23 +0100

    Improve net/http taint-tracking fidelity
    * Don't taint error returns from http.Request methods
    * Track taint across mime/multipart.Part methods

--- ql/test/query-tests/Security/CWE-079/ReflectedXss.go
+++ ql/test/query-tests/Security/CWE-079/ReflectedXss.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -39,5 +40,31 @@ func ServeJsonDirect(w http.ResponseWriter, r http.Request) {
 	tainted := r.Header.Get("Origin")
 	noLongerTainted, _ := json.Marshal(tainted)
 	w.Write(noLongerTainted)
+
+}
+
+func ErrTest(w http.ResponseWriter, r http.Request) {
+
+	cookie, err := r.Cookie("somecookie")
+	w.Write([]byte(fmt.Sprintf("Cookie result: %v", cookie)))   // BAD: Cookie's value is user-controlled
+	w.Write([]byte(fmt.Sprintf("Cookie check error: %v", err))) // GOOD: Cookie's err return is harmless
+
+	file, header, err := r.FormFile("someFile")
+	content, err2 := ioutil.ReadAll(file)
+	w.Write([]byte(fmt.Sprintf("File content: %v", content)))      // BAD: file content is user-controlled
+	w.Write([]byte(fmt.Sprintf("File name: %v", header.Filename))) // BAD: file header is user-controlled
+	w.Write([]byte(fmt.Sprintf("FormFile error: %v", err)))        // GOOD: FormFile's err return is harmless
+	w.Write([]byte(fmt.Sprintf("FormFile error: %v", err2)))       // GOOD: ReadAll's err return is harmless
+
+	reader, err := r.MultipartReader()
+	part, err2 := reader.NextPart()
+	partName := part.FileName()
+	byteSlice := make([]byte, 100)
+	part.Read(byteSlice)
+
+	w.Write([]byte(fmt.Sprintf("Part name: %v", partName)))         // BAD: part name is user-controlled
+	w.Write(byteSlice)                                              // BAD: part contents are user-controlled
+	w.Write([]byte(fmt.Sprintf("MultipartReader error: %v", err)))  // GOOD: MultipartReader's err return is harmless
+	w.Write([]byte(fmt.Sprintf("MultipartReader error: %v", err2))) // GOOD: NextPart's err return is harmless
 
 }
