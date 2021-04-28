2f14a6218a07b95f6aca2ecb3381ba1997a38e7f
Erik Krogh Kristensen <erik-krogh@github.com>
2021-04-28 12:26:02 +0200

    generalize RxJS pipes

--- javascript/ql/test/library-tests/TaintTracking/rxjs.js
+++ javascript/ql/test/library-tests/TaintTracking/rxjs.js
@@ -1,9 +1,21 @@
-import { map, catchError } from 'rxjs/operators';
+import { map, tap, catchError } from 'rxjs/operators';
 
 source()
     .pipe(
         map(x => x + 'foo'),
         map(x => x + 'bar'),
+        catchError(err => {})
+    )
+    .subscribe(data => {
+        sink(data)
+    });
+
+source()
+    .pipe(
+        map(x => x + 'foo'),
+        // `tap` taps into the source observable, so like `subscribe` but inside the pipe.
+        tap(x => sink(x)),
+        tap(x => sink(x)),
         catchError(err => {})
     )
     .subscribe(data => {
