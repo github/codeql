bb97507ebc195ecdb43126c4fcab6741b274791e
ihsinme <ihsinme@gmail.com>
2021-05-02 22:59:56 +0300

    Update test.c

--- cpp/ql/test/experimental/query-tests/Security/CWE/CWE-1126/semmle/tests/test.c
+++ cpp/ql/test/experimental/query-tests/Security/CWE/CWE-1126/semmle/tests/test.c
@@ -6,10 +6,27 @@ void workFunction_0(char *s) {
     buf[intIndex] = 1;
     intIndex--;
   }
+  intIndex = 10;
   while(intIndex > 2)
   {
     buf[intIndex] = 1;
     int intIndex; // BAD
+    intIndex--;
+  }
+  intIndex = 10;
+  while(intIndex > 2) // GOOD
+  {
+    buf[intIndex] = 1;
+    intIndex -= 2;
+    int intIndex;
+    intIndex--;
+  }
+  intIndex = 10;
+  while(intIndex > 2) // GOOD
+  {
+    buf[intIndex] = 1;
+    --intIndex;
+    int intIndex;
     intIndex--;
   }
 }
