d790f3ccbbc28f704060dc61d9f0100718b91883
Erik Krogh Kristensen <erik-krogh@github.com>
2021-05-05 19:34:44 +0200

    add test for unsafe-code-construction query

---
+++ javascript/ql/test/query-tests/Security/CWE-094/CodeInjection/lib/index.js
@@ -0,0 +1,11 @@
+export function unsafeDeserialize(data) {
+  return eval("(" + data + ")"); // NOT OK
+}
+
+export function unsafeGetter(obj, name) {
+    return eval("obj." + name); // NOT OK
+}
+
+export function safeAssignment(obj, value) {
+    eval("obj.foo = " + JSON.stringify(value)); // OK
+}
