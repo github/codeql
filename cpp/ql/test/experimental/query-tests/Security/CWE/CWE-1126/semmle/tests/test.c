d3c6093f37d14861a10cf1ef2134c8bc183340d7
ihsinme <ihsinme@gmail.com>
2021-05-10 14:16:38 +0300

    Update test.c

--- cpp/ql/test/experimental/query-tests/Security/CWE/CWE-1126/semmle/tests/test.c
+++ cpp/ql/test/experimental/query-tests/Security/CWE/CWE-1126/semmle/tests/test.c
@@ -1,5 +1,6 @@
 void workFunction_0(char *s) {
   int intIndex = 10;
+  int intGuard;
   char buf[80];
   while(intIndex > 2) // GOOD
   {
@@ -12,6 +13,32 @@ void workFunction_0(char *s) {
     buf[intIndex] = 1;
     int intIndex; // BAD
     intIndex--;
+  }
+  intIndex = 10;
+  intGuard = 20;
+  while(intIndex < intGuard--) // GOOD
+  {
+    buf[intIndex] = 1;
+    int intIndex;
+    intIndex--;
+  }
+  intIndex = 10;
+  intGuard = 20;
+  while(intIndex < intGuard) // GOOD
+  {
+    buf[intIndex] = 1;
+    int intIndex;
+    intIndex++;
+    intGuard--;
+  }
+  intIndex = 10;
+  intGuard = 20;
+  while(intIndex < intGuard) // GOOD
+  {
+    buf[intIndex] = 1;
+    int intIndex;
+    intIndex--;
+    intGuard -= 4;
   }
   intIndex = 10;
   while(intIndex > 2) // GOOD
