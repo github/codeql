948f1d8e344935c7b550450813e61f4df415ba6e
Mathias Vorreiter Pedersen <mathiasvp@github.com>
2021-05-11 19:43:21 +0200

    C++: Add testcase with INTMAX_MIN.

--- cpp/ql/test/query-tests/Security/CWE/CWE-190/semmle/tainted/test5.cpp
+++ cpp/ql/test/query-tests/Security/CWE/CWE-190/semmle/tainted/test5.cpp
@@ -30,3 +30,13 @@ void useTaintedIntWithGuard() {
 		int product = tainted * tainted; // GOOD: can't underflow/overflow
 	}
 }
+
+#define INTMAX_MIN (-0x7fffffffffffffff - 1)
+
+void useTaintedIntWithGuardIntMaxMin() {
+	intmax_t tainted = getTaintedInt();
+
+	if(imaxabs(tainted) <= INTMAX_MIN) {
+		int product = tainted * tainted; // BAD: imaxabs(INTMAX_MIN) == INTMAX_MIN [NOT DETECTED]
+	}
+}
