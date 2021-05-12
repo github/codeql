66b2c39985950c2fc2ab9930f7b452cfa4853f9d
Arthur Baars <aibaars@github.com>
2021-05-12 21:19:51 +0200

    More tests

--- ql/test/library-tests/modules/calls.rb
+++ ql/test/library-tests/modules/calls.rb
@@ -58,3 +58,104 @@ d = D.new
 d.baz
 d.singleton_m # NoMethodError
 d.instance_m
+
+def optional_arg(a = 4, b: 5)
+    a.bit_length
+    b.bit_length
+end
+
+def call_block
+    yield 1
+end
+
+def foo()
+    var = Hash.new
+    var[1]
+    call_block { |x| var[x] }
+end
+
+class Integer
+    def bit_length; end
+    def abs; end
+end
+
+class String
+    def capitalize; end
+end
+
+module Kernel
+    def puts; end
+end
+
+class Module
+    def module_eval; end
+    def include; end
+    def prepend; end
+end
+
+class Object < Module
+    include Kernel
+    def new; end
+end
+
+class Hash
+    def []; end
+end
+
+class Array
+  def []; end
+  def length; end
+
+  def foreach &body
+    x = 0
+    while x < self.length
+        yield x, self[x]
+        x += 1
+    end
+  end
+end
+
+def funny
+    yield "prefix: "
+end
+
+funny { |i| puts i.capitalize}
+
+"a".capitalize
+1.bit_length
+1.abs
+
+["a","b","c"].foreach { |i, v| puts "#{i} -> #{v.capitalize}"} # TODO should resolve to String.capitalize
+
+[1,2,3].foreach { |i| i.bit_length}
+
+[1,2,3].foreach { |i| puts i.capitalize} # NoMethodError
+
+[1,-2,3].foreach { |_, v| puts v.abs} # TODO should resolve to Integer.abs
+
+def indirect &b
+    call_block &b
+end
+
+indirect { |i| i.bit_length}
+
+
+class S
+    def s_method
+        self.to_s
+    end
+end
+
+class A < S
+    def to_s
+    end
+end
+
+class B < S
+    def to_s
+    end
+end
+
+S.new.s_method
+A.new.s_method
+B.new.s_method
