c4e244eb80204cd511152274404ce859f6eb69ca
Rasmus Wriedt Larsen <rasmuswl@github.com>
2021-05-21 17:09:15 +0200

    Python: Add `getAwaited` to API::Node
    I _really_ wanted to call this `.await()`, but that did not fit in with
    the convention, or the corresponding `getPromised` in JS.
    
    https://github.com/github/codeql/blob/54f191cfe37e136bcc7189b2fb01f44dbb01758b/javascript/ql/src/semmle/javascript/ApiGraphs.qll#L184

--- python/ql/test/experimental/dataflow/ApiGraphs/async_test.py
+++ python/ql/test/experimental/dataflow/ApiGraphs/async_test.py
@@ -3,13 +3,13 @@ import pkg # $ use=moduleImport("pkg")
 async def foo():
     coro = pkg.async_func() # $ use=moduleImport("pkg").getMember("async_func").getReturn()
     coro # $ use=moduleImport("pkg").getMember("async_func").getReturn()
-    result = await coro # $ use=moduleImport("pkg").getMember("async_func").getReturn()
-    result # $ MISSING: use=...
-    return result # $ MISSING: use=...
+    result = await coro # $ use=moduleImport("pkg").getMember("async_func").getReturn().getAwaited()
+    result # $ use=moduleImport("pkg").getMember("async_func").getReturn().getAwaited()
+    return result # $ use=moduleImport("pkg").getMember("async_func").getReturn().getAwaited()
 
 async def bar():
-    result = await pkg.async_func() # $ use=moduleImport("pkg").getMember("async_func").getReturn()
-    return result # $ MISSING: use=...
+    result = await pkg.async_func() # $ use=moduleImport("pkg").getMember("async_func").getReturn().getAwaited()
+    return result # $ use=moduleImport("pkg").getMember("async_func").getReturn().getAwaited()
 
 def check_annotations():
     # Just to make sure how annotations should look like :)
