d2e29fc72c00c56bdb8002ab92798a67b85b2ef4
Artem Smotrakov <artem.smotrakov@gmail.com>
2021-05-23 10:18:40 +0200

    Renamed RmiUnsafeDeserialization.ql -> UnsafeDeserializationRmi.ql

--- java/ql/test/experimental/query-tests/security/CWE-502/UnsafeDeserializationRmi.java
+++ java/ql/test/experimental/query-tests/security/CWE-502/UnsafeDeserializationRmi.java
@@ -5,8 +5,8 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-public class RmiUnsafeDeserialization {
-    
+public class UnsafeDeserializationRmi {
+
     // BAD (bind a remote object that has a vulnerable method that takes Object)
     public static void testRegistryBindWithObjectParameter() throws Exception {
         Registry registry = LocateRegistry.createRegistry(1099);
