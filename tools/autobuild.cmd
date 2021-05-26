5b830fdcd131e9b39609ba425a5b789343a642bc
shati-patel <42641846+shati-patel@users.noreply.github.com>
2021-05-26 12:20:23 +0100

    Fix commands on Windows (take 2)

--- tools/autobuild.cmd
+++ tools/autobuild.cmd
@@ -1,8 +1,8 @@
 @echo off
 
 type NUL && "%CODEQL_DIST%\codeql.exe" database index-files ^
-    --include-extension=.rb ^
-    --include-extension=.erb ^
+    --include-extension=.ql ^
+    --include-extension=.qll ^
     --size-limit=5m ^
     --language=ql ^
     "%CODEQL_EXTRACTOR_QL_WIP_DATABASE%"
