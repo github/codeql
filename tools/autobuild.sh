eaf6d8123d664dea5137ed2cf678a119a171b8be
Taus <tausbn@github.com>
2021-05-30 17:38:26 +0000

    Add support for dbscheme and qlpack.yml
    Currently the the YAML "parser" fails for some legacy files, but the
    actual files appear to be parsed correctly.

--- tools/autobuild.sh
+++ tools/autobuild.sh
@@ -5,6 +5,8 @@ set -eu
 exec "${CODEQL_DIST}/codeql" database index-files \
     --include-extension=.ql \
     --include-extension=.qll \
+    --include-extension=.dbscheme \
+    --include=**/qlpack.yml \
     --size-limit=5m \
     --language=ql \
     --working-dir=.\
