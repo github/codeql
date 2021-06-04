dd2fe2a489e3294ec02443dab3d16ecca299a1f6
Erik Krogh Kristensen <erik-krogh@github.com>
2021-06-04 17:16:12 +0200

    add the resolve library as a sink to js/path-injection

--- javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/tainted-require.js
+++ javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/tainted-require.js
@@ -6,3 +6,8 @@ app.get('/some/path', function(req, res) {
   // BAD: loading a module based on un-sanitized query parameters
   var m = require(req.param("module"));
 });
+
+const resolve = require("resolve");
+app.get('/some/path', function(req, res) {
+  var module = resolve(req.param("module")); // NOT OK - resolving module based on query parameters
+});
