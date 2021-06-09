74569ce3168c1311c6e276a01c76e1af0a6cb934
Chris Smowton <smowton@github.com>
2021-06-09 18:04:14 +0100

    Tidy Jax-RS test

--- java/ql/test/query-tests/security/CWE-918/JaxWsSSRF.java
+++ java/ql/test/query-tests/security/CWE-918/JaxWsSSRF.java
@@ -1,13 +1,6 @@
 import javax.ws.rs.client.*;
 import java.io.IOException;
-import java.net.URI;
-import java.net.*;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.Proxy.Type;
-import java.io.InputStream;
 
-import org.apache.http.client.methods.HttpGet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
