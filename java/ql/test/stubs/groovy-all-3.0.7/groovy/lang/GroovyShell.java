5d56eb6ea1013fa087a5c6fa1719d86d698ab875
Tony Torralba <atorralba@users.noreply.github.com>
2021-06-11 11:51:16 +0200

    Add stubs

--- java/ql/test/stubs/groovy-all-3.0.7/groovy/lang/GroovyShell.java
+++ java/ql/test/stubs/groovy-all-3.0.7/groovy/lang/GroovyShell.java
@@ -1,23 +1,21 @@
 /*
- *  Licensed to the Apache Software Foundation (ASF) under one
- *  or more contributor license agreements.  See the NOTICE file
- *  distributed with this work for additional information
- *  regarding copyright ownership.  The ASF licenses this file
- *  to you under the Apache License, Version 2.0 (the
- *  "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package groovy.lang;
 
+import java.io.Reader;
+import java.net.URI;
 import java.util.*;
 
 public class GroovyShell {
@@ -25,6 +23,14 @@ public class GroovyShell {
     public GroovyShell() {}
 
     public Object evaluate(GroovyCodeSource codeSource) {
+        return null;
+    }
+
+    public Object evaluate(Reader in) {
+        return null;
+    }
+
+    public Object evaluate(Reader in, String fileName) {
         return null;
     }
 
@@ -48,11 +54,36 @@ public class GroovyShell {
         return null;
     }
 
-    public Object run(GroovyCodeSource source, List<String> args) {
+
+    public Object run(Reader in, String fileName, List<String> list) {
+        return null;
+    }
+
+    public Object run(Reader in, String fileName, String[] args) {
+        return null;
+    }
+
+    public Object run(GroovyCodeSource source, List<String> list) {
         return null;
     }
 
     public Object run(GroovyCodeSource source, String[] args) {
+        return null;
+    }
+
+    public Object run​(URI source, List<String> list) {
+        return null;
+    }
+
+    public Object run​(URI source, String[] args) {
+        return null;
+    }
+
+    public Script parse(Reader in) {
+        return null;
+    }
+
+    public Script parse(Reader reader, String fileName) {
         return null;
     }
 
@@ -61,6 +92,10 @@ public class GroovyShell {
     }
 
     public Script parse(final String scriptText, final String fileName) {
+        return null;
+    }
+
+    public Script parse(URI uri) {
         return null;
     }
 }
