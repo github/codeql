5506365b0e92046e8fb2a72588a4beadb235123f
Rasmus Wriedt Larsen <rasmuswl@github.com>
2021-06-17 14:20:28 +0200

    Python: Split cleartext tests

--- python/ql/test/query-tests/Security/CWE-312-CleartextStorage/test.py
+++ python/ql/test/query-tests/Security/CWE-312-CleartextStorage/test.py
@@ -3,18 +3,8 @@
 def get_password():
     pass
 
-def log_password():
-    password = get_password()
-    logging.info("Password '%s'", password)
-
 def get_cert():
     pass
-
-def log_cert():
-    logging.debug("Cert=%s", get_cert())
-
-def print_password():
-    print(get_password())
 
 def write_cert(filename):
     cert = get_cert()
