6a6d6fbe92c8b3533ea85e540f5c64a3d0f410d3
Rasmus Wriedt Larsen <rasmuswl@github.com>
2021-06-17 18:09:33 +0200

    Python: Add leading space in some inline tests

--- python/ql/test/library-tests/frameworks/stdlib-py3/FileSystemAccess.py
+++ python/ql/test/library-tests/frameworks/stdlib-py3/FileSystemAccess.py
@@ -4,18 +4,18 @@ p = Path("filepath")
 posix = PosixPath("posix/filepath")
 windows = WindowsPath("windows/filepath")
 
-p.chmod(0o777)  # $getAPathArgument=p
-posix.chmod(0o777)  # $getAPathArgument=posix
-windows.chmod(0o777)  # $getAPathArgument=windows
+p.chmod(0o777)  # $ getAPathArgument=p
+posix.chmod(0o777)  # $ getAPathArgument=posix
+windows.chmod(0o777)  # $ getAPathArgument=windows
 
-with p.open() as f:  # $getAPathArgument=p
+with p.open() as f:  # $ getAPathArgument=p
     f.read()
 
-p.write_bytes(b"hello")  # $getAPathArgument=p
+p.write_bytes(b"hello")  # $ getAPathArgument=p
 
 name = windows.parent.name
 o = open
-o(name)  # $getAPathArgument=name
+o(name)  # $ getAPathArgument=name
 
 wb = p.write_bytes
-wb(b"hello")  # $getAPathArgument=p
+wb(b"hello")  # $ getAPathArgument=p
