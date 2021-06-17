e2facd098111240c0f4cbc5607a9b776e68940bc
Rasmus Wriedt Larsen <rasmuswl@github.com>
2021-06-17 15:14:58 +0200

    Python: Expand cleartext query tests

--- python/ql/test/query-tests/Security/CWE-312-CleartextLogging/test.py
+++ python/ql/test/query-tests/Security/CWE-312-CleartextLogging/test.py
@@ -1,17 +1,46 @@
-#Don't import logging; it transitively imports a lot of stuff
+import logging
+import sys
+
+LOGGER = logging.getLogger("LOGGER")
+
+def get_logger():
+    return LOGGER
+
 
 def get_password():
-    pass
+    return "<PASSWORD>"
+
+
+def get_cert():
+    return "<CERT>"
+
 
 def log_password():
     password = get_password()
-    logging.info("Password '%s'", password)
+    logging.info("logging.info Password '%s'", password) # NOT OK
 
-def get_cert():
-    pass
+    LOGGER.log(logging.INFO, "LOGGER.log Password '%s'", password) # NOT OK
+    logging.root.info("logging.root.info Password '%s'", password) # NOT OK
+
+    # name of logger variable should not matter
+    foo = LOGGER
+    foo.info("foo.info Password '%s'", password) # NOT OK
+
+    # return value from function
+    get_logger().info("get_logger().info Password '%s'", password) # NOT OK
+
 
 def log_cert():
-    logging.debug("Cert=%s", get_cert())
+    logging.debug("Cert=%s", get_cert()) # NOT OK
 
 def print_password():
-    print(get_password())
+    print(get_password()) # NOT OK
+
+    sys.stdout.write(get_password()) # NOT OK
+    sys.stderr.write(get_password()) # NOT OK
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    log_password()
+    log_cert()
+    print_password()
--- python/ql/test/query-tests/Security/CWE-312-CleartextStorage/test.py
+++ python/ql/test/query-tests/Security/CWE-312-CleartextStorage/test.py
@@ -1,12 +1,10 @@
-#Don't import logging; it transitively imports a lot of stuff
-
-def get_password():
-    pass
-
 def get_cert():
-    pass
+    return "<CERT>"
+
 
 def write_cert(filename):
     cert = get_cert()
     with open(filename, "w") as file:
-        file.write(cert)
+        file.write(cert) # NOT OK
+        lines = [cert + "\n"]
+        file.writelines(lines) # NOT OK
