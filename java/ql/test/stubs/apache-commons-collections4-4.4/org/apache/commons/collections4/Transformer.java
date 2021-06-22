5feee9cc1744bd45be2731dbb04fd5b98eded3ed
Owen Mansel-Chan <owen-mc@github.com>
2021-06-22 16:46:05 +0100

    Add automatically-generated stubs

---
+++ java/ql/test/stubs/apache-commons-collections4-4.4/org/apache/commons/collections4/Transformer.java
@@ -0,0 +1,9 @@
+// Generated automatically from org.apache.commons.collections4.Transformer for testing purposes
+
+package org.apache.commons.collections4;
+
+
+public interface Transformer<I, O>
+{
+    O transform(I p0);
+}
