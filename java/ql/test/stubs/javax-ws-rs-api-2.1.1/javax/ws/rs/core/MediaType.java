6b3bc42ef2f74aad580b43e8f15054f254668ea9
Chris Smowton <smowton@github.com>
2021-06-24 16:59:22 +0100

    Add JAX-RS XSS tests

--- java/ql/test/stubs/javax-ws-rs-api-2.1.1/javax/ws/rs/core/MediaType.java
+++ java/ql/test/stubs/javax-ws-rs-api-2.1.1/javax/ws/rs/core/MediaType.java
@@ -18,39 +18,127 @@ package javax.ws.rs.core;
 import java.util.Map;
 
 public class MediaType {
-    public final static MediaType WILDCARD_TYPE = new MediaType();
-
-    public final static MediaType APPLICATION_XML_TYPE = new MediaType("application", "xml");
-
-    public final static MediaType APPLICATION_ATOM_XML_TYPE = new MediaType("application", "atom+xml");
-
-    public final static MediaType APPLICATION_XHTML_XML_TYPE = new MediaType("application", "xhtml+xml");
-
-    public final static MediaType APPLICATION_SVG_XML_TYPE = new MediaType("application", "svg+xml");
-
-    public final static MediaType APPLICATION_JSON_TYPE = new MediaType("application", "json");
-
-    public final static MediaType APPLICATION_FORM_URLENCODED_TYPE = new MediaType("application", "x-www-form-urlencoded");
-
-    public final static MediaType MULTIPART_FORM_DATA_TYPE = new MediaType("multipart", "form-data");
-
-    public final static MediaType APPLICATION_OCTET_STREAM_TYPE = new MediaType("application", "octet-stream");
-
-    public final static String TEXT_PLAIN = "text/plain";
-
-    public final static MediaType TEXT_PLAIN_TYPE = new MediaType("text", "plain");
-
-    public final static String TEXT_XML = "text/xml";
-    
-    public final static MediaType TEXT_XML_TYPE = new MediaType("text", "xml");
-
-    public final static String TEXT_HTML = "text/html";
-
-    public final static MediaType TEXT_HTML_TYPE = new MediaType("text", "html");
-
-    public static final MediaType SERVER_SENT_EVENTS_TYPE = new MediaType("text", "event-stream");
-
-    public static final MediaType APPLICATION_JSON_PATCH_JSON_TYPE = new MediaType("application", "json-patch+json");
+    /**
+     * The media type {@code charset} parameter name.
+     */
+    public static final String CHARSET_PARAMETER = "";
+    /**
+     * The value of a type or subtype wildcard {@value #MEDIA_TYPE_WILDCARD}.
+     */
+    public static final String MEDIA_TYPE_WILDCARD = "";
+    // Common media type constants
+    /**
+     * A {@code String} constant representing wildcard {@value #WILDCARD} media type .
+     */
+    public static final String WILDCARD = "";
+    /**
+     * A {@link MediaType} constant representing wildcard {@value #WILDCARD} media type.
+     */
+    public static final MediaType WILDCARD_TYPE = null;
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_XML} media type.
+     */
+    public static final String APPLICATION_XML = "";
+    /**
+     * A {@link MediaType} constant representing {@value #APPLICATION_XML} media type.
+     */
+    public static final MediaType APPLICATION_XML_TYPE = null;
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_ATOM_XML} media type.
+     */
+    public static final String APPLICATION_ATOM_XML = "";
+    /**
+     * A {@link MediaType} constant representing {@value #APPLICATION_ATOM_XML} media type.
+     */
+    public static final MediaType APPLICATION_ATOM_XML_TYPE = null;
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_XHTML_XML} media type.
+     */
+    public static final String APPLICATION_XHTML_XML = "";
+    /**
+     * A {@link MediaType} constant representing {@value #APPLICATION_XHTML_XML} media type.
+     */
+    public static final MediaType APPLICATION_XHTML_XML_TYPE = null;
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_SVG_XML} media type.
+     */
+    public static final String APPLICATION_SVG_XML = "";
+    /**
+     * A {@link MediaType} constant representing {@value #APPLICATION_SVG_XML} media type.
+     */
+    public static final MediaType APPLICATION_SVG_XML_TYPE = null;
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_JSON} media type.
+     */
+    public static final String APPLICATION_JSON = "";
+    /**
+     * A {@link MediaType} constant representing {@value #APPLICATION_JSON} media type.
+     */
+    public static final MediaType APPLICATION_JSON_TYPE = null;
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_FORM_URLENCODED} media type.
+     */
+    public static final String APPLICATION_FORM_URLENCODED = "";
+    /**
+     * A {@link MediaType} constant representing {@value #APPLICATION_FORM_URLENCODED} media type.
+     */
+    public static final MediaType APPLICATION_FORM_URLENCODED_TYPE = null;
+    /**
+     * A {@code String} constant representing {@value #MULTIPART_FORM_DATA} media type.
+     */
+    public static final String MULTIPART_FORM_DATA = "";
+    /**
+     * A {@link MediaType} constant representing {@value #MULTIPART_FORM_DATA} media type.
+     */
+    public static final MediaType MULTIPART_FORM_DATA_TYPE = null;
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_OCTET_STREAM} media type.
+     */
+    public static final String APPLICATION_OCTET_STREAM = "";
+    /**
+     * A {@link MediaType} constant representing {@value #APPLICATION_OCTET_STREAM} media type.
+     */
+    public static final MediaType APPLICATION_OCTET_STREAM_TYPE = null;
+    /**
+     * A {@code String} constant representing {@value #TEXT_PLAIN} media type.
+     */
+    public static final String TEXT_PLAIN = "";
+    /**
+     * A {@link MediaType} constant representing {@value #TEXT_PLAIN} media type.
+     */
+    public static final MediaType TEXT_PLAIN_TYPE = null;
+    /**
+     * A {@code String} constant representing {@value #TEXT_XML} media type.
+     */
+    public static final String TEXT_XML = "";
+    /**
+     * A {@link MediaType} constant representing {@value #TEXT_XML} media type.
+     */
+    public static final MediaType TEXT_XML_TYPE = null;
+    /**
+     * A {@code String} constant representing {@value #TEXT_HTML} media type.
+     */
+    public static final String TEXT_HTML = "";
+    /**
+     * A {@link MediaType} constant representing {@value #TEXT_HTML} media type.
+     */
+    public static final MediaType TEXT_HTML_TYPE = null;
+    /**
+     * {@link String} representation of Server sent events media type. ("{@value}").
+     */
+    public static final String SERVER_SENT_EVENTS = "";
+    /**
+     * Server sent events media type.
+     */
+    public static final MediaType SERVER_SENT_EVENTS_TYPE = null;
+    /**
+     * {@link String} representation of {@value #APPLICATION_JSON_PATCH_JSON} media type..
+     */
+    public static final String APPLICATION_JSON_PATCH_JSON = "";
+    /**
+     * A {@link MediaType} constant representing {@value #APPLICATION_JSON_PATCH_JSON} media type.
+     */
+    public static final MediaType APPLICATION_JSON_PATCH_JSON_TYPE = null;
 
     public static MediaType valueOf(String type){
       return null;
