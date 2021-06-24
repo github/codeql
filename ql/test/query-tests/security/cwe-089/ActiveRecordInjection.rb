9883a9b60688679aadd8930eccf35c9732524bf2
Alex Ford <alexrford@github.com>
2021-06-24 13:30:56 +0100

    update SqlInjection tests

--- ql/test/query-tests/security/cwe-089/ActiveRecordInjection.rb
+++ ql/test/query-tests/security/cwe-089/ActiveRecordInjection.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   belongs_to :user_group
 
   def self.authenticate(name, pass)
+    # BAD: possible untrusted input interpolated into SQL fragment
     find(:first, :conditions => "name='#{name}' and pass='#{pass}'")
   end
 end
@@ -19,21 +20,28 @@ class FooController < ActionController::Base
 
   # A string tainted by user input is inserted into an SQL query
   def some_request_handler
-    # SELECT AVG(#{params[:column]}) FROM "users"
+    # BAD: executes `SELECT AVG(#{params[:column]}) FROM "users"`
+    # where `params[:column]` is unsanitized
     User.calculate(:average, params[:column])
 
-    # DELETE FROM "users" WHERE (id = #{params[:id]})
-    User.delete_all("id = #{params[:id]}")
+    # BAD: executes `DELETE FROM "users" WHERE (id = '#{params[:id]}')`
+    # where `params[:id]` is unsanitized
+    User.delete_all("id = '#{params[:id]}'")
 
-    # SELECT "users".* FROM "users" WHERE (id = #{params[:id]})
-    User.destroy_all(["id = #{params[:id]}"])
+    # BAD: executes `SELECT "users".* FROM "users" WHERE (id = '#{params[:id]}')`
+    # where `params[:id]` is unsanitized
+    User.destroy_all(["id = '#{params[:id]}'"])
 
-    # SELECT "users".* FROM "users" WHERE id BETWEEN #{params[:min_id]} AND 100000
+    # BAD: executes `SELECT "users".* FROM "users" WHERE id BETWEEN '#{params[:min_id]}' AND 100000`
+    # where `params[:min_id]` is unsanitized
     User.where(<<-SQL, MAX_USER_ID)
-      id BETWEEN #{params[:min_id]} AND ?
+      id BETWEEN '#{params[:min_id]}' AND ?
     SQL
 
-    UserGroup.joins(:users).where("user.id = #{params[:id]}")
+    # BAD: chained method case
+    # executes `SELECT "users".* FROM "users" WHERE (NOT (user_id = 'params[:id]'))`
+    # where `params[:id]` is unsanitized
+    User.where.not("user.id = '#{params[:id]}'")
 
     User.authenticate(params[:name], params[:pass])
   end
@@ -45,24 +53,29 @@ class BarController < ApplicationController
   def some_other_request_handler
     ps = params
     uid = ps[:id]
-    uidEq = "= #{uid}"
+    uidEq = "= '#{uid}'"
 
-    # DELETE FROM "users" WHERE (id = #{uid})
+    # BAD: executes `DELETE FROM "users" WHERE (id = #{uid})`
+    # where `uid` is unsantized
     User.delete_all("id " + uidEq)
   end
 
   def sanitized_paths
 
     dir = params[:order]
-    # barrier guard prevents taint flow
+    # GOOD: barrier guard prevents taint flow
     dir = "DESC" unless dir == "ASC"
     User.order("name #{dir}")
 
     name = params[:user_name]
-    # barrier guard prevents taint flow
+    # GOOD: barrier guard prevents taint flow
     if %w(alice bob charlie).include? name
       User.find_by("username = #{name}")
     end
+
+    name = params[:user_name]
+    # GOOD: hash arguments are sanitized by ActiveRecord
+    User.find_by(user_name: name)
   end
 end
 
