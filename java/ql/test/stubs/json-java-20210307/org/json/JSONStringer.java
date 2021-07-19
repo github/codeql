34a4b71891ea5b5e62ce4241ff501f1b840fa6a4
Chris Smowton <smowton@github.com>
2021-07-19 17:57:27 +0100

    Add models of JSON-java, aka `org.json`

---
+++ java/ql/test/stubs/json-java-20210307/org/json/JSONStringer.java
@@ -0,0 +1,11 @@
+// Generated automatically from org.json.JSONStringer for testing purposes
+
+package org.json;
+
+import org.json.JSONWriter;
+
+public class JSONStringer extends JSONWriter
+{
+    public JSONStringer(){}
+    public String toString(){ return null; }
+}
