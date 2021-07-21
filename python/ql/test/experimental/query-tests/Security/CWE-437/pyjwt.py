e14b10370e8e86e3082a5d612f039ace650c6fdd
jorgectf <jorgectf@protonmail.com>
2021-07-21 21:30:54 +0200

    Add indeterminate test to pyjwt

--- python/ql/test/experimental/query-tests/Security/CWE-437/pyjwt.py
+++ python/ql/test/experimental/query-tests/Security/CWE-437/pyjwt.py
@@ -3,15 +3,15 @@ import jwt
 # Encoding
 
 # good - key and algorithm supplied
-jwt.encode({"foo": "bar"}, "key", "HS256")
-jwt.encode({"foo": "bar"}, key="key", algorithm="HS256")
+jwt.encode(token, "key", "HS256")
+jwt.encode(token, key="key", algorithm="HS256")
 
 # bad - both key and algorithm set to None
-jwt.encode({"foo": "bar"}, None, None)
+jwt.encode(token, None, None)
 
 # bad - empty key
-jwt.encode({"foo": "bar"}, "", algorithm="HS256")
-jwt.encode({"foo": "bar"}, key="", algorithm="HS256")
+jwt.encode(token, "", algorithm="HS256")
+jwt.encode(token, key="", algorithm="HS256")
 
 # Decoding
 
@@ -25,3 +25,7 @@ jwt.decode(token, key, options={"verify_signature": False})
 # good - verified decoding
 jwt.decode(token, verify=True)
 jwt.decode(token, key, options={"verify_signature": True})
+
+
+def indeterminate(verify):
+    jwt.decode(token, key, verify)
