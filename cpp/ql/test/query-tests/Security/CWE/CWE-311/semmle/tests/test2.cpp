86ee5fea4003539af2bc02cea4cd820a7f949ea0
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2021-07-22 12:35:51 +0100

    C++: More test cases and correct an existing one.

--- cpp/ql/test/query-tests/Security/CWE/CWE-311/semmle/tests/test2.cpp
+++ cpp/ql/test/query-tests/Security/CWE/CWE-311/semmle/tests/test2.cpp
@@ -1,6 +1,10 @@
 
 #define FILE int
 
+typedef unsigned long size_t;
+
+FILE *fopen(const char *filename, const char *mode);
+int snprintf(char *s, size_t n, const char *format, ...);
 int fprintf(FILE *stream, const char *format, ...);
 char *strcpy(char *s1, const char *s2);
 
@@ -12,6 +16,7 @@ struct myStruct
 	char *password;
 	char *thepasswd;
 	char *accountkey;
+	wchar_t *widepassword;
 
 	// encrypted
 	char password_hash[64];
@@ -19,11 +24,14 @@ struct myStruct
 
 	// not sensitive
 	char *password_file;
+	char *password_path;
 	int num_passwords;
+	int *password_tries;
 	bool have_passwd;
 
 	// dubious
 	char *passwd_config;
+	char *passwd_config2;
 };
 
 char *getPassword();
@@ -38,12 +46,16 @@ void tests(FILE *log, myStruct &s)
 	fprintf(log, "password_hash = %s\n", s.password_hash); // GOOD
 	fprintf(log, "encrypted_passwd = %s\n", s.encrypted_passwd); // GOOD
 	fprintf(log, "password_file = %s\n", s.password_file); // GOOD
+	fprintf(log, "password_path = %s\n", s.password_path); // GOOD [FALSE POSITIVE]
 	fprintf(log, "passwd_config = %s\n", s.passwd_config); // DUBIOUS [REPORTED]
 	fprintf(log, "num_passwords = %i\n", s.num_passwords); // GOOD
+	fprintf(log, "password_tries = %i\n", *(s.password_tries)); // GOOD
 	fprintf(log, "have_passwd = %i\n", s.have_passwd); // GOOD
+	fprintf(log, "widepassword = %ls\n", s.widepassword); // BAD
+	fprintf(log, "widepassword = %S\n", s.widepassword); // BAD
 
-	fprintf(log, "getPassword() = %i\n", getPassword()); // BAD
-	fprintf(log, "getPasswordHash() = %i\n", getPasswordHash()); // GOOD
+	fprintf(log, "getPassword() = %s\n", getPassword()); // BAD
+	fprintf(log, "getPasswordHash() = %s\n", getPasswordHash()); // GOOD
 	fprintf(log, "getPasswordMaxChars() = %i\n", getPasswordMaxChars()); // GOOD
 
 	{
@@ -62,5 +74,21 @@ void tests(FILE *log, myStruct &s)
 		
 		strcpy(buf, s.password_hash);
 		fprintf(log, "buf = %s\n", buf); // GOOD
+	}
+
+	fprintf(log, "password = %p\n", s.password); // GOOD [FALSE POSITIVE]
+
+	{
+		if (fopen(s.passwd_config2, "rt") == 0)
+		{
+			fprintf(log, "could not open file '%s'.\n", s.passwd_config2); // GOOD [FALSE POSITIVE]
+		}
+	}
+
+	{
+		char buffer[1024];
+
+		snprintf(buffer, 1024, "password = %s", s.password);
+		fprintf(log, "log: %s", buffer); // BAD [NOT DETECTED]
 	}
 }
