1444ec52552d30c23757e2c2b75f41a0fe3c504b
Asger Feldthaus <asgerf@github.com>
2021-07-29 08:09:24 +0200

    JS: Add similar test for hbs

--- javascript/ql/test/library-tests/frameworks/Templating/app.js
+++ javascript/ql/test/library-tests/frameworks/Templating/app.js
@@ -20,3 +20,22 @@ app.get('/ejs', (req, res) => {
         dataInEventHandlerStringRaw: req.query.dataInEventHandlerStringRaw,
     });
 });
+
+app.get('/hbs', (req, res) => {
+    res.render('hbs_sinks', {
+        escapedHtml: req.query.escapedHtml,
+        rawHtml: req.query.rawHtml,
+        rawHtmlSafeValue: 'safe',
+        object: {
+            rawHtmlProp: req.query.rawHtmlProp
+        },
+        dataInStringLiteral: req.query.dataInStringLiteral,
+        dataInStringLiteralRaw: req.query.dataInStringLiteralRaw,
+        dataInGeneratedCode: req.query.dataInGeneratedCode,
+        dataInGeneratedCodeRaw: req.query.dataInGeneratedCodeRaw,
+        backslashSink1: req.query.backslashSink1,
+        backslashSink2: req.query.backslashSink2,
+        dataInEventHandlerString: req.query.dataInEventHandlerString,
+        dataInEventHandlerStringRaw: req.query.dataInEventHandlerStringRaw,
+    });
+});
