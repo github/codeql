28fe8da37c5186ab8e2c6d847abc2f9a40b44eff
Asger Feldthaus <asgerf@github.com>
2021-07-29 08:13:21 +0200

    JS: Add similar test for .njk file

--- javascript/ql/test/library-tests/frameworks/Templating/app.js
+++ javascript/ql/test/library-tests/frameworks/Templating/app.js
@@ -39,3 +39,23 @@ app.get('/hbs', (req, res) => {
         dataInEventHandlerStringRaw: req.query.dataInEventHandlerStringRaw,
     });
 });
+
+app.get('/njk', (req, res) => {
+    res.render('njk_sinks', {
+        escapedHtml: req.query.escapedHtml,
+        rawHtml: req.query.rawHtml,
+        rawHtmlSafeValue: 'safe',
+        object: {
+            rawHtmlProp: req.query.rawHtmlProp
+        },
+        dataInStringLiteral: req.query.dataInStringLiteral,
+        dataInStringLiteralRaw: req.query.dataInStringLiteralRaw,
+        dataInGeneratedCode: req.query.dataInGeneratedCode,
+        dataInGeneratedCodeRaw: req.query.dataInGeneratedCodeRaw,
+        dataInGeneratedCodeJsonRaw: req.query.dataInGeneratedCodeJsonRaw,
+        backslashSink1: req.query.backslashSink1,
+        backslashSink2: req.query.backslashSink2,
+        dataInEventHandlerString: req.query.dataInEventHandlerString,
+        dataInEventHandlerStringRaw: req.query.dataInEventHandlerStringRaw,
+    });
+});
