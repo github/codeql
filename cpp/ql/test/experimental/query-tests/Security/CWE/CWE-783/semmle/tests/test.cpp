36131ee16a6d07e88765db07b7815557bbbb2ec9
ihsinme <ihsinme@gmail.com>
2021-08-01 16:43:05 +0300

    Update test.cpp

--- cpp/ql/test/experimental/query-tests/Security/CWE/CWE-783/semmle/tests/test.cpp
+++ cpp/ql/test/experimental/query-tests/Security/CWE/CWE-783/semmle/tests/test.cpp
@@ -1,8 +1,5 @@
 void testFunction(int i1, int i2, int i3, bool b1, bool b2, bool b3, char c1)
 {
-
-
-
   
   if(b1||b2&&b3) //BAD
     return;
@@ -14,6 +11,10 @@ void testFunction(int i1, int i2, int i3, bool b1, bool b2, bool b3, char c1)
   if(b1||b2&i1) //BAD
     return;
   if((b1||b2)&i1) //GOOD
+    return;
+  if(b1||b2&1) //GOOD
+    return;
+  if(b1&&b2&0) //GOOD
     return;
   if(b1||b2|i1) //BAD
     return;
