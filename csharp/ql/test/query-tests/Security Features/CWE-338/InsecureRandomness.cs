2708326624164520251816b204627811cf0019ef
Raul Garcia <42392023+raulgarciamsft@users.noreply.github.com>
2021-08-05 16:33:01 -0700

    Update csharp/ql/test/query-tests/Security Features/CWE-338/InsecureRandomness.cs
    Co-authored-by: intrigus-lgtm <60750685+intrigus-lgtm@users.noreply.github.com>

--- csharp/ql/test/query-tests/Security Features/CWE-338/InsecureRandomness.cs
+++ csharp/ql/test/query-tests/Security Features/CWE-338/InsecureRandomness.cs
@@ -76,7 +76,7 @@ public class InsecureRandomness
 
     public static string BiasPasswordGeneration()
     {
-        // BAD: Membership.GeneratePassword is generates a password with a bias
+        // BAD: Membership.GeneratePassword generates a password with a bias
         string password =  System.Web.Security.Membership.GeneratePassword(12, 3);
         return password;
     }
