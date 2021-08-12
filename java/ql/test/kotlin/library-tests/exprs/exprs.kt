492dc3dfb31b69c7d277af1f985c42bffa4a98eb
Ian Lynagh <igfoo@github.com>
2021-08-12 15:21:15 +0100

    Kotlin: More tests (of unhandled expressions)

--- java/ql/test/kotlin/library-tests/exprs/exprs.kt
+++ java/ql/test/kotlin/library-tests/exprs/exprs.kt
@@ -5,6 +5,13 @@ fun topLevelMethod(x: Int, y: Int): Int {
     val i3 = x - y
     val i4 = x / y
     val i5 = x % y
+    val i6 = x shl y
+    val i7 = x shr y
+    val i8 = x ushr y
+    val i9 = x and y
+    val i10 = x or y
+    val i11 = x xor y
+    val i12 = x.inv()
     return 123 + 456
 }
 
