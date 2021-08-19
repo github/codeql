c4554836ca60c25fafd05221530c87c815109d34
Rasmus Lerchedahl Petersen <yoff@github.com>
2021-08-19 10:24:32 +0200

    Python: merge `test.py` into `unittests.py`

--- python/ql/test/library-tests/regexparser/unittests.py
+++ python/ql/test/library-tests/regexparser/unittests.py
@@ -7,3 +7,14 @@ re.compile(r"X(Æ|\Æ)+$") # Has ReDoS.
 # Treatment of line breaks
 re.compile(r'(?:.|\n)*b') # No ReDoS.
 re.compile(r'(?:.|\n)*b', re.DOTALL) # Has ReDoS.
+
+# minimal example constructed by @erik-krogh
+baz = re.compile(r'\+0')
+
+# exerpts from LGTM.com
+re.compile(r'\+0x')
+re.compile(r'\+0x.*')
+re.compile(r'+\-0+\.')
+re.compile('\s+\+0x[0-9]+')
+re.compile(r'\+0000 .*')
+re.compile('\#[0-9]+ 0x[0-9]')
