eef044f4d054d92eecd41013922a0ef4d0a2f2f9
Benjamin Muskalla <bmuskalla@github.com>
2021-09-09 13:05:15 +0200

    Add test to capture expected parameter format

--- java/ql/test/query-tests/Telemetry/UnsupportedExternalAPIs/Test.java
+++ java/ql/test/query-tests/Telemetry/UnsupportedExternalAPIs/Test.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 class ExternalApiUsage {
 	public static void main(String[] args) {
@@ -13,6 +14,11 @@ class ExternalApiUsage {
 		Duration d = java.time.Duration.ofMillis(1000); // not supported
 
 		long l = "foo".length(); // not interesting
+
+		AtomicReference<String> ref = new AtomicReference<>(); // not supported
+		ref.set("foo");
+
+		String.class.isAssignableFrom(Object.class); // parameter with generic type
 
 		System.out.println(d);
 		System.out.println(map);
