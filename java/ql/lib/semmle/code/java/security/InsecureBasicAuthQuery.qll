5ed9949498378e60b18a75a3e8c7308ee1ff327a
Tony Torralba <atorralba@users.noreply.github.com>
2021-09-15 17:16:37 +0200

    Adapt InsecureBasicAuth to the previous commit

--- java/ql/lib/semmle/code/java/security/InsecureBasicAuthQuery.qll
+++ java/ql/lib/semmle/code/java/security/InsecureBasicAuthQuery.qll
@@ -1,6 +1,7 @@
 /** Provides taint tracking configurations to be used in Insecure Basic Authentication queries. */
 
 import java
+import semmle.code.java.security.HttpsUrls
 import semmle.code.java.security.InsecureBasicAuth
 import semmle.code.java.dataflow.TaintTracking
 
@@ -16,6 +17,6 @@ class BasicAuthFlowConfig extends TaintTracking::Configuration {
   override predicate isSink(DataFlow::Node sink) { sink instanceof InsecureBasicAuthSink }
 
   override predicate isAdditionalTaintStep(DataFlow::Node node1, DataFlow::Node node2) {
-    any(InsecureBasicAuthAdditionalTaintStep c).step(node1, node2)
+    any(HttpUrlsAdditionalTaintStep c).step(node1, node2)
   }
 }
--- java/ql/test/query-tests/security/CWE-522/InsecureBasicAuthTest.java
+++ java/ql/test/query-tests/security/CWE-522/InsecureBasicAuthTest.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import java.net.HttpURLConnection;
 import java.net.URLConnection;
 import java.util.Base64;
+import javax.net.ssl.HttpsURLConnection;
 
 public class InsecureBasicAuthTest {
 	/**
@@ -17,42 +18,64 @@ public class InsecureBasicAuthTest {
 	 */
 	public void testApacheHttpRequest(String username, String password) {
 		String host = "www.example.com";
-		HttpRequestBase post = new HttpPost("http://" + host + "/rest/getuser.do?uid=abcdx");
-		post.setHeader("Accept", "application/json");
-		post.setHeader("Content-type", "application/json");
-
 		String authString = username + ":" + password;
 		byte[] authEncBytes = Base64.getEncoder().encode(authString.getBytes());
 		String authStringEnc = new String(authEncBytes);
-
-		post.addHeader("Authorization", "Basic " + authStringEnc); // $hasInsecureBasicAuth
+		{
+			HttpRequestBase post = new HttpPost("http://" + host + "/rest/getuser.do?uid=abcdx");
+			post.setHeader("Accept", "application/json");
+			post.setHeader("Content-type", "application/json");
+			post.addHeader("Authorization", "Basic " + authStringEnc); // $hasInsecureBasicAuth
+		}
+		{
+			HttpRequestBase post = new HttpPost("https://" + host + "/rest/getuser.do?uid=abcdx");
+			post.setHeader("Accept", "application/json");
+			post.setHeader("Content-type", "application/json");
+			post.addHeader("Authorization", "Basic " + authStringEnc); // Safe
+		}
 	}
 
 	/**
 	 * Test basic authentication with Apache HTTP GET request.
 	 */
 	public void testApacheHttpRequest2(String url) throws java.io.IOException {
-		String urlStr = "http://www.example.com:8000/payment/retrieve";
-		HttpGet get = new HttpGet(urlStr);
-		get.setHeader("Accept", "application/json");
-		get.setHeader("Authorization", // $hasInsecureBasicAuth
-				"Basic " + new String(Base64.getEncoder().encode("admin:test".getBytes())));
+		{
+			String urlStr = "http://www.example.com:8000/payment/retrieve";
+			HttpGet get = new HttpGet(urlStr);
+			get.setHeader("Accept", "application/json");
+			get.setHeader("Authorization", // $hasInsecureBasicAuth
+					"Basic " + new String(Base64.getEncoder().encode("admin:test".getBytes())));
+		}
+		{
+			String urlStr = "https://www.example.com:8000/payment/retrieve";
+			HttpGet get = new HttpGet(urlStr);
+			get.setHeader("Accept", "application/json");
+			get.setHeader("Authorization", // Safe
+					"Basic " + new String(Base64.getEncoder().encode("admin:test".getBytes())));
+		}
 	}
 
 	/**
 	 * Test basic authentication with Apache HTTP POST request using URI create method.
 	 */
 	public void testApacheHttpRequest3(String username, String password) {
-		String uriStr = "http://www.example.com/rest/getuser.do?uid=abcdx";
-		HttpRequestBase post = new HttpPost(URI.create(uriStr));
-		post.setHeader("Accept", "application/json");
-		post.setHeader("Content-type", "application/json");
-
 		String authString = username + ":" + password;
 		byte[] authEncBytes = Base64.getEncoder().encode(authString.getBytes());
 		String authStringEnc = new String(authEncBytes);
-
-		post.addHeader("Authorization", "Basic " + authStringEnc); // $hasInsecureBasicAuth
+		{
+			String uriStr = "http://www.example.com/rest/getuser.do?uid=abcdx";
+			HttpRequestBase post = new HttpPost(URI.create(uriStr));
+			post.setHeader("Accept", "application/json");
+			post.setHeader("Content-type", "application/json");
+			post.addHeader("Authorization", "Basic " + authStringEnc); // $hasInsecureBasicAuth
+		}
+		{
+			String uriStr = "https://www.example.com/rest/getuser.do?uid=abcdx";
+			HttpRequestBase post = new HttpPost(URI.create(uriStr));
+			post.setHeader("Accept", "application/json");
+			post.setHeader("Content-type", "application/json");
+			post.addHeader("Authorization", "Basic " + authStringEnc); // Safe
+		}
 	}
 
 	/**
@@ -60,17 +83,25 @@ public class InsecureBasicAuthTest {
 	 * argument.
 	 */
 	public void testApacheHttpRequest4(String username, String password) throws Exception {
-		String uriStr = "http://www.example.com/rest/getuser.do?uid=abcdx";
-		URI uri = new URI(uriStr);
-		HttpRequestBase post = new HttpPost(uri);
-		post.setHeader("Accept", "application/json");
-		post.setHeader("Content-type", "application/json");
-
 		String authString = username + ":" + password;
 		byte[] authEncBytes = Base64.getEncoder().encode(authString.getBytes());
 		String authStringEnc = new String(authEncBytes);
-
-		post.addHeader("Authorization", "Basic " + authStringEnc); // $hasInsecureBasicAuth
+		{
+			String uriStr = "http://www.example.com/rest/getuser.do?uid=abcdx";
+			URI uri = new URI(uriStr);
+			HttpRequestBase post = new HttpPost(uri);
+			post.setHeader("Accept", "application/json");
+			post.setHeader("Content-type", "application/json");
+			post.addHeader("Authorization", "Basic " + authStringEnc); // $hasInsecureBasicAuth
+		}
+		{
+			String uriStr = "https://www.example.com/rest/getuser.do?uid=abcdx";
+			URI uri = new URI(uriStr);
+			HttpRequestBase post = new HttpPost(uri);
+			post.setHeader("Accept", "application/json");
+			post.setHeader("Content-type", "application/json");
+			post.addHeader("Authorization", "Basic " + authStringEnc); // Safe
+		}
 	}
 
 	/**
@@ -78,49 +109,71 @@ public class InsecureBasicAuthTest {
 	 * arguments.
 	 */
 	public void testApacheHttpRequest5(String username, String password) throws Exception {
-		HttpRequestBase post =
-				new HttpPost(new URI("http", "www.example.com", "/test", "abc=123", null));
-		post.setHeader("Accept", "application/json");
-		post.setHeader("Content-type", "application/json");
-
 		String authString = username + ":" + password;
 		byte[] authEncBytes = Base64.getEncoder().encode(authString.getBytes());
 		String authStringEnc = new String(authEncBytes);
-
-		post.addHeader("Authorization", "Basic " + authStringEnc); // $hasInsecureBasicAuth
+		{
+			HttpRequestBase post =
+					new HttpPost(new URI("http", "www.example.com", "/test", "abc=123", null));
+			post.setHeader("Accept", "application/json");
+			post.setHeader("Content-type", "application/json");
+			post.addHeader("Authorization", "Basic " + authStringEnc); // $hasInsecureBasicAuth
+		}
+		{
+			HttpRequestBase post =
+					new HttpPost(new URI("https", "www.example.com", "/test", "abc=123", null));
+			post.setHeader("Accept", "application/json");
+			post.setHeader("Content-type", "application/json");
+			post.addHeader("Authorization", "Basic " + authStringEnc); // Safe
+		}
 	}
 
 	/**
 	 * Test basic authentication with Apache HTTP `BasicHttpRequest` using string constructor.
 	 */
 	public void testApacheHttpRequest6(String username, String password) {
-		String uriStr = "http://www.example.com/rest/getuser.do?uid=abcdx";
-		BasicHttpRequest post = new BasicHttpRequest("POST", uriStr);
-		post.setHeader("Accept", "application/json");
-		post.setHeader("Content-type", "application/json");
-
 		String authString = username + ":" + password;
 		byte[] authEncBytes = Base64.getEncoder().encode(authString.getBytes());
 		String authStringEnc = new String(authEncBytes);
-
-		post.addHeader("Authorization", "Basic " + authStringEnc); // $hasInsecureBasicAuth
+		{
+			String uriStr = "http://www.example.com/rest/getuser.do?uid=abcdx";
+			BasicHttpRequest post = new BasicHttpRequest("POST", uriStr);
+			post.setHeader("Accept", "application/json");
+			post.setHeader("Content-type", "application/json");
+			post.addHeader("Authorization", "Basic " + authStringEnc); // $hasInsecureBasicAuth
+		}
+		{
+			String uriStr = "https://www.example.com/rest/getuser.do?uid=abcdx";
+			BasicHttpRequest post = new BasicHttpRequest("POST", uriStr);
+			post.setHeader("Accept", "application/json");
+			post.setHeader("Content-type", "application/json");
+			post.addHeader("Authorization", "Basic " + authStringEnc); // Safe
+		}
 	}
 
 	/**
 	 * Test basic authentication with Apache HTTP `BasicHttpRequest` using `RequestLine`.
 	 */
 	public void testApacheHttpRequest7(String username, String password) {
-		String uriStr = "http://www.example.com/rest/getuser.do?uid=abcdx";
-		RequestLine requestLine = new BasicRequestLine("POST", uriStr, null);
-		BasicHttpRequest post = new BasicHttpRequest(requestLine);
-		post.setHeader("Accept", "application/json");
-		post.setHeader("Content-type", "application/json");
-
 		String authString = username + ":" + password;
 		byte[] authEncBytes = Base64.getEncoder().encode(authString.getBytes());
 		String authStringEnc = new String(authEncBytes);
-
-		post.addHeader("Authorization", "Basic " + authStringEnc); // $hasInsecureBasicAuth
+		{
+			String uriStr = "http://www.example.com/rest/getuser.do?uid=abcdx";
+			RequestLine requestLine = new BasicRequestLine("POST", uriStr, null);
+			BasicHttpRequest post = new BasicHttpRequest(requestLine);
+			post.setHeader("Accept", "application/json");
+			post.setHeader("Content-type", "application/json");
+			post.addHeader("Authorization", "Basic " + authStringEnc); // $hasInsecureBasicAuth
+		}
+		{
+			String uriStr = "https://www.example.com/rest/getuser.do?uid=abcdx";
+			RequestLine requestLine = new BasicRequestLine("POST", uriStr, null);
+			BasicHttpRequest post = new BasicHttpRequest(requestLine);
+			post.setHeader("Accept", "application/json");
+			post.setHeader("Content-type", "application/json");
+			post.addHeader("Authorization", "Basic " + authStringEnc); // Safe
+		}
 	}
 
 	/**
@@ -128,14 +181,24 @@ public class InsecureBasicAuthTest {
 	 * constructor.
 	 */
 	public void testHttpUrlConnection(String username, String password) throws Exception {
-		String urlStr = "http://www.example.com/rest/getuser.do?uid=abcdx";
 		String authString = username + ":" + password;
 		String encoding = Base64.getEncoder().encodeToString(authString.getBytes("UTF-8"));
-		URL url = new URL(urlStr);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("POST");
-		conn.setDoOutput(true);
-		conn.setRequestProperty("Authorization", "Basic " + encoding); // $hasInsecureBasicAuth
+		{
+			String urlStr = "http://www.example.com/rest/getuser.do?uid=abcdx";
+			URL url = new URL(urlStr);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			conn.setRequestProperty("Authorization", "Basic " + encoding); // $hasInsecureBasicAuth
+		}
+		{
+			String urlStr = "https://www.example.com/rest/getuser.do?uid=abcdx";
+			URL url = new URL(urlStr);
+			HttpURLConnection conn = (HttpsURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			conn.setRequestProperty("Authorization", "Basic " + encoding); // Safe
+		}
 	}
 
 	/**
@@ -143,16 +206,26 @@ public class InsecureBasicAuthTest {
 	 * String host, String file)` constructor.
 	 */
 	public void testHttpUrlConnection2(String username, String password) throws Exception {
-		String host = "www.example.com";
-		String path = "/rest/getuser.do?uid=abcdx";
-		String protocol = "http";
 		String authString = username + ":" + password;
 		String encoding = Base64.getEncoder().encodeToString(authString.getBytes("UTF-8"));
-		URL url = new URL(protocol, host, path);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("POST");
-		conn.setDoOutput(true);
-		conn.setRequestProperty("Authorization", "Basic " + encoding); // $hasInsecureBasicAuth
+		String host = "www.example.com";
+		String path = "/rest/getuser.do?uid=abcdx";
+		{
+			String protocol = "http";
+			URL url = new URL(protocol, host, path);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			conn.setRequestProperty("Authorization", "Basic " + encoding); // $hasInsecureBasicAuth
+		}
+		{
+			String protocol = "https";
+			URL url = new URL(protocol, host, path);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			conn.setRequestProperty("Authorization", "Basic " + encoding); // Safe
+		}
 	}
 
 	/**
@@ -162,10 +235,19 @@ public class InsecureBasicAuthTest {
 		String host = "LOCALHOST";
 		String authString = username + ":" + password;
 		String encoding = Base64.getEncoder().encodeToString(authString.getBytes("UTF-8"));
-		HttpURLConnection conn = (HttpURLConnection) new URL(
-				"http://" + (((host + "/rest/getuser.do") + "?uid=abcdx"))).openConnection();
-		conn.setRequestMethod("POST");
-		conn.setDoOutput(true);
-		conn.setRequestProperty("Authorization", "Basic " + encoding); // Safe
+		{
+			HttpURLConnection conn = (HttpURLConnection) new URL(
+					"http://" + (((host + "/rest/getuser.do") + "?uid=abcdx"))).openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			conn.setRequestProperty("Authorization", "Basic " + encoding); // Safe
+		}
+		{
+			HttpURLConnection conn = (HttpURLConnection) new URL(
+					"https://" + (((host + "/rest/getuser.do") + "?uid=abcdx"))).openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			conn.setRequestProperty("Authorization", "Basic " + encoding); // Safe
+		}
 	}
 }
