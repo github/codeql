ef6e502ff068adb41c90886d874481292a4d6b55
Rasmus Wriedt Larsen <rasmuswl@github.com>
2021-09-23 10:18:18 +0200

    Python: Make LDAP global options test better
    Before it didn't really showcase that we know it can make connections
    secure.

--- python/ql/test/experimental/query-tests/Security/CWE-522-global-option/ldap2_global.py
+++ python/ql/test/experimental/query-tests/Security/CWE-522-global-option/ldap2_global.py
@@ -1,3 +1,6 @@
+# since global options are considered to affect all files in a repo, we need to keep
+# this test in its' own directory (so it doesn't interfere with other tests).
+
 import ldap
 from flask import request, Flask
 
@@ -7,11 +10,13 @@ app = Flask(__name__)
 
 # SSL through ldap global variable option
 
-ldap.set_option(ldap.OPT_X_TLS_NEVER)
+ldap.set_option(ldap.OPT_X_TLS_DEMAND, True)
 
 
 @app.route("/one")
 def one():
+    # The following connection would have been insecure if the global option above was
+    # not set
     ldap_connection_5 = ldap.initialize("ldap://somethingon.theinternet.com")
     ldap_connection_5.simple_bind_s('', '')
     user = ldap_connection_5.search_s(
