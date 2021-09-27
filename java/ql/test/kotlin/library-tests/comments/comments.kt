ab77ed085fc1d901f57933ffa777ae94d0ee11f9
Tamas Vajk <tamasvajk@github.com>
2021-09-27 15:24:11 +0200

    Add QL classes and tests for comments

---
+++ java/ql/test/kotlin/library-tests/comments/comments.kt
@@ -0,0 +1,25 @@
+/** Kdoc with no owner */
+package foo.bar
+
+/**
+ * A group of *members*.
+ *
+ * This class has no useful logic; it's just a documentation example.
+ *
+ * @property name the name of this group.
+ * @constructor Creates an empty group.
+ */
+class Group(val name: String) {
+    /**
+     * Adds a [member] to this group.
+     * @return the new size of the group.
+     */
+    fun add(member: Int): Int {
+        // A line comment
+        return 42
+    }
+
+    /*
+    A block comment
+    */
+}
