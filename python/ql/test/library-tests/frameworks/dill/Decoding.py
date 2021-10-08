f9333fc5513b87f56703064ff4e0d24732abfeea
Rasmus Wriedt Larsen <rasmuswl@github.com>
2021-10-08 09:32:34 +0200

    Python: Expand `dill` tests

--- python/ql/test/library-tests/frameworks/dill/Decoding.py
+++ python/ql/test/library-tests/frameworks/dill/Decoding.py
@@ -1,3 +1,6 @@
 import dill
 
-dill.loads(payload)  # $decodeInput=payload decodeOutput=dill.loads(..) decodeFormat=dill decodeMayExecuteInput
+dill.load(file_)  # $ MISSING: decodeInput=file_ decodeOutput=dill.loads(..) decodeFormat=dill decodeMayExecuteInput
+dill.load(file=file_)  # $ MISSING: decodeInput=file_ decodeOutput=dill.loads(..) decodeFormat=dill decodeMayExecuteInput
+dill.loads(payload)  # $ decodeInput=payload decodeOutput=dill.loads(..) decodeFormat=dill decodeMayExecuteInput
+dill.loads(str=payload)  # $ decodeOutput=dill.loads(..) decodeFormat=dill decodeMayExecuteInput MISSING: decodeInput=payload
