76144a7f7da701e9be0f8745cd420cdbd2a47dd1
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2021-10-13 09:06:13 +0100

    Make the create-extractor-pack.ps1 script more reliable.

--- create-extractor-pack.ps1
+++ create-extractor-pack.ps1
@@ -3,7 +3,9 @@ cargo build --release
 cargo run --release -p ql-generator
 codeql query format -i ql\src\codeql_ql\ast\internal\TreeSitter.qll
 
-rm -Recurse -Force extractor-pack
+if (Test-Path -Path extractor-pack) {
+	rm -Recurse -Force extractor-pack
+}
 mkdir extractor-pack | Out-Null
 cp codeql-extractor.yml, ql\src\ql.dbscheme, ql\src\ql.dbscheme.stats extractor-pack
 cp -Recurse tools extractor-pack
