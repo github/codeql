514f8f3344778cf739226290783cd1406a5072b0
Taus <tausbn@github.com>
2021-10-15 09:34:14 +0000

    QL: Also update `qltest.cmd`

--- tools/qltest.cmd
+++ tools/qltest.cmd
@@ -4,6 +4,8 @@ type NUL && "%CODEQL_DIST%\codeql.exe" database index-files ^
     --prune=**/*.testproj ^
     --include-extension=.ql ^
     --include-extension=.qll ^
+    --include-extension=.dbscheme ^
+    --include-extension=.yml ^
     --size-limit=5m ^
     --language=ql ^
     "%CODEQL_EXTRACTOR_QL_WIP_DATABASE%"
