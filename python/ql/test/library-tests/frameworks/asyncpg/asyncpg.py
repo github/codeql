06586a13a35d04f46cb00b750e524d9db954c475
Rasmus Lerchedahl Petersen <yoff@github.com>
2021-10-27 11:55:04 +0200

    Python: merge tests files

--- python/ql/test/library-tests/frameworks/asyncpg/asyncpg.py
+++ python/ql/test/library-tests/frameworks/asyncpg/asyncpg.py
@@ -5,7 +5,14 @@ async def test_connection():
     conn = await asyncpg.connect()
 
     try:
+        # The file-like object is passed in as a keyword-only argument.
+        # See https://magicstack.github.io/asyncpg/current/api/index.html#asyncpg.connection.Connection.copy_from_query
         await conn.copy_from_query("sql", output="filepath")  # $ getSql="sql" getAPathArgument="filepath"
+        await conn.copy_from_query("sql", "arg1", "arg2", output="filepath")  # $ getSql="sql" getAPathArgument="filepath"
+
+        await conn.copy_from_table("table", output="filepath")  # $ getAPathArgument="filepath"
+        await conn.copy_to_table("table", source="filepath")  # $ getAPathArgument="filepath"
+
         await conn.execute("sql")  # $ getSql="sql"
         await conn.executemany("sql")  # $ getSql="sql"
         await conn.fetch("sql")  # $ getSql="sql"
@@ -63,6 +70,10 @@ async def test_connection_pool():
 
     try:
         await pool.copy_from_query("sql", output="filepath")  # $ getSql="sql" getAPathArgument="filepath"
+        await pool.copy_from_query("sql", "arg1", "arg2", output="filepath")  # $ getSql="sql" getAPathArgument="filepath"
+        await pool.copy_from_table("table", output="filepath")  # $ getAPathArgument="filepath"
+        await pool.copy_to_table("table", source="filepath")  # $ getAPathArgument="filepath"
+
         await pool.execute("sql")  # $ getSql="sql"
         await pool.executemany("sql")  # $ getSql="sql"
         await pool.fetch("sql")  # $ getSql="sql"
