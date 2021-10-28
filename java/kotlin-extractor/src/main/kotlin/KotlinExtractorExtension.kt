8acf7d74c166ca914f61fba607b4b36ecfa8b361
Chris Smowton <smowton@github.com>
2021-10-28 13:53:52 +0100

    Restore check for Array<T> type argument

--- java/kotlin-extractor/src/main/kotlin/KotlinExtractorExtension.kt
+++ java/kotlin-extractor/src/main/kotlin/KotlinExtractorExtension.kt
@@ -509,7 +509,7 @@ class X {
 }
 */
 
-            s.isBoxedArray || s.isPrimitiveArray() -> {
+            (s.isBoxedArray && s.arguments.isNotEmpty()) || s.isPrimitiveArray() -> {
                 // TODO: fix this, this is only a dummy implementation to let the tests pass
                 // TODO: Figure out what signatures should be returned
                 // TODO: Generate a short name for array types
