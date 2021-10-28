436152a46da23d5bbecad45396c4ba7aa819042a
Rasmus Wriedt Larsen <rasmuswl@github.com>
2021-10-28 12:37:07 +0200

    Python: Refactor flask file sending tests

--- python/ql/test/library-tests/frameworks/flask/save_uploaded_file.py
+++ python/ql/test/library-tests/frameworks/flask/save_uploaded_file.py
@@ -1,17 +1,6 @@
-from flask import Flask, request, send_from_directory, send_file
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/save-uploaded-file")  # $routeSetup="/save-uploaded-file"
 def test_taint():  # $requestHandler
     request.files['key'].save("path") # $ getAPathArgument="path"
-
-
-@app.route("/path-injection")  # $routeSetup="/path-injection"
-def test_path():  # $requestHandler
-
-    send_from_directory("filepath","file")  # $ getAPathArgument="filepath" getAPathArgument="file"
-    send_file("file")  # $ getAPathArgument="file"
-
-    send_from_directory(directory="filepath","file") # $ getAPathArgument="filepath" getAPathArgument="file"
-    send_from_directory(filename="filepath","file") # $ getAPathArgument="filepath" getAPathArgument="file"
-    send_file(filename_or_fp="file")  # $ getAPathArgument="file"
