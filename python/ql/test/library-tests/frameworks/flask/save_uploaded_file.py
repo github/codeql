358663ffbbb660e349c3ee375b76865218407ef2
Rasmus Wriedt Larsen <rasmuswl@github.com>
2021-10-28 11:14:41 +0200

    Python: Fix tests

--- python/ql/test/library-tests/frameworks/flask/save_uploaded_file.py
+++ python/ql/test/library-tests/frameworks/flask/save_uploaded_file.py
@@ -9,9 +9,9 @@ def test_taint():  # $requestHandler
 @app.route("/path-injection")  # $routeSetup="/path-injection"
 def test_path():  # $requestHandler
 
-    flask.send_from_directory("filepath","file")  # $ getAPathArgument="filepath" getAPathArgument="file"
-    flask.send_file("file")  # $ getAPathArgument="file"
-    
-    flask.send_from_directory(directory="filepath","file") # $ getAPathArgument="filepath" getAPathArgument="file"
-    flask.send_from_directory(filename="filepath","file") # $ getAPathArgument="filepath" getAPathArgument="file"
-    flask.send_file(filename_or_fp="file")  # $ getAPathArgument="file"
+    send_from_directory("filepath","file")  # $ getAPathArgument="filepath" getAPathArgument="file"
+    send_file("file")  # $ getAPathArgument="file"
+
+    send_from_directory(directory="filepath","file") # $ getAPathArgument="filepath" getAPathArgument="file"
+    send_from_directory(filename="filepath","file") # $ getAPathArgument="filepath" getAPathArgument="file"
+    send_file(filename_or_fp="file")  # $ getAPathArgument="file"
