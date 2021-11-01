d565a16fe61a01d25a404ee2bea2d014e3db4e20
Ian Lynagh <igfoo@github.com>
2021-11-01 17:31:22 +0000

    Kotlin: Add enums to expr test

--- java/ql/test/kotlin/library-tests/exprs/exprs.kt
+++ java/ql/test/kotlin/library-tests/exprs/exprs.kt
@@ -84,3 +84,18 @@ fun foo(p: Polygon) {
         val r2: Rectangle = r
     }
 }
+
+enum class Direction {
+    NORTH, SOUTH, WEST, EAST
+}
+
+enum class Color(val rgb: Int) {
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF)
+}
+
+fun enums() {
+    val south = Direction.SOUTH
+    val green = Color.GREEN
+}
