d316974157c363824c778b27d5d1de51e975f00f
jorgectf <jorgectf@protonmail.com>
2021-11-08 10:23:50 +0100

    Add `HtmlContent` additional taint step

--- python/ql/src/experimental/semmle/python/security/dataflow/ReflectedXSS.qll
+++ python/ql/src/experimental/semmle/python/security/dataflow/ReflectedXSS.qll
@@ -9,6 +9,7 @@ import semmle.python.dataflow.new.TaintTracking
 import semmle.python.dataflow.new.RemoteFlowSources
 import semmle.python.dataflow.new.BarrierGuards
 import experimental.semmle.python.Concepts
+import semmle.python.ApiGraphs
 
 /**
  * A taint-tracking configuration for detecting reflected server-side cross-site
@@ -23,5 +24,18 @@ class ReflectedXssConfiguration extends TaintTracking::Configuration {
 
   override predicate isSanitizerGuard(DataFlow::BarrierGuard guard) {
     guard instanceof StringConstCompare
+  }
+
+  override predicate isAdditionalTaintStep(DataFlow::Node nodeTo, DataFlow::Node nodeFrom) {
+    exists(DataFlow::CallCfgNode htmlContentCall |
+      htmlContentCall =
+        API::moduleImport("sendgrid")
+            .getMember("helpers")
+            .getMember("mail")
+            .getMember("HtmlContent")
+            .getACall() and
+      nodeFrom = htmlContentCall and
+      nodeTo = htmlContentCall.getArg(0)
+    )
   }
 }
--- python/ql/test/experimental/query-tests/Security/CWE-079/sendgrid_mail.py
+++ python/ql/test/experimental/query-tests/Security/CWE-079/sendgrid_mail.py
@@ -1,30 +1,44 @@
 from flask import request, Flask
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail, Email, To, Content, MimeType
+from sendgrid.helpers.mail import Mail, Email, To, Content, MimeType, HtmlContent
 
 app = Flask(__name__)
 
+
 @app.route("/send")
 def send():
-  message = Mail(
-      from_email='from_email@example.com',
-      to_emails='to@example.com',
-      subject='Sending with Twilio SendGrid is Fun',
-      html_content=request.args["html_content"])
+    message = Mail(
+        from_email='from_email@example.com',
+        to_emails='to@example.com',
+        subject='Sending with Twilio SendGrid is Fun',
+        html_content=request.args["html_content"])
 
-  sg = SendGridAPIClient('SENDGRID_API_KEY')
-  sg.send(message)
+    sg = SendGridAPIClient('SENDGRID_API_KEY')
+    sg.send(message)
+
+
+@app.route("/send-HtmlContent")
+def send():
+    message = Mail(
+        from_email='from_email@example.com',
+        to_emails='to@example.com',
+        subject='Sending with Twilio SendGrid is Fun',
+        html_content=HtmlContent(request.args["html_content"]))
+
+    sg = SendGridAPIClient('SENDGRID_API_KEY')
+    sg.send(message)
+
 
 @app.route("/send_post")
 def send_post():
-  from_email = Email("test@example.com")
-  to_email = To("test@example.com")
-  subject = "Sending with SendGrid is Fun"
-  content = Content("text/html", request.args["html_content"]) 
+    from_email = Email("test@example.com")
+    to_email = To("test@example.com")
+    subject = "Sending with SendGrid is Fun"
+    content = Content("text/html", request.args["html_content"])
 
-  content = Content(MimeType.html, request.args["html_content"]) 
+    content = Content(MimeType.html, request.args["html_content"])
 
-  mail = Mail(from_email, to_email, subject, content)
+    mail = Mail(from_email, to_email, subject, content)
 
-  sg = SendGridAPIClient(api_key='SENDGRID_API_KEY')
-  response = sg.client.mail.send.post(request_body=mail.get())
+    sg = SendGridAPIClient(api_key='SENDGRID_API_KEY')
+    response = sg.client.mail.send.post(request_body=mail.get())
