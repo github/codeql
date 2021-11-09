cb8f1b4593175bd4eb0f4bf962fbdaf61942e3d0
Rasmus Lerchedahl Petersen <yoff@github.com>
2021-11-09 11:49:31 +0100

    Python: Add tests for aiopg

---
+++ python/ql/test/library-tests/frameworks/aiopg/test.py
@@ -0,0 +1,19 @@
+import aiopg
+
+# Only a cursor can execute sql.
+async def test_cursor():
+    # Create connection directly
+    conn = await aiopg.connect()
+    cur = await conn.cursor()
+    await cur.execute("sql")  # $ MISSING: getSql="sql"
+
+    # Create connection via pool
+    async with aiopg.create_pool() as pool:
+        # Create Cursor via Connection
+        async with pool.acquire() as conn:
+            cur = await conn.cursor()
+            await cur.execute("sql")  # $ MISSING: getSql="sql"
+
+        # Create Cursor directly
+        async with pool.cursor() as cur:
+            await cur.execute("sql")  # $ MISSING: getSql="sql"
