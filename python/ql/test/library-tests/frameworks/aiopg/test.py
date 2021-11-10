a856395d565547b19cb6cc9cb47e666e09ff7a8f
yoff <lerchedahl@gmail.com>
2021-11-10 10:51:40 +0100

    Apply suggestions from code review
    Co-authored-by: Rasmus Wriedt Larsen <rasmuswriedtlarsen@gmail.com>

--- python/ql/test/library-tests/frameworks/aiopg/test.py
+++ python/ql/test/library-tests/frameworks/aiopg/test.py
@@ -11,13 +11,20 @@ async def test_cursor():
     async with aiopg.create_pool() as pool:
         # Create Cursor via Connection
         async with pool.acquire() as conn:
-            cur = await conn.cursor()
-            await cur.execute("sql")  # $ getSql="sql" constructedSql="sql"
+            async with conn.cursor() as cur:
+                await cur.execute("sql")  # $ getSql="sql" constructedSql="sql"
 
         # Create Cursor directly
         async with pool.cursor() as cur:
             await cur.execute("sql")  # $ getSql="sql" constructedSql="sql"
 
+    # variants using as few `async with` as possible
+    pool = await aiopg.create_pool()
+    conn = pool.acquire()
+    cur = await conn.cursor()
+    await cur.execute("sql")  # $ getSql="sql" constructedSql="sql"
+
+# Test SQLAlchemy integration
 from aiopg.sa import create_engine
 
 async def test_engine():
