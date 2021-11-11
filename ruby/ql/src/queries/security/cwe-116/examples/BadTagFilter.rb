b639a8d183672be1abce5dc1776b2801a947f68a
Erik Krogh Kristensen <erik-krogh@github.com>
2021-11-11 14:04:38 +0100

    update ruby example
    Co-authored-by: Nick Rolfe <nickrolfe@github.com>

--- ruby/ql/src/queries/security/cwe-116/examples/BadTagFilter.rb
+++ ruby/ql/src/queries/security/cwe-116/examples/BadTagFilter.rb
@@ -1,8 +1,8 @@
-def filterScripTags(html)
-  oldHtml = "";
-  while (html != oldHtml)
-    oldHtml = html;
-    html = html.gsub(/<script[^>]*>.*<\/script>/m, "");
+def filter_script_tags(html)
+  old_html = ""
+  while (html != old_html)
+    old_html = html
+    html = html.gsub(/<script[^>]*>.*<\/script>/m, "")
   end
-  return html;
+  html
 end
