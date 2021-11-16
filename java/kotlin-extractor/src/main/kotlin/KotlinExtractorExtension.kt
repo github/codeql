ab86778e1d09548c9aa104199da1abf94c5252f8
Tamas Vajk <tamasvajk@github.com>
2021-11-16 15:26:23 +0100

    Remove external property related log messages

--- java/kotlin-extractor/src/main/kotlin/KotlinExtractorExtension.kt
+++ java/kotlin-extractor/src/main/kotlin/KotlinExtractorExtension.kt
@@ -1306,7 +1306,9 @@ open class KotlinFileExtractor(
             val getterId = extractFunction(getter, parentId) as Label<out DbMethod>
             tw.writeKtPropertyGetters(id, getterId)
         } else {
-            logger.warnElement(Severity.ErrorSevere, "IrProperty without a getter", p)
+            if (p.modality != Modality.FINAL || !isExternalDeclaration(p)) {
+                logger.warnElement(Severity.ErrorSevere, "IrProperty without a getter", p)
+            }
         }
 
         if(setter != null) {
@@ -1317,7 +1319,7 @@ open class KotlinFileExtractor(
             val setterId = extractFunction(setter, parentId) as Label<out DbMethod>
             tw.writeKtPropertySetters(id, setterId)
         } else {
-            if(p.isVar) {
+            if (p.isVar && !isExternalDeclaration(p)) {
                 logger.warnElement(Severity.ErrorSevere, "isVar property without a setter", p)
             }
         }
