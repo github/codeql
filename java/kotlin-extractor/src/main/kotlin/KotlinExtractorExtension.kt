956c479db3da00c922147461deccb7af21b07672
Chris Smowton <smowton@github.com>
2021-11-19 17:01:24 +0000

    Document TypeResult

--- java/kotlin-extractor/src/main/kotlin/KotlinExtractorExtension.kt
+++ java/kotlin-extractor/src/main/kotlin/KotlinExtractorExtension.kt
@@ -324,6 +324,18 @@ open class KotlinUsesExtractor(
     }
 
     data class UseClassInstanceResult(val typeResult: TypeResult<DbClassorinterface>, val javaClass: IrClass)
+    /**
+     * A triple of a type's database label, its signature for use in callable signatures, and its short name for use
+     * in all tables that provide a user-facing type name.
+     *
+     * `signature` is a Java primitive name (e.g. "int"), a fully-qualified class name ("package.OuterClass.InnerClass"),
+     * or an array ("componentSignature[]")
+     * Type variables have the signature of their upper bound.
+     * Type arguments and anonymous types do not have a signature.
+     *
+     * `shortName` is a Java primitive name (e.g. "int"), a class short name with Java-style type arguments ("InnerClass<E>" or
+     * "OuterClass<ConcreteArgument>" or "OtherClass<? extends Bound>") or an array ("componentShortName[]").
+     */
     data class TypeResult<out LabelType>(val id: Label<out LabelType>, val signature: String?, val shortName: String)
     data class TypeResults(val javaResult: TypeResult<DbType>, val kotlinResult: TypeResult<DbKt_type>)
 
