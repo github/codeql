591aeff9065ea6334cbb9c8685ad50deeaa711a4
Erik Krogh Kristensen <erik-krogh@github.com>
2021-11-23 12:56:47 +0100

    add TypeScript test for new private field syntax

--- javascript/ql/test/library-tests/TypeScript/Types/tst.ts
+++ javascript/ql/test/library-tests/TypeScript/Types/tst.ts
@@ -218,4 +218,18 @@ module TS45 {
           let token = r.body;
       }
   }
+
+  class Person {
+    #name: string;
+    constructor(name: string) {
+        this.#name = name;
+    }
+
+    equals(other: unknown) {
+        return other &&
+            typeof other === "object" &&
+            #name in other && // <- this is new!
+            this.#name === other.#name; // <- other has type Person here.
+    }
+  }
 }
