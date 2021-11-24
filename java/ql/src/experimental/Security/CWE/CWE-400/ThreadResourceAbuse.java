5101a8e9f32b8ec3658851dc09a3b01f9393e7e0
Chris Smowton <smowton@github.com>
2021-11-24 12:12:56 +0000

    Fix qhelp test

--- java/ql/src/experimental/Security/CWE/CWE-400/ThreadResourceAbuse.java
+++ java/ql/src/experimental/Security/CWE/CWE-400/ThreadResourceAbuse.java
@@ -1,11 +1,11 @@
 class SleepTest {
 	public void test(int userSuppliedWaitTime) throws Exception {
 		// BAD: no boundary check on wait time
-		Thread.sleep(waitTime);
+		Thread.sleep(userSuppliedWaitTime);
 
 		// GOOD: enforce an upper limit on wait time
-		if (waitTime > 0 && waitTime < 5000) {
-			Thread.sleep(waitTime);
+		if (userSuppliedWaitTime > 0 && userSuppliedWaitTime < 5000) {
+			Thread.sleep(userSuppliedWaitTime);
 		}
 	}
 }
