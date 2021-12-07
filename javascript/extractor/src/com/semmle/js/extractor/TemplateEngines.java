ce68a6d1c58a3bb2235e768538b65e686bc43cae
Asger Feldthaus <asgerf@github.com>
2021-12-07 12:44:24 +0100

    JS: Remove unneeded qualifier in static field access

--- javascript/extractor/src/com/semmle/js/extractor/TemplateEngines.java
+++ javascript/extractor/src/com/semmle/js/extractor/TemplateEngines.java
@@ -22,7 +22,7 @@ public class TemplateEngines {
   public static final Pattern TEMPLATE_TAGS =
       Pattern.compile(
           StringUtil.glue(
-              "|", TemplateEngines.MUSTACHE_TAG_DOUBLE, MUSTACHE_TAG_TRIPLE, MUSTACHE_TAG_PERCENT, EJS_TAG),
+              "|", MUSTACHE_TAG_DOUBLE, MUSTACHE_TAG_TRIPLE, MUSTACHE_TAG_PERCENT, EJS_TAG),
           Pattern.DOTALL);
 
   /**
