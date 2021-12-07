9d9b2530242b3761d5bc24db122474aff28a9093
Harry Maclean <harry@hmac.dev>
2021-12-08 09:33:45 +1300

    Ruby: rename codeql-extractor.yml template
    This file is copied into the extractor pack when it is built, but having
    it in the source tree confuses the codeql CLI into thinking that
    `codeql/ruby` is itself an extractor pack. This can cause errors when
    running `codeql database create`.
    
    This change renames the file to `codeql-extractor.template.yml`, so the
    CLI ignores it.

--- ruby/scripts/create-extractor-pack.ps1
+++ ruby/scripts/create-extractor-pack.ps1
@@ -5,7 +5,8 @@ codeql query format -i ql\lib\codeql/ruby\ast\internal\TreeSitter.qll
 
 rm -Recurse -Force extractor-pack
 mkdir extractor-pack | Out-Null
-cp codeql-extractor.yml, ql\lib\ruby.dbscheme, ql\lib\ruby.dbscheme.stats extractor-pack
+cp codeql-extractor.template.yml extractor-pack\codeql-extractor.yml
+cp ql\lib\ruby.dbscheme, ql\lib\ruby.dbscheme.stats extractor-pack
 cp -Recurse tools extractor-pack
 mkdir extractor-pack\tools\win64 | Out-Null
 cp target\release\ruby-extractor.exe extractor-pack\tools\win64\extractor.exe
