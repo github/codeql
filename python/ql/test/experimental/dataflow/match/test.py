7b9cc6d31f16fce8cb5d4b78ee9c88b92fac5cb9
Rasmus Lerchedahl Petersen <yoff@github.com>
2021-12-09 12:47:59 +0100

    python: minimal test of match

---
+++ python/ql/test/experimental/dataflow/match/test.py
@@ -0,0 +1,35 @@
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname((__file__))))
+from testlib import *
+
+# These are defined so that we can evaluate the test code.
+NONSOURCE = "not a source"
+SOURCE = "source"
+
+
+def is_source(x):
+    return x == "source" or x == b"source" or x == 42 or x == 42.0 or x == 42j
+
+
+def SINK(x):
+    if is_source(x):
+        print("OK")
+    else:
+        print("Unexpected flow", x)
+
+
+def SINK_F(x):
+    if is_source(x):
+        print("Unexpected flow", x)
+    else:
+        print("OK")
+
+
+def test_tuple():
+    match (NONSOURCE, SOURCE):
+        case x, y:
+            SINK_F(x)
+            SINK(y) #$ MISSING: flow="SOURCE, l:-3 -> y"
