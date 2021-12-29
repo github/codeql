a616059761c83efcb78d25fd5855d47522c87eaa
Edoardo Pirovano <edoardo.pirovano@gmail.com>
2021-12-29 12:01:09 +0000

    Fix example in JavaScript query

--- javascript/ql/src/Security/CWE-843/examples/TypeConfusionThroughParameterTampering.js
+++ javascript/ql/src/Security/CWE-843/examples/TypeConfusionThroughParameterTampering.js
@@ -4,8 +4,8 @@ var app = require("express")(),
 app.get("/user-files", function(req, res) {
   var file = req.param("file");
   if (file.indexOf("..") !== -1) {
-    // BAD
-    // forbid paths outside the /public directory
+    // BAD: we forbid relative paths that contain ..
+    // as these could leave the public directory
     res.status(400).send("Bad request");
   } else {
     var absolute = path.resolve("/public/" + file);
