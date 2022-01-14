6c1bb4a3a9c9a9447f8804f2b4f1058f528e5cc7
Michael Nebel <michaelnebel@github.com>
2022-01-14 16:49:44 +0100

    C#: Add test case for record class and record structs.

--- csharp/ql/test/library-tests/csharp10/RecordTypes.cs
+++ csharp/ql/test/library-tests/csharp10/RecordTypes.cs
@@ -4,3 +4,9 @@ public record MyEntry(string Name, string Address)
 {
     sealed public override string ToString() => $"{Name} lives at {Address}";
 };
+
+public record class MyClassRecord(DateTime stuff) { }
+
+public readonly record struct MyReadonlyRecordStruct(string Stuff) { }
+
+public record struct MyRecordStruct(int Stuff) { }
