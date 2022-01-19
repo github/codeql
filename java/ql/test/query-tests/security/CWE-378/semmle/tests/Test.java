253c96f706e0d2330d4887b97db3a783f97aa11b
Jonathan Leitschuh <jonathan.leitschuh@gmail.com>
2022-01-19 19:35:24 +0000

    Improve TempDir hijacking detection with Guard

--- java/ql/test/query-tests/security/CWE-378/semmle/tests/Test.java
+++ java/ql/test/query-tests/security/CWE-378/semmle/tests/Test.java
@@ -1,21 +1,37 @@
 import java.io.File;
+import java.io.IOException;
 
 public class Test {
 
-    static File vulnerable() {
+    private static final File vulnerableAssigned;
+
+    static {
+        File temp = null;
+        try {
+            temp = File.createTempFile("test", "directory");
+            temp.delete();
+            temp.mkdir();
+        } catch(IOException e) {
+            System.out.println("Could not create temporary directory");
+            System.exit(-1);
+        }
+        vulnerableAssigned = temp;
+    }
+
+    static File vulnerable() throws IOException {
         File temp = File.createTempFile("test", "directory");
         temp.delete();
         temp.mkdir();
         return temp;
     }
 
-    static File notVulnerableToHijacking() {
+    static File notVulnerableToHijacking() throws IOException {
         File temp = File.createTempFile("test", "directory");
         temp.mkdir();
         return temp;
     }
 
-    static File safe() {
+    static File safe() throws IOException {
         File temp = File.createTempFile("test", "directory");
         temp.delete();
         if (temp.mkdir()) {
@@ -25,7 +41,7 @@ public class Test {
         }
     }
 
-    static File safe2() {
+    static File safe2() throws IOException {
         File temp = File.createTempFile("test", "directory");
         temp.delete();
         boolean didMkdirs = temp.mkdirs();
@@ -36,26 +52,39 @@ public class Test {
         }
     }
 
-    static File safe3() {
+    static File safe3() throws IOException {
         File temp = File.createTempFile("test", "directory");
         temp.delete();
-        if (!(temp.mkdirs()))) {
+        if (!(temp.mkdirs())) {
             throw new RuntimeException("Failed to create directory");
         }
         return temp;
     }
 
-    static File safe4() {
+    static File safe4() throws IOException {
         boolean success = true;
         File temp = File.createTempFile("test", "directory");
         success &= temp.delete();
-        success &= f.mkdir();
+        success &= temp.mkdir();
         if (!success) {
+            throw new RuntimeException("Failed to create directory");
+        }
+        return temp;
+    }
+
+    static File safe5() throws IOException {
+        boolean success = true;
+        File temp = File.createTempFile("test", "directory");
+        success &= temp.delete();
+        success &= temp.mkdir();
+        if (success) {
+            return temp;
+        } else {
             throw new RuntimeException("Failed to create directory");
         }
     }
 
-    static File safe5() {
+    static File safe6() throws IOException {
         File temp = File.createTempFile("test", "directory");
         if (temp.delete() && temp.mkdir()) {
             return temp;
