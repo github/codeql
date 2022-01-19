85319b2dbfa089d777c54a2eb3589614751523a7
Owen Mansel-Chan <owen-mc@github.com>
2022-01-19 00:12:07 +0000

    Add tests for tainted path sanitizers and sanitizer guards

--- ql/test/query-tests/Security/CWE-022/TaintedPath.go
+++ ql/test/query-tests/Security/CWE-022/TaintedPath.go
@@ -4,6 +4,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -16,4 +18,37 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// BAD: This could still read any file on the file system
 	data, _ = ioutil.ReadFile(filepath.Join("/home/user/", path))
 	w.Write(data)
+
+	// GOOD: This can only read inside the provided safe path
+	sanitized_filepath, _ := filepath.Rel("/home/user/safepath", path)
+	data, _ = ioutil.ReadFile(sanitized_filepath)
+	w.Write(data)
+
+	// GOOD: This can only read inside the provided safe path
+	if !strings.Contains(path, "..") {
+		data, _ = ioutil.ReadFile(path)
+		w.Write(data)
+	}
+
+	// GOOD: This can only read inside the provided safe path
+	_, err := filepath.Rel("/home/user/safepath", path)
+	if err == nil {
+		data, _ = ioutil.ReadFile(path)
+		w.Write(data)
+	}
+
+	// GOOD: An attempt has been made to ensure that this can only read inside
+	// the provided safe path
+	if strings.HasPrefix(path, "/home/user/safepath/") {
+		data, _ = ioutil.ReadFile(path)
+		w.Write(data)
+	}
+
+	// GOOD: An attempt has been made to ensure that this can only read inside
+	// the provided safe path
+	matched, _ := regexp.MatchString("\\.\\.", path)
+	if !matched {
+		data, _ = ioutil.ReadFile(filepath.Join("/home/user/", path))
+		w.Write(data)
+	}
 }
