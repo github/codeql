4ee290acd33d00de70c167e5fe08d333ea3bcddc
Stephan Brandauer <kaeluka@github.com>
2022-01-25 14:23:40 +0100

    update test for 'node:' prefix

--- javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/tainted-access-paths.js
+++ javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/tainted-access-paths.js
@@ -1,4 +1,4 @@
-var fs = require('node:fs'),
+var fs = require('fs'),
     http = require('http'),
     url = require('url');
 
@@ -32,3 +32,12 @@ var server = http.createServer(function(req, res) {
 });
 
 server.listen();
+
+var nodefs = require('node:fs');
+
+var server2 = http.createServer(function(req, res) {
+  let path = url.parse(req.url, true).query.path;
+  nodefs.readFileSync(path); // NOT OK
+});
+
+server2.listen();
