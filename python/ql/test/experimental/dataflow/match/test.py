d6f415bae2f61f40911a8bbc9be0f2114689bcaa
Rasmus Wriedt Larsen <rasmuswl@github.com>
2022-02-01 15:37:22 +0100

    Python: Run `match` tests if Python 3.10 or newer
    Also fixes a bug in the tests

--- python/ql/test/experimental/dataflow/match/test.py
+++ python/ql/test/experimental/dataflow/match/test.py
@@ -48,8 +48,8 @@ def test_or_pattern():
 # No flow for literal pattern
 def test_literal_pattern():
     match SOURCE:
-        case 42 as x:
-            SINK(x) #$ flow="SOURCE, l:-2 -> x" flow="42, l:-1 -> x"
+        case "source" as x:
+            SINK(x) #$ flow="SOURCE, l:-2 -> x" flow="'source', l:-1 -> x"
 
 def test_capture_pattern():
     match SOURCE:
