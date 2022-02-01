095c73f1feda6ec7af13b46e0bc88022f9524843
Erik Krogh Kristensen <erik-krogh@github.com>
2022-02-01 19:15:33 +0100

    redo the ApiGraph testing framework

--- python/ql/test/library-tests/ApiGraphs/py3/deftest2.py
+++ python/ql/test/library-tests/ApiGraphs/py3/deftest2.py
@@ -4,7 +4,7 @@ from flask.views import View #$ use=moduleImport("flask").getMember("views").get
 
 class MyView(View): #$ use=moduleImport("flask").getMember("views").getMember("View").getASubclass()
     myvar = 45 #$ def=moduleImport("flask").getMember("views").getMember("View").getASubclass().getMember("myvar")
-    def my_method(self): #$ def=moduleImport("flask").getMember("views").getMember("View").getASubclass().getMember("my_method") use=moduleImport("flask").getMember("views").getMember("View").getASubclass().getMember("my_method").getParameter(0)
+    def my_method(self): #$ def=moduleImport("flask").getMember("views").getMember("View").getASubclass().getMember("my_method")
         return 3 #$ def=moduleImport("flask").getMember("views").getMember("View").getASubclass().getMember("my_method").getReturn()
 
 instance = MyView() #$ use=moduleImport("flask").getMember("views").getMember("View").getASubclass().getReturn()
@@ -13,7 +13,7 @@ def internal():
     from pflask.views import View #$ use=moduleImport("pflask").getMember("views").getMember("View")
     class IntMyView(View): #$ use=moduleImport("pflask").getMember("views").getMember("View").getASubclass()
         my_internal_var = 35 #$ def=moduleImport("pflask").getMember("views").getMember("View").getASubclass().getMember("my_internal_var")
-        def my_internal_method(self): #$ def=moduleImport("pflask").getMember("views").getMember("View").getASubclass().getMember("my_internal_method") use=moduleImport("pflask").getMember("views").getMember("View").getASubclass().getMember("my_internal_method").getParameter(0)
+        def my_internal_method(self): #$ def=moduleImport("pflask").getMember("views").getMember("View").getASubclass().getMember("my_internal_method")
             pass
 
     int_instance = IntMyView() #$ use=moduleImport("pflask").getMember("views").getMember("View").getASubclass().getReturn()
