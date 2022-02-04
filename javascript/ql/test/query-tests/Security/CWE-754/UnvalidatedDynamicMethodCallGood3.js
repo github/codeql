5e1ca3154f5ea173dee17c079e6bccb14340d359
Naman Jain <naman1205jain@gmail.com>
2022-02-04 16:13:05 +0530

    Update javascript/ql/test/query-tests/Security/CWE-754/UnvalidatedDynamicMethodCallGood3.js
    Co-authored-by: Erik Krogh Kristensen <erik-krogh@github.com>

--- javascript/ql/test/query-tests/Security/CWE-754/UnvalidatedDynamicMethodCallGood3.js
+++ javascript/ql/test/query-tests/Security/CWE-754/UnvalidatedDynamicMethodCallGood3.js
@@ -11,9 +11,8 @@ actions.put("pause", function pause(data) {
 
 app.get('/perform/:action/:payload', function(req, res) {
     let action = actions.get(req.params.action);
-    // GOOD: `action` is either the `play` or the `pause` function from above
     if (typeof action === 'function') {
-        res.end(action(req.params.payload));
+        res.end(action(req.params.payload)); // GOOD: `action` is either the `play` or the `pause` function from above
     } else {
         res.end("Unsupported action.");
     }
