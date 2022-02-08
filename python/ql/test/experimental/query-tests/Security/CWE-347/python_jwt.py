7b51b91d137f710ee08e8b9f788c240bac52ba13
jorgectf <jorgectf@protonmail.com>
2022-02-08 23:33:43 +0100

    Improve test

--- python/ql/test/experimental/query-tests/Security/CWE-347/python_jwt.py
+++ python/ql/test/experimental/query-tests/Security/CWE-347/python_jwt.py
@@ -2,12 +2,11 @@ import python_jwt
 
 # GOOD
 
-token1 = "1"
-python_jwt.process_jwt(token1)
-python_jwt.verify_jwt(token1, "key", "HS256")
+def good1(token):
+    python_jwt.process_jwt(token)
+    python_jwt.verify_jwt(token, "key", "HS256")
 
 # BAD
 
-# no call to verify_jwt
-token2 = "123"
-python_jwt.process_jwt(token2)
+def bad(token):
+    python_jwt.process_jwt(token)
