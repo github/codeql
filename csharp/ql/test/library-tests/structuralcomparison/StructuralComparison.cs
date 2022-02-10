fcb66d09bf9c0dfd19174ec91f14a337aca6c67b
Michael Nebel <michaelnebel@github.com>
2022-02-10 16:42:50 +0100

    C#: Add some more examples.

--- csharp/ql/test/library-tests/structuralcomparison/StructuralComparison.cs
+++ csharp/ql/test/library-tests/structuralcomparison/StructuralComparison.cs
@@ -5,8 +5,10 @@ public class Class
     public int MUnit() => 0;
 
     public int M0(int a) => a;
+    public int M1(int v1, int v2) => v1 + v2;
 
-    public void M1()
+
+    public void M2()
     {
         var x = 0;
         var y = 1;
@@ -14,6 +16,8 @@ public class Class
         var z2 = x + y;
         var z3 = M0(x);
         var z4 = M0(x);
-        var z5 = MUnit();
+        var z5 = M0(y);
+        var z6 = MUnit();
+        var z7 = M1(x, y) + M1(x, y);
     }
 }
