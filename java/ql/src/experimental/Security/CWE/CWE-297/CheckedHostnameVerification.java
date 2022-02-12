651e43dee6f73da60dad4c281cf187e7d5a94a1a
Artem Smotrakov <artem.smotrakov@gmail.com>
2022-02-12 12:24:48 +0000

    Clarify what verifier is

--- java/ql/src/experimental/Security/CWE/CWE-297/CheckedHostnameVerification.java
+++ java/ql/src/experimental/Security/CWE/CWE-297/CheckedHostnameVerification.java
@@ -1,8 +1,10 @@
-SSLSocket socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket(host, port);
-socket.startHandshake();
-boolean successful = verifier.verify(host, socket.getSession());
-if (!successful) {
-    socket.close();
-    throw new SSLException("Oops! Hostname verification failed!");
+public SSLSocket connect(String host, int port, HostnameVerifier verifier) {
+    SSLSocket socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket(host, port);
+    socket.startHandshake();
+    boolean successful = verifier.verify(host, socket.getSession());
+    if (!successful) {
+        socket.close();
+        throw new SSLException("Oops! Hostname verification failed!");
+    }
+    return socket;
 }
-return socket;
--- java/ql/src/experimental/Security/CWE/CWE-297/IgnoredHostnameVerification.java
+++ java/ql/src/experimental/Security/CWE/CWE-297/IgnoredHostnameVerification.java
@@ -1,4 +1,6 @@
-SSLSocket socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket(host, port);
-socket.startHandshake();
-verifier.verify(host, socket.getSession());
-return socket;
+public SSLSocket connect(String host, int port, HostnameVerifier verifier) {
+    SSLSocket socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket(host, port);
+    socket.startHandshake();
+    verifier.verify(host, socket.getSession());
+    return socket;
+}
