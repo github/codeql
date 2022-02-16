17450a5b274b4cecc62c45d10fe2ed581b551afc
Nick Rolfe <nickrolfe@github.com>
2022-02-16 12:21:52 +0000

    Python/Ruby: rm old prepare-db-upgrade.sh files

--- ruby/scripts/prepare-db-upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Prepare the upgrade script directory for a Ruby database schema upgrade. Now
-# just forwards to the language-independent script.
-
-set -eu
-app_dir="$(dirname "$0")"
-"${app_dir}/../../misc/scripts/prepare-db-upgrade.sh" --lang ruby "$@"
