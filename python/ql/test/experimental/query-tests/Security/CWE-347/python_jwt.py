7c108c78929bd27a8604aaa408b9f98c2cac2150
jorgectf <jorgectf@protonmail.com>
2022-02-22 20:57:20 +0100

    Polish test

--- python/ql/test/experimental/query-tests/Security/CWE-347/python_jwt.py
+++ python/ql/test/experimental/query-tests/Security/CWE-347/python_jwt.py
@@ -2,11 +2,13 @@ import python_jwt
 
 # GOOD
 
-def good1(token):
+
+def good(token):
     python_jwt.process_jwt(token)
     python_jwt.verify_jwt(token, "key", "HS256")
 
 # BAD
+
 
 def bad(token):
     python_jwt.process_jwt(token)
