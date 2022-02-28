65b1de4a3f24687f29b968d06f312038de5ea12a
Rasmus Wriedt Larsen <rasmuswl@github.com>
2022-02-28 11:23:27 +0100

    Test: set `--max-import-depth=100`

--- python/ql/test/library-tests/PointsTo/regressions/wrong/module-imports/conflict-stdlib/code-invalid-package-name/test_fail.py
+++ python/ql/test/library-tests/PointsTo/regressions/wrong/module-imports/conflict-stdlib/code-invalid-package-name/test_fail.py
@@ -1,3 +1,3 @@
 # we import `pdb` which import the `cmd` module from the standard library
-# and allows us to set --max-import-depth=2, to make the test run fast
+# and allows us to set --max-import-depth=100, to make the test run fast
 import pdb
--- python/ql/test/library-tests/frameworks/django-v2-v3/dummy_import.py
+++ python/ql/test/library-tests/frameworks/django-v2-v3/dummy_import.py
@@ -1,4 +1,4 @@
-# to force extractor to see files. since we use `--max-import-depth=1`, we use this
+# to force extractor to see files. since we use `--max-import-depth=100`, we use this
 # "fake" import that doesn't actually work, but tricks the python extractor to look at
 # all the files
 
