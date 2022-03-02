80be767a7ae7c88f83f6e08fd0832b7d6cfa5538
Rasmus Lerchedahl Petersen <yoff@github.com>
2022-03-02 12:59:34 +0100

    python: implement stdlib xpath support

--- python/ql/test/library-tests/frameworks/stdlib/XPathExecution.py
+++ python/ql/test/library-tests/frameworks/stdlib/XPathExecution.py
@@ -5,14 +5,14 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('country_data.xml')
 root = tree.getroot()
 
-root.find(match, namespaces=ns)  # $ MISSING: getXPath=match
-root.findall(match, namespaces=ns)  # $ MISSING: getXPath=match
-root.findtext(match, default=None, namespaces=ns)  # $ MISSING: getXPath=match
+root.find(match, namespaces=ns)  # $ getXPath=match
+root.findall(match, namespaces=ns)  # $ getXPath=match
+root.findtext(match, default=None, namespaces=ns)  # $ getXPath=match
 
 from xml.etree.ElementTree import ElementTree
 tree = ElementTree()
 tree.parse("index.xhtml")
 
-tree.find(match, namespaces=ns)  # $ MISSING: getXPath=match
-tree.findall(match, namespaces=ns)  # $ MISSING: getXPath=match
-tree.findtext(match, default=None, namespaces=ns)  # $ MISSING: getXPath=match
+tree.find(match, namespaces=ns)  # $ getXPath=match
+tree.findall(match, namespaces=ns)  # $ getXPath=match
+tree.findtext(match, default=None, namespaces=ns)  # $ getXPath=match
