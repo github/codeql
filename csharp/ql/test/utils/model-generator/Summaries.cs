6fa434a13cfcd7c9ea2b05f25d653b2a1089738f
Michael Nebel <michaelnebel@github.com>
2022-03-03 10:46:46 +0100

    C#: Add initial test.

---
+++ csharp/ql/test/utils/model-generator/Summaries.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Summaries
+{
+    public class BasicFlow
+    {
+
+        public BasicFlow ReturnsThis(object input)
+        {
+            return this;
+        }
+
+        public string ReturnsInput(string input)
+        {
+            return input;
+        }
+
+        public string ReturnParam0(string input0, object input1)
+        {
+            return input0;
+        }
+
+        public object ReturnParam1(string input0, object input1)
+        {
+            return input1;
+        }
+
+        public object ReturnParamMultiple(object input0, object input1)
+        {
+            return (System.DateTime.Now.DayOfWeek == System.DayOfWeek.Monday) ? input0 : input1;
+        }
+
+        // TODO: Flow is missing
+        public int ReturnArrayElement(int[] input)
+        {
+            return input[0];
+        }
+
+        // TODO: Flow is missing.
+        public void AddToArray(int data, int[] target)
+        {
+            target[0] = data;
+        }
+    }
+}
