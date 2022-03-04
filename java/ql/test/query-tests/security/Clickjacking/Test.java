9a42f22b362aeb08d1d28a135cc4f56f22bb2206
Ahmed Farid <53880570+ahmed532009@users.noreply.github.com>
2022-03-04 13:40:39 +0100

    Create Test.java

---
+++ java/ql/test/query-tests/security/Clickjacking/Test.java
@@ -0,0 +1,14 @@
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletResponse;
+
+public class Test {
+
+   public void safeXframeOption(HttpServletResponse response) {
+        response.addHeader("X-Frame-Options", "DENY");  //GOOD
+    }
+
+    public void unsafeXframeOption(HttpServletResponse res) {
+        res.setHeader("X-Frame-Options", "SAMEORIGIN"); //BAD 
+    }
+
+}
