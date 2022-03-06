35a1c80ceb1475df06c13edcb6507ded25e12774
Ahmed Farid <53880570+ahmed532009@users.noreply.github.com>
2022-03-07 00:24:45 +0100

    Update zipslip_bad.py

--- python/ql/test/experimental/query-tests/Security/CWE-022/zipslip_bad.py
+++ python/ql/test/experimental/query-tests/Security/CWE-022/zipslip_bad.py
@@ -2,15 +2,12 @@ import zipfile
 import shutil
 
 def unzip(filename):
-
    with zipfile.ZipFile(filename) as zipf:
     #BAD : This could write any file on the filesystem.
       for entry in zipf:
           shutil.copy(entry, "/tmp/unpack/")
           
 def unzip1(filename):
-
-
    with zipfile.ZipFile(filename) as zipf:
       for entry in zipf:
          with open(entry, 'wb') as dstfile:
