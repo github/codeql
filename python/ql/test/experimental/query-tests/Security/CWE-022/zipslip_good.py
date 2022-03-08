23bd53a325f568bdbfdedae2bccf3e6811daf7b2
Ahmed Farid <53880570+ahmed532009@users.noreply.github.com>
2022-03-08 23:55:17 +0100

    Update zipslip_good.py

--- python/ql/test/experimental/query-tests/Security/CWE-022/zipslip_good.py
+++ python/ql/test/experimental/query-tests/Security/CWE-022/zipslip_good.py
@@ -1,4 +1,6 @@
 import zipfile 
+import tarfile
+import shutil
 
 def unzip(filename, dir):
   zf = zipfile.ZipFile(filename)
@@ -8,3 +10,11 @@ def unzip(filename, dir):
 def unzip1(filename, dir):
   zf = zipfile.ZipFile(filename)
   zf.extract(dir)
+  
+def unzip2(filename):
+  with tarfile.open(filename) as tar:
+    for entry in tar:
+        #GOOD: Check that entry is safe
+        if os.path.isabs(entry.name) or ".." in entry.name:
+            raise ValueError("Illegal tar archive entry")
+        shutil.copy(entry, "/tmp/unpack/")
