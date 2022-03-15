495c3cc36998e64b818b880a190ac23625f575e2
Ahmed Farid <53880570+ahmed-farid-dev@users.noreply.github.com>
2022-03-15 10:27:30 +0100

    Update Test.java

--- java/ql/src/experimental/Security/CWE/CWE-451/Test.java
+++ java/ql/src/experimental/Security/CWE/CWE-451/Test.java
@@ -6,9 +6,4 @@ public class Test {
    public void safeXframeOption(HttpServletResponse response) {
         response.addHeader("X-Frame-Options", "DENY");  //GOOD
     }
-
-    public void unsafeXframeOption(HttpServletResponse res) {
-        res.setHeader("X-Frame-Options", "SAMEORIGIN"); //BAD 
-    }
-
 }
