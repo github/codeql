92d748e0065c1b95f7181b3671e47136c7c2ea8e
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2022-03-15 19:57:24 +0000

    C++: Fix ODR/dbcheck issue in test.

--- cpp/ql/test/query-tests/Security/CWE/CWE-497/semmle/tests/tests.cpp
+++ cpp/ql/test/query-tests/Security/CWE/CWE-497/semmle/tests/tests.cpp
@@ -2,7 +2,9 @@
 
 // library functions etc
 
-typedef unsigned long size_t;
+#include "tests.h"
+
+
 typedef struct {} FILE;
 FILE *stdout;
 
@@ -13,29 +15,27 @@ int snprintf(char *s, size_t n, const char *format, ...);
 size_t strlen(const char *s);
 char *getenv(const char *name);
 
-namespace std
-{
-	typedef size_t streamsize;
-
-	template<class charT> struct char_traits;
-
-	template <class charT, class traits = char_traits<charT> >
-	class basic_ostream /*: virtual public basic_ios<charT,traits> - not needed for this test */ {
-	public:
-		typedef charT char_type;
-		basic_ostream<charT,traits>& write(const char_type* s, streamsize n);
-
-		basic_ostream<charT, traits>& operator<<(int n);
-	};
-	template<class charT, class traits> basic_ostream<charT,traits>& operator<<(basic_ostream<charT,traits>&, const charT*);
-
-	typedef basic_ostream<char> ostream;
-
-	extern ostream cout;
-	extern ostream cerr;
-	extern ostream clog;
-}
 extern std::ostream someotherostream;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #define NULL (0)
 
--- cpp/ql/test/query-tests/Security/CWE/CWE-497/semmle/tests/tests2.cpp
+++ cpp/ql/test/query-tests/Security/CWE/CWE-497/semmle/tests/tests2.cpp
@@ -2,24 +2,24 @@
 
 // library functions etc
 
+#include "tests.h"
+
 char *getenv(const char *name);
 char *strcpy(char *s1, const char *s2);
 
-namespace std
-{
-	template<class charT> struct char_traits;
 
-	template <class charT, class traits = char_traits<charT> >
-	class basic_ostream /*: virtual public basic_ios<charT,traits> - not needed for this test */ {
-	public: 
-	};
 
-	template<class charT, class traits> basic_ostream<charT,traits>& operator<<(basic_ostream<charT,traits>&, const charT*);
 
-	typedef basic_ostream<char> ostream;
 
-	extern ostream cout;
-}
+
+
+
+
+
+
+
+
+
 
 int socket(int p1, int p2, int p3);
 void send(int sock, const char *buffer, int p3, int p4);
