8dea7248ea14f6d94471c66766373aaa02b3e612
Ahmed Farid <53880570+ahmed-farid-dev@users.noreply.github.com>
2022-03-24 00:34:52 +0100

    Update zipslip_bad.py

--- python/ql/test/experimental/query-tests/Security/CWE-022/zipslip_bad.py
+++ python/ql/test/experimental/query-tests/Security/CWE-022/zipslip_bad.py
@@ -5,34 +5,33 @@ import gzip
 import zipfile
 
 def unzip(filename):
-
-   with tarfile.open(filename) as zipf:
+    with tarfile.open(filename) as zipf:
     #BAD : This could write any file on the filesystem.
-      for entry in zipf:
-          shutil.move(entry, "/tmp/unpack/")
+        for entry in zipf:
+            shutil.move(entry, "/tmp/unpack/")
           
 def unzip1(filename):
-   with gzip.open(filename) as zipf:
+    with gzip.open(filename) as zipf:
     #BAD : This could write any file on the filesystem.
-      for entry in zipf:
-          shutil.copy2(entry, "/tmp/unpack/")
+        for entry in zipf:
+            shutil.copy2(entry, "/tmp/unpack/")
           
 def unzip2(filename):
-   with bz2.open(filename) as zipf:
+    with bz2.open(filename) as zipf:
     #BAD : This could write any file on the filesystem.
-      for entry in zipf:
-          shutil.copyfile(entry, "/tmp/unpack/")
+        for entry in zipf:
+            shutil.copyfile(entry, "/tmp/unpack/")
           
 def unzip3(filename):
-   with zipfile.ZipFile(filename) as zipf:
+    with zipfile.ZipFile(filename) as zipf:
     #BAD : This could write any file on the filesystem.
-      for entry in zipf:
-          shutil.copy(entry, "/tmp/unpack/")
+        for entry in zipf:
+            shutil.copy(entry, "/tmp/unpack/")
 
 def unzip4(filename):
-   with zipfile.ZipFile(filename) as zipf:
-      for entry in zipf:
-         with open(entry, 'wb') as dstfile:
-            shutil.copyfileobj(zipf, dstfile)
+    with zipfile.ZipFile(filename) as zipf:
+        for entry in zipf:
+            with open(entry, 'wb') as dstfile:
+                shutil.copyfileobj(zipf, dstfile)
           
 
