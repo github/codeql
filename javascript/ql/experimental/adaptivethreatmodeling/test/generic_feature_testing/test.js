7c47e717ebde8c72c26b2fbbdf6a5539045d4906
Esben Sparre Andreasen <esbena@github.com>
2022-03-30 23:10:14 +0200

    improve feature tests with more cases

--- javascript/ql/experimental/adaptivethreatmodeling/test/generic_feature_testing/test.js
+++ javascript/ql/experimental/adaptivethreatmodeling/test/generic_feature_testing/test.js
@@ -1,9 +1,14 @@
-(function(){
+(async function(){
     f(endpoint);
-    f({p: endpoint})
-    f({p: {q: endpoint}})
+    f({p: endpoint});
+    f({p: {q: endpoint}});
     o.m(endpoint);
-    o.m({p: endpoint})
-    o.m({p: {q: endpoint}})
-
+    o.m({p: endpoint});
+    o.m({p: {q: endpoint}});
+    new F(endpoint);
+    o.m().m().m(endpoint);
+    f()(endpoint);
+    o[x].m(endpoint);
+    o.m[x].p.m(endpoint);
+    (await p)(endpoint);
 });
