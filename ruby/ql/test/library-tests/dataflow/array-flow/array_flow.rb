a5040fd0ce4dbca07f39b55714b9ffb1c94383f5
Tom Hvitved <hvitved@github.com>
2022-04-01 16:26:24 +0200

    Ruby: Add data-flow test for reverse array stores

--- ruby/ql/test/library-tests/dataflow/array-flow/array_flow.rb
+++ ruby/ql/test/library-tests/dataflow/array-flow/array_flow.rb
@@ -1604,3 +1604,10 @@ def m135
     sink c[1] # $ hasValueFlow=135.1 $ hasValueFlow=135.2
     sink c[2] # $ hasValueFlow=135.1 $ hasValueFlow=135.2
 end
+
+def m136(i)
+    a = [[0]]
+    a[i][0] = source(136.1)
+    sink(a[0][0]) # $ hasValueFlow=136.1
+    sink(a[0][1])
+end
