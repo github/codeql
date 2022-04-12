5219ead4d4f4eadfe966a0b74b2974cb48936333
Chris Smowton <smowton@github.com>
2022-04-12 22:25:51 +0100

    Plugin version selection: fix test polarity and use integer not string comparison
    (Otherwise we'll think that 1.6.10 comes before 1.6.9, for example.) This now implements the desired test: pick a version that exactly matches major and minor versions and which is the least patchlevel that is >= the target compiler.

--- java/kotlin-extractor/kotlin_plugin_versions.py
+++ java/kotlin-extractor/kotlin_plugin_versions.py
@@ -11,29 +11,35 @@ def is_windows():
         return True
     return False
 
+def version_tuple_to_string(version):
+    return f'{version[0]}.{version[1]}.{version[2]}'
+
+def version_string_to_tuple(version):
+    m = re.match(r'([0-9]+)\.([0-9]+)\.([0-9]+)', version)
+    return tuple([int(m.group(i)) for i in range(1, 4)])
+
 many_versions = [ '1.4.32', '1.5.31', '1.6.10', '1.6.20' ]
 
-def get_single_version():
+many_versions_tuples = [version_string_to_tuple(v) for v in many_versions]
+
+def get_single_version(fakeVersionOutput = None):
     # TODO: `shell=True` is a workaround to get CI working on Windows. It breaks the build on Linux.
-    versionOutput = subprocess.run(['kotlinc', '-version'], capture_output=True, text=True, shell=is_windows())
-    m = re.match(r'.* kotlinc-jvm ([0-9]+)\.([0-9]+)\.([0-9]+) .*', versionOutput.stderr)
+    versionOutput = subprocess.run(['kotlinc', '-version'], capture_output=True, text=True, shell=is_windows()).stderr if fakeVersionOutput is None else fakeVersionOutput
+    m = re.match(r'.* kotlinc-jvm ([0-9]+\.[0-9]+\.[0-9]+) .*', versionOutput)
     if m is None:
         raise Exception('Cannot detect version of kotlinc (got ' + str(versionOutput) + ')')
-    major = m.group(1)
-    minor = m.group(2)
-    patch = m.group(3)
-    current_version = f'{major}.{minor}.{patch}'
-    matching_minor_versions = [ version for version in many_versions if version.startswith(f'{major}.{minor}') ]
+    current_version = version_string_to_tuple(m.group(1))
+    matching_minor_versions = [ version for version in many_versions_tuples if version[0:2] == current_version[0:2] ]
     if len(matching_minor_versions) == 0:
         raise Exception(f'Cannot find a matching minor version for kotlinc version {current_version} (got {versionOutput}; know about {str(many_versions)})')
 
     matching_minor_versions.sort()
 
     for version in matching_minor_versions:
-        if current_version >= version:
-            return version
+        if version >= current_version:
+            return version_tuple_to_string(version)
 
-    return matching_minor_versions[-1]
+    return version_tuple_to_string(matching_minor_versions[-1])
 
     raise Exception(f'No suitable kotlinc version found for {current_version} (got {versionOutput}; know about {str(many_versions)})')
 
@@ -44,11 +50,13 @@ def get_latest_url():
 
 if __name__ == "__main__":
     args = sys.argv
-    if len(args) != 2:
+    if len(args) < 2:
         raise Exception("Bad arguments")
     command = args[1]
     if command == 'latest-url':
         print(get_latest_url())
+    elif command == 'single-version':
+        print(get_single_version(*args[2:]))
     else:
         raise Exception("Unknown command: " + command)
 
