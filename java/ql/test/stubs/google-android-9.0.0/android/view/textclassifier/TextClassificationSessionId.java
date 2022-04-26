2ee83e2ba2ed320544e282fecc676c52b4c8e137
Tony Torralba <atorralba@users.noreply.github.com>
2022-04-26 11:00:41 +0200

    Add Editable.toString flow step

---
+++ java/ql/test/stubs/google-android-9.0.0/android/view/textclassifier/TextClassificationSessionId.java
@@ -0,0 +1,17 @@
+// Generated automatically from android.view.textclassifier.TextClassificationSessionId for testing purposes
+
+package android.view.textclassifier;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class TextClassificationSessionId implements Parcelable
+{
+    public String getValue(){ return null; }
+    public String toString(){ return null; }
+    public boolean equals(Object p0){ return false; }
+    public int describeContents(){ return 0; }
+    public int hashCode(){ return 0; }
+    public static Parcelable.Creator<TextClassificationSessionId> CREATOR = null;
+    public void writeToParcel(Parcel p0, int p1){}
+}
