dd258781ed9c4eaa6dd452a913cdc640a9aad470
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2022-04-28 18:14:39 +0100

    C++: More test cases.

--- cpp/ql/test/query-tests/Security/CWE/CWE-611/tests5.cpp
+++ cpp/ql/test/query-tests/Security/CWE/CWE-611/tests5.cpp
@@ -36,19 +36,42 @@ void test5_2(DOMImplementationLS *impl, InputSource &data) {
 	p->parse(data); // GOOD
 }
 
+void test5_3(DOMImplementationLS *impl, InputSource &data) {
+	DOMLSParser *p = impl->createLSParser();
+
+	p->getDomConfig()->setParameter(XMLUni::fgXercesDisableDefaultEntityResolution, false);
+	p->parse(data); // BAD (parser not correctly configured) [NOT DETECTED]
+}
+
+void test5_4(DOMImplementationLS *impl, InputSource &data) {
+	DOMLSParser *p = impl->createLSParser();
+	DOMConfiguration *cfg = p->getDomConfig();
+
+	cfg->setParameter(XMLUni::fgXercesDisableDefaultEntityResolution, true);
+	p->parse(data); // GOOD
+}
+
+void test5_5(DOMImplementationLS *impl, InputSource &data) {
+	DOMLSParser *p = impl->createLSParser();
+	DOMConfiguration *cfg = p->getDomConfig();
+
+	cfg->setParameter(XMLUni::fgXercesDisableDefaultEntityResolution, false);
+	p->parse(data); // BAD (parser not correctly configured) [NOT DETECTED]
+}
+
 DOMImplementationLS *g_impl;
 DOMLSParser *g_p1, *g_p2;
 InputSource *g_data;
 
-void test5_3_init() {
+void test5_6_init() {
 	g_p1 = g_impl->createLSParser();
 	g_p1->getDomConfig()->setParameter(XMLUni::fgXercesDisableDefaultEntityResolution, true);
 
 	g_p2 = g_impl->createLSParser();
 }
 
-void test5_3() {
-	test5_3_init();
+void test5_6() {
+	test5_6_init();
 
 	g_p1->parse(*g_data); // GOOD
 	g_p2->parse(*g_data); // BAD (parser not correctly configured) [NOT DETECTED]
