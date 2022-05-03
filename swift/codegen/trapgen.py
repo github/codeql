10c5c8e71f6c61acac4192f25a7d6b66ab51a443
Paolo Tranquilli <redsun82@github.com>
2022-05-03 17:33:25 +0200

    Swift: add `trapgen` unit tests
    Closes: https://github.com/github/codeql-c-team/issues/981

--- swift/codegen/trapgen.py
+++ swift/codegen/trapgen.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-import collections
 import logging
 import os
 import re
 import sys
 
 import inflection
+from toposort import toposort_flatten
 
 sys.path.append(os.path.dirname(__file__))
 
-from lib import paths, dbscheme, generator, cpp
+from swift.codegen.lib import paths, dbscheme, generator, cpp
 
 field_overrides = [
     (re.compile(r"locations.*::(start|end).*|.*::(index|num_.*)"), {"type": "unsigned"}),
@@ -76,54 +76,26 @@ def get_trap(t: dbscheme.Table):
     )
 
 
-def get_guard(path):
-    path = path.relative_to(paths.swift_dir)
-    return str(path.with_suffix("")).replace("/", "_").upper()
-
-
-def get_topologically_ordered_tags(tags):
-    degree_to_nodes = collections.defaultdict(set)
-    nodes_to_degree = {}
-    lookup = {}
-    for name, t in tags.items():
-        degree = len(t["bases"])
-        degree_to_nodes[degree].add(name)
-        nodes_to_degree[name] = degree
-    while degree_to_nodes[0]:
-        sinks = degree_to_nodes.pop(0)
-        for sink in sorted(sinks):
-            yield sink
-            for d in tags[sink]["derived"]:
-                degree = nodes_to_degree[d]
-                degree_to_nodes[degree].remove(d)
-                degree -= 1
-                nodes_to_degree[d] = degree
-                degree_to_nodes[degree].add(d)
-    if any(degree_to_nodes.values()):
-        raise ValueError("not a dag!")
-
-
 def generate(opts, renderer):
-    tag_graph = collections.defaultdict(lambda: {"bases": [], "derived": []})
+    tag_graph = {}
     out = opts.trap_output
 
     traps = []
-    with open(opts.dbscheme) as input:
-        for e in dbscheme.iterload(input):
-            if e.is_table:
-                traps.append(get_trap(e))
-            elif e.is_union:
-                for d in e.rhs:
-                    tag_graph[e.lhs]["derived"].append(d.type)
-                    tag_graph[d.type]["bases"].append(e.lhs)
+    for e in dbscheme.iterload(opts.dbscheme):
+        if e.is_table:
+            traps.append(get_trap(e))
+        elif e.is_union:
+            tag_graph.setdefault(e.lhs, set())
+            for d in e.rhs:
+                tag_graph.setdefault(d.type, set()).add(e.lhs)
 
     renderer.render(cpp.TrapList(traps), out / "TrapEntries.h")
 
     tags = []
-    for index, tag in enumerate(get_topologically_ordered_tags(tag_graph)):
+    for index, tag in enumerate(toposort_flatten(tag_graph)):
         tags.append(cpp.Tag(
             name=get_tag_name(tag),
-            bases=[get_tag_name(b) for b in sorted(tag_graph[tag]["bases"])],
+            bases=[get_tag_name(b) for b in sorted(tag_graph[tag])],
             index=index,
             id=tag,
         ))
