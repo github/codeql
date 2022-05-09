20317a280b8403b0169678a44f56714386846a88
Paolo Tranquilli <redsun82@github.com>
2022-05-09 10:04:18 +0200

    Swift: make `width` fields `unsigned`

--- swift/codegen/test/test_cppgen.py
+++ swift/codegen/test/test_cppgen.py
@@ -36,8 +36,8 @@ def test_empty_class(generate):
     assert generate([
         schema.Class(name="MyClass"),
     ]) == [
-        cpp.Class(name="MyClass", final=True, trap_name="MyClasses")
-    ]
+               cpp.Class(name="MyClass", final=True, trap_name="MyClasses")
+           ]
 
 
 def test_two_class_hierarchy(generate):
@@ -46,9 +46,9 @@ def test_two_class_hierarchy(generate):
         schema.Class(name="A", derived={"B"}),
         schema.Class(name="B", bases={"A"}),
     ]) == [
-        base,
-        cpp.Class(name="B", bases=[base], final=True, trap_name="Bs"),
-    ]
+               base,
+               cpp.Class(name="B", bases=[base], final=True, trap_name="Bs"),
+           ]
 
 
 def test_complex_hierarchy_topologically_ordered(generate):
@@ -84,37 +84,38 @@ def test_class_with_field(generate, type, expected, property_cls, optional, repe
     assert generate([
         schema.Class(name="MyClass", properties=[property_cls("prop", type)]),
     ]) == [
-        cpp.Class(name="MyClass",
-                  fields=[cpp.Field("prop", expected, is_optional=optional,
-                                    is_repeated=repeated, trap_name=trap_name)],
-                  trap_name="MyClasses",
-                  final=True)
-    ]
+               cpp.Class(name="MyClass",
+                         fields=[cpp.Field("prop", expected, is_optional=optional,
+                                           is_repeated=repeated, trap_name=trap_name)],
+                         trap_name="MyClasses",
+                         final=True)
+           ]
 
 
-@pytest.mark.parametrize("name", ["start_line", "start_column", "end_line", "end_column", "index", "num_whatever"])
+@pytest.mark.parametrize("name",
+                         ["start_line", "start_column", "end_line", "end_column", "index", "num_whatever", "width"])
 def test_class_with_overridden_unsigned_field(generate, name):
     assert generate([
         schema.Class(name="MyClass", properties=[
-                     schema.SingleProperty(name, "bar")]),
+            schema.SingleProperty(name, "bar")]),
     ]) == [
-        cpp.Class(name="MyClass",
-                  fields=[cpp.Field(name, "unsigned")],
-                  trap_name="MyClasses",
-                  final=True)
-    ]
+               cpp.Class(name="MyClass",
+                         fields=[cpp.Field(name, "unsigned")],
+                         trap_name="MyClasses",
+                         final=True)
+           ]
 
 
 def test_class_with_overridden_underscore_field(generate):
     assert generate([
         schema.Class(name="MyClass", properties=[
-                     schema.SingleProperty("something_", "bar")]),
+            schema.SingleProperty("something_", "bar")]),
     ]) == [
-        cpp.Class(name="MyClass",
-                  fields=[cpp.Field("something", "bar")],
-                  trap_name="MyClasses",
-                  final=True)
-    ]
+               cpp.Class(name="MyClass",
+                         fields=[cpp.Field("something", "bar")],
+                         trap_name="MyClasses",
+                         final=True)
+           ]
 
 
 @pytest.mark.parametrize("name", cpp.cpp_keywords)
@@ -123,11 +124,11 @@ def test_class_with_keyword_field(generate, name):
         schema.Class(name="MyClass", properties=[
             schema.SingleProperty(name, "bar")]),
     ]) == [
-        cpp.Class(name="MyClass",
-                  fields=[cpp.Field(name + "_", "bar")],
-                  trap_name="MyClasses",
-                  final=True)
-    ]
+               cpp.Class(name="MyClass",
+                         fields=[cpp.Field(name + "_", "bar")],
+                         trap_name="MyClasses",
+                         final=True)
+           ]
 
 
 if __name__ == '__main__':
