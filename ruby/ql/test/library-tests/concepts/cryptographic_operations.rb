6b496c78ef63ef7dbfab12b01051a26550faefad
Alex Ford <alexrford@github.com>
2022-05-17 14:36:54 +0100

    Ruby: failing crypto op test

--- ruby/ql/test/library-tests/concepts/cryptographic_operations.rb
+++ ruby/ql/test/library-tests/concepts/cryptographic_operations.rb
@@ -5,3 +5,7 @@ strong = OpenSSL::Cipher.new 'AES-128-CBC' # $ CryptographicOperation Cryptograp
 weak_algorithm = OpenSSL::Cipher::DES.new # $ CryptographicOperation CryptographicOperationAlgorithm=DES CryptographicOperationBlockMode=CBC
 
 weak_block_mode = OpenSSL::Cipher::AES.new(128, :ecb) # $ CryptographicOperation CryptographicOperationAlgorithm=AES CryptographicOperationBlockMode=ECB
+
+weak_block_mode = OpenSSL::Cipher.new 'bf-ecb' # $ CryptographicOperation CryptographicOperationAlgorithm=BF CryptographicOperationBlockMode=ECB
+
+stream_cipher = OpenSSL::Cipher.new 'chacha' # $ CryptographicOperation CryptographicOperationAlgorithm=CHACHA
