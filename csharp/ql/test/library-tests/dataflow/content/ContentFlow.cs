9cc9991c74f5cb311afc98d95b00dedd7fc6af0f
Tom Hvitved <hvitved@github.com>
2022-05-23 13:03:56 +0200

    C#: Update `ContentDataFlow` test
    Illustrates missing flow when the sink is inside a method that is also part of
    a `subpath`.

--- csharp/ql/test/library-tests/dataflow/content/ContentFlow.cs
+++ csharp/ql/test/library-tests/dataflow/content/ContentFlow.cs
@@ -16,16 +16,36 @@ public class ContentFlow
     public void M(A a, B b)
     {
         var a1 = new A();
-        Sink(a1.FieldA.FieldB);
+        Sink(Through(a1.FieldA.FieldB));
 
         a.FieldA.FieldB = new B();
-        Sink(a);
+        Sink(Through(a));
 
         var a2 = new A();
         b.FieldB.FieldA = a2.FieldB.FieldA;
-        Sink(b);
+        Sink(Through(b));
+
+        Sink(Through(Out()));
+
+        In(new A().FieldA.FieldB);
     }
 
     public static void Sink<T>(T t) { }
 
+    public T Through<T>(T t)
+    {
+        Sink(t);
+        return t;
+    }
+
+    public void In<T>(T t)
+    {
+        Sink(t);
+    }
+
+    public B Out()
+    {
+        var a = new A();
+        return a.FieldA.FieldB;
+    }
 }
