0dd6eef202f6b7cd15a82d7095af3dcc227b71cb
Erik Krogh Kristensen <erik-krogh@github.com>
2022-06-17 08:20:56 +0000

    more command line shenanigans

--- ruby/autobuilder/src/main.rs
+++ ruby/autobuilder/src/main.rs
@@ -31,7 +31,7 @@ fn main() -> std::io::Result<()> {
         if let Some(stripped) = line.strip_prefix("include:") {
             cmd.arg("--also-match=".to_owned() + stripped);
         } else if let Some(stripped) = line.strip_prefix("exclude:") {
-            cmd.arg("--exclude").arg(stripped);
+            cmd.arg("--exclude=".to_owned() + stripped);
         }
     }
     let exit = &cmd.spawn()?.wait()?;
