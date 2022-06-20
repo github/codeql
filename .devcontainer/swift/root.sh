52ee4d4057a9e5c16e4da45d36116e99c6148d07
Paolo Tranquilli <redsun82@github.com>
2022-06-20 11:43:35 +0000

    Swift: add devcontainer setup

---
+++ .devcontainer/swift/root.sh
@@ -0,0 +1,14 @@
+set -xe
+
+apt-get update
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y install --no-install-recommends \
+    zlib1g-dev \
+    uuid-dev \
+    python3-distutils
+
+# Install Bazel
+curl -fSsL -o /usr/local/bin/bazelisk https://github.com/bazelbuild/bazelisk/releases/download/${BAZELISK_VERSION}/bazelisk-linux-amd64
+([ "${BAZELISK_DOWNLOAD_SHA}" = "dev-mode" ] || echo "${BAZELISK_DOWNLOAD_SHA} */usr/local/bin/bazelisk" | sha256sum --check - )
+chmod 0755 /usr/local/bin/bazelisk
+ln -s bazelisk /usr/local/bin/bazel
---
+++ .devcontainer/swift/user.sh
@@ -0,0 +1,18 @@
+set -xe
+
+# add the workspace to the codeql search path
+mkdir -p /home/vscode/.config/codeql
+echo "--search-path /workspaces/codeql" > /home/vscode/.config/codeql/config
+
+# create a swift extractor pack with the current state
+cd /workspaces/codeql
+bazel run swift/create-extractor-pack
+
+cat >> $HOME/.bashrc << EOF
+
+# have the codeql binary installed by vscode automatically in PATH
+bin=\$(ls \$HOME/.vscode-remote/data/User/globalStorage/github.vscode-codeql/*/codeql/codeql -t 2>/dev/null | head -1)
+if [[ -n \$bin ]]; then
+  export PATH=\$PATH:$(dirname "\$bin")
+fi
+EOF
