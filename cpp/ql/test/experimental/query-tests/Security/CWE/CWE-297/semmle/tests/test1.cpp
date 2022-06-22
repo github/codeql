52ade48851c8940bb7c2ff6759df94971a5802a7
ihsinme <ihsinme@gmail.com>
2022-06-22 19:48:16 +0300

    Update test1.cpp

--- cpp/ql/test/experimental/query-tests/Security/CWE/CWE-297/semmle/tests/test1.cpp
+++ cpp/ql/test/experimental/query-tests/Security/CWE/CWE-297/semmle/tests/test1.cpp
@@ -1,23 +1,65 @@
-#define X509_V_OK 0
 #define NULL  0
+struct gnutls_session_t{};
+struct gnutls_x509_crt_t{};
 
-struct SSL {};
-struct X509 {};
+int gnutls_x509_crt_init(gnutls_x509_crt_t * cert);
+int gnutls_x509_crt_check_hostname(gnutls_x509_crt_t cert, const char *hostname);
+int gnutls_x509_crt_check_hostname2(gnutls_x509_crt_t cert,const char *hostname, int flags);
 
-X509 *SSL_get_peer_certificate(const SSL *ssl);
-int SSL_get_verify_result(const SSL *ssl);
-
-
-bool badTest1(SSL *ssl) // BAD :no hostname verification in certificate
-{
-  X509 *cert;
-  int result;
-
-  result = SSL_get_verify_result(ssl);
-  if (result == X509_V_OK)
-  {
-    cert = SSL_get_peer_certificate(ssl);
-    if(cert) return true;
-  }
-  return false;
+int gnutls_certificate_verify_peers(gnutls_session_t session);
+int gnutls_certificate_verify_peers2(gnutls_session_t session, int * status);
+int gnutls_certificate_verify_peers3(gnutls_session_t session, const char * hostname, int * status);
+/*
+int goodTest1(gnutls_session_t session, gnutls_x509_crt_t cert){ // GOOD
+  if(gnutls_certificate_verify_peers(session)<0)
+    return 1;
+  if (gnutls_x509_crt_init (&cert) < 0)
+    return 1;
+  if (!gnutls_x509_crt_check_hostname (cert, "hostname"))
+    return 1;
+  return 0;
+}
+int goodTest2(gnutls_session_t session, gnutls_x509_crt_t cert){ // GOOD
+  int status;
+  if(gnutls_certificate_verify_peers2(session,&status)<0)
+    return 1;
+  if (gnutls_x509_crt_init (&cert) < 0)
+    return 1;
+  if (!gnutls_x509_crt_check_hostname2 (cert, "hostname", status))
+    return 1;
+  return 0;
+}
+int goodTest3(gnutls_session_t session, gnutls_x509_crt_t cert){ // GOOD
+  int status;
+  if(gnutls_certificate_verify_peers3(session,NULL,&status)<0)
+    return 1;
+  if (gnutls_x509_crt_init (&cert) < 0)
+    return 1;
+  if (!gnutls_x509_crt_check_hostname2 (cert, "hostname"))
+    return 1;
+  return 0;
+}
+int goodTest4(gnutls_session_t session){ // GOOD
+  int status;
+  if(gnutls_certificate_verify_peers3(session,"host.name",&status)<0)
+    return 1;
+  return 0;
+}
+*/
+int badTest1(gnutls_session_t session){ // BAD
+  int status;
+  if(gnutls_certificate_verify_peers3(session,NULL,&status)<0)
+    return 1;
+  return 0;
+}
+int badTest2(gnutls_session_t session){ // BAD
+  int status;
+  if(gnutls_certificate_verify_peers2(session,&status)<0)
+    return 1;
+  return 0;
+}
+int badTest3(gnutls_session_t session){ // BAD
+  if(gnutls_certificate_verify_peers(session)<0)
+    return 1;
+  return 0;
 }
