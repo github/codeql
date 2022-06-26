d09de269a0e19d0e918399da99a61f819fbf3f7f
ihsinme <ihsinme@gmail.com>
2022-06-26 17:18:58 +0300

    Update test1.cpp

--- cpp/ql/test/experimental/query-tests/Security/CWE/CWE-297/semmle/tests/test1.cpp
+++ cpp/ql/test/experimental/query-tests/Security/CWE/CWE-297/semmle/tests/test1.cpp
@@ -9,7 +9,6 @@ int gnutls_x509_crt_check_hostname2(gnutls_x509_crt_t cert,const char *hostname,
 int gnutls_certificate_verify_peers(gnutls_session_t session);
 int gnutls_certificate_verify_peers2(gnutls_session_t session, int * status);
 int gnutls_certificate_verify_peers3(gnutls_session_t session, const char * hostname, int * status);
-/*
 int goodTest1(gnutls_session_t session, gnutls_x509_crt_t cert){ // GOOD
   if(gnutls_certificate_verify_peers(session)<0)
     return 1;
@@ -35,7 +34,7 @@ int goodTest3(gnutls_session_t session, gnutls_x509_crt_t cert){ // GOOD
     return 1;
   if (gnutls_x509_crt_init (&cert) < 0)
     return 1;
-  if (!gnutls_x509_crt_check_hostname2 (cert, "hostname"))
+  if (!gnutls_x509_crt_check_hostname2 (cert, "hostname", status))
     return 1;
   return 0;
 }
@@ -45,7 +44,6 @@ int goodTest4(gnutls_session_t session){ // GOOD
     return 1;
   return 0;
 }
-*/
 int badTest1(gnutls_session_t session){ // BAD
   int status;
   if(gnutls_certificate_verify_peers3(session,NULL,&status)<0)
