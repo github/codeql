9d97fe7f307a3f8789228c2e5dc6041ab182ff1e
Paolo Tranquilli <redsun82@github.com>
2022-06-27 17:22:48 +0200

    Swift: generalize EnumIsCaseExpr test

--- swift/ql/test/extractor-tests/generated/expr/EnumIsCaseExpr/enum_is_case.swift
+++ swift/ql/test/extractor-tests/generated/expr/EnumIsCaseExpr/enum_is_case.swift
@@ -1,5 +1,5 @@
 // EnumIsCaseExpr despite its generic nature is actually only generated when an `is` expression passes through an
-// intrinsic Optional check
+// intrinsic Optional check, or an array, dictionary or set downcast
 
 Optional.some(42) is Int
 Optional.some(Optional.some(42)) is Int
@@ -9,6 +9,16 @@ Optional.some(Optional.some(42)) is Int
 [Optional.some(42)] is [Int]
 [42] is [Int?]
 
-class X {}
+class X : Hashable {
+    static func == (lhs: X, rhs: X) -> Bool { return true }
+    func hash(into hasher: inout Hasher) {}
+}
+
 class Y: X {}
+
 Optional.some(Y()) is X
+
+[X()] is [Y]
+["x": X()] is [String: Y]
+["x": X()] is [String: Y]
+Set<X>() is Set<Y>
