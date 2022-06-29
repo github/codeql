7ab8f0262c533e0f3f3cfe7b5cf3f32b7d6484ad
Shyam Mehta <shyam.mehta@hotmail.com>
2022-06-29 18:01:12 -0400

    Fix duplicate class header and better fix using toPath()

--- java/ql/src/Security/CWE/CWE-023/PartialPathTraversalGood.java
+++ java/ql/src/Security/CWE/CWE-023/PartialPathTraversalGood.java
@@ -1,8 +1,8 @@
 import java.io.File;
 
-public class PartialPathTraversalBad {
+public class PartialPathTraversalGood {
     public void esapiExample(File dir, File parent) throws IOException {
-        if (!dir.getCanonicalPath().startsWith(parent.getCanonicalPath() + File.separator)) {
+        if (!dir.getCanonicalPath().startsWith(parent.getCanonicalPath().toPath())) {
             throw new IOException("Invalid directory: " + dir.getCanonicalPath());
         }
     }
