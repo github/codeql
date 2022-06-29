488befb57760b7b6caf493c9021d45facd950c2a
Alex Denisov <alexdenisov@github.com>
2022-06-29 07:16:59 +0200

    Swift: store TRAP files in a temporary folder until the extraction is complete
    Currently, we have a number of assertions in the codebase and certain
    assumptions about the AST. These don't always hold, sometimes leading to
    a crash in the extractor.
    The crashes leave incomplete TRAP files that cannot be imported into the
    database.
    
    With this change, we still get those incomplete TRAP files, but we also
    get a database in the end (even thoough it is also incomplete as we
    cannot import everything).

--- swift/extractor/SwiftExtractorConfiguration.h
+++ swift/extractor/SwiftExtractorConfiguration.h
@@ -9,7 +9,14 @@ struct SwiftExtractorConfiguration {
   std::string trapDir;
   // The location for storing extracted source files.
   std::string sourceArchiveDir;
-  // The arguments passed to the extractor. Used for debugging.
+  // A temporary directory that exists during database creation, but is deleted once the DB is
+  // finalized.
+  std::string scratchDir;
+  // A temporary directory that contains TRAP files before they moved into the final destination.
+  // Subdirectory of the scratchDir.
+  std::string tempTrapDir;
+
+  // The original arguments passed to the extractor. Used for debugging.
   std::vector<std::string> frontendOptions;
 };
 }  // namespace codeql
