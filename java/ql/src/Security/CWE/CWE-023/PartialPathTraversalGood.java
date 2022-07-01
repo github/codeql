209a21655ade3906cbf0b10f9c43f1c389716cd6
smehta23 <47726705+smehta23@users.noreply.github.com>
2022-07-01 10:40:38 -0400

    Update java/ql/src/Security/CWE/CWE-023/PartialPathTraversalGood.java
    Co-authored-by: Jonathan Leitschuh <jonathan.leitschuh@gmail.com>

--- java/ql/src/Security/CWE/CWE-023/PartialPathTraversalGood.java
+++ java/ql/src/Security/CWE/CWE-023/PartialPathTraversalGood.java
@@ -1,7 +1,7 @@
 import java.io.File;
 
 public class PartialPathTraversalGood {
-    public void esapiExample(File dir, File parent) throws IOException {
+    public void example(File dir, File parent) throws IOException {
         if (!dir.getCanonicalPath().startsWith(parent.getCanonicalPath().toPath())) {
             throw new IOException("Invalid directory: " + dir.getCanonicalPath());
         }
