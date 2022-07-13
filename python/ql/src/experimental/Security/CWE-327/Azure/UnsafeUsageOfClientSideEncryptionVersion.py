f7c47b6c75f79430e75147d224ce5049352be8ee
Raul Garcia <42392023+raulgarciamsft@users.noreply.github.com>
2022-07-13 08:34:48 -0700

    Update python/ql/src/experimental/Security/CWE-327/Azure/UnsafeUsageOfClientSideEncryptionVersion.py
    Co-authored-by: Taus <tausbn@github.com>

--- python/ql/src/experimental/Security/CWE-327/Azure/UnsafeUsageOfClientSideEncryptionVersion.py
+++ python/ql/src/experimental/Security/CWE-327/Azure/UnsafeUsageOfClientSideEncryptionVersion.py
@@ -3,5 +3,5 @@ blob_client.require_encryption = True
 blob_client.key_encryption_key = kek
 # GOOD: Must use `encryption_version` set to `2.0`
 blob_client.encryption_version = '2.0'  # Use Version 2.0!
-with open(“decryptedcontentfile.txt”, “rb”) as stream:
+with open("decryptedcontentfile.txt", "rb") as stream:
     blob_client.upload_blob(stream, overwrite=OVERWRITE_EXISTING)
