068ec8ea20c2be25f70c379c7be6c58b72d935a1
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2022-08-09 13:07:23 +0100

    Swift: More tests of taint flow through Strings.

--- swift/ql/test/library-tests/dataflow/taint/string.swift
+++ swift/ql/test/library-tests/dataflow/taint/string.swift
@@ -20,3 +20,70 @@ func taintThroughInterpolatedStrings() {
   x = 0
   sink(arg: "\(x)") // clean
 }
+
+func source2() -> String { return ""; }
+
+func taintThroughStringConcatenation() {
+  var clean = "abcdef"
+  var tainted = source2()
+
+  sink(arg: clean)
+  sink(arg: tainted) // tainted
+
+  sink(arg: clean + clean)
+  sink(arg: clean + tainted) // tainted [NOT DETECTED]
+  sink(arg: tainted + clean) // tainted [NOT DETECTED]
+  sink(arg: tainted + tainted) // tainted [NOT DETECTED]
+
+  sink(arg: ">" + clean + "<")
+  sink(arg: ">" + tainted + "<") // tainted [NOT DETECTED]
+
+  var str = "abc"
+
+  sink(arg: str)
+
+  str += "def"
+  sink(arg: str)
+
+  str += source2()
+  sink(arg: str) // tainted [NOT DETECTED]
+
+  var str2 = "abc"
+
+  sink(arg: str2)
+
+  str2.append("def")
+  sink(arg: str2)
+
+  str2.append(source2())
+  sink(arg: str2) // tainted [NOT DETECTED]
+
+  var str3 = "abc"
+
+  sink(arg: str3)
+
+  str3.append(contentsOf: "def")
+  sink(arg: str3)
+
+  str3.append(contentsOf: source2())
+  sink(arg: str2) // tainted [NOT DETECTED]
+}
+
+func taintThroughStringOperations() {
+  var clean = ""
+  var tainted = source2()
+  var taintedInt = source()
+
+  sink(arg: String(clean))
+  sink(arg: String(tainted)) // tainted [NOT DETECTED]
+  sink(arg: String(taintedInt)) // tainted [NOT DETECTED]
+
+  sink(arg: String(repeating: clean, count: 2))
+  sink(arg: String(repeating: tainted, count: 2)) // tainted [NOT DETECTED]
+
+  sink(arg: clean.description)
+  sink(arg: tainted.description) // tainted [NOT DETECTED]
+  
+  sink(arg: clean.debugDescription)
+  sink(arg: tainted.debugDescription) // tainted [NOT DETECTED]
+}
