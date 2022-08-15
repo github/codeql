5ecadd06ae8a2e7723e0476027383d823b4cbf5f
Ahmed Farid <farid532009@gmail.com>
2022-08-15 15:21:10 +0100

    Update TimingAttackAgainstHash.py

--- python/ql/test/experimental/query-tests/Security/CWE-208/TimingAttackAgainstHash.py
+++ python/ql/test/experimental/query-tests/Security/CWE-208/TimingAttackAgainstHash.py
@@ -11,10 +11,10 @@ key = "e179017a-62b0-4996-8a38-e91aa9f1"
 def sign(pre_key, msg, alg):
     return hmac.new(pre_key, msg, alg).digest()
 
-def verify1(msg, sig):
+def VerifyGood(msg, sig):
     return constant_time_string_compare(sig, sign(key, msg, hashlib.sha256)) #good
  
-def verify2(msg, sig):
+def verifyBad(msg, sig):
     return sig == sign(key, msg, hashlib.sha256) #bad
 
 def constant_time_string_compare(a, b):
