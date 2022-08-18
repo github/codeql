fb9a34851a75ab880e61ebc3415a63fcff86bf8f
Tamas Vajk <tamasvajk@github.com>
2022-08-18 09:01:10 +0200

    Apply code review changes

--- java/kotlin-extractor/src/main/kotlin/utils/versions/v_1_4_32/allOverriddenIncludingSelf.kt
+++ java/kotlin-extractor/src/main/kotlin/utils/versions/v_1_4_32/allOverriddenIncludingSelf.kt
@@ -3,4 +3,4 @@ package com.github.codeql.utils.versions
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.backend.common.ir.allOverridden
 
-fun IrSimpleFunction.allOverridden(includeSelf: Boolean = false) = this.allOverridden(includeSelf)
+fun IrSimpleFunction.allOverriddenIncludingSelf() = this.allOverridden(includeSelf = true)
