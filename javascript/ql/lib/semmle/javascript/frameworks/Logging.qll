erik-semmle/ql

deprecate `RequestExpr` and `ResponseExpr` and use `ResponseNode` and `RequestNode` instead

javascript/ql/lib/semmle/javascript/frameworks/Logging.qll
@@ -351,8 +351,8 @@ private module Pino {
     or
     // `pino` is installed as the "log" property on the request object in `Express` and similar libraries.
     // in `Hapi` the property is "logger".
-    exists(HTTP::RequestExpr req, API::Node reqNode |
-      reqNode.getAnImmediateUse() = req.flow().getALocalSource() and
+    exists(HTTP::RequestNode req, API::Node reqNode |
+      reqNode.getAnImmediateUse() = req.getALocalSource() and
       result = reqNode.getMember(["log", "logger"])
     )
   }

github/codeql

javascript/ql/lib/semmle/javascript/frameworks/Logging.qll
@@ -349,8 +349,8 @@ private module Pino {
     or
     // `pino` is installed as the "log" property on the request object in `Express` and similar libraries.
     // in `Hapi` the property is "logger".
-    exists(HTTP::RequestExpr req, API::Node reqNode |
-      reqNode.asSource() = req.flow().getALocalSource() and
+    exists(HTTP::RequestNode req, API::Node reqNode |
+      reqNode.asSource() = req.getALocalSource() and
       result = reqNode.getMember(["log", "logger"])
     )
   }