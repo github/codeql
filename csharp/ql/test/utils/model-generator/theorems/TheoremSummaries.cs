e66dce152444b8ba588881b403e7512b8b84cc04
Michael Nebel <michaelnebel@github.com>
2022-09-06 11:22:00 +0200

    C#: Update test relevant test cases.

--- csharp/ql/test/utils/model-generator/theorems/TheoremSummaries.cs
+++ csharp/ql/test/utils/model-generator/theorems/TheoremSummaries.cs
@@ -20,8 +20,6 @@ public class Theorems1<T> {
 
     public T Id(T x) { throw null; }
 
-    public T Transform(int x, T y) { throw null; }
-
     public S Transform<S>(S x) { throw null; }
 
     public void Set(T x) { throw null; }
@@ -32,13 +30,13 @@ public class Theorems1<T> {
     public void Set<S>(S x) { throw null; }
 
     public int Apply(Func<T, int> f) { throw null; }
+
+    public IList<T> GetMany() { throw null; }
+
+    public void AddMany(IEnumerable<T> xs) { throw null; }
 }
 
-public class Theorems2 {
-
-    public T Transform<T>(T x) { throw null; }
-}
-
+// It is assumed that this is a collection with elements of type T.
 public class CollectionTheorems1<T> : IEnumerable<T> {
     IEnumerator<T> IEnumerable<T>.GetEnumerator() { throw null; }
     IEnumerator IEnumerable.GetEnumerator() { throw null; }
@@ -46,4 +44,17 @@ public class CollectionTheorems1<T> : IEnumerable<T> {
     public T First() { throw null; }
 
     public void Add(T x) { throw null; }
+
+    public ICollection<T> GetMany() { throw null; }
+
+    public void AddMany(IEnumerable<T> x) { throw null; }
+}
+
+// It is assumed that this is NOT a collection with elements of type T.
+public class CollectionTheorems2<T> : IEnumerable {
+    IEnumerator IEnumerable.GetEnumerator() { throw null; }
+
+    public T Get() { throw null; }
+
+    public void Set(T x) { throw null; }
 }
