055072b1c086f6590808badd1c97e3daef562421
Michael Nebel <michaelnebel@github.com>
2022-09-08 14:09:55 +0200

    C#: Add more test examples.

--- csharp/ql/test/utils/model-generator/theorems/TheoremSummaries.cs
+++ csharp/ql/test/utils/model-generator/theorems/TheoremSummaries.cs
@@ -29,11 +29,27 @@ public class Theorems1<T> {
     // No summary as S is unrelated to T
     public void Set<S>(S x) { throw null; }
 
-    public int Apply(Func<T, int> f) { throw null; }
-
     public IList<T> GetMany() { throw null; }
 
     public void AddMany(IEnumerable<T> xs) { throw null; }
+
+    public int Apply(Func<T, int> f) { throw null; }
+
+    public S Apply<S>(Func<T, S> f) { throw null; }
+
+    public T2 Apply<T1,T2>(T1 x, Func<T1, T2> f) { throw null; }
+
+    public S Map<S>(Func<T, S> f) { throw null; }
+
+    public Theorems1<S> MapTheorem<S>(Func<T, S> f) { throw null; }
+
+    public Theorems1<S> FlatMap<S>(Func<T, IEnumerable<S>> f) { throw null; }
+
+    public Theorems1<T> FlatMap(Func<T, IEnumerable<T>> f) { throw null; }
+
+    public Theorems1<T> Return(Func<T, Theorems1<T>> f) { throw null; }
+    // Examples still not working:
+    // public void Set(int x, Func<int, T> f) { throw null;}
 }
 
 // It is assumed that this is a collection with elements of type T.
