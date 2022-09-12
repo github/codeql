43e5abac39ad30614bad6bcf35408604bc839843
Paolo Tranquilli <redsun82@github.com>
2022-09-12 16:31:51 +0200

    Swift: do not extract unresolved things from `IfConfigDecl`
    This avoids extracting things that are unresolved within an
    `IfConfigDecl` instance:
    * all conditions
    * all inactive code blocks
    This is meant to test out the hypothesis that this should solve some
    extractor issues. If going through with it we should definitely change
    the schema model for this.
    
    Also, tests have not been updated and are expected to fail.

--- swift/extractor/visitors/DeclVisitor.cpp
+++ swift/extractor/visitors/DeclVisitor.cpp
@@ -380,13 +380,16 @@ void DeclVisitor::fillAbstractStorageDecl(const swift::AbstractStorageDecl& decl
 
 codeql::IfConfigDecl DeclVisitor::translateIfConfigDecl(const swift::IfConfigDecl& decl) {
   auto entry = createEntry(decl);
-  entry.clauses = dispatcher_.fetchRepeatedLabels(decl.getClauses());
+  //  entry.clauses = dispatcher_.fetchRepeatedLabels(decl.getClauses());
+  if (auto activeClause = decl.getActiveClause()) {
+    entry.clauses.push_back(dispatcher_.fetchLabel(activeClause));
+  }
   return entry;
 }
 
 codeql::IfConfigClause DeclVisitor::translateIfConfigClause(const swift::IfConfigClause& clause) {
   auto entry = dispatcher_.createEntry(clause);
-  entry.condition = dispatcher_.fetchOptionalLabel(clause.Cond);
+  //  entry.condition = dispatcher_.fetchOptionalLabel(clause.Cond);
   entry.elements = dispatcher_.fetchRepeatedLabels(clause.Elements);
   entry.is_active = clause.isActive;
   return entry;
