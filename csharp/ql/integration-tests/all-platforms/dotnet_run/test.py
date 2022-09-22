3d5767e72309fef3252b89ed4903ce01218c5117
Michael Nebel <michaelnebel@github.com>
2022-09-22 14:01:53 +0200

    C#: Make sure that dotnet clean also removes the bin and obj folders.

--- csharp/ql/integration-tests/all-platforms/dotnet_run/test.py
+++ csharp/ql/integration-tests/all-platforms/dotnet_run/test.py
@@ -4,14 +4,14 @@ from create_database_utils import *
 run_codeql_database_create(['dotnet run'], test_db="test-db", db=None, lang="csharp")
 
 # no arguments, but `--`
-run_codeql_database_create(['dotnet clean', 'rm -rf test-db obj bin', 'dotnet run --'], test_db="test2-db", db=None, lang="csharp")
+run_codeql_database_create(['dotnet clean', 'rm -rf test-db', 'dotnet run --'], test_db="test2-db", db=None, lang="csharp")
 
 # two arguments, no `--`
-run_codeql_database_create(['dotnet clean', 'rm -rf test2-db obj bin', 'dotnet run hello world'], test_db="test3-db", db=None, lang="csharp")
+run_codeql_database_create(['dotnet clean', 'rm -rf test2-db', 'dotnet run hello world'], test_db="test3-db", db=None, lang="csharp")
 
 # two arguments, and `--`
-run_codeql_database_create(['dotnet clean', 'rm -rf test3-db obj bin', 'dotnet run -- hello world'], test_db="test4-db", db=None, lang="csharp")
+run_codeql_database_create(['dotnet clean', 'rm -rf test3-db', 'dotnet run -- hello world'], test_db="test4-db", db=None, lang="csharp")
 
 # shared compilation enabled; tracer should override by changing the command
 # to `dotnet run -p:UseSharedCompilation=true -p:UseSharedCompilation=false -- hello world`
-run_codeql_database_create(['dotnet clean', 'rm -rf test4-db obj bin', 'dotnet run -p:UseSharedCompilation=true -- hello world'], test_db="test5-db", db=None, lang="csharp")
+run_codeql_database_create(['dotnet clean', 'rm -rf test4-db', 'dotnet run -p:UseSharedCompilation=true -- hello world'], test_db="test5-db", db=None, lang="csharp")
