de9714de84c097c838c0a340ed0ea46334daa6bc
Tamas Vajk <tamasvajk@github.com>
2022-09-29 10:01:02 +0200

    Kotlin: Add test cases for operators being called by name

--- java/ql/test/kotlin/library-tests/java-map-methods/test.kt
+++ java/ql/test/kotlin/library-tests/java-map-methods/test.kt
@@ -1,3 +1,9 @@
 fun test(m: Map<Int, Int>) = m.getOrDefault(1, 2)
 
 fun test2(s: String) = s.length
+
+fun fn1(s: String) = s.plus(other = "")
+fun fn2(s: String) = s + ""
+
+fun fn1(i: Int) = i.minus(10)
+fun fn2(i: Int) = i - 10
