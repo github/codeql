84f9c9b224d878f89605a437af500b73b8a492fb
Robert Marsh <rdmarsh@semmle.com>
2022-09-30 15:15:24 -0400

    C++: query help for ConstantSizeArrayOffByOne.ql

---
+++ cpp/ql/src/experimental/Security/CWE/CWE-193/ConstantSizeArrayOffByOne.cpp
@@ -0,0 +1,17 @@
+#define MAX_SIZE 1024
+
+struct FixedArray {
+  int buf[MAX_SIZE];
+};
+
+int main(){
+  FixedArray arr;
+
+  for(int i = 0; i <= MAX_SIZE; i++) {
+    arr[i] = 0
+  }
+
+  for(int i = 0; i < MAX_SIZE; i++) {
+    arr[i = 0]
+  }
+}
