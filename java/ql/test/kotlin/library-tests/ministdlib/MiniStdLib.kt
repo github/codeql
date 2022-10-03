01fe465bdfc8908d9e74dc6dc43623cc0d54dc86
Ian Lynagh <igfoo@github.com>
2022-10-03 16:16:11 +0100

    Kotlin: Make MiniStdLib more closely match the real stdlib

--- java/ql/test/kotlin/library-tests/ministdlib/MiniStdLib.kt
+++ java/ql/test/kotlin/library-tests/ministdlib/MiniStdLib.kt
@@ -14,20 +14,20 @@ public open class Any {
     open operator fun equals(other: Any?): Boolean { return this.equals(other) }
 }
 
-public open class String {
+public class String {
     operator fun plus(other: Any?): String { return this.plus(other) }
 }
 
-public open class Boolean {
+public class Boolean {
     operator fun not(): Boolean { return this.not() }
 }
 
-public open class Int {
+public class Int {
     operator fun plus(other: Int): Int { return this.plus(other) }
     operator fun times(other: Int): Int { return this.times(other) }
     infix fun xor(other: Int): Int { return this.xor(other) }
 }
 
-public open class Unit {
+public object Unit {
 }
 
