726cd2ca8a900f11db528242377e47e8fe8ebb7e
tyage <namatyage@gmail.com>
2022-10-04 17:11:37 +0900

    refactor test

--- javascript/ql/test/query-tests/Security/CWE-079/DomBasedXss/json-stringify.jsx
+++ javascript/ql/test/query-tests/Security/CWE-079/DomBasedXss/json-stringify.jsx
@@ -3,27 +3,28 @@ var app = express();
 
 app.get("/some/path", function (req, res) {
   const locale = req.param("locale");
+  const breadcrumbList = [
+    {
+      "@type": "ListItem",
+      position: 1,
+      item: {
+        "@id": `https://example.com/some?locale=${locale}`,
+        name: "Some",
+      },
+    },
+    {
+      "@type": "ListItem",
+      position: 2,
+      item: {
+        "@id": `https://example.com/some/path?locale=${locale}`,
+        name: "Path",
+      },
+    },
+  ];
   const jsonLD = {
     "@context": "https://schema.org",
     "@type": "BreadcrumbList",
-    itemListElement: [
-      {
-        "@type": "ListItem",
-        position: 1,
-        item: {
-          "@id": `https://example.com/some?locale=${locale}`,
-          name: "Some",
-        },
-      },
-      {
-        "@type": "ListItem",
-        position: 2,
-        item: {
-          "@id": `https://example.com/some/path?locale=${locale}`,
-          name: "Real Dresses",
-        },
-      },
-    ],
+    itemListElement: breadcrumbList,
   };
   <script
     type="application/ld+json"
