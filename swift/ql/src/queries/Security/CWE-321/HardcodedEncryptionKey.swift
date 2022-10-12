bb3bf64364e66b1bfc73947e17b84aaf56627dd3
Karim Ali <karim.ali@ualberta.ca>
2022-10-12 10:27:02 +0200

    update example with both AES and Blowfish for better clarity

--- swift/ql/src/queries/Security/CWE-321/HardcodedEncryptionKey.swift
+++ swift/ql/src/queries/Security/CWE-321/HardcodedEncryptionKey.swift
@@ -8,13 +8,16 @@ func encrypt(padding : Padding) {
 	let ivString = getRandomIV()
 	_ = try AES(key: key, blockMode: CBC(), padding: padding)
 	_ = try AES(key: keyString, iv: ivString)
-
+	_ = try Blowfish(key: key, blockMode: CBC(), padding: padding)
+	_ = try Blowfish(key: keyString, iv: ivString)
 
 
 	// GOOD: Using randomly generated keys for encryption
 	let key = (0..<10).map({ _ in UInt8.random(in: 0...UInt8.max) })
 	let keyString = String(cString: key)
 	let ivString = getRandomIV()
+	_ = try AES(key: key, blockMode: CBC(), padding: padding)
+	_ = try AES(key: keyString, iv: ivString)
 	_ = try Blowfish(key: key, blockMode: CBC(), padding: padding)
 	_ = try Blowfish(key: keyString, iv: ivString)
 
