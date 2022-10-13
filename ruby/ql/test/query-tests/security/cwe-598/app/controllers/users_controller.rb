cda7d84633b4fec9c8b34b0befcb0dc58c094863
Alex Ford <alexrford@github.com>
2022-10-13 22:41:34 +0100

    Ruby: update rb/sensitive-get-query tests

--- ruby/ql/test/query-tests/security/cwe-598/app/controllers/users_controller.rb
+++ ruby/ql/test/query-tests/security/cwe-598/app/controllers/users_controller.rb
@@ -1,18 +1,28 @@
 class UsersController < ApplicationController
 
-  def login_get
-    password = params[:password] # BAD: route handler uses GET query parameters to receive sensitive data
+  def login_get_1
+    foo = params[:password] # BAD: route handler uses GET query parameters to receive sensitive data
+    authenticate_user(params[:username], foo)
+  end
+
+  def login_get_2
+    password = params[:foo] # BAD: route handler uses GET query parameters to receive sensitive data
     authenticate_user(params[:username], password)
+  end
+
+  def login_get_3
+    @password = params[:foo] # BAD: route handler uses GET query parameters to receive sensitive data
+    authenticate_user(params[:username], @password)
   end
 
   def login_post
-    password = params[:password] # GOOD: handler uses POST form parameters to receive sensitive data
-    authenticate_user(params[:username], password)
+    foo = params[:password] # GOOD: handler uses POST form parameters to receive sensitive data
+    authenticate_user(params[:username], foo)
   end
 
   def login_get_cookies
-    password = cookies[:password] # GOOD: data sourced from cookies rather than (plaintext) query params
-    authenticate_user(params[:username], password)
+    foo = cookies[:password] # GOOD: data sourced from cookies rather than (plaintext) query params
+    authenticate_user(params[:username], foo)
   end
 
   private
