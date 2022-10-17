f3ed54e7ba2d74cfd541958ad0d6a61958270cfe
Alex Denisov <alexdenisov@github.com>
2022-10-17 10:14:06 +0200

    Swift: only run Xcode autobuilder on macOS

--- swift/tools/autobuild.sh
+++ swift/tools/autobuild.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-exec "${CODEQL_EXTRACTOR_SWIFT_ROOT}/tools/${CODEQL_PLATFORM}/xcode-autobuilder"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  exec "${CODEQL_EXTRACTOR_SWIFT_ROOT}/tools/${CODEQL_PLATFORM}/xcode-autobuilder"
+else
+	echo "Not implemented yet"
+  exit 1
+fi
