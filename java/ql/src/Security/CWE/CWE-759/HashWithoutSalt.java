3d3f22fc1831efe8830f613aa4108d4ad93d73e5
Joe Farebrother <joefarebrother@github.com>
2022-10-19 17:26:42 +0100

    Update qhelp to recommend the use of PBKDF2.

--- java/ql/src/Security/CWE/CWE-759/HashWithoutSalt.java
+++ java/ql/src/Security/CWE/CWE-759/HashWithoutSalt.java
@@ -1,14 +1,21 @@
 public class HashWithoutSalt {
 	// BAD - Hash without a salt.
-	public void getSHA256Hash(String password) throws NoSuchAlgorithmException {
+	public byte[] getSHA256Hash(String password) throws NoSuchAlgorithmException {
 		MessageDigest md = MessageDigest.getInstance("SHA-256");
-		byte[] messageDigest = md.digest(password.getBytes());
+		return md.digest(password.getBytes());
 	}
 
-	// GOOD - Hash with a salt.
-	public void getSHA256Hash(String password, byte[] salt) throws NoSuchAlgorithmException {
+	// BETTER - Hash with a salt using SHA256.
+	public byte[] getSHA256Hash(String password, byte[] salt) throws NoSuchAlgorithmException {
 		MessageDigest md = MessageDigest.getInstance("SHA-256");
 		md.update(salt);
-		byte[] messageDigest = md.digest(password.getBytes());
+		return md.digest(password.getBytes());
+	}
+
+	// GOOD - Hash with a salt using PBKDF2.
+	public byte[] getPBKDF2Hash(String password, byte[] salt) throws NoSuchAlgorithmException {
+		KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+		SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+		return f.generateSecret(spec).getEncoded();
 	}
 }
