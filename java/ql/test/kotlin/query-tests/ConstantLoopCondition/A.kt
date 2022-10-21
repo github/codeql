9d1af76c02066f341688966b8aa9ec477cd774a5
Tamas Vajk <tamasvajk@github.com>
2022-10-21 08:51:11 +0200

    Add more test cases

--- java/ql/test/kotlin/query-tests/ConstantLoopCondition/A.kt
+++ java/ql/test/kotlin/query-tests/ConstantLoopCondition/A.kt
@@ -2,9 +2,23 @@ fun fn0(f: Function0<Unit>) = f()
 
 fun fn1() {
     var c = true
-    while (c) {
+    while (c) { // TODO: false positive
         fn0 {
             c = false
+        }
+    }
+
+    var d = true
+    while (d) {
+        fn0 {
+            println(d)
+        }
+    }
+
+    val e = true
+    while (e) {
+        fn0 {
+            println(e)
         }
     }
 }
