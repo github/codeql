dbdf6ea4890a84ad755ad74797d86316cd6255e9
Paolo Tranquilli <redsun82@github.com>
2022-10-21 12:51:13 +0200

    Swift: fix qltest failure reporting
    `qltest.sh` was not exiting with a failure when the extractor was
    failing.

--- swift/tools/qltest.sh
+++ swift/tools/qltest.sh
@@ -6,11 +6,19 @@ QLTEST_LOG="$CODEQL_EXTRACTOR_SWIFT_LOG_DIR"/qltest.log
 
 export LD_LIBRARY_PATH="$CODEQL_EXTRACTOR_SWIFT_ROOT/tools/$CODEQL_PLATFORM"
 
+EXTRACTOR="$CODEQL_EXTRACTOR_SWIFT_ROOT/tools/$CODEQL_PLATFORM/extractor"
+SDK="$CODEQL_EXTRACTOR_SWIFT_ROOT/qltest/$CODEQL_PLATFORM/sdk"
+
 for src in *.swift; do
-  opts=(-sdk "$CODEQL_EXTRACTOR_SWIFT_ROOT/qltest/$CODEQL_PLATFORM/sdk" -c -primary-file $src)
+  opts=(-sdk "$SDK" -c -primary-file "$src")
   opts+=($(sed -n '1 s=//codeql-extractor-options:==p' $src))
   echo -e "calling extractor with flags: ${opts[@]}\n" >> $QLTEST_LOG
-  "$CODEQL_EXTRACTOR_SWIFT_ROOT/tools/$CODEQL_PLATFORM/extractor" "${opts[@]}" >> $QLTEST_LOG 2>&1 || FAILED=1
+  if ! "$EXTRACTOR" "${opts[@]}" >> $QLTEST_LOG 2>&1; then
+    FAILED=1
+  fi
 done
 
-[ -z "$FAILED" ] || cat "$QLTEST_LOG" # Show compiler errors on extraction failure
+if [ -n "$FAILED" ]; then
+  cat "$QLTEST_LOG" # Show compiler errors on extraction failure
+  exit 1
+fi
