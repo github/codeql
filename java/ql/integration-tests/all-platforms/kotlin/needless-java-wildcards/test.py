58cf5ef7a1d3a44cdb64eba079028dc735a75628
Ian Lynagh <igfoo@github.com>
2022-10-31 15:55:23 +0000

    Kotlin: needless-java-wildcards test: Use portable search path separator

--- java/ql/integration-tests/all-platforms/kotlin/needless-java-wildcards/test.py
+++ java/ql/integration-tests/all-platforms/kotlin/needless-java-wildcards/test.py
@@ -2,4 +2,4 @@ from create_database_utils import *
 
 os.mkdir('build1')
 os.mkdir('build2')
-run_codeql_database_create(["kotlinc kConsumer.kt -d build1", "javac Test.java -cp build1 -d build2", "kotlinc user.kt -cp build1:build2"], lang="java")
+run_codeql_database_create(["kotlinc kConsumer.kt -d build1", "javac Test.java -cp build1 -d build2", "kotlinc user.kt -cp build1" + os.path.pathsep + "build2"], lang="java")
