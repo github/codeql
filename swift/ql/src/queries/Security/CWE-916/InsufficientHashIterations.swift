723ca8ed8811b202261d37266836a457ac0c8960
Karim Ali <karim.ali@ualberta.ca>
2022-10-31 13:50:30 +0200

    update documentation following docs review

--- swift/ql/src/queries/Security/CWE-916/InsufficientHashIterations.swift
+++ swift/ql/src/queries/Security/CWE-916/InsufficientHashIterations.swift
@@ -2,11 +2,11 @@
 func encrypt() {
 	// ...
 
-	// BAD: Using insufficient (i.e., < 120,000) hash iterations keys for encryption
+	// BAD: Using insufficient (that is, < 120,000) hash iterations keys for encryption
 	_ = try PKCS5.PBKDF1(password: getRandomArray(), salt: getRandomArray(), iterations: 90000, keyLength: 0)
 	_ = try PKCS5.PBKDF2(password: getRandomArray(), salt: getRandomArray(), iterations: 90000, keyLength: 0)
 
-	// GOOD: Using sufficient (i.e., >= 120,000) hash iterations keys for encryption
+	// GOOD: Using sufficient (that is, >= 120,000) hash iterations keys for encryption
 	_ = try PKCS5.PBKDF1(password: getRandomArray(), salt: getRandomArray(), iterations: 120120, keyLength: 0)
 	_ = try PKCS5.PBKDF2(password: getRandomArray(), salt: getRandomArray(), iterations: 120120, keyLength: 0)
 
