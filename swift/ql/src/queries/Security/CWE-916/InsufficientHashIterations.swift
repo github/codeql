76a330d4b98d2a55580e7bd5059625ab272a027f
Karim Ali <karim.ali@ualberta.ca>
2022-10-31 13:52:49 +0200

    update code example to be OWASP compliant

--- swift/ql/src/queries/Security/CWE-916/InsufficientHashIterations.swift
+++ swift/ql/src/queries/Security/CWE-916/InsufficientHashIterations.swift
@@ -1,14 +1,14 @@
 
-func encrypt() {
+func hash() {
 	// ...
 
-	// BAD: Using insufficient (that is, < 120,000) hash iterations keys for encryption
+	// BAD: Using insufficient (that is, < 120,000) iterations for password hashing
 	_ = try PKCS5.PBKDF1(password: getRandomArray(), salt: getRandomArray(), iterations: 90000, keyLength: 0)
 	_ = try PKCS5.PBKDF2(password: getRandomArray(), salt: getRandomArray(), iterations: 90000, keyLength: 0)
 
-	// GOOD: Using sufficient (that is, >= 120,000) hash iterations keys for encryption
+	// GOOD: Using sufficient (that is, >= 120,000) iterations for password hashing
 	_ = try PKCS5.PBKDF1(password: getRandomArray(), salt: getRandomArray(), iterations: 120120, keyLength: 0)
-	_ = try PKCS5.PBKDF2(password: getRandomArray(), salt: getRandomArray(), iterations: 120120, keyLength: 0)
+	_ = try PKCS5.PBKDF2(password: getRandomArray(), salt: getRandomArray(), iterations: 310000, keyLength: 0)
 
 	// ...
 }
