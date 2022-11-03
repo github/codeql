655b4a4d1726fb9ec44afe59f445a5cf30d33da9
erik-krogh <erik-krogh@github.com>
2022-11-03 11:08:00 +0100

    recognize more re-exported values as exported

--- javascript/ql/test/query-tests/Security/CWE-915/PrototypePollutingAssignment/sublib/other.js
+++ javascript/ql/test/query-tests/Security/CWE-915/PrototypePollutingAssignment/sublib/other.js
@@ -3,9 +3,13 @@
 
   Foobar.prototype = {
     method: function (obj, path, value) {
-      obj[path[0]][path[1]] = value; // NOT OK - but not flagged [INCONSISTENCY]
+      obj[path[0]][path[1]] = value; // NOT OK
     },
   };
 
   module.exports.foobar = Foobar;
+
+  module.other.notExported = function (obj, path, value) {
+    obj[path[0]][path[1]] = value; // OK - not exported
+  }
 })();
