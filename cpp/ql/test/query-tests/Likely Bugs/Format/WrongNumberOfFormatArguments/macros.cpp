d72ea52f68339c4f27d8711f0bf09800e6a19104
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2022-11-07 16:26:26 +0000

    C++: More accurate test tags.

--- cpp/ql/test/query-tests/Likely Bugs/Format/WrongNumberOfFormatArguments/macros.cpp
+++ cpp/ql/test/query-tests/Likely Bugs/Format/WrongNumberOfFormatArguments/macros.cpp
@@ -13,7 +13,7 @@ void testMacros(int a, int b, int c)
 	GOODPRINTF("%i %i %i\n", a, b, c); // GOOD
 	GOODPRINTF("%i %i %i %i\n", a, b, c); // BAD: too few format arguments
 
-	BADPRINTF("%i %i\n", a, b, 0); // BAD: too many format arguments
+	BADPRINTF("%i %i\n", a, b, 0); // DUBIOUS: too many format arguments
 		// ^ here there are too many format arguments, but the design of the Macro forces the user
 		//   to do this, and the extra argument is harmlessly ignored in practice.  Reporting these
 		//   results can be extremely noisy (e.g. in openldap).
