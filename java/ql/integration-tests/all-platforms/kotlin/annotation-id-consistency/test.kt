73df3166ebd8ba172409c3dc2d2fb1d58d73aac3
Chris Smowton <smowton@github.com>
2022-11-14 15:49:45 +0000

    Test varargs annotation

--- java/ql/integration-tests/all-platforms/kotlin/annotation-id-consistency/test.kt
+++ java/ql/integration-tests/all-platforms/kotlin/annotation-id-consistency/test.kt
@@ -8,6 +8,15 @@ annotation class Ann3(val a: Int) { }
 
 annotation class GenericAnnotation<T : Any>(val x: KClass<T>, val y: Array<KClass<T>>) { }
 
+@Repeatable
+annotation class VarargAnnotation(vararg val x: Int) { }
+
 @Ann1(1, Ann2("Hello", String::class, intArrayOf(1, 2, 3), arrayOf(Ann3(1), Ann3(2)), arrayOf(String::class, Int::class)))
 @GenericAnnotation<String>(String::class, arrayOf(String::class, String::class))
+@VarargAnnotation
+@VarargAnnotation(1)
+@VarargAnnotation(1, 2)
+@VarargAnnotation(*[1, 2, 3])
+@VarargAnnotation(*intArrayOf(1, 2, 3))
 class Annotated { }
+
