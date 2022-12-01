5194108233b14b4a1a0586f0321596bcd106aed8
Michael Nebel <michaelnebel@github.com>
2022-12-01 10:54:42 +0100

    Java/C#: Improve the newlines in the generated model files.

--- misc/scripts/models-as-data/generate_flow_model_extensions.py
+++ misc/scripts/models-as-data/generate_flow_model_extensions.py
@@ -180,8 +180,7 @@ extensions:
 {sinkAddsTo}
 {sourceAddsTo}
 {summaryAddsTo}
-{negativeSummaryAddsTo}
-        """
+{negativeSummaryAddsTo}"""
 
     def makeTypeBasedContent(self):
         if self.generateTypeBasedSummaries:
@@ -189,13 +188,11 @@ extensions:
         else:
             typeBasedSummaryAddsTo = ""
 
-        return f"""
-# THIS FILE IS AN AUTO-GENERATED MODELS AS DATA FILE. DO NOT EDIT.
+        return f"""# THIS FILE IS AN AUTO-GENERATED MODELS AS DATA FILE. DO NOT EDIT.
 # Definitions of type based summaries in the {self.friendlyname} framework.
 
 extensions:
-{typeBasedSummaryAddsTo}
-        """
+{typeBasedSummaryAddsTo}"""
 
     def save(self, content, target):
         with open(target, "w") as targetYml:
