2ae0c7e115a83e36870c8282a05d94221c24681e
Matt Rothenberg <mattrothenberg@users.noreply.github.com>
2022-12-02 14:02:54 +0100

    Update RequestForgeryGood.js

--- javascript/ql/src/Security/CWE-918/examples/RequestForgeryGood.js
+++ javascript/ql/src/Security/CWE-918/examples/RequestForgeryGood.js
@@ -1,10 +1,9 @@
 import http from 'http';
-import url from 'url';
 
-var server = http.createServer(function(req, res) {
-    var target = url.parse(req.url, true).query.target;
+const server = http.createServer(function(req, res) {
+    const target = new URL(req.url).searchParams.get("target");
 
-    var subdomain;
+    const subdomain;
     if (target === 'EU') {
         subdomain = "europe"
     } else {
