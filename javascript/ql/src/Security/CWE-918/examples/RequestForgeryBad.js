95f994a82b594e4e8d3f13f89de88282d4e57a02
Matt Rothenberg <mattrothenberg@users.noreply.github.com>
2022-12-02 14:17:37 +0100

    Update RequestForgeryBad.js

--- javascript/ql/src/Security/CWE-918/examples/RequestForgeryBad.js
+++ javascript/ql/src/Security/CWE-918/examples/RequestForgeryBad.js
@@ -1,7 +1,7 @@
 import http from 'http';
 
 const server = http.createServer(function(req, res) {
-    const target = new URL(req.url).searchParams.get("target");
+    const target = new URL(req.url, "http://example.com").searchParams.get("target");
 
     // BAD: `target` is controlled by the attacker
     http.get('https://' + target + ".example.com/data/", res => {
