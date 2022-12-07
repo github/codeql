c526020fd413af89d26f09d6361cfb47f2478c0f
Chris Smowton <smowton@github.com>
2022-12-07 11:51:48 +0000

    Note TODO re: re-enabling suspend function Java interop testing

--- java/ql/test/kotlin/library-tests/java_and_kotlin/Java.java
+++ java/ql/test/kotlin/library-tests/java_and_kotlin/Java.java
@@ -13,6 +13,7 @@ public class Java {
 
 /*
 // Java interop disabled as it currently doesn't work (no symbol fn1(int, Completion<...>) gets created)
+// TODO: re-enable this test once a correct function signature is extracted
 		@Override
 		public Object fn1(int x, Continuation<? super String> $completion) {
 			return super.fn1(x, $completion);
