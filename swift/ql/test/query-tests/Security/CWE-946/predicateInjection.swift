11c03fb8c9d556740324e66f4e0257eb2c1cba1a
Tony Torralba <atorralba@users.noreply.github.com>
2022-12-15 12:28:48 +0100

    Add 'good' test cases

--- swift/ql/test/query-tests/Security/CWE-946/predicateInjection.swift
+++ swift/ql/test/query-tests/Security/CWE-946/predicateInjection.swift
@@ -21,10 +21,18 @@ class NSPredicate {
 
 func test() {
     let remoteString = String(contentsOf: URL(string: "http://example.com/")!)
+    let safeString = "safe"
 
     NSPredicate(format: remoteString, argumentArray: []) // $ hasPredicateInjection=23
+    NSPredicate(format: safeString, argumentArray: []) // Safe
+    NSPredicate(format: safeString, argumentArray: [remoteString]) // Safe
     NSPredicate(format: remoteString, arguments: CVaListPointer(_fromUnsafeMutablePointer: UnsafeMutablePointer(bitPattern: 0)!)) // $ hasPredicateInjection=23
+    NSPredicate(format: safeString, arguments: CVaListPointer(_fromUnsafeMutablePointer: UnsafeMutablePointer(bitPattern: 0)!)) // Safe
     NSPredicate(format: remoteString) // $ hasPredicateInjection=23
+    NSPredicate(format: safeString) // Safe
     NSPredicate(format: remoteString, "" as! CVarArg) // $ hasPredicateInjection=23
+    NSPredicate(format: safeString, "" as! CVarArg) // Safe
+    NSPredicate(format: safeString, remoteString as! CVarArg) // Safe
     NSPredicate(fromMetadataQueryString: remoteString) // $ hasPredicateInjection=23
+    NSPredicate(fromMetadataQueryString: safeString) // Safe
 }
