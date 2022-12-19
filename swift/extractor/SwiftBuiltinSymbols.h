1c64bf4bbc8778992858b412d30c91c562262e79
Nora Dimitrijević <d10c@users.noreply.github.com>
2022-12-19 18:00:20 -0500

    Swift: remove superfluous newline

--- swift/extractor/SwiftBuiltinSymbols.h
+++ swift/extractor/SwiftBuiltinSymbols.h
@@ -67,7 +67,6 @@ constexpr std::array swiftBuiltins = {
     "Vec16xFPIEEE64",
     "Vec32xFPIEEE64",
     "Vec64xFPIEEE64",
-
     "buildDefaultActorExecutorRef",
     "buildMainActorExecutorRef",
 };
