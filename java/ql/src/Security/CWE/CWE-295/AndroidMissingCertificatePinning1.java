a47ef17a0d06ec85acdd3c3e8143253b0bcec3c6
Tony Torralba <atorralba@users.noreply.github.com>
2022-12-19 18:11:54 +0100

    Update java/ql/src/Security/CWE/CWE-295/AndroidMissingCertificatePinning1.java
    Co-authored-by: Edward Minnix III <egregius313@github.com>

--- java/ql/src/Security/CWE/CWE-295/AndroidMissingCertificatePinning1.java
+++ java/ql/src/Security/CWE/CWE-295/AndroidMissingCertificatePinning1.java
@@ -1,2 +1,2 @@
 // BAD - By default, this network call does not use certificate pinning
-URLConnection conn = new URL("https://example.com").openonnection();
+URLConnection conn = new URL("https://example.com").openConnection();
