33c4a8233c831f5c7eb89dabbd9c33636715a344
Paolo Tranquilli <redsun82@github.com>
2022-12-19 12:16:44 +0100

    Swift: collect encountered modules

--- swift/extractor/config/SwiftExtractorState.h
+++ swift/extractor/config/SwiftExtractorState.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <unordered_set>
 #include <filesystem>
 
 #include <swift/AST/Decl.h>
@@ -14,6 +15,10 @@ struct SwiftExtractorState {
   // All the trap files related to this extraction. This may also include trap files generated in a
   // previous run but that this run requested as well. Paths are relative to `configuration.trapDir`
   std::vector<std::filesystem::path> traps;
+
+  // All modules encountered during this extractor run, which therefore are dependencies of the
+  // outcomes of this run
+  std::unordered_set<const swift::ModuleDecl*> encounteredModules;
 };
 
 }  // namespace codeql
