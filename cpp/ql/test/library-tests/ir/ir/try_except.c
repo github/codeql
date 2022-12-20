cd6e421cf5dc9f9d707fda7e7bb1af5de48386ef
Mathias Vorreiter Pedersen <mathiasvp@github.com>
2022-12-20 12:54:35 +0000

    C++: Add a test with '__try __except' and '__try __finally' in the 'ir' test directory.

---
+++ cpp/ql/test/library-tests/ir/ir/try_except.c
@@ -0,0 +1,28 @@
+// semmle-extractor-options: --microsoft
+
+void ProbeFunction();
+void sink();
+
+void f() {
+  int x, y = 0;
+  __try {
+    ProbeFunction(0);
+    x = y;
+    ProbeFunction(0);
+  }
+  __except (0) {
+    sink(x);
+  }
+}
+
+void g() {
+  int x, y = 0;
+  __try {
+    ProbeFunction(0);
+    x = y;
+    ProbeFunction(0);
+  }
+  __finally {
+    sink(x);
+  }
+}
