c251da799fd3908fb7b17b9d9eb88c237820bcd1
Jami Cogswell <jcogs33@Jamis-MacBook-Pro.local>
2022-12-21 13:19:09 -0500

    Java: update TopJdkApis test

--- java/ql/test/ext/TopJdkApis/TopJdkApisTest.java
+++ java/ql/test/ext/TopJdkApis/TopJdkApisTest.java
@@ -6,9 +6,22 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringJoiner;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.lang.System;
 import java.lang.IllegalStateException;
+import java.lang.IndexOutOfBoundsException;
+import java.lang.Math;
+import java.io.ByteArrayInputStream;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class TopJdkApisTest { }
