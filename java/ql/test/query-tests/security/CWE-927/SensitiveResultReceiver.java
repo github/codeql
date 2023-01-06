7e7b5b4488fa71a920cee1be83c2c583067b7c7a
Joe Farebrother <joefarebrother@github.com>
2023-01-06 16:38:36 +0000

    Improve test case

--- java/ql/test/query-tests/security/CWE-927/SensitiveResultReceiver.java
+++ java/ql/test/query-tests/security/CWE-927/SensitiveResultReceiver.java
@@ -1,11 +1,13 @@
 import android.os.Bundle;
 import android.os.ResultReceiver;
+import android.content.Intent;
 
 class SensitiveResultReceiver {
     <T> T source() { return null; }
 
     void test1(String password) {
-        ResultReceiver rec = source();
+        Intent intent = source();
+        ResultReceiver rec = intent.getParcelableExtra("hi");
         Bundle b = new Bundle();
         b.putCharSequence("pass", password);
         rec.send(0, b); // $hasSensitiveResultReceiver
