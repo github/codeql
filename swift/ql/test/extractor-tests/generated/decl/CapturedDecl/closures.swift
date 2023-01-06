52366cd89aa4e7159075973857875eb60b22b519
Nora Dimitrijević <d10c@users.noreply.github.com>
2023-01-06 16:24:26 -0500

    Swift: more CapturedDecl test cases

--- swift/ql/test/extractor-tests/generated/decl/CapturedDecl/closures.swift
+++ swift/ql/test/extractor-tests/generated/decl/CapturedDecl/closures.swift
@@ -24,3 +24,25 @@ func callEscape() {
   baz()
   escape?()
 }
+
+func logical() -> Bool {
+  let f: ((Int) -> Int)? = { (x) in x + 1 }
+  let x: Int? = 42
+  return f != nil
+      && (x != nil
+          && f!(x!) == 43)
+}
+
+func asyncTest() {
+  func withCallback(_ callback: @escaping (Int) async -> Int) {
+    func wrapper(_ x: Int) async -> Int {
+      return await callback(x + 1)
+    }
+    Task {
+      print(await wrapper(42))
+    }
+  }
+  withCallback { x in
+    x + 1
+  }
+}
