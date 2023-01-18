72fa3bd905a94bc1534cef33b6ae691931c2abda
Michael Nebel <michaelnebel@github.com>
2023-01-18 13:34:11 +0100

    C#: Add test cases for raw string literals.

--- csharp/ql/test/library-tests/csharp11/Strings.cs
+++ csharp/ql/test/library-tests/csharp11/Strings.cs
@@ -11,4 +11,28 @@ public class MyTestClass
             _ => "something else"
         }}.";
     }
+
+    public void M2()
+    {
+        // Raw string literal.
+        var message1 = """
+        This is my very long
+            text message that spans
+          accross multiple lines
+        and is very useful.
+        """;
+
+        // String interpolation using a raw string literal.
+        var message2 = $"""
+        The nested message
+          is "{message1}" and everything
+        spans multiple lines.
+        """;
+
+        // String interpolation using a raw string literal that requires double curly braces.
+        var message3 = $$"""
+        Show no curly braces: {{message1}}
+        Show matching set of curly braces: {{{message2}}}
+        """;
+    }
 }
--- csharp/ql/test/library-tests/csharp11/strings.ql
+++ csharp/ql/test/library-tests/csharp11/strings.ql
@@ -4,3 +4,7 @@ query predicate interpolatedstrings(InterpolatedStringExpr se, Expr e) {
   se.getFile().getStem() = "Strings" and
   (e = se.getAText() or e = se.getAnInsert())
 }
+
+query predicate stringliterals(StringLiteral s) {
+  s.getFile().getStem() = "Strings" and s.getEnclosingCallable().getName() = "M2"
+}
