80324735bb6e9366a6b0dfcf7ee3373dcfc89a3f
Rasmus Wriedt Larsen <rasmuswl@github.com>
2023-01-23 11:31:23 +0100

    Python: Fixup annotation for `CWE-022-PathInjection/pathlib_use.py`

--- python/ql/test/query-tests/Security/CWE-022-PathInjection/pathlib_use.py
+++ python/ql/test/query-tests/Security/CWE-022-PathInjection/pathlib_use.py
@@ -11,7 +11,7 @@ STATIC_DIR = pathlib.Path("/server/static/")
 def path_injection():
     filename = request.args.get('filename', '')
     p = STATIC_DIR / filename
-    p.open() # NOT OK
+    p.open() # $ result=BAD
 
     p2 = pathlib.Path(STATIC_DIR, filename)
-    p2.open() # NOT OK
+    p2.open() # $ result=BAD
