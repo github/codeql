fd1ce5d12c9b61b7d084b86c82e77633f011d7d6
Chris Smowton <smowton@github.com>
2023-01-25 12:51:07 +0000

    Switch to using language-specific diagnostic environment variable

--- java/ql/integration-tests/all-platforms/java/diagnostics/no-build-system/test.py
+++ java/ql/integration-tests/all-platforms/java/diagnostics/no-build-system/test.py
@@ -3,6 +3,6 @@ from create_database_utils import *
 from diagnostics_test_utils import *
 
 os.mkdir("diagnostics")
-run_codeql_database_create([], lang="java", runFunction = runUnsuccessfully, db = None, extra_env = {"CODEQL_EXTRACTOR_DIAGNOSTIC_DIR": "diagnostics"})
+run_codeql_database_create([], lang="java", runFunction = runUnsuccessfully, db = None, extra_env = {"CODEQL_EXTRACTOR_JAVA_DIAGNOSTIC_DIR": "diagnostics"})
 
 check_diagnostics(test_dir = ".", diagnostics_dir = "diagnostics")
