2c1c41d8a3e65ef29f8a677b34205e65c3bc48e1
erik-krogh <erik-krogh@github.com>
2023-01-27 18:47:08 +0100

    add test confirming end-to-end support for well-typed decorators with the new TS 5.0 type ClassMethodDecoratorContext

--- javascript/ql/test/library-tests/TypeScript/Types/tst.ts
+++ javascript/ql/test/library-tests/TypeScript/Types/tst.ts
@@ -424,3 +424,38 @@ module TS49 {
     }
   }
 }
+
+/////////////////
+
+module TS50 {
+    function loggedMethod<This, Args extends any[], Return>(
+        target: (this: This, ...args: Args) => Return,
+        context: ClassMethodDecoratorContext<This, (this: This, ...args: Args) => Return>
+    ) {
+        const methodName = String(context.name);
+    
+        function replacementMethod(this: This, ...args: Args): Return {
+            console.log(`LOG: Entering method '${methodName}'.`)
+            const result = target.call(this, ...args);
+            console.log(`LOG: Exiting method '${methodName}'.`)
+            return result;
+        }
+    
+        return replacementMethod;
+    }
+
+    class Person {
+        name: string;
+        constructor(name: string) {
+            this.name = name;
+        }
+    
+        @loggedMethod("")
+        greet() {
+            console.log(`Hello, my name is ${this.name}.`);
+            return 2;
+        }
+    }
+
+    const p = new Person("John").greet(); // <- number, part of well-typed decorators in TS 5.0.
+}
