960f776e29b1267680747898e73c06c6b66c6fca
Michael Nebel <michaelnebel@github.com>
2023-02-02 10:51:32 +0100

    C#: Avoid that the dotnet publish test refers to a specific .NET version.

--- csharp/ql/integration-tests/all-platforms/dotnet_publish/test.py
+++ csharp/ql/integration-tests/all-platforms/dotnet_publish/test.py
@@ -1,8 +1,9 @@
 import os
 from create_database_utils import *
 
-run_codeql_database_create(['dotnet publish'], test_db="default-db", db=None, lang="csharp")
+artifacts = 'bin/Temp'
+run_codeql_database_create([f"dotnet publish -o {artifacts}"], test_db="default-db", db=None, lang="csharp")
 
 ## Check that the publish folder is created.
-if not os.path.isdir("bin/Debug/net7.0/publish/"):
+if not os.path.isdir(artifacts):
     raise Exception("The publish artifact folder was not created.") 
