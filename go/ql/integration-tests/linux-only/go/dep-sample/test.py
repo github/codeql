7ebe472cfee6bed65bd44469789845009c3df59c
Owen Mansel-Chan <owen-mc@github.com>
2023-02-10 16:54:40 +0000

    Move repo files into subfolder
    This is so that the test-db doesn't get moved when all the repo files
    are moved, which was causing some problems.

--- go/ql/integration-tests/linux-only/go/dep-sample/test.py
+++ go/ql/integration-tests/linux-only/go/dep-sample/test.py
@@ -4,4 +4,4 @@ import sys
 from create_database_utils import *
 
 os.environ['LGTM_INDEX_IMPORT_PATH'] = "deptest"
-run_codeql_database_create([], lang="go")
+run_codeql_database_create([], lang="go", source="work")
--- go/ql/integration-tests/linux-only/go/glide-sample/test.py
+++ go/ql/integration-tests/linux-only/go/glide-sample/test.py
@@ -4,4 +4,4 @@ import sys
 from create_database_utils import *
 
 os.environ['LGTM_INDEX_IMPORT_PATH'] = "glidetest"
-run_codeql_database_create([], lang="go")
+run_codeql_database_create([], lang="go", source="work")
