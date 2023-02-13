b30f4c712d1f210207c16d8fa01009a89bee10f4
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-02-13 17:07:38 +0000

    Added 1.8.10 to kotlin version range

--- java/kotlin-extractor/kotlin_plugin_versions.py
+++ java/kotlin-extractor/kotlin_plugin_versions.py
@@ -25,7 +25,7 @@ def version_string_to_tuple(version):
 ci_version = '1.8.10'
 
 # Version numbers in the list need to be in semantically increasing order
-many_versions = [ '1.4.32', '1.5.0', '1.5.10', '1.5.20', '1.5.30', '1.6.0', '1.6.20', '1.7.0', '1.7.20', '1.8.0' ]
+many_versions = [ '1.4.32', '1.5.0', '1.5.10', '1.5.20', '1.5.30', '1.6.0', '1.6.20', '1.7.0', '1.7.20', '1.8.0', '1.8.10' ]
 
 many_versions_tuples = [version_string_to_tuple(v) for v in many_versions]
 
