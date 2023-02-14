b0c8992eef2f263197c26559716b5ea59b96e8bb
Chad Bentz <1760475+felickz@users.noreply.github.com>
2023-02-13 19:44:02 -0500

    Adding CWE-798 MSSQL Tests

---
+++ java/ql/test/query-tests/security/CWE-798/semmle/tests/HardcodedMSSQLCredentials.java
@@ -0,0 +1,9 @@
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
+public class HardcodedMSSQLCredentials {
+    public static void main(SQLServerDataSource ds) throws Exception {        
+        ds.setUser("Username"); // $ HardcodedCredentialsApiCall      
+        ds.setPassword("password"); // $ HardcodedCredentialsApiCall
+        ds.getConnection("Username", "password"); // $ HardcodedCredentialsApiCall
+      }
+}
---
+++ java/ql/test/stubs/mssql-jdbc-12.2.0/javax/naming/Referenceable.java
@@ -0,0 +1,10 @@
+// Generated automatically from javax.naming.Referenceable for testing purposes
+
+package javax.naming;
+
+import javax.naming.Reference;
+
+public interface Referenceable
+{
+    Reference getReference();
+}
