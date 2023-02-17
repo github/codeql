5d125572ec3daa563ff717b65432804d38036dfe
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2023-02-17 18:01:45 +0000

    Swift: Test for FileManager taint sources.

---
+++ swift/ql/test/library-tests/dataflow/flowsources/filemanager.swift
@@ -0,0 +1,52 @@
+// --- stubs ---
+
+class NSObject {
+}
+
+struct URL {
+}
+
+struct URLResourceKey {
+}
+
+struct Data {
+}
+
+class FileManager : NSObject {
+	struct DirectoryEnumerationOptions : OptionSet{
+    	let rawValue: Int
+	}
+
+	func contentsOfDirectory(at url: URL, includingPropertyForKeys keys: [URLResourceKey]?, options mask: FileManager.DirectoryEnumerationOptions = []) throws -> [URL] { return [] }
+	func contentsOfDirectory(atPath path: String) throws -> [String] { return [] }
+	func directoryContents(atPath path: String) -> [Any]? { return [] } // returns array of NSString
+	func subpathsOfDirectory(atPath path: String) throws -> [String] { return [] }
+	func subpaths(atPath path: String) -> [String]? { return [] }
+
+	func destinationOfSymbolicLink(atPath path: String) throws -> String { return "" }
+	func pathContentOfSymbolicLink(atPath path: String) -> String? { return "" }
+
+	func contents(atPath path: String) -> Data? { return nil }
+}
+
+// --- tests ---
+
+func testFileHandle(fm: FileManager, url: URL, path: String) {
+	do
+	{
+		let contents1 = try fm.contentsOfDirectory(at: url, includingPropertyForKeys: nil) // SOURCE
+		let content1 = contents1[0]
+		let contents2 = try fm.contentsOfDirectory(atPath: path) // SOURCE
+		let contents3 = fm.directoryContents(atPath: path)! // SOURCE
+
+		let subpaths1 = try fm.subpathsOfDirectory(atPath: path) // SOURCE
+		let subpaths2 = fm.subpaths(atPath: path)! // SOURCE
+
+		let link1 = try fm.destinationOfSymbolicLink(atPath: path) // SOURCE
+		let link2 = fm.pathContentOfSymbolicLink(atPath: path)! // SOURCE
+
+		let data = fm.contents(atPath: path)! // SOURCE
+	} catch {
+		// ...
+	}
+}
