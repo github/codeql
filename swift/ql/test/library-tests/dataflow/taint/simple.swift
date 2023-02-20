9b117fefd7ef2eb64aecffb6417b2effae31b70d
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2023-02-20 16:23:39 +0000

    Swift: Generalize the arithmetic we allow taint through.

--- swift/ql/test/library-tests/dataflow/taint/simple.swift
+++ swift/ql/test/library-tests/dataflow/taint/simple.swift
@@ -9,18 +9,18 @@ func sink(arg: Any) {}
 func taintThroughArithmetic() {
   // arithmetic
 
-  sink(arg: 1 + source()) // $ MISSING: tainted=
-  sink(arg: source() + 1) // $ MISSING: tainted=
-  sink(arg: 1 - source()) // $ MISSING: tainted=
-  sink(arg: source() - 1) // $ MISSING: tainted=
-  sink(arg: 2 * source()) // $ MISSING: tainted=
-  sink(arg: source() * 2) // $ MISSING: tainted=
-  sink(arg: 100 / source()) // $ MISSING: tainted=
-  sink(arg: source() / 100) // $ MISSING: tainted=
-  sink(arg: 100 % source()) // $ MISSING: tainted=
-  sink(arg: source() % 100) // $ MISSING: tainted=
+  sink(arg: 1 + source()) // $ tainted=12
+  sink(arg: source() + 1) // $ tainted=13
+  sink(arg: 1 - source()) // $ tainted=14
+  sink(arg: source() - 1) // $ tainted=15
+  sink(arg: 2 * source()) // $ tainted=16
+  sink(arg: source() * 2) // $ tainted=17
+  sink(arg: 100 / source()) // $ tainted=18
+  sink(arg: source() / 100) // $ tainted=19
+  sink(arg: 100 % source()) // $ tainted=20
+  sink(arg: source() % 100) // $ tainted=21
 
-  sink(arg: -source()) // $ MISSING: tainted=
+  sink(arg: -source()) // $ tainted=23
 
   // overflow operators
 
