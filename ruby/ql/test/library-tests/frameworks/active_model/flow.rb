2430cf2f125160be2aaa292e33bb3c7da82458da
Harry Maclean <hmac@github.com>
2023-02-27 14:20:32 +1300

    Ruby: Add another test

--- ruby/ql/test/library-tests/frameworks/active_model/flow.rb
+++ ruby/ql/test/library-tests/frameworks/active_model/flow.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   include ActiveModel::API
   
   attr_reader :name
+  attr_reader :age
 end
 
 user = User.new
@@ -9,3 +10,13 @@ user.name = source "a"
 
 h = user.serializable_hash
 sink h["name"] # $ hasValueFlow=a
+sink h["age"]
+sink h["unknown_field"]
+
+user2 = User.new
+user2.age = source "b"
+
+h2 = user2.serializable_hash
+sink h2["age"] # $ hasValueFlow=b
+sink h2["name"]
+sink h2["unknown_field"]
