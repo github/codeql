b6db0de4375eac606408ec1f027969c830869ace
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2023-03-01 09:26:30 +0000

    Swift: Add inline expectation results.

--- swift/ql/test/library-tests/dataflow/flowsources/nsstring.swift
+++ swift/ql/test/library-tests/dataflow/flowsources/nsstring.swift
@@ -28,16 +28,16 @@ func testNSStrings() {
 	do
 	{
 		let path = "file.txt"
-		let string1 = try NSString(contentsOfFile: path) // SOURCE
-		let string2 = try NSString(contentsOfFile: path, encoding: 0) // SOURCE
-		let string3 = try NSString(contentsOfFile: path, usedEncoding: nil) // SOURCE
-		let string4 = NSString.string(withContentsOfFile: path) // SOURCE
+		let string1 = try NSString(contentsOfFile: path) // $ $ source=local
+		let string2 = try NSString(contentsOfFile: path, encoding: 0) // $ source=local
+		let string3 = try NSString(contentsOfFile: path, usedEncoding: nil) // $ source=local
+		let string4 = NSString.string(withContentsOfFile: path) // $ source=local
 
 		let url = URL(string: "http://example.com/")!
-		let string5 = try NSString(contentsOf: url) // SOURCE
-		let string6 = try NSString(contentsOf: url, encoding: 0) // SOURCE
-		let string7 = try NSString(contentsOf: url, usedEncoding: nil) // SOURCE
-		let string8 = NSString.string(withContentsOf: url) // SOURCE
+		let string5 = try NSString(contentsOf: url) // $ source=remote
+		let string6 = try NSString(contentsOf: url, encoding: 0) // $ source=remote
+		let string7 = try NSString(contentsOf: url, usedEncoding: nil) // $ source=remote
+		let string8 = NSString.string(withContentsOf: url) // $ source=remote
 	} catch {
 		// ...
 	}
