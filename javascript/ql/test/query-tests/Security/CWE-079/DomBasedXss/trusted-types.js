856b50735d64144c219e18430758cd40f57a8ea5
Asger F <asgerf@github.com>
2023-03-07 13:04:26 +0100

    JS: Expand test case

--- javascript/ql/test/query-tests/Security/CWE-079/DomBasedXss/trusted-types.js
+++ javascript/ql/test/query-tests/Security/CWE-079/DomBasedXss/trusted-types.js
@@ -1,10 +1,13 @@
-(function() {
-    const policy1 = trustedTypes.createPolicy('x', { createHTML: x => x }); // NOT OK
-    policy1.createHTML(window.name);
+import * as lib from './trusted-types-lib';
 
-    const policy2 = trustedTypes.createPolicy('x', { createHTML: x => 'safe' }); // OK
-    policy2.createHTML(window.name);
+const policy1 = trustedTypes.createPolicy('x', { createHTML: x => x }); // NOT OK
+policy1.createHTML(window.name);
 
-    const policy3 = trustedTypes.createPolicy('x', { createHTML: x => x }); // OK
-    policy3.createHTML('safe');
-})();
+const policy2 = trustedTypes.createPolicy('x', { createHTML: x => 'safe' }); // OK
+policy2.createHTML(window.name);
+
+const policy3 = trustedTypes.createPolicy('x', { createHTML: x => x }); // OK
+policy3.createHTML('safe');
+
+const policy4 = trustedTypes.createPolicy('x', { createHTML: lib.createHtml });
+policy4.createHTML(window.name);
