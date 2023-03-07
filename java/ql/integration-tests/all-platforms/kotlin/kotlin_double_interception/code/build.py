81e71c4669e291336a9d3a8e19df760f847dddef
Ian Lynagh <igfoo@github.com>
2023-03-07 15:29:01 +0000

    Kotlin: Add a test for double niterception

---
+++ java/ql/integration-tests/all-platforms/kotlin/kotlin_double_interception/code/build.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import glob
+import re
+from create_database_utils import *
+
+# This is a normal intercepted compilation
+runSuccessfully([get_cmd('kotlinc'), 'normal.kt'])
+
+# Find the extractor jar that is being used
+trapDir = os.environ['CODEQL_EXTRACTOR_JAVA_TRAP_DIR']
+invocationTraps = glob.glob('invocations/*.trap', root_dir = trapDir)
+if len(invocationTraps) != 1:
+    raise Exception('Expected to find 1 invocation TRAP, but found ' + str(invocationTraps))
+invocationTrap = trapDir + '/' + invocationTraps[0]
+with open(invocationTrap, 'r') as f:
+    content = f.read()
+    m = re.search('^// Using extractor: (.*)$', content, flags = re.MULTILINE)
+    extractorJar = m.group(1)
+
+def getManualFlags(invocationTrapName):
+    return ['-Xplugin=' + extractorJar, '-P', 'plugin:kotlin-extractor:invocationTrapFile=' + trapDir + '/invocations/' + invocationTrapName + '.trap']
+
+# This is both normally intercepted, and it has the extractor flags manually added
+runUnsuccessfully([get_cmd('kotlinc'), 'doubleIntercepted.kt'] + getManualFlags('doubleIntercepted'))
+os.environ['CODEQL_EXTRACTOR_JAVA_AGENT_DISABLE_KOTLIN'] = 'true'
+# We don't see this compilation at all
+runSuccessfully([get_cmd('kotlinc'), 'notSeen.kt'])
+# This is extracted as it has the extractor flags manually added
+runSuccessfully([get_cmd('kotlinc'), 'manual.kt'] + getManualFlags('manual'))
