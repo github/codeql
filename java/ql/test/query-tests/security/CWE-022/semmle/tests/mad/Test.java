f07f0888aadcdb71659608d466ff3cd7b29f3fc1
Tony Torralba <atorralba@users.noreply.github.com>
2023-03-10 10:09:16 +0100

    Fix tests

--- java/ql/test/query-tests/security/CWE-022/semmle/tests/mad/Test.java
+++ java/ql/test/query-tests/security/CWE-022/semmle/tests/mad/Test.java
@@ -8,27 +8,30 @@ import org.codehaus.cargo.container.installer.ZipURLInstaller;
 
 public class Test {
 
+    public Object source(InetAddress address) {
+        return address.getHostName();
+    }
+
     void test(InetAddress address) throws IOException {
-        String t = address.getHostName();
         // "java.lang;Module;true;getResourceAsStream;(String);;Argument[0];read-file;ai-generated"
-        getClass().getModule().getResourceAsStream(t);
+        getClass().getModule().getResourceAsStream((String) source(null));
         // "java.lang;Class;false;getResource;(String);;Argument[0];read-file;ai-generated"
-        getClass().getResource(t);
+        getClass().getResource((String) source(null));
         // "java.lang;ClassLoader;true;getSystemResourceAsStream;(String);;Argument[0];read-file;ai-generated"
-        ClassLoader.getSystemResource(t);
+        ClassLoader.getSystemResource((String) source(null));
         // "java.io;File;true;createTempFile;(String,String,File);;Argument[2];create-file;ai-generated"
-        File.createTempFile(";", t);
+        File.createTempFile(";", (String) source(null));
         // "java.io;File;true;renameTo;(File);;Argument[0];create-file;ai-generated"
-        new File("").renameTo((File) t);
+        new File("").renameTo((File) source(null));
         // "java.io;FileInputStream;true;FileInputStream;(File);;Argument[0];read-file;ai-generated"
-        new FileInputStream((File) t);
+        new FileInputStream((File) source(null));
         // "java.io;FileReader;true;FileReader;(File);;Argument[0];read-file;ai-generated"
-        new FileReader((File) t);
+        new FileReader((File) source(null));
         // "java.io;FileReader;true;FileReader;(String);;Argument[0];read-file;ai-generated"
-        new FileReader(t);
+        new FileReader((String) source(null));
         // "org.codehaus.cargo.container.installer;ZipURLInstaller;true;ZipURLInstaller;(URL,String,String);;Argument[1];create-file;ai-generated"
-        new ZipURLInstaller((URL) null, t, "");
+        new ZipURLInstaller((URL) null, (String) source(null), "");
         // "org.codehaus.cargo.container.installer;ZipURLInstaller;true;ZipURLInstaller;(URL,String,String);;Argument[2];create-file;ai-generated"
-        new ZipURLInstaller((URL) null, "", t);
+        new ZipURLInstaller((URL) null, "", (String) source(null));
     }
 }
--- java/ql/test/query-tests/security/CWE-918/mad/Test.java
+++ java/ql/test/query-tests/security/CWE-918/mad/Test.java
@@ -1,3 +1,4 @@
+import java.net.URL;
 import javax.servlet.http.HttpServletRequest;
 import javafx.scene.web.WebEngine;
 import org.codehaus.cargo.container.installer.ZipURLInstaller;
@@ -9,14 +10,13 @@ public class Test {
     }
 
     public void test(WebEngine webEngine) {
-        String taint = source(null);
         // "javafx.scene.web;WebEngine;false;load;(String);;Argument[0];open-url;ai-generated"
-        webEngine.load(taint); // $ SSRF
+        webEngine.load((String) source(null)); // $ SSRF
     }
 
     public void test() {
         // "org.codehaus.cargo.container.installer;ZipURLInstaller;true;ZipURLInstaller;(URL,String,String);;Argument[0];open-url:ai-generated"
-        new ZipURLInstaller((URL) source(), "", ""); // $ SSRF
+        new ZipURLInstaller((URL) source(null), "", ""); // $ SSRF
     }
 
 }
