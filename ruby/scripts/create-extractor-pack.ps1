7ee7a0df34abc646c185c6e40d369a8f78493a02
Harry Maclean <hmac@github.com>
2023-03-14 13:14:25 +1300

    Ruby: fix prefix in create-extractor-pack scripts

--- ruby/scripts/create-extractor-pack.ps1
+++ ruby/scripts/create-extractor-pack.ps1
@@ -1,7 +1,8 @@
 cd extractor
 cargo build --release
-cargo run --release --bin generator -- --dbscheme ../ql/lib/ruby.dbscheme --library ../ql/lib/codeql/ruby/ast/internal/TreeSitter.qll
 cd ..
+
+extractor\target\release\generator --dbscheme ql/lib/ruby.dbscheme --library ql/lib/codeql/ruby/ast/internal/TreeSitter.qll
 
 codeql query format -i ql\lib\codeql/ruby\ast\internal\TreeSitter.qll
 
@@ -11,5 +12,5 @@ cp codeql-extractor.yml, ql\lib\ruby.dbscheme, ql\lib\ruby.dbscheme.stats extrac
 cp -Recurse tools extractor-pack
 cp -Recurse downgrades extractor-pack
 mkdir extractor-pack\tools\win64 | Out-Null
-cp extractor\target\release\ruby-extractor.exe extractor-pack\tools\win64\extractor.exe
-cp extractor\target\release\ruby-autobuilder.exe extractor-pack\tools\win64\autobuilder.exe
+cp extractor\target\release\extractor.exe extractor-pack\tools\win64\extractor.exe
+cp extractor\target\release\autobuilder.exe extractor-pack\tools\win64\autobuilder.exe
--- ruby/scripts/create-extractor-pack.sh
+++ ruby/scripts/create-extractor-pack.sh
@@ -13,10 +13,8 @@ else
   exit 1
 fi
 
-pushd extractor
-"$CARGO" build --release
-"$CARGO" run --release --bin generator -- --dbscheme ../ql/lib/ruby.dbscheme --library ../ql/lib/codeql/ruby/ast/internal/TreeSitter.qll
-popd
+(cd extractor && "$CARGO" build --release)
+extractor/target/release/generator --dbscheme ql/lib/ruby.dbscheme --library ql/lib/codeql/ruby/ast/internal/TreeSitter.qll
 
 codeql query format -i ql/lib/codeql/ruby/ast/internal/TreeSitter.qll
 
@@ -24,5 +22,5 @@ rm -rf extractor-pack
 mkdir -p extractor-pack
 cp -r codeql-extractor.yml downgrades tools ql/lib/ruby.dbscheme ql/lib/ruby.dbscheme.stats extractor-pack/
 mkdir -p extractor-pack/tools/${platform}
-cp extractor/target/release/ruby-extractor extractor-pack/tools/${platform}/extractor
-cp extractor/target/release/ruby-autobuilder extractor-pack/tools/${platform}/autobuilder
+cp extractor/target/release/extractor extractor-pack/tools/${platform}/extractor
+cp extractor/target/release/autobuilder extractor-pack/tools/${platform}/autobuilder
