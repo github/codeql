fe1cc405ebbf31a7d4d70bac72028c99747219b4
Tony Torralba <atorralba@users.noreply.github.com>
2023-03-14 18:21:38 +0100

    org.kohsuke.stapler.framework.adjunct tests

---
+++ java/ql/test/stubs/javax-servlet-2.5/javax/servlet/ServletContextEvent.java
@@ -0,0 +1,20 @@
+// Generated automatically from javax.servlet.ServletContextEvent for testing purposes
+
+package javax.servlet;
+
+import java.util.EventObject;
+import javax.servlet.ServletContext;
+
+public class ServletContextEvent extends EventObject {
+    protected ServletContextEvent() {
+        super(null);
+    }
+
+    public ServletContext getServletContext() {
+        return null;
+    }
+
+    public ServletContextEvent(ServletContext p0) {
+        super(null);
+    }
+}
