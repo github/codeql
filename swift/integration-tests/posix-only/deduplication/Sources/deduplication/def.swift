dcca0278b8f999f23ee876450a917acdf6e5eb80
Paolo Tranquilli <redsun82@github.com>
2023-03-14 17:22:10 +0100

    Swift: add more functions to deduplication test

--- swift/integration-tests/posix-only/deduplication/Sources/deduplication/def.swift
+++ swift/integration-tests/posix-only/deduplication/Sources/deduplication/def.swift
@@ -6,4 +6,19 @@ var def_instantiated_generic = Generic<Int>()
 
 func def_function(_: Int) -> Int { return 42 }
 
+func def_function_overloaded_on_return(_: Int, _: Double) -> Int { return 0 }
+func def_function_overloaded_on_return(_: Int, _: Double) -> Double { return 0.0 }
+
+func def_function_overloaded_on_parameter_type(_: Int) {}
+func def_function_overloaded_on_parameter_type(_: Double) {}
+
+func def_function_overloaded_on_parameter_label(one x: Int) {}
+func def_function_overloaded_on_parameter_label(two x: Int) {}
+
+func def_throwing_function(_: Int) throws {}
+func def_rethrowing_function(_: () throws -> ()) rethrows {}
+func def_async_function(_: Int) async {}
+func def_async_throwing_function(_: Int) async throws {}
+func def_async_rethrowing_function(_: () throws -> ()) async rethrows {}
+
 func def_generic_function<A, B>(_: A, _: B) {}
