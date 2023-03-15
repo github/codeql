c5a1905302d3d69dc68112b46270ba4b8e91048c
Tony Torralba <atorralba@users.noreply.github.com>
2023-03-15 12:43:45 +0100

    Fix stubs

--- java/ql/test/stubs/javax-servlet-2.5/javax/servlet/ServletException.java
+++ java/ql/test/stubs/javax-servlet-2.5/javax/servlet/ServletException.java
@@ -23,11 +23,5 @@
 
 package javax.servlet;
 
-import java.io.IOException;
-
-public interface RequestDispatcher {
-    public void forward(ServletRequest request, ServletResponse response)
-            throws ServletException, IOException;
-    public void include(ServletRequest request, ServletResponse response)
-            throws ServletException, IOException;
+public class ServletException extends Exception {
 }
