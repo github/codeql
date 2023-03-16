3a04e42ae05b04a92564585bbed2ae2e5f8cbe91
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2023-03-16 17:04:42 +0000

    Swift: Add taint test for string interpolation.

---
+++ swift/ql/test/library-tests/dataflow/taint/core/stringinterpolation.swift
@@ -0,0 +1,33 @@
+func clean() -> String { return ""; }
+func source() -> String { return ""; }
+func sink(arg: String) {}
+
+class MyStringPair {
+	var first: String = ""
+	var second: String = ""
+}
+
+extension String.StringInterpolation {
+	mutating func appendInterpolation(_ pair: MyStringPair) {
+		// for MyStringPair, string interpolation outputs just the `first` value.
+		appendInterpolation("first is: \(pair.first)")
+	}
+}
+
+func taintThroughCustomStringInterpolation() {
+	let p1 = MyStringPair()
+	p1.first = source()
+	p1.second = clean()
+
+	sink(arg: "pair: \(p1.first)") // $ tainted=19
+	sink(arg: "pair: \(p1.second)")
+	sink(arg: "pair: \(p1)") // $ tainted=19
+
+	let p2 = MyStringPair()
+	p2.first = clean()
+	p2.second = source()
+
+	sink(arg: "pair: \(p2.first)")
+	sink(arg: "pair: \(p2.second)") // $ tainted=28
+	sink(arg: "pair: \(p2)")
+}
