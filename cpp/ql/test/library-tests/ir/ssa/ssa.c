3daefa8b9e26ef031dd903f5c48dff03fe185869
Mathias Vorreiter Pedersen <mathiasvp@github.com>
2023-04-04 10:54:12 +0100

    C++: Add more tests.

--- cpp/ql/test/library-tests/ir/ssa/ssa.c
+++ cpp/ql/test/library-tests/ir/ssa/ssa.c
@@ -6,3 +6,16 @@ void named_designators() {
 void repeated_designators() {
   int x[1] = {[0] = 1234, [0] = 5678};
 }
+
+struct Foo2 { int x; int y; };
+void named_designators_2() {
+  struct Foo2 foo = {.x = 1234, .y = 5678};
+
+  struct Foo2 foo_swapped = {.y = 5678, .x = 1234};
+}
+
+void non_repeated_designators() {
+  int x[2] = {[0] = 1234, [1] = 5678};
+
+  int y[2] = {[1] = 1234, [0] = 5678};
+}
