b8c55612e5e5c6eda3435b40760a87e9bf789c88
Paolo Tranquilli <redsun82@github.com>
2023-04-04 17:21:53 +0200

    Swift: route compiler diagnostics through our log

--- swift/extractor/invocation/SwiftDiagnosticsConsumer.cpp
+++ swift/extractor/invocation/SwiftDiagnosticsConsumer.cpp
@@ -1,5 +1,5 @@
 #include "swift/extractor/invocation/SwiftDiagnosticsConsumer.h"
-#include "swift/extractor/trap/generated/TrapEntries.h"
+#include "swift/extractor/trap/generated/TrapClasses.h"
 #include "swift/extractor/trap/TrapDomain.h"
 #include "swift/extractor/infra/SwiftDiagnosticKind.h"
 
@@ -13,13 +13,17 @@ using namespace codeql;
 
 void SwiftDiagnosticsConsumer::handleDiagnostic(swift::SourceManager& sourceManager,
                                                 const swift::DiagnosticInfo& diagInfo) {
-  auto message = getDiagMessage(sourceManager, diagInfo);
-  DiagnosticsTrap diag{};
-  diag.id = trap.createLabel<DiagnosticsTag>();
+  if (diagInfo.IsChildNote) return;
+  Diagnostics diag{trap.createLabel<DiagnosticsTag>()};
   diag.kind = translateDiagnosticsKind(diagInfo.Kind);
-  diag.text = message;
+  diag.text = getDiagMessage(sourceManager, diagInfo);
   trap.emit(diag);
-  locationExtractor.attachLocation(sourceManager, diagInfo.Loc, diagInfo.Loc, diag.id);
+  locationExtractor.attachLocation(sourceManager, diagInfo.Loc, diag.id);
+
+  forwardToLog(sourceManager, diagInfo, diag.text);
+  for (const auto& child : diagInfo.ChildDiagnosticInfo) {
+    forwardToLog(sourceManager, *child);
+  }
 }
 
 std::string SwiftDiagnosticsConsumer::getDiagMessage(swift::SourceManager& sourceManager,
@@ -28,4 +32,30 @@ std::string SwiftDiagnosticsConsumer::getDiagMessage(swift::SourceManager& sourc
   llvm::raw_svector_ostream out(text);
   swift::DiagnosticEngine::formatDiagnosticText(out, diagInfo.FormatString, diagInfo.FormatArgs);
   return text.str().str();
+}
+
+void SwiftDiagnosticsConsumer::forwardToLog(swift::SourceManager& sourceManager,
+                                            const swift::DiagnosticInfo& diagInfo,
+                                            const std::string& message) {
+  auto file = sourceManager.getDisplayNameForLoc(diagInfo.Loc);
+  auto [line, column] = sourceManager.getLineAndColumnInBuffer(diagInfo.Loc);
+  using Kind = swift::DiagnosticKind;
+  switch (diagInfo.Kind) {
+    case Kind::Error:
+      LOG_ERROR("{}:{}:{} {}", file, line, column, message);
+      break;
+    case Kind::Warning:
+      LOG_WARNING("{}:{}:{} {}", file, line, column, message);
+      break;
+    case Kind::Remark:
+      LOG_INFO("{}:{}:{} {}", file, line, column, message);
+      break;
+    case Kind::Note:
+      LOG_DEBUG("{}:{}:{} {}", file, line, column, message);
+      break;
+    default:
+      LOG_ERROR("unknown diagnostic kind {}, {}:{}:{} {}", diagInfo.Kind, file, line, column,
+                message);
+      break;
+  }
 }
