4033ed33d88a10fbf091dd902e5ad59f133369f3
Mathias Vorreiter Pedersen <mathiasvp@github.com>
2023-04-04 13:50:24 +0100

    C++: Add evil testcase.

--- cpp/ql/test/library-tests/ir/ssa/ssa.c
+++ cpp/ql/test/library-tests/ir/ssa/ssa.c
@@ -19,3 +19,12 @@ void non_repeated_designators() {
 
   int y[2] = {[1] = 1234, [0] = 5678};
 }
+
+struct Foo_array_and_int {
+ int x[2];
+ int y;
+};
+
+void test_foo_array_and_int() {
+  struct Foo_array_and_int f = { .x = {0, 1}, .x[0] = 42, .y = 42 };
+}
