7fb8819a08ff977fd9f488a26ceeb339d00eaccf
Jami Cogswell <jcogs33@Jamis-MacBook-Pro.local>
2023-04-07 12:44:43 -0400

    Java: fix generated stub causing lang test failure

--- java/ql/test/stubs/apache-http-5/org/apache/hc/core5/http/HttpEntity.java
+++ java/ql/test/stubs/apache-http-5/org/apache/hc/core5/http/HttpEntity.java
@@ -3,6 +3,7 @@
 package org.apache.hc.core5.http;
 
 import java.io.Closeable;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
@@ -12,9 +13,9 @@ import org.apache.hc.core5.http.Header;
 
 public interface HttpEntity extends Closeable, EntityDetails
 {
-    InputStream getContent();
+    InputStream getContent() throws IOException, UnsupportedOperationException;
     Supplier<List<? extends Header>> getTrailers();
     boolean isRepeatable();
     boolean isStreaming();
-    void writeTo(OutputStream p0);
+    void writeTo(OutputStream p0) throws IOException;
 }
