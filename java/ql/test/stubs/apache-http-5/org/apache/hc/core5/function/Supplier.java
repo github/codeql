fde855b25f16c26eaf86d69c6272949624fbccb8
Jami Cogswell <jcogs33@Jamis-MacBook-Pro.local>
2023-04-11 16:55:08 -0400

    Java: temp removal of stubs

--- java/ql/test/stubs/apache-http-5/org/apache/hc/core5/function/Supplier.java
+++
@@ -1,9 +0,0 @@
-// Generated automatically from org.apache.hc.core5.function.Supplier for testing purposes
-
-package org.apache.hc.core5.function;
-
-
-public interface Supplier<T>
-{
-    T get();
-}
--- java/ql/test/stubs/apache-http-5/org/apache/hc/core5/http/nio/ResourceHolder.java
+++
@@ -1,9 +0,0 @@
-// Generated automatically from org.apache.hc.core5.http.nio.ResourceHolder for testing purposes
-
-package org.apache.hc.core5.http.nio;
-
-
-public interface ResourceHolder
-{
-    void releaseResources();
-}
