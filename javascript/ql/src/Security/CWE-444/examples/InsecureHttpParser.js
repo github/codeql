2e27447c65ca63973d24cf5758d22636c5614838
Nate Johnson <nathanjohnson@cedarville.edu>
2023-04-18 00:41:11 -0400

    Include example

--- javascript/ql/src/Security/CWE-444/examples/InsecureHttpParser.js
+++ javascript/ql/src/Security/CWE-444/examples/InsecureHttpParser.js
@@ -1,37 +1,7 @@
-// test NodeJS
-const https = require('node:https');
 const http = require('node:http');
-
-https.createServer({
-    insecureHTTPParser: true
-}, (req, res) => {
-    res.writeHead(200);
-    res.end('hello world\n');
-});
 
 http.createServer({
     insecureHTTPParser: true
 }, (req, res) => {
-    res.writeHead(200);
-    res.end('hello world\n');
-});
-
-http.get({ insecureHTTPParser: true }, (res) => {
-    res.writeHead(200);
-    res.end('hello world\n');
-});
-
-http.get('url', { insecureHTTPParser: true }, (res) => {
-    res.writeHead(200);
-    res.end('hello world\n');
-});
-
-http.request({ insecureHTTPParser: true }, (res) => {
-    res.writeHead(200);
-    res.end('hello world\n');
-});
-
-http.request('url', { insecureHTTPParser: true }, (res) => {
-    res.writeHead(200);
     res.end('hello world\n');
 });
