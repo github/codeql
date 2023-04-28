fd2f0257b6ab10f661be97576d270995597439a5
Mathias Vorreiter Pedersen <mathiasvp@github.com>
2023-04-28 10:00:45 +0100

    C++: Accept query changes.

--- cpp/ql/test/query-tests/Likely Bugs/Format/NonConstantFormat/test.cpp
+++ cpp/ql/test/query-tests/Likely Bugs/Format/NonConstantFormat/test.cpp
@@ -48,7 +48,7 @@ int main(int argc, char **argv) {
   printf(choose_message(argc - 1), argc - 1); // GOOD
   printf(messages[1]); // GOOD
   printf(message); // GOOD
-  printf(make_message(argc - 1)); // BAD  [NOT DETECTED]
+  printf(make_message(argc - 1)); // BAD
   printf("Hello, World\n"); // GOOD
   printf(_("Hello, World\n")); // GOOD
   {
