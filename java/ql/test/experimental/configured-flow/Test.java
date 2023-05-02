71387d5fa88b6c44d3a411a5916c53d96924d2af
Michael Nebel <michaelnebel@github.com>
2023-05-02 13:35:14 +0200

    Java: Separate the testcases in multiple methods and update the expected test output.

--- java/ql/test/experimental/configured-flow/Test.java
+++ java/ql/test/experimental/configured-flow/Test.java
@@ -2,27 +2,27 @@ import java.sql.*;
 import java.net.*;
 
 class Test {
-  public static void main(String[] args) throws Exception {
-    Connection conn = DriverManager.getConnection("");
-    Statement readStmt = conn.createStatement();
-    Statement writeStmt = conn.createStatement();
-    Socket sock = new Socket("localhost", 1234);
-
-    // Source only if "sql" is a selected threat model
-    ResultSet rs = readStmt.executeQuery("SELECT * FROM foo");
-
+  public void M1(Statement handle) throws Exception {
     // Only a source if "remote" is a selected threat model
+    Socket sock = new Socket("localhost", 1234);
     int val = sock.getInputStream().read();
 
     // Sink
     sock.getOutputStream().write(val);
 
     // Sink
-    writeStmt.executeUpdate("INSERT INTO foo VALUES ('" + val + "')");
+    handle.executeUpdate("INSERT INTO foo VALUES ('" + val + "')");
+  }
+
+  public void M2(Statement handle) throws Exception {    
+    // Only a source if "sql" is a selected threat model
+    ResultSet rs = handle.executeQuery("SELECT * FROM foo");
 
     // Sink
-    writeStmt.executeUpdate("INSERT INTO foo VALUES ('" + rs.getString("name") + "')");
+    handle.executeUpdate("INSERT INTO foo VALUES ('" + rs.getString("name") + "')");
+
     // Sink
+    Socket sock = new Socket("localhost", 1234);
     sock.getOutputStream().write(rs.getString("name").getBytes());
   }
 }
