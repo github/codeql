be92661eeb862fce0440b41fe9f3e01571ad65df
Michael Nebel <michaelnebel@github.com>
2023-05-02 14:11:28 +0200

    Java: Update test to use InputStream.read(byte[] data) and update expected test output.

--- java/ql/test/experimental/configured-flow/Test.java
+++ java/ql/test/experimental/configured-flow/Test.java
@@ -1,17 +1,23 @@
 import java.sql.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 class Test {
+  private String byteToString(byte[] data) {
+    return new String(data, StandardCharsets.UTF_8);
+  }
+
   public void M1(Statement handle) throws Exception {
     // Only a source if "remote" is a selected threat model
     Socket sock = new Socket("localhost", 1234);
-    int val = sock.getInputStream().read();
+    byte[] data = new byte[1024];
+    sock.getInputStream().read(data);
 
     // Sink
-    sock.getOutputStream().write(val);
+    sock.getOutputStream().write(data);
 
     // Sink
-    handle.executeUpdate("INSERT INTO foo VALUES ('" + val + "')");
+    handle.executeUpdate("INSERT INTO foo VALUES ('" + byteToString(data) + "')");
   }
 
   public void M2(Statement handle) throws Exception {    
