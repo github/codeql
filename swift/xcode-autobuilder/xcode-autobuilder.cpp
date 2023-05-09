c507754324388540b0c9efeaf49ead296c57cfaf
Paolo Tranquilli <redsun82@github.com>
2023-05-09 16:14:51 +0200

    Swift: surface error about no viable swift targets found

--- swift/xcode-autobuilder/xcode-autobuilder.cpp
+++ swift/xcode-autobuilder/xcode-autobuilder.cpp
@@ -5,11 +5,23 @@
 #include "swift/xcode-autobuilder/XcodeBuildRunner.h"
 #include "swift/xcode-autobuilder/XcodeProjectParser.h"
 #include "swift/logging/SwiftLogging.h"
+#include "swift/xcode-autobuilder/CustomizingBuildDiagnostics.h"
 
-static const char* Application = "com.apple.product-type.application";
-static const char* Framework = "com.apple.product-type.framework";
+static const char* uiTest = "com.apple.product-type.bundle.ui-testing";
+static const char* unitTest = "com.apple.product-type.bundle.unit-test";
 
 const std::string_view codeql::programName = "autobuilder";
+
+namespace codeql_diagnostics {
+constexpr codeql::SwiftDiagnosticsSource no_swift_target{
+    "no_swift_target", "No Swift compilation target found", customizingBuildAction,
+    customizingBuildHelpLinks};
+}  // namespace codeql_diagnostics
+
+static codeql::Logger& logger() {
+  static codeql::Logger ret{"main"};
+  return ret;
+}
 
 struct CLIArgs {
   std::string workingDir;
@@ -18,11 +30,13 @@ struct CLIArgs {
 
 static void autobuild(const CLIArgs& args) {
   auto targets = collectTargets(args.workingDir);
-
-  // Filter out non-application/framework targets
+  for (const auto& t : targets) {
+    LOG_INFO("{}", t);
+  }
+  // Filter out targets that are tests or have no swift source files
   targets.erase(std::remove_if(std::begin(targets), std::end(targets),
                                [&](Target& t) -> bool {
-                                 return t.type != Application && t.type != Framework;
+                                 return t.fileCount == 0 || t.type == uiTest || t.type == unitTest;
                                }),
                 std::end(targets));
 
@@ -30,15 +44,12 @@ static void autobuild(const CLIArgs& args) {
   std::sort(std::begin(targets), std::end(targets),
             [](Target& lhs, Target& rhs) { return lhs.fileCount > rhs.fileCount; });
 
-  for (auto& t : targets) {
-    std::cerr << t.workspace << " " << t.project << " " << t.type << " " << t.name << " "
-              << t.fileCount << "\n";
-  }
   if (targets.empty()) {
-    std::cerr << "[xcode autobuilder] Suitable targets not found\n";
+    DIAGNOSE_ERROR(no_swift_target, "All targets found within Xcode projects or workspaces either "
+                                    "have no Swift sources or are tests");
     exit(1);
   }
-
+  LOG_INFO("Selected {}", targets.front());
   buildTarget(targets.front(), args.dryRun);
 }
 
@@ -61,5 +72,6 @@ static CLIArgs parseCLIArgs(int argc, char** argv) {
 int main(int argc, char** argv) {
   auto args = parseCLIArgs(argc, argv);
   autobuild(args);
+  codeql::Log::flush();
   return 0;
 }
