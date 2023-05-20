e345d7dca488d305203c963a6af5d884ad1e0100
Sim4n6 <sim4n6@gmail.com>
2023-05-20 12:54:03 +0100

    Update ruby/ql/src/experimental/cwe-176/examples/unicode_normalization.rb
    Co-authored-by: Arthur Baars <aibaars@github.com>

--- ruby/ql/src/experimental/cwe-176/examples/unicode_normalization.rb
+++ ruby/ql/src/experimental/cwe-176/examples/unicode_normalization.rb
@@ -1,7 +1,7 @@
 class UnicodeNormalizationHtMLSafeController < ActionController::Base
   def unicodeNormalize
     unicode_input = params[:unicode_input]
-    unicode_html_safe = unicode_input.html_safe
+    unicode_html_safe = CGI.escapeHTML(unicode_input).html_safe
     normalized_nfkc = unicode_html_safe.unicode_normalize(:nfkc) # $result=BAD
     normalized_nfc = unicode_html_safe.unicode_normalize(:nfc) # $result=BAD
   end
