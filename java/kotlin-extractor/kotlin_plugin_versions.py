de876d5af12dbe3baa6ac9bdd4a188106e043362
Ian Lynagh <igfoo@github.com>
2023-05-24 13:54:22 +0100

    Kotlin: Don't require that minor versions match

--- java/kotlin-extractor/kotlin_plugin_versions.py
+++ java/kotlin-extractor/kotlin_plugin_versions.py
@@ -42,17 +42,12 @@ def get_single_version(fakeVersionOutput = None):
     if m is None:
         raise Exception('Cannot detect version of kotlinc (got ' + str(versionOutput) + ')')
     current_version = version_string_to_tuple(m.group(1))
-    matching_minor_versions = [ version for version in many_versions_tuples if version[0:2] == current_version[0:2] ]
-    if len(matching_minor_versions) == 0:
-        raise Exception(f'Cannot find a matching minor version for kotlinc version {current_version} (got {versionOutput}; know about {str(many_versions)})')
 
-    matching_minor_versions.sort(reverse = True)
+    many_versions_tuples.sort(reverse = True)
 
-    for version in matching_minor_versions:
+    for version in many_versions_tuples:
         if version[0:3] <= current_version[0:3]:
             return version_tuple_to_string(version)
-
-    return version_tuple_to_string(matching_minor_versions[-1])
 
     raise Exception(f'No suitable kotlinc version found for {current_version} (got {versionOutput}; know about {str(many_versions)})')
 
