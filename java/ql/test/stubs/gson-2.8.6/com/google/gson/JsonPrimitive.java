d3d67f0fb07406242913e9443398a9fa2238c8bf
Tony Torralba <atorralba@users.noreply.github.com>
2023-05-30 17:52:00 +0200

    Add tests & stubs

---
+++ java/ql/test/stubs/gson-2.8.6/com/google/gson/JsonPrimitive.java
@@ -0,0 +1,34 @@
+// Generated automatically from com.google.gson.JsonPrimitive for testing purposes
+
+package com.google.gson;
+
+import com.google.gson.JsonElement;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class JsonPrimitive extends JsonElement
+{
+    protected JsonPrimitive() {}
+    public BigDecimal getAsBigDecimal(){ return null; }
+    public BigInteger getAsBigInteger(){ return null; }
+    public JsonPrimitive deepCopy(){ return null; }
+    public JsonPrimitive(Boolean p0){}
+    public JsonPrimitive(Character p0){}
+    public JsonPrimitive(Number p0){}
+    public JsonPrimitive(String p0){}
+    public Number getAsNumber(){ return null; }
+    public String getAsString(){ return null; }
+    public boolean equals(Object p0){ return false; }
+    public boolean getAsBoolean(){ return false; }
+    public boolean isBoolean(){ return false; }
+    public boolean isNumber(){ return false; }
+    public boolean isString(){ return false; }
+    public byte getAsByte(){ return 0; }
+    public char getAsCharacter(){ return '0'; }
+    public double getAsDouble(){ return 0; }
+    public float getAsFloat(){ return 0; }
+    public int getAsInt(){ return 0; }
+    public int hashCode(){ return 0; }
+    public long getAsLong(){ return 0; }
+    public short getAsShort(){ return 0; }
+}
