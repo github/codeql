de974cc18a4792e7329de3f4323f42e4485048b0
Jeroen Ketema <jketema@github.com>
2023-05-30 18:15:59 +0200

    C++: Add `cpp/invalid-pointer-deref` test case that shows some duplicate results

--- cpp/ql/test/experimental/query-tests/Security/CWE/CWE-193/pointer-deref/test.cpp
+++ cpp/ql/test/experimental/query-tests/Security/CWE/CWE-193/pointer-deref/test.cpp
@@ -350,3 +350,11 @@ void test24(unsigned size) {
     int val = *xs++; // GOOD [FALSE POSITIVE]
   }
 }
+
+void test25(unsigned size) {
+  char *xs = new char[size];
+  char *end = xs + size;
+  char *end_plus_one = end + 1;
+  int val1 = *end_plus_one; // BAD
+  int val2 = *(end_plus_one + 1); // BAD
+}
