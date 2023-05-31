1e081058637d99a48f6714be43d891742995ee5c
erik-krogh <erik-krogh@github.com>
2023-05-31 13:51:38 +0200

    less duplicated headers in the sql-injection samples

--- javascript/ql/src/Security/CWE-089/examples/NoSqlInjectionFix.js
+++ javascript/ql/src/Security/CWE-089/examples/NoSqlInjectionFix.js
@@ -1,14 +1,3 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Todo = mongoose.model(
-  "Todo",
-  new mongoose.Schema({ text: { type: String } }, { timestamps: true })
-);
-
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 app.delete("/api/delete", async (req, res) => {
   let id = req.body.id;
   await Todo.deleteOne({ _id: { $eq: id } }); // GOOD: using $eq operator for the comparison
--- javascript/ql/src/Security/CWE-089/examples/NoSqlInjectionFix2.js
+++ javascript/ql/src/Security/CWE-089/examples/NoSqlInjectionFix2.js
@@ -1,14 +1,3 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Todo = mongoose.model(
-  "Todo",
-  new mongoose.Schema({ text: { type: String } }, { timestamps: true })
-);
-
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 app.delete("/api/delete", async (req, res) => {
   let id = req.body.id;
   if (typeof id !== "string") {
