9aeba4f31edcaaa88263f4b8039452581d944ff4
erik-krogh <erik-krogh@github.com>
2023-06-01 17:24:44 +0200

    changes based on review

--- javascript/ql/src/Security/CWE-089/examples/SqlInjectionFix.js
+++ javascript/ql/src/Security/CWE-089/examples/SqlInjectionFix.js
@@ -5,7 +5,7 @@ const app = require("express")(),
 app.get("search", function handler(req, res) {
   // GOOD: use parameters
   var query2 =
-    "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY=$1" + " ORDER BY PRICE";
+    "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY=$1 ORDER BY PRICE";
   pool.query(query2, [req.params.category], function(err, results) {
     // process results
   });
