e88508088ec91f5c9b75d0150cfeeb2ab42019b2
Sim4n6 <sim4n6@gmail.com>
2023-06-01 11:56:42 +0100

    Add test using strings.Index()

--- go/ql/test/experimental/CWE-176/example_bad.go
+++ go/ql/test/experimental/CWE-176/example_bad.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html"
 	"net/http"
+	"strings"
 
 	"golang.org/x/text/unicode/norm"
 )
@@ -11,11 +12,23 @@ import (
 func main() {}
 
 func bad() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+	http.HandleFunc("/bad1", func(w http.ResponseWriter, req *http.Request) {
 
 		unicode_input := req.URL.Query().Get("unicode_input")
 		escaped := html.EscapeString(unicode_input)
 		unicode_norm := norm.NFKC.String(escaped) // $result=BAD
 		fmt.Println(w, "Results: %q", unicode_norm)
 	})
+
+	http.HandleFunc("/bad2", func(w http.ResponseWriter, req *http.Request) {
+
+		unicode_input := req.URL.Query().Get("unicode_input")
+		escaped := html.EscapeString(unicode_input)
+		if strings.Index(escaped, "<") == -1 {
+			unicode_norm := norm.NFKC.String(escaped) // $result=BAD
+			fmt.Println(w, "Results: %q", unicode_norm)
+		}
+
+	})
+
 }
