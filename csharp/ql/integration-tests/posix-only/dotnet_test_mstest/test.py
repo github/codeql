2fece9d72148593f39a770c5660ddd1f93bb1c44
Michael Nebel <michaelnebel@github.com>
2023-06-08 09:58:52 +0200

    C#: Add MSTEST test project and check that the call to vstest doesn't get the UseSharedCompilation=false flag forwarded.

---
+++ csharp/ql/integration-tests/posix-only/dotnet_test_mstest/test.py
@@ -0,0 +1,10 @@
+from create_database_utils import *
+from diagnostics_test_utils import *
+
+# Implicitly build and then run tests.
+run_codeql_database_create(['dotnet test'], test_db="test-db", lang="csharp")
+check_diagnostics()
+
+# Explicitly build and then run tests.
+run_codeql_database_create(['dotnet clean', 'rm -rf test-db', 'dotnet build -o myout --os win', 'dotnet test myout/dotnet_test_mstest.exe'], test_db="test2-db", lang="csharp")
+check_diagnostics(test_db="test2-db")
