a961fffda8ade49400406ee645c2641a36a95062
Michael B. Gale <mbg@github.com>
2023-06-08 16:24:16 +0100

    Pass architecture to dotnet test

--- csharp/ql/integration-tests/posix-only/dotnet_test_mstest/test.py
+++ csharp/ql/integration-tests/posix-only/dotnet_test_mstest/test.py
@@ -1,3 +1,4 @@
+import platform
 from create_database_utils import *
 from diagnostics_test_utils import *
 
@@ -5,6 +6,9 @@ from diagnostics_test_utils import *
 run_codeql_database_create(['dotnet test'], test_db="test-db", lang="csharp")
 check_diagnostics()
 
+# Fix `dotnet test` picking `x64` on arm-based macOS
+architecture = '-a arm64' if platform.machine() == 'arm64' else ''
+
 # Explicitly build and then run tests.
-run_codeql_database_create(['dotnet clean', 'rm -rf test-db', 'dotnet build -o myout --os win', 'dotnet test myout/dotnet_test_mstest.exe'], test_db="test2-db", lang="csharp")
+run_codeql_database_create(['dotnet clean', 'rm -rf test-db', 'dotnet build -o myout --os win', 'dotnet test myout/dotnet_test_mstest.exe ' + architecture], test_db="test2-db", lang="csharp")
 check_diagnostics(test_db="test2-db")
