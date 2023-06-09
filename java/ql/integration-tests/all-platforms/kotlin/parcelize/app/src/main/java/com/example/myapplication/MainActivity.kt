d3fd2c993fc2fccc5f9b0e7bb1b05f5dc0297993
Ian Lynagh <igfoo@github.com>
2023-06-09 17:00:52 +0100

    Kotlin: Add an integration test for parcelize plugin

---
+++ java/ql/integration-tests/all-platforms/kotlin/parcelize/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.os.Parcelable
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.parcelize.Parcelize
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+interface TemplateModel : Parcelable {
+}
+
+@Parcelize
+data class Foo(val x: Int, val templateModel: TemplateModel, val y: Type): Parcelable {
+    enum class Type {
+        User, Organization
+    }
+}
+
+@Parcelize
+data class User(val firstName: String, val lastName: String, val age: Int, val foo: Foo): Parcelable
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
