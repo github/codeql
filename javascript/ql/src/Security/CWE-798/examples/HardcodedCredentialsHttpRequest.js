7dfb404fd758d3633d7af1479aa1c020b1c26904
Adrien Pessu <7055334+adrienpessu@users.noreply.github.com>
2023-06-21 08:11:39 +0000

    clean examples

--- javascript/ql/src/Security/CWE-798/examples/HardcodedCredentialsHttpRequest.js
+++ javascript/ql/src/Security/CWE-798/examples/HardcodedCredentialsHttpRequest.js
@@ -6,13 +6,13 @@ let password = 'passwd';
 
 let headers = new Headers();
 
-//headers.append('Content-Type', 'text/json');
+headers.append('Content-Type', 'text/json');
 headers.append('Authorization', 'Basic' + base64.encode(username + ":" + password));
 
-fetch(url, {method:'GET',
-        headers: headers,
-        credentials: `${user}:${passwd}`
+fetch(url, {
+          method:'GET',
+          headers: headers
        })
 .then(response => response.json())
-.then(json => console.log(json));
+.then(json => console.log(json))
 .done();
