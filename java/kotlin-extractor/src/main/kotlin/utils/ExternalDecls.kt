77451de9a3885e6c3d26ff5419eda19e990307dc
Ian Lynagh <igfoo@github.com>
2023-06-23 12:21:50 +0100

    Kotlin: Fix isExternalDeclaration for Kotlin 2

--- java/kotlin-extractor/src/main/kotlin/utils/ExternalDecls.kt
+++ java/kotlin-extractor/src/main/kotlin/utils/ExternalDecls.kt
@@ -7,9 +7,31 @@ import org.jetbrains.kotlin.ir.util.isFileClass
 import org.jetbrains.kotlin.ir.util.parentClassOrNull
 
 fun isExternalDeclaration(d: IrDeclaration): Boolean {
-    return d.origin == IrDeclarationOrigin.IR_EXTERNAL_DECLARATION_STUB ||
-           d.origin == IrDeclarationOrigin.IR_EXTERNAL_JAVA_DECLARATION_STUB ||
-           d.origin.toString() == "FUNCTION_INTERFACE_CLASS" // Treat kotlin.coroutines.* like ordinary library classes
+    /*
+    With Kotlin 1 we get things like (from .dump()):
+        PROPERTY IR_EXTERNAL_JAVA_DECLARATION_STUB name:MIN_VALUE visibility:public modality:FINAL [const,val]
+          FIELD IR_EXTERNAL_JAVA_DECLARATION_STUB name:MIN_VALUE type:kotlin.Int visibility:public [final,static]
+            EXPRESSION_BODY
+              CONST Int type=kotlin.Int value=-2147483648
+    */
+    if (d.origin == IrDeclarationOrigin.IR_EXTERNAL_DECLARATION_STUB ||
+        d.origin == IrDeclarationOrigin.IR_EXTERNAL_JAVA_DECLARATION_STUB ||
+        d.origin.toString() == "FUNCTION_INTERFACE_CLASS") { // Treat kotlin.coroutines.* like ordinary library classes
+        return true
+    }
+    /*
+    With Kotlin 2, the property itself is not marked as an external stub, but it parent is:
+        CLASS IR_EXTERNAL_DECLARATION_STUB OBJECT name:Companion modality:OPEN visibility:public [companion] superTypes:[]
+          PROPERTY name:MIN_VALUE visibility:public modality:FINAL [const,val]
+            FIELD PROPERTY_BACKING_FIELD name:MIN_VALUE type:kotlin.Int visibility:public [final]
+              EXPRESSION_BODY
+                CONST Int type=kotlin.Int value=-2147483648
+    */
+    val p = d.parent
+    if (p is IrDeclaration) {
+        return isExternalDeclaration(p)
+    }
+    return false
 }
 
 /**
