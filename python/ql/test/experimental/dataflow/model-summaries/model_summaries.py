86dfc7b66e87b1877ddeb653195d59478685997a
Rasmus Lerchedahl Petersen <yoff@github.com>
2023-06-23 08:18:06 +0200

    python: format

--- python/ql/test/experimental/dataflow/model-summaries/model_summaries.py
+++ python/ql/test/experimental/dataflow/model-summaries/model_summaries.py
@@ -54,8 +54,8 @@ SINK(via_reversed[0])  # $ flow="SOURCE, l:-1 -> via_reversed[0]"
 tainted_list = MS_reversed(TAINTED_LIST)
 ensure_tainted(
     tainted_list,  # $ tainted
-    tainted_list[0]  # $ tainted
-    )
+    tainted_list[0],  # $ tainted
+)
 
 # Complex summaries
 def box(x):
@@ -67,8 +67,8 @@ SINK(via_map[0][0])  # $ flow="SOURCE, l:-1 -> via_map[0][0]"
 tainted_mapped = MS_list_map(box, TAINTED_LIST)
 ensure_tainted(
     tainted_mapped,  # $ tainted
-    tainted_mapped[0][0]  # $ tainted
-    )
+    tainted_mapped[0][0],  # $ tainted
+)
 
 def explicit_identity(x):
     return x
@@ -79,8 +79,8 @@ SINK(via_map_explicit[0])  # $ flow="SOURCE, l:-1 -> via_map_explicit[0]"
 tainted_mapped_explicit = MS_list_map(explicit_identity, TAINTED_LIST)
 ensure_tainted(
     tainted_mapped_explicit,  # $ tainted
-    tainted_mapped_explicit[0]  # $ tainted
-    )
+    tainted_mapped_explicit[0],  # $ tainted
+)
 
 via_map_summary = MS_list_map(MS_identity, [SOURCE])
 SINK(via_map_summary[0])  # $ flow="SOURCE, l:-1 -> via_map_summary[0]"
@@ -88,8 +88,8 @@ SINK(via_map_summary[0])  # $ flow="SOURCE, l:-1 -> via_map_summary[0]"
 tainted_mapped_summary = MS_list_map(MS_identity, TAINTED_LIST)
 ensure_tainted(
     tainted_mapped_summary,  # $ tainted
-    tainted_mapped_summary[0]  # $ tainted
-    )
+    tainted_mapped_summary[0],  # $ tainted
+)
 
 via_append_el = MS_append_to_list([], SOURCE)
 SINK(via_append_el[0])  # $ flow="SOURCE, l:-1 -> via_append_el[0]"
@@ -97,8 +97,8 @@ SINK(via_append_el[0])  # $ flow="SOURCE, l:-1 -> via_append_el[0]"
 tainted_list_el = MS_append_to_list([], TAINTED_STRING)
 ensure_tainted(
     tainted_list_el,  # $ tainted
-    tainted_list_el[0]  # $ tainted
-    )
+    tainted_list_el[0],  # $ tainted
+)
 
 via_append = MS_append_to_list([SOURCE], NONSOURCE)
 SINK(via_append[0])  # $ flow="SOURCE, l:-1 -> via_append[0]"
@@ -106,8 +106,8 @@ SINK(via_append[0])  # $ flow="SOURCE, l:-1 -> via_append[0]"
 tainted_list_implicit = MS_append_to_list(TAINTED_LIST, NONSOURCE)
 ensure_tainted(
     tainted_list,  # $ tainted
-    tainted_list[0]  # $ tainted
-    )
+    tainted_list[0],  # $ tainted
+)
 
 # Modeled flow-summary is not value preserving
 from json import MS_loads as json_loads
@@ -120,5 +120,5 @@ SINK_F(json_loads(SOURCE)[0])
 tainted_resultlist = json_loads(TAINTED_STRING)
 ensure_tainted(
     tainted_resultlist,  # $ tainted
-    tainted_resultlist[0] # $ tainted
-    )
+    tainted_resultlist[0], # $ tainted
+)
