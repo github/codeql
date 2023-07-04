74af0b1f05d19c9d4397025383aecf868c17f44a
Max Schaefer <max-schaefer@github.com>
2023-07-04 14:44:20 +0100

    Improve command-injection example and provide a fixed version.

--- javascript/ql/src/Security/CWE-078/examples/command-injection_fixed.js
+++ javascript/ql/src/Security/CWE-078/examples/command-injection_fixed.js
@@ -6,5 +6,5 @@ var cp = require("child_process"),
 var server = http.createServer(function(req, res) {
     let file = path.basename(url.parse(req.url, true).query.path);
 
-    cp.execSync(`wc -l ${file}`); // BAD
+    cp.execFileSync('wc', ['-l', file]); // GOOD
 });
