6f3dba5f98a01c95456eecbee412c41ba16cf1f6
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2023-07-04 13:20:41 +0100

    Swift: Tweak qhelp and example.

--- swift/ql/src/queries/Security/CWE-730/RegexInjectionGood.swift
+++ swift/ql/src/queries/Security/CWE-730/RegexInjectionGood.swift
@@ -4,7 +4,7 @@ func processRemoteInput(remoteInput: String) {
   # GOOD: Regular expression is not derived from user input
   let regex1 = try Regex(myRegex)
 
-  # GOOD: Sanitized user input is used to construct a regular expression
+  # GOOD: User input is sanitized before being used to construct a regular expression
   let escapedInput = NSRegularExpression.escapedPattern(for: remoteInput)
   let regexStr = "abc|\(escapedInput)"
   let regex2 = try NSRegularExpression(pattern: regexStr)
