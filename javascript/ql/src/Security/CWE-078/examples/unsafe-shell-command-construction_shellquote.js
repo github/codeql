63c45a0da31894397d7ca6c760b641066316c9a5
Max Schaefer <max-schaefer@github.com>
2023-07-10 14:02:17 +0100

    Add another example of when and how to use shell-quote.

--- javascript/ql/src/Security/CWE-078/examples/unsafe-shell-command-construction_shellquote.js
+++
@@ -1,6 +0,0 @@
-var cp = require("child_process"),
-    shellQuote = require('shell-quote');
-
-module.exports = function download(path, options, callback) {
-  cp.execFile("wget", shellQuote.parse(options).concat(path), callback);
-}
