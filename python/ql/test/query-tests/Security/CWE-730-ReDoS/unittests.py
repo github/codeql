dbde99df91b76e919ee2e7b9312bffa1daecd11b
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2023-07-19 18:45:18 +0100

    Python: Add test cases.

--- python/ql/test/query-tests/Security/CWE-730-ReDoS/unittests.py
+++ python/ql/test/query-tests/Security/CWE-730-ReDoS/unittests.py
@@ -7,3 +7,6 @@ re.compile(r"X(Æ|\Æ)+$") # Has ReDoS.
 # Treatment of line breaks
 re.compile(r'(?:.|\n)*b') # No ReDoS.
 re.compile(r'(?:.|\n)*b', re.DOTALL) # Has ReDoS.
+re.compile(r'(?i)(?:.|\n)*b') # No ReDoS.
+re.compile(r'(?s)(?:.|\n)*b') # Has ReDoS.
+re.compile(r'(?is)(?:.|\n)*b') # Has ReDoS. [NOT DETECTED]
