bb16731b868eb7df46536209fdbd9567e338c6da
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2023-07-19 18:53:00 +0100

    Python: Fix for multiple parse mode flags.

--- python/ql/test/query-tests/Security/CWE-730-ReDoS/unittests.py
+++ python/ql/test/query-tests/Security/CWE-730-ReDoS/unittests.py
@@ -9,4 +9,4 @@ re.compile(r'(?:.|\n)*b') # No ReDoS.
 re.compile(r'(?:.|\n)*b', re.DOTALL) # Has ReDoS.
 re.compile(r'(?i)(?:.|\n)*b') # No ReDoS.
 re.compile(r'(?s)(?:.|\n)*b') # Has ReDoS.
-re.compile(r'(?is)(?:.|\n)*b') # Has ReDoS. [NOT DETECTED]
+re.compile(r'(?is)(?:.|\n)*b') # Has ReDoS.
