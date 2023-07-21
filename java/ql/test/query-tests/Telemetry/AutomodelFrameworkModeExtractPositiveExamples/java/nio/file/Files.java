abed936556f65b08d6ed07f7a3ec39f0e58d5dd9
Stephan Brandauer <kaeluka@github.com>
2023-07-21 16:52:26 +0200

    Java: tests for automodel framework mode positive example extraction

---
+++ java/ql/test/query-tests/Telemetry/AutomodelFrameworkModeExtractPositiveExamples/java/nio/file/Files.java
@@ -0,0 +1,12 @@
+package java.nio.file;
+
+import java.nio.file.Path;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Files {
+    // source is a positive example because there's a model for it
+    public static void copy(Path source, OutputStream out) throws IOException {
+        // ...
+    }
+}
