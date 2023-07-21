a2f4628522a3e52a70d6cd28e0b7bb6440624a91
Michael B. Gale <mbg@github.com>
2023-07-21 20:14:46 +0100

    C#: Add integration test for tracing config fix

---
+++ csharp/ql/integration-tests/all-platforms/dotnet_no_args_inject/test.py
@@ -0,0 +1,8 @@
+from create_database_utils import *
+from diagnostics_test_utils import *
+
+# the tracer configuration should not inject the extra command-line arguments for these commands
+# and they should therefore run successfully
+run_codeql_database_init(lang="csharp")
+run_codeql_database_trace_command(['dotnet', 'tool', 'search', 'publish'])
+run_codeql_database_trace_command(['dotnet', 'new', 'console', '--name', 'build', '--output', '.'])
