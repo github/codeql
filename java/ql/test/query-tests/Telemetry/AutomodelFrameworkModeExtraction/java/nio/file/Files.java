da87d82d08caebf1599878094afa0d6887892a54
Stephan Brandauer <kaeluka@github.com>
2023-07-28 14:38:27 +0200

    Java: fix a comment

--- java/ql/test/query-tests/Telemetry/AutomodelFrameworkModeExtraction/java/nio/file/Files.java
+++ java/ql/test/query-tests/Telemetry/AutomodelFrameworkModeExtraction/java/nio/file/Files.java
@@ -7,7 +7,12 @@ import java.io.OutputStream;
 public class Files {
     public static void copy(
         Path source, // a positive example because a manual model exists
-        OutputStream out // a candidate. NB: may be worthwhile to implement the same behavior as in application mode where out
+        OutputStream out /* a candidate. NB: may be worthwhile to implement the
+           same behavior as in application mode where out would not be a
+           candidate because there already is a model for another parameter of
+           the same method and we assume that methods are always modeled
+           completely.
+         */
     ) throws IOException {
         // ...
     }
