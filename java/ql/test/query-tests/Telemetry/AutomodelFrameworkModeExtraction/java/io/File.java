5ad984f22f611b7156a9f9786ac6097abfac0c40
Stephan Brandauer <kaeluka@github.com>
2023-07-28 18:10:50 +0200

    Java: update text expectations after merging #13823

--- java/ql/test/query-tests/Telemetry/AutomodelFrameworkModeExtraction/java/io/File.java
+++ java/ql/test/query-tests/Telemetry/AutomodelFrameworkModeExtraction/java/io/File.java
@@ -1,7 +1,7 @@
 package java.io;
 
 public class File {
-    int compareTo(
+    int compareTo( // `this` is a negative example - this is modeled as a neutral model
         File pathname // negative example - this is modeled as a neutral model
     ) {
         return 0;
