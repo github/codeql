47a536c85d2adccb24522045f5e9dd06816fa745
Owen Mansel-Chan <owen-mc@github.com>
2023-07-31 16:09:47 +0100

    Always output valid JSON containing `paths-ignore`

--- go/codeql-tools/configure-baseline.cmd
+++ go/codeql-tools/configure-baseline.cmd
@@ -1,4 +1,6 @@
 @echo off
 if exist vendor\modules.txt (
-  type %CODEQL_EXTRACTOR_GO_ROOT%\codeql-tools\baseline-config.json
+  type %CODEQL_EXTRACTOR_GO_ROOT%\codeql-tools\baseline-config-vendor.json
+) else (
+  type %CODEQL_EXTRACTOR_GO_ROOT%\codeql-tools\baseline-config-empty.json
 )
--- go/codeql-tools/configure-baseline.sh
+++ go/codeql-tools/configure-baseline.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 if [ -f vendor/modules.txt ]; then
-  cat $CODEQL_EXTRACTOR_GO_ROOT/codeql-tools/baseline-config.json
+  cat $CODEQL_EXTRACTOR_GO_ROOT/codeql-tools/baseline-config-vendor.json
+else
+  cat $CODEQL_EXTRACTOR_GO_ROOT/codeql-tools/baseline-config-empty.json
 fi
