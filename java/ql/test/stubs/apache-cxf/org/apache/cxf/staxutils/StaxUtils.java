2448bc8ce2bb41a60375ffa5a1df4bde0cad1922
Tony Torralba <atorralba@users.noreply.github.com>
2023-08-23 10:46:57 +0200

    Java: Add new Apache CXF models

---
+++ java/ql/test/stubs/apache-cxf/org/apache/cxf/staxutils/StaxUtils.java
@@ -0,0 +1,10 @@
+package org.apache.cxf.staxutils;
+
+import java.io.File;
+import org.w3c.dom.Document;
+
+public class StaxUtils {
+    public static Document read(File is) {
+        return null;
+    }
+}
