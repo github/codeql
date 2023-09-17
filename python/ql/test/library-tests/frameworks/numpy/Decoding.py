6d0ba5f97b7294cca4fd7de48f5a69e1a4fad5b3
Maiky <76447395+maikypedia@users.noreply.github.com>
2023-09-17 18:53:18 +0200

    Add `allow_pickle` to tests
    Co-authored-by: Jorge <46056498+jorgectf@users.noreply.github.com>

--- python/ql/test/library-tests/frameworks/numpy/Decoding.py
+++ python/ql/test/library-tests/frameworks/numpy/Decoding.py
@@ -1,4 +1,6 @@
 import numpy
 
-numpy.load(file_)  # $ decodeInput=file_ decodeOutput=numpy.load(..) decodeFormat=numpy decodeMayExecuteInput
-numpy.load(filename=file_)  # $ decodeInput=file_ decodeOutput=numpy.load(..) decodeFormat=numpy decodeMayExecuteInput
+numpy.load(file_)  # $ decodeInput=file_ decodeOutput=numpy.load(..) decodeFormat=numpy
+numpy.load(filename=file_)  # $ decodeInput=file_ decodeOutput=numpy.load(..) decodeFormat=numpy
+numpy.load(file_, allow_pickle=True)  # $ decodeInput=file_ decodeOutput=numpy.load(..) decodeFormat=numpy decodeMayExecuteInput
+numpy.load(filename=file_, allow_pickle=True)  # $ decodeInput=file_ decodeOutput=numpy.load(..) decodeFormat=numpy decodeMayExecuteInput
