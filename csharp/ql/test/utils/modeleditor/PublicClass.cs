eace7a4bbff80f9ced9994b56a3d9408f295e56e
Koen Vlaswinkel <koesie10@github.com>
2023-09-19 15:49:35 +0200

    C#: Add tests for supported framework methods

--- csharp/ql/test/utils/modeleditor/PublicClass.cs
+++ csharp/ql/test/utils/modeleditor/PublicClass.cs
@@ -31,4 +31,24 @@ public class PublicClass : PublicInterface
     }
 
     string PublicInterface.PublicProperty { get; set; }
+
+    public string summaryStuff(String arg)
+    {
+        return arg;
+    }
+
+    public string sourceStuff()
+    {
+        return "stuff";
+    }
+
+    public void sinkStuff(String arg)
+    {
+        // do nothing
+    }
+
+    public void neutralStuff(String arg)
+    {
+        // do nothing
+    }
 }
