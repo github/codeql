a8afa05b1d4bf4fa70d78f6886f01c59aa731161
Chris Smowton <smowton@github.com>
2023-09-20 10:00:53 +0100

    Correct ReplaceAll params
    ReplaceAll doesn't take a count argument

--- go/ql/src/Security/CWE-117/LogInjectionGood.go
+++ go/ql/src/Security/CWE-117/LogInjectionGood.go
@@ -9,7 +9,7 @@ import (
 // GOOD: The user-provided value is escaped before being written to the log.
 func handlerGood(req *http.Request) {
 	username := req.URL.Query()["username"][0]
-	escapedUsername := strings.ReplaceAll(username, "\n", "", -1)
-	escapedUsername = strings.ReplaceAll(escapedUsername, "\r", "", -1)
+	escapedUsername := strings.ReplaceAll(username, "\n", "")
+	escapedUsername = strings.ReplaceAll(escapedUsername, "\r", "")
 	log.Printf("user %s logged in.\n", escapedUsername)
 }
