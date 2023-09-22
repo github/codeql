6f6705585273b70c7b75711146e4f51a3eb6b919
Max Schaefer <max-schaefer@github.com>
2023-09-22 10:22:12 +0100

    Correctly account for length of string literal prefix when computing locations for RegExpTerms.

--- python/ql/test/library-tests/regexparser/locations.py
+++ python/ql/test/library-tests/regexparser/locations.py
@@ -6,8 +6,8 @@ import re
 # regexp term `[this]`, appearing in various kinds of regexps.
 #
 # To make the location information easier to understand, we generally put each
-# regexp on its own line, even though this is not the way one would normally
-# write regexps in Python.
+# regexp on its own line, even though this is not idiomatic Python.
+# Comments indicate cases we currently do not handle correctly.
 
 # plain string
 re.compile(
@@ -49,25 +49,25 @@ re.compile(
 br'''[this] is a test'''
 )
 
-# plain string with multiple parts
+# plain string with multiple parts (second [this] gets wrong column: 23 instead of 26)
 re.compile(
 '[this] is a test' ' and [this] is another test'
 )
 
-# plain string with multiple parts across lines
+# plain string with multiple parts across lines (second [this] gets wrong location: 59:23 instead of 60:7)
 re.compile(
 '[this] is a test'
 ' and [this] is another test'
 )
 
-# plain string with multiple parts across lines and comments
+# plain string with multiple parts across lines and comments (second [this] gets wrong location: 65:23 instead of 67:7)
 re.compile(
 '[this] is a test'
 # comment
 ' and [this] is another test'
 )
 
-# actual multiline string
+# actual multiline string (both [this]s get wrong location: 72:6 and 72:27 instead of 73:1 and 74:5)
 re.compile(
 r'''
 [this] is a test
@@ -75,7 +75,7 @@ and [this] is another test
 '''
 )
 
-# plain string with escape sequences
+# plain string with escape sequences ([this] gets wrong location: 80:3 instead of 80:4)
 re.compile(
 '\t[this] is a test'
 )
@@ -85,7 +85,7 @@ re.compile(
 r'\A[this] is a test'
 )
 
-# plain string with escaped newline
+# plain string with escaped newline (second [this] gets wrong location: 90:23 instead of 91:6)
 re.compile(
 '[this] is a test\
  and [this] is another test'
