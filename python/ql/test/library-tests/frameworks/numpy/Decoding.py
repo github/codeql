a45e10d64f10c33c1e277ba42b457c89a725658f
Rasmus Wriedt Larsen <rasmuswl@github.com>
2023-09-25 10:25:11 +0200

    Python: Slight rewrite of numpy test
    To use positional argument for allow_pickle

--- python/ql/test/library-tests/frameworks/numpy/Decoding.py
+++ python/ql/test/library-tests/frameworks/numpy/Decoding.py
@@ -2,5 +2,5 @@ import numpy
 
 numpy.load(file_)  # $ decodeInput=file_ decodeOutput=numpy.load(..) decodeFormat=numpy
 numpy.load(filename=file_)  # $ decodeInput=file_ decodeOutput=numpy.load(..) decodeFormat=numpy
-numpy.load(file_, allow_pickle=True)  # $ decodeInput=file_ decodeOutput=numpy.load(..) decodeFormat=numpy decodeMayExecuteInput
-numpy.load(filename=file_, allow_pickle=True)  # $ decodeInput=file_ decodeOutput=numpy.load(..) decodeFormat=numpy decodeMayExecuteInput
+numpy.load(file_, allow_pickle=True)  # $ decodeInput=file_ decodeOutput=numpy.load(..) decodeFormat=numpy decodeFormat=pickle decodeMayExecuteInput
+numpy.load(file_, None, True)  # $ decodeInput=file_ decodeOutput=numpy.load(..) decodeFormat=numpy decodeFormat=pickle decodeMayExecuteInput
