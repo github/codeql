00b6e1f0b04cb5de4863a0a3b7c86c822c12b174
amammad <77095239+amammad@users.noreply.github.com>
2023-10-08 11:03:19 +0200

    fix tests

--- javascript/ql/test/experimental/Security/CWE-094-dataURL/test.js
+++ javascript/ql/test/experimental/Security/CWE-094-dataURL/test.js
@@ -3,24 +3,20 @@ var app = require('express')();
 
 app.post('/path', async function (req, res) {
     const payload = req.query.queryParameter // like:  payload = 'data:text/javascript,console.log("hello!");//'
-    let payloadURL = new URL(payload + sth)
-    // NOT OK
+    let payloadURL = new URL(payload + sth) // NOT OK
     new Worker(payloadURL);
-    // NOT OK
-    payloadURL = new URL(payload + sth)
+
+    payloadURL = new URL(payload + sth) // NOT OK
     new Worker(payloadURL);
-    // OK
-    payloadURL = new URL(sth + payload)
+
+    payloadURL = new URL(sth + payload) // OK
     new Worker(payloadURL);
 });
 
 app.post('/path2', async function (req, res) {
     const payload = req.query.queryParameter // like:  payload = 'data:text/javascript,console.log("hello!");//'
-    // NOT OK
-    await import(payload)
-    // NOT OK
-    await import(payload + sth)
-    // OK
-    await import(sth + payload)
+    await import(payload) // NOT OK
+    await import(payload + sth) // NOT OK
+    await import(sth + payload) // OK
 });
 
