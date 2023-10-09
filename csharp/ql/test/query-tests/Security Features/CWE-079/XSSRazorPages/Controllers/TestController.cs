4f5ecb899b5f277d09f2e462526cebdfb701e4f5
Joe Farebrother <joefarebrother@github.com>
2023-10-09 18:01:10 +0100

    Add unit tests + fix issue in stubs

--- csharp/ql/test/query-tests/Security Features/CWE-079/XSSRazorPages/Controllers/TestController.cs
+++ csharp/ql/test/query-tests/Security Features/CWE-079/XSSRazorPages/Controllers/TestController.cs
@@ -10,7 +10,73 @@ public class UserData
 }
 
 public class TestController : Controller {
-    public IActionResult test1(UserData tainted) {
-        return View("Test1", tainted);
+    public IActionResult test1(UserData tainted1) {
+        // Expected to find file /Views/Test/Test1.cshtml
+        return View("Test1", tainted1);
     }
+
+    public IActionResult test2(UserData tainted2) {
+        // Expected to find file /Views/Shared/Test2.cshtml
+        return View("Test2", tainted2);
+    }
+
+    public IActionResult test3(UserData tainted3) {
+        // Expected to find file /Views/Test/Test3.cshtml and NOT /Views/Shared/Test3.cshtml
+        return View("Test3", tainted3);
+    }
+
+    public IActionResult test4(UserData tainted4) {
+        // Expected to find file /Views/Test/Test4.cshtml
+        return View("./Test4", tainted4);
+    }
+
+    public IActionResult test5(UserData tainted5) {
+        // Expected to find file /Views/Other/Test5.cshtml
+        return View("../Other/Test5", tainted5);
+    }
+
+    public IActionResult test6(UserData tainted6) {
+        // Expected to find file /Views/Other/Test6.cshtml
+        return View("../../Views/.////Shared/../Other//Test6", tainted6);
+    }
+
+    public IActionResult Test7(UserData tainted7) {
+        // Expected to find file /Views/Test/Test7.cshtml
+        return View(tainted7);
+    }
+
+    public IActionResult test8(UserData tainted8) {
+        // Expected to find file /Views/Other/Test8.cshtml
+        return View("/Views/Other/Test8.cshtml", tainted8);
+    }
+
+    public IActionResult test9(UserData tainted9) {
+        // Expected to find file /Views/Test/Test9.cshtml
+        return View("~/Views/Other/Test9.cshtml", tainted9);
+    }
+}
+
+public class Test2Controller : Controller { 
+    public IActionResult test10(UserData tainted10) {
+        // Expected to find file /Views/Test2/Test10.cshtml
+        return View("Test10", tainted10);
+    }
+
+    public IActionResult test11(UserData tainted11) {
+        // Expected to find file /Views/Test2/Test10.cshtml
+        return helper(tainted11);
+    }
+
+    private IActionResult helper(UserData x) {
+        return View("Test11", x);
+    }
+
+    public IActionResult Test12(UserData tainted12) {
+        // Expected to find nothing.
+        return helper2(tainted12);
+    }
+
+    private IActionResult helper2(UserData x) {
+        return View(x);
+    }    
 }
