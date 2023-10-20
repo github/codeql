af2d783b38d6ee476e170287acbcc8e26cd54e14
Rasmus Wriedt Larsen <rasmuswl@github.com>
2023-10-20 10:23:45 +0200

    Python: More examples of things to handle in find-subclass

--- python/ql/test/experimental/library-tests/FindSubclass/find_subclass_test.py
+++ python/ql/test/experimental/library-tests/FindSubclass/find_subclass_test.py
@@ -8,3 +8,35 @@ class B(A):
 
 
 ViewAlias = View
+
+try:
+    from flask.views import View as ViewAliasInTry
+except:
+    from flask.views import View as ViewAliasInExcept
+
+
+if cond:
+    from flask.views import View as clash # $ MISSING
+else:
+    from flask.views import MethodView as clash # $ SPURIOUS: MethodView
+
+if cond:
+    from flask.views import View as clash2 # $ MISSING
+else:
+    from flask.views import MethodView as clash2 # $ MISSING
+print(clash2)
+
+if cond:
+    from flask.views import View as clash3
+else:
+    from flask.views import MethodView as clash3 # $ MISSING
+    print(clash3)
+
+import flask.views as containing_module_alias # $ MISSING
+# now `find_subclass_test.containing_module_alias.View` is an alias of flask.views.View
+
+# NOTE: this is not valid code, since View is not a module... but it could be in some cases, like for xml.etree.ElementTree, which is actually not a class but a module 😕
+import flask.views.View as complete_module_alias
+
+def wrapper():
+    return View # $ MISSING
