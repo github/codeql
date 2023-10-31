c82eb4dff3eccdd628721cbb4e1a229139a9adfe
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2023-10-31 17:15:01 +0000

    Swift: Test spacing.

--- swift/ql/test/query-tests/Security/CWE-078/CommandInjection.swift
+++ swift/ql/test/query-tests/Security/CWE-078/CommandInjection.swift
@@ -117,6 +117,7 @@ func testCommandInjectionMore(mySafeString: String) {
 
 	let task4 = Process()
 	task4.executableURL = URL(fileURLWithPath: userControlledString) // BAD
+
 	task4.arguments = ["abc", "def" + userControlledString] // BAD
 	try! task4.run()
 
@@ -126,6 +127,7 @@ func testCommandInjectionMore(mySafeString: String) {
 	try! task5?.run()
 
 	let task6 = MyProcess()
+
 	task6.executableURL = URL(string: userControlledString)! // BAD [NOT DETECTED]
 	task6.arguments = [userControlledString] // BAD [NOT DETECTED]
 	task6.setArguments([userControlledString]) // BAD [NOT DETECTED]
