315bdc2b48712455f8f524804e6eeb837cf8558d
amammad <77095239+amammad@users.noreply.github.com>
2023-11-06 19:13:57 +0100

    add tests for new frameworks

--- python/ql/test/library-tests/frameworks/baize/FileSystemAccess.py
+++ python/ql/test/library-tests/frameworks/baize/FileSystemAccess.py
@@ -1,5 +1,4 @@
-from starlette.responses import FileResponse
 from baize.asgi import FileResponse as baizeFileResponse
 
 baizeFileResponse("file")  # $ getAPathArgument="file"
-FileResponse("file")  # $ getAPathArgument="file"
+FileSystemAccess
