cc5dd3180ad9bb2622edf4673640727ba8dd5a91
amammad <77095239+amammad@users.noreply.github.com>
2023-11-06 15:18:55 +0100

    fix better-sqlite3 tests according to Review

--- javascript/ql/test/library-tests/frameworks/SQL/better-sqlite3.js
+++ javascript/ql/test/library-tests/frameworks/SQL/better-sqlite3.js
@@ -1,9 +1,14 @@
-const db = require('better-sqlite3')('BetterSqlite.db', { verbose: console.log });
+import Database from 'better-sqlite3';
 
-const sql = 'SELECT name, id FROM table1'
-const stmt = db.prepare(sql);
+const db = new Database('BetterSqlite.db', { verbose: console.log });
+
+let sql = 'SELECT name, id FROM table1'
+let stmt = db.prepare(sql);
 let exec = db.exec(sql);
 exec.prepare(sql)
+const db2 = Database('BetterSqlite.db', { verbose: console.log });
 
-
-
+sql = 'SELECT name, id FROM table1'
+stmt = db2.prepare(sql);
+exec = db2.exec(sql);
+exec.prepare(sql)
--- javascript/ql/test/library-tests/frameworks/SQL/better-sqlite3.mjs
+++
@@ -1,7 +0,0 @@
-import Database from 'better-sqlite3';
-const db = new Database('BetterSqlite.db', { verbose: console.log });
-
-const sql = 'SELECT name, id FROM table1'
-const stmt = db.prepare(sql);
-let exec = db.exec(sql);
-exec.prepare(sql)
