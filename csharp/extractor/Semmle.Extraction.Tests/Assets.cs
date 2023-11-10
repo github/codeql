1d6d47a94ec0c6ce0ee1f619c07caa7cd3e50f1a
Michael Nebel <michaelnebel@github.com>
2023-11-10 14:34:12 +0100

    C#: Add more assets tests (use real assets.json output for .NET 7, Net Framework 4.8 and Net standard 2.1)

--- csharp/extractor/Semmle.Extraction.Tests/Assets.cs
+++ csharp/extractor/Semmle.Extraction.Tests/Assets.cs
@@ -42,28 +42,6 @@ namespace Semmle.Extraction.Tests
         }
 
         [Fact]
-        public void TestAssetsNet48()
-        {
-            // Setup
-            var assets = new Assets(new ProgressMonitor(new LoggerStub()));
-            var json = assetsJson2;
-            var dependencies = new DependencyContainer();
-
-            // Execute
-            var success = assets.TryParse(json, dependencies);
-
-            // Verify
-            Assert.True(success);
-            Assert.Single(dependencies.Paths);
-
-            var normalizedPaths = dependencies.Paths.Select(FixExpectedPathOnWindows);
-            // Used references
-            Assert.Contains("microsoft.netframework.referenceassemblies.net48/1.0.3", normalizedPaths);
-            // Used frameworks
-            Assert.Contains("microsoft.netframework.referenceassemblies.net48", dependencies.Packages);
-        }
-
-        [Fact]
         public void TestAssetsFailure()
         {
             // Setup
@@ -79,6 +57,95 @@ namespace Semmle.Extraction.Tests
             Assert.Empty(dependencies.Paths);
         }
 
+        [Fact]
+        public void TestAssetsNet70()
+        {
+            // Setup
+            var assets = new Assets(new ProgressMonitor(new LoggerStub()));
+            var json = assetsNet70;
+            var dependencies = new DependencyContainer();
+
+            // Execute
+            var success = assets.TryParse(json, dependencies);
+
+            // Verify
+            Assert.True(success);
+            Assert.Equal(4, dependencies.Paths.Count);
+            Assert.Equal(4, dependencies.Packages.Count);
+
+
+            var normalizedPaths = dependencies.Paths.Select(FixExpectedPathOnWindows);
+            // Used paths
+            Assert.Contains("microsoft.netcore.app.ref", dependencies.Packages);
+            Assert.Contains("microsoft.aspnetcore.app.ref", dependencies.Packages);
+            Assert.Contains("newtonsoft.json/12.0.1/lib/netstandard2.0/Newtonsoft.Json.dll", normalizedPaths);
+            Assert.Contains("newtonsoft.json.bson/1.0.2/lib/netstandard2.0/Newtonsoft.Json.Bson.dll", normalizedPaths);
+            // Used packages
+            Assert.Contains("microsoft.netcore.app.ref", dependencies.Packages);
+            Assert.Contains("microsoft.aspnetcore.app.ref", dependencies.Packages);
+            Assert.Contains("newtonsoft.json", dependencies.Packages);
+            Assert.Contains("newtonsoft.json.bson", dependencies.Packages);
+
+        }
+
+        [Fact]
+        public void TestAssetsNet48()
+        {
+            // Setup
+            var assets = new Assets(new ProgressMonitor(new LoggerStub()));
+            var json = assetsNet48;
+            var dependencies = new DependencyContainer();
+
+            // Execute
+            var success = assets.TryParse(json, dependencies);
+
+            // Verify
+            Assert.True(success);
+            Assert.Equal(3, dependencies.Paths.Count);
+            Assert.Equal(3, dependencies.Packages.Count);
+
+            var normalizedPaths = dependencies.Paths.Select(FixExpectedPathOnWindows);
+            // Used references
+            Assert.Contains("microsoft.netframework.referenceassemblies.net48/1.0.2", normalizedPaths);
+            Assert.Contains("newtonsoft.json/12.0.1/lib/net45/Newtonsoft.Json.dll", normalizedPaths);
+            Assert.Contains("newtonsoft.json.bson/1.0.2/lib/net45/Newtonsoft.Json.Bson.dll", normalizedPaths);
+            // Used packages
+            Assert.Contains("microsoft.netframework.referenceassemblies.net48", dependencies.Packages);
+            Assert.Contains("newtonsoft.json", dependencies.Packages);
+            Assert.Contains("newtonsoft.json.bson", dependencies.Packages);
+        }
+
+        [Fact]
+        public void TestAssetsNetstandard21()
+        {
+            // Setup
+            var assets = new Assets(new ProgressMonitor(new LoggerStub()));
+            var json = assetsNetstandard21;
+            var dependencies = new DependencyContainer();
+
+            // Execute
+            var success = assets.TryParse(json, dependencies);
+
+            // Verify
+            Assert.True(success);
+            Assert.Equal(3, dependencies.Paths.Count);
+            Assert.Equal(3, dependencies.Packages.Count);
+
+            var normalizedPaths = dependencies.Paths.Select(FixExpectedPathOnWindows);
+
+            // Used references
+            Assert.Contains("netstandard.library.ref", normalizedPaths);
+            Assert.Contains("newtonsoft.json/12.0.1/lib/netstandard2.0/Newtonsoft.Json.dll", normalizedPaths);
+            Assert.Contains("newtonsoft.json.bson/1.0.2/lib/netstandard2.0/Newtonsoft.Json.Bson.dll", normalizedPaths);
+            // Used packages
+            Assert.Contains("netstandard.library.ref", dependencies.Packages);
+            Assert.Contains("newtonsoft.json", dependencies.Packages);
+            Assert.Contains("newtonsoft.json.bson", dependencies.Packages);
+        }
+
+        /// <summary>
+        /// This is manually created JSON string with the same structure as the assets file.
+        /// </summary>
         private readonly string assetsJson1 = """
 {
     "version": 3,
@@ -182,25 +249,384 @@ namespace Semmle.Extraction.Tests
 }
 """;
 
-        private readonly string assetsJson2 = """
+        /// <summary>
+        /// This is part of the content of the assets file that dotnet generates based on the
+        /// following project file content.
+        ///
+        /// <Project Sdk="Microsoft.NET.Sdk.Web">
+        ///   <PropertyGroup>
+        ///     <TargetFramework>net70</TargetFramework>
+        ///     <Nullable>enable</Nullable>
+        ///     <ImplicitUsings>enable</ImplicitUsings>
+        ///   </PropertyGroup>
+        ///   <ItemGroup>
+        ///     <PackageReference Include="Newtonsoft.Json.Bson" Version="1.0.2" />
+        ///   </ItemGroup>
+        /// </Project>
+        /// </summary>
+        private readonly string assetsNet70 = """
 {
-    "version": 3,
-    "targets": {
-        ".NETFramework,Version=v4.8": {
-            "Microsoft.NETFramework.ReferenceAssemblies/1.0.3": {
-                "type": "package",
-                "dependencies": {
-                    "Microsoft.NETFramework.ReferenceAssemblies.net48": "1.0.3"
-                }
-            },
-            "Microsoft.NETFramework.ReferenceAssemblies.net48/1.0.3": {
-                "type": "package",
-                "build": {
-                    "build/Microsoft.NETFramework.ReferenceAssemblies.net48.targets": {}
-                }
-            }
+  "version": 3,
+  "targets": {
+    "net7.0": {
+      "Newtonsoft.Json/12.0.1": {
+        "type": "package",
+        "compile": {
+          "lib/netstandard2.0/Newtonsoft.Json.dll": {
+            "related": ".pdb;.xml"
+          }
+        },
+        "runtime": {
+          "lib/netstandard2.0/Newtonsoft.Json.dll": {
+            "related": ".pdb;.xml"
+          }
         }
+      },
+      "Newtonsoft.Json.Bson/1.0.2": {
+        "type": "package",
+        "dependencies": {
+          "Newtonsoft.Json": "12.0.1"
+        },
+        "compile": {
+          "lib/netstandard2.0/Newtonsoft.Json.Bson.dll": {
+            "related": ".pdb;.xml"
+          }
+        },
+        "runtime": {
+          "lib/netstandard2.0/Newtonsoft.Json.Bson.dll": {
+            "related": ".pdb;.xml"
+          }
+        }
+      }
     }
+  },
+  "project": {
+    "version": "1.0.0",
+    "restore": {
+      "projectUniqueName": "/Users/michaelnebel/Work/playground/csharpwebapp/csharpwebapp.csproj",
+      "projectName": "csharpwebapp",
+      "projectPath": "/Users/michaelnebel/Work/playground/csharpwebapp/csharpwebapp.csproj",
+      "packagesPath": "/Users/michaelnebel/Work/playground/csharpwebapp/packages",
+      "outputPath": "/Users/michaelnebel/Work/playground/csharpwebapp/obj/",
+      "projectStyle": "PackageReference",
+      "configFilePaths": [
+        "/Users/michaelnebel/.nuget/NuGet/NuGet.Config"
+      ],
+      "originalTargetFrameworks": [
+        "net70"
+      ],
+      "sources": {
+        "https://api.nuget.org/v3/index.json": {}
+      },
+      "frameworks": {
+        "net7.0": {
+          "targetAlias": "net70",
+          "projectReferences": {}
+        }
+      },
+      "warningProperties": {
+        "warnAsError": [
+          "NU1605"
+        ]
+      }
+    },
+    "frameworks": {
+      "net7.0": {
+        "targetAlias": "net70",
+        "dependencies": {
+          "Newtonsoft.Json.Bson": {
+            "target": "Package",
+            "version": "[1.0.2, )"
+          }
+        },
+        "imports": [
+          "net461",
+          "net462",
+          "net47",
+          "net471",
+          "net472",
+          "net48",
+          "net481"
+        ],
+        "assetTargetFallback": true,
+        "warn": true,
+        "downloadDependencies": [
+          {
+            "name": "Microsoft.AspNetCore.App.Ref",
+            "version": "[7.0.2, 7.0.2]"
+          },
+          {
+            "name": "Microsoft.NETCore.App.Host.osx-x64",
+            "version": "[7.0.2, 7.0.2]"
+          },
+          {
+            "name": "Microsoft.NETCore.App.Ref",
+            "version": "[7.0.2, 7.0.2]"
+          }
+        ],
+        "frameworkReferences": {
+          "Microsoft.AspNetCore.App": {
+            "privateAssets": "none"
+          },
+          "Microsoft.NETCore.App": {
+            "privateAssets": "all"
+          }
+        },
+        "runtimeIdentifierGraphPath": "/Users/michaelnebel/.dotnet/sdk/7.0.102/RuntimeIdentifierGraph.json"
+      }
+    }
+  }
+}
+""";
+
+        /// <summary>
+        /// This is part of the content of the assets file that dotnet generates based on the
+        /// following project file content.
+        ///
+        /// <Project Sdk="Microsoft.NET.Sdk.Web">
+        ///   <PropertyGroup>
+        ///     <TargetFramework>net4.8</TargetFramework>
+        ///     <Nullable>enable</Nullable>
+        ///     <ImplicitUsings>enable</ImplicitUsings>
+        ///   </PropertyGroup>
+        ///   <ItemGroup>
+        ///     <PackageReference Include="Newtonsoft.Json.Bson" Version="1.0.2" />
+        ///   </ItemGroup>
+        /// </Project>
+        /// </summary>
+        private readonly string assetsNet48 = """
+{
+  "version": 3,
+  "targets": {
+    ".NETFramework,Version=v4.8": {
+      "Microsoft.NETFramework.ReferenceAssemblies/1.0.2": {
+        "type": "package",
+        "dependencies": {
+          "Microsoft.NETFramework.ReferenceAssemblies.net48": "1.0.2"
+        }
+      },
+      "Microsoft.NETFramework.ReferenceAssemblies.net48/1.0.2": {
+        "type": "package",
+        "build": {
+          "build/Microsoft.NETFramework.ReferenceAssemblies.net48.targets": {}
+        }
+      },
+      "Newtonsoft.Json/12.0.1": {
+        "type": "package",
+        "compile": {
+          "lib/net45/Newtonsoft.Json.dll": {
+            "related": ".pdb;.xml"
+          }
+        },
+        "runtime": {
+          "lib/net45/Newtonsoft.Json.dll": {
+            "related": ".pdb;.xml"
+          }
+        }
+      },
+      "Newtonsoft.Json.Bson/1.0.2": {
+        "type": "package",
+        "dependencies": {
+          "Newtonsoft.Json": "12.0.1"
+        },
+        "compile": {
+          "lib/net45/Newtonsoft.Json.Bson.dll": {
+            "related": ".pdb;.xml"
+          }
+        },
+        "runtime": {
+          "lib/net45/Newtonsoft.Json.Bson.dll": {
+            "related": ".pdb;.xml"
+          }
+        }
+      }
+    }
+  },
+  "projectFileDependencyGroups": {
+    ".NETFramework,Version=v4.8": [
+      "Microsoft.NETFramework.ReferenceAssemblies >= 1.0.2",
+      "Newtonsoft.Json.Bson >= 1.0.2"
+    ]
+  },
+  "packageFolders": {
+    "/Users/michaelnebel/Work/playground/csharpwebapp/packages": {}
+  },
+  "project": {
+    "version": "1.0.0",
+    "restore": {
+      "projectUniqueName": "/Users/michaelnebel/Work/playground/csharpwebapp/csharpwebapp.csproj",
+      "projectName": "csharpwebapp",
+      "projectPath": "/Users/michaelnebel/Work/playground/csharpwebapp/csharpwebapp.csproj",
+      "packagesPath": "/Users/michaelnebel/Work/playground/csharpwebapp/packages",
+      "outputPath": "/Users/michaelnebel/Work/playground/csharpwebapp/obj/",
+      "projectStyle": "PackageReference",
+      "configFilePaths": [
+        "/Users/michaelnebel/.nuget/NuGet/NuGet.Config"
+      ],
+      "originalTargetFrameworks": [
+        "net4.8"
+      ],
+      "sources": {
+        "https://api.nuget.org/v3/index.json": {}
+      },
+      "frameworks": {
+        "net48": {
+          "targetAlias": "net4.8",
+          "projectReferences": {}
+        }
+      },
+      "warningProperties": {
+        "warnAsError": [
+          "NU1605"
+        ]
+      }
+    },
+    "frameworks": {
+      "net48": {
+        "targetAlias": "net4.8",
+        "dependencies": {
+          "Microsoft.NETFramework.ReferenceAssemblies": {
+            "suppressParent": "All",
+            "target": "Package",
+            "version": "[1.0.2, )",
+            "autoReferenced": true
+          },
+          "Newtonsoft.Json.Bson": {
+            "target": "Package",
+            "version": "[1.0.2, )"
+          }
+        },
+        "runtimeIdentifierGraphPath": "/Users/michaelnebel/.dotnet/sdk/7.0.102/RuntimeIdentifierGraph.json"
+      }
+    }
+  }
+}
+""";
+
+        /// <summary>
+        /// This is part of the content of the assets file that dotnet generates based on the
+        /// following project file content.
+        ///
+        /// <Project Sdk="Microsoft.NET.Sdk.Web">
+        ///   <PropertyGroup>
+        ///     <TargetFramework>netstandard2.1</TargetFramework>
+        ///     <Nullable>enable</Nullable>
+        ///     <ImplicitUsings>enable</ImplicitUsings>
+        ///   </PropertyGroup>
+        ///   <ItemGroup>
+        ///     <PackageReference Include="Newtonsoft.Json.Bson" Version="1.0.2" />
+        ///   </ItemGroup>
+        /// </Project>
+        /// </summary>
+        private readonly string assetsNetstandard21 = """
+{
+  "version": 3,
+  "targets": {
+    ".NETStandard,Version=v2.1": {
+      "Newtonsoft.Json/12.0.1": {
+        "type": "package",
+        "compile": {
+          "lib/netstandard2.0/Newtonsoft.Json.dll": {
+            "related": ".pdb;.xml"
+          }
+        },
+        "runtime": {
+          "lib/netstandard2.0/Newtonsoft.Json.dll": {
+            "related": ".pdb;.xml"
+          }
+        }
+      },
+      "Newtonsoft.Json.Bson/1.0.2": {
+        "type": "package",
+        "dependencies": {
+          "Newtonsoft.Json": "12.0.1"
+        },
+        "compile": {
+          "lib/netstandard2.0/Newtonsoft.Json.Bson.dll": {
+            "related": ".pdb;.xml"
+          }
+        },
+        "runtime": {
+          "lib/netstandard2.0/Newtonsoft.Json.Bson.dll": {
+            "related": ".pdb;.xml"
+          }
+        }
+      }
+    }
+  },
+  "projectFileDependencyGroups": {
+    ".NETStandard,Version=v2.1": [
+      "Newtonsoft.Json.Bson >= 1.0.2"
+    ]
+  },
+  "packageFolders": {
+    "/Users/michaelnebel/Work/playground/csharpwebapp/packages": {}
+  },
+  "project": {
+    "version": "1.0.0",
+    "restore": {
+      "projectUniqueName": "/Users/michaelnebel/Work/playground/csharpwebapp/csharpwebapp.csproj",
+      "projectName": "csharpwebapp",
+      "projectPath": "/Users/michaelnebel/Work/playground/csharpwebapp/csharpwebapp.csproj",
+      "packagesPath": "/Users/michaelnebel/Work/playground/csharpwebapp/packages",
+      "outputPath": "/Users/michaelnebel/Work/playground/csharpwebapp/obj/",
+      "projectStyle": "PackageReference",
+      "configFilePaths": [
+        "/Users/michaelnebel/.nuget/NuGet/NuGet.Config"
+      ],
+      "originalTargetFrameworks": [
+        "netstandard2.1"
+      ],
+      "sources": {
+        "https://api.nuget.org/v3/index.json": {}
+      },
+      "frameworks": {
+        "netstandard2.1": {
+          "targetAlias": "netstandard2.1",
+          "projectReferences": {}
+        }
+      },
+      "warningProperties": {
+        "warnAsError": [
+          "NU1605"
+        ]
+      }
+    },
+    "frameworks": {
+      "netstandard2.1": {
+        "targetAlias": "netstandard2.1",
+        "dependencies": {
+          "Newtonsoft.Json.Bson": {
+            "target": "Package",
+            "version": "[1.0.2, )"
+          }
+        },
+        "imports": [
+          "net461",
+          "net462",
+          "net47",
+          "net471",
+          "net472",
+          "net48",
+          "net481"
+        ],
+        "assetTargetFallback": true,
+        "warn": true,
+        "downloadDependencies": [
+          {
+            "name": "NETStandard.Library.Ref",
+            "version": "[2.1.0, 2.1.0]"
+          }
+        ],
+        "frameworkReferences": {
+          "NETStandard.Library": {
+            "privateAssets": "all"
+          }
+        },
+        "runtimeIdentifierGraphPath": "/Users/michaelnebel/.dotnet/sdk/7.0.102/RuntimeIdentifierGraph.json"
+      }
+    }
+  }
 }
 """;
     }
