b7b10ce5499df36028c40da44323caf39c95aabc
Michael Nebel <michaelnebel@github.com>
2023-11-17 09:47:36 +0100

    C#: Address review comments.

--- csharp/extractor/Semmle.Extraction.CSharp.DependencyFetching/FrameworkPackageNames.cs
+++ csharp/extractor/Semmle.Extraction.CSharp.DependencyFetching/FrameworkPackageNames.cs
@@ -5,8 +5,12 @@ namespace Semmle.Extraction.CSharp.DependencyFetching
 {
     internal static class FrameworkPackageNames
     {
+        public static string AspNetCoreFramework { get; } = "microsoft.aspnetcore.app.ref";
+
+        public static string WindowsDesktopFramework { get; } = "microsoft.windowsdesktop.app.ref";
+
         // The order of the packages is important.
-        public static readonly string[] NetFrameworks = new string[]
+        public static string[] NetFrameworks { get; } = new string[]
             {
                 "microsoft.netcore.app.ref", // net7.0, ... net5.0, netcoreapp3.1, netcoreapp3.0
                 "microsoft.netframework.referenceassemblies.", // net48, ..., net20
@@ -14,13 +18,7 @@ namespace Semmle.Extraction.CSharp.DependencyFetching
                 "netstandard.library" // netstandard2.0
             };
 
-        public static string AspNetCoreFramework =>
-            "microsoft.aspnetcore.app.ref";
-
-        public static string WindowsDesktopFramework =>
-            "microsoft.windowsdesktop.app.ref";
-
-        public static readonly IEnumerable<string> AllFrameworks =
+        public static IEnumerable<string> AllFrameworks { get; } =
             NetFrameworks
                 .Union(new string[] { AspNetCoreFramework, WindowsDesktopFramework });
     }
