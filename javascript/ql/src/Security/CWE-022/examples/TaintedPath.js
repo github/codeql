dfffa1e23740e487f02ffe3e7e7f6aa287f7aefc
Max Schaefer <54907921+max-schaefer@users.noreply.github.com>
2023-11-21 10:07:11 +0000

    Apply suggestions from code review
    Co-authored-by: Sam Browning <106113886+sabrowning1@users.noreply.github.com>

--- javascript/ql/src/Security/CWE-022/examples/TaintedPath.js
+++ javascript/ql/src/Security/CWE-022/examples/TaintedPath.js
@@ -7,6 +7,6 @@ const ROOT = "/var/www/";
 var server = http.createServer(function(req, res) {
   let filePath = url.parse(req.url, true).query.path;
 
-  // BAD: This could read any file on the file system
+  // BAD: This function uses unsanitized input that can read any file on the file system.
   res.write(fs.readFileSync(ROOT + filePath, 'utf8'));
 });
