cef3ce1cde42bb0fad9aa691e6a070a15d088f82
Harry Maclean <hmac@github.com>
2023-11-28 15:32:16 +0000

    add a basic failing test

--- shared/yeast/src/lib.rs
+++ shared/yeast/src/lib.rs
@@ -1,3 +1,34 @@
-pub fn foo() -> i64 {
-    5
+extern crate proc_macro;
+use proc_macro::TokenStream;
+
+pub struct Runner {}
+
+impl Runner {
+    pub fn new(language: tree_sitter::Language, rules: Vec<Rule>) -> Self {
+        Self {}
+    }
+
+    pub fn run(&self, _input: &str) -> Ast {
+        Ast {}
+    }
 }
+
+// We probably want to change this
+#[derive(PartialEq, Eq, Debug)]
+pub struct Ast {}
+
+pub struct Query {}
+impl Query {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+pub struct Rule {}
+impl Rule {
+    pub fn new(query: Query, transform: impl Fn(Match) -> Ast) -> Self {
+        Self {}
+    }
+}
+
+pub struct Match {}
--- shared/yeast/tests/test.rs
+++ shared/yeast/tests/test.rs
@@ -2,6 +2,65 @@
 use yeast::*;
 
 #[test]
-fn test() {
-    assert_eq!(foo(), 5);
+fn test_ruby_multiple_assignment() {
+    // We want to convert this
+    //
+    // x, y, z = e
+    //
+    // into this
+    //
+    // __tmp_1 = e
+    // x = __tmp_1[0]
+    // y = __tmp_1[1]
+    // z = __tmp_1[2]
+
+    // Define a desugaring rule, which is a query together with a transformation.
+
+    let query = Query::new();
+    let transform = |captures| {
+        // construct the new tree here maybe
+        // captures is probably a HashMap from capture name to AST node
+        Ast {}
+    };
+    let rule = Rule::new(query, transform);
+
+    let input = "x, y, z = e";
+
+    // Construct the thing that runs our desugaring process
+    let runner = Runner::new(tree_sitter_ruby::language(), vec![rule]);
+
+    // Run it on our example
+    let output = runner.run(input);
+
+    // we could create a macro for this
+    // let expected_output = ast! {
+    //     assignment {
+    //         left: identifier { name: "__tmp" },
+    //         right: identifier { name: "e" },
+    //     },
+    //     assignment {
+    //         left: identifier { name: "x" },
+    //         right: element_reference {
+    //             object: identifier { name: "__tmp" },
+    //             index: integer(0)
+    //         },
+    //     },
+    //     assignment {
+    //         left: identifier { name: "y" },
+    //         right: element_reference {
+    //             object: identifier { name: "__tmp" },
+    //             index: integer(1)
+    //         },
+    //     },
+    //     assignment {
+    //         left: identifier { name: "z" },
+    //         right: element_reference {
+    //             object: identifier { name: "__tmp" },
+    //             index: integer(2)
+    //         },
+    //     },
+    // };
+    let expected_output = todo!();
+
+    assert_eq!(output, expected_output);
 }
