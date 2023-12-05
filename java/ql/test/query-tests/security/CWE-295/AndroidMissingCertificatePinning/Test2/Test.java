bd8f35bef7020510abd26d28df4816843613dece
Tony Torralba <atorralba@users.noreply.github.com>
2023-12-05 16:49:36 +0100

    Java: Fix FPs in Missing certificate pinning
    Local URIs should never require pinning

--- java/ql/test/query-tests/security/CWE-295/AndroidMissingCertificatePinning/Test2/Test.java
+++ java/ql/test/query-tests/security/CWE-295/AndroidMissingCertificatePinning/Test2/Test.java
@@ -1,8 +1,20 @@
 import java.net.URL;
 import java.net.URLConnection;
 
-class Test{
+class Test {
     URLConnection test2() throws Exception {
         return new URL("https://example.com").openConnection(); // $hasNoTrustedResult
+    }
+
+    URLConnection test3() throws Exception {
+        return new URL("classpath:example/directory/test.class").openConnection();
+    }
+
+    URLConnection test4() throws Exception {
+        return new URL("file:///example/file").openConnection();
+    }
+
+    URLConnection test5() throws Exception {
+        return new URL("jar:file:///C:/example/test.jar!/test.xml").openConnection();
     }
 }
