2c624c23ed66e84324d60952fb6074611ef9ff6b
Tamas Vajk <tamasvajk@github.com>
2023-12-07 09:19:12 +0100

    Add test cases for missing flow with interpolated strings and `StringBuilder`

--- csharp/ql/test/library-tests/dataflow/global/GlobalDataFlowStringBuilder.cs
+++ csharp/ql/test/library-tests/dataflow/global/GlobalDataFlowStringBuilder.cs
@@ -19,15 +19,34 @@ public class DataFlowStringBuilder
         sb.Append(s);
     }
 
+    static void AppendToStringBuilderInterpolated(StringBuilder sb, string s)
+    {
+        sb.Append($"a{s}b");
+    }
+
     void TestStringBuilderFlow()
     {
         var sb = new StringBuilder();
         AppendToStringBuilder(sb, "taint source");
-        var sink43 = sb.ToString();
-        Check(sink43);
+        var sink0 = sb.ToString();
+        Check(sink0);
+
+        var sb1 = new StringBuilder();
+        sb1.Append(sb);
+        var sink1 = sb1.ToString();
+        Check(sink1);
+
+        var sb2 = new StringBuilder();
+        sb2.Append($"{sb}");
+        var sink2 = sb2.ToString();
+        Check(sink2);
 
         sb.Clear();
         var nonSink = sb.ToString();
         Check(nonSink);
+
+        AppendToStringBuilderInterpolated(sb, "taint source");
+        var sink3 = sb.ToString();
+        Check(sink3);
     }
 }
