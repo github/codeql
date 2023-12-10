102f09aa2348a6a2aef97ab3a7c94f5213153069
amammad <77095239+amammad@users.noreply.github.com>
2023-12-10 20:33:00 +0100

    extend tests

--- javascript/ql/test/experimental/Security/CWE-099/test.js
+++ javascript/ql/test/experimental/Security/CWE-099/test.js
@@ -4,6 +4,7 @@ http.createServer((req, res) => {
   const { EnvValue } = req.body;
   process.env["A_Critical_Env"] = EnvValue; // NOT OK
   process.env[AKey] = EnvValue; // NOT OK
+  process.env.AKey = EnvValue; // NOT OK
 
   res.end('env has been injected!');
 });
