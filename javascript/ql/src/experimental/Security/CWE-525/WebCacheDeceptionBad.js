2e0465517784a545fce62ca20057412534475ac3
Yunus AYDIN <yunus.aydin@trendyol.com>
2023-12-20 23:56:46 +0300

    add rule and example code for express

---
+++ javascript/ql/src/experimental/Security/CWE-525/WebCacheDeceptionBad.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+port = 3000
+
+app.get('/test*', (req, res) => {
+  res.send('Company: Trendyol, Birth Date: 1997, Country: Turkey, Phone: 5554443322')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
