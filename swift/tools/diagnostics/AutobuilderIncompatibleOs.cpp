39edfa3c1434ceee4ce587189c1a99aa35df2d31
Calum Grant <calumgrant@github.com>
2024-01-15 09:34:28 +0000

    Swift extractor: Rename a SwiftDiagnostic to Diagnostic

--- swift/tools/diagnostics/AutobuilderIncompatibleOs.cpp
+++ swift/tools/diagnostics/AutobuilderIncompatibleOs.cpp
@@ -8,23 +8,22 @@
 
 const std::string_view codeql::programName = "autobuilder";
 
-constexpr codeql::SwiftDiagnostic incompatibleOs{
-    .id = "incompatible-os",
-    .name = "Incompatible operating system (expected macOS)",
-    .action =
-        "[Change the Actions runner][1] to run on macOS.\n"
-        "\n"
-        "You may be able to run analysis on Linux by setting up a [manual build command][2].\n"
-        "\n"
-        "[1]: "
-        "https://docs.github.com/en/actions/using-workflows/"
-        "workflow-syntax-for-github-actions#jobsjob_idruns-on\n"
-        "[2]: "
-        "https://docs.github.com/en/enterprise-server/code-security/code-scanning/"
-        "automatically-scanning-your-code-for-vulnerabilities-and-errors/"
-        "configuring-the-codeql-workflow-for-compiled-languages#adding-build-steps-for-a-compiled-"
-        "language",
-};
+constexpr codeql::Diagnostic incompatibleOs = codeql::swiftDiagnostic(
+    "incompatible-os",
+    "Incompatible operating system (expected macOS)",
+    "[Change the Actions runner][1] to run on macOS.\n"
+    "\n"
+    "You may be able to run analysis on Linux by setting up a [manual build command][2].\n"
+    "\n"
+    "[1]: "
+    "https://docs.github.com/en/actions/using-workflows/"
+    "workflow-syntax-for-github-actions#jobsjob_idruns-on\n"
+    "[2]: "
+    "https://docs.github.com/en/enterprise-server/code-security/code-scanning/"
+    "automatically-scanning-your-code-for-vulnerabilities-and-errors/"
+    "configuring-the-codeql-workflow-for-compiled-languages#adding-build-steps-for-a-"
+    "compiled-"
+    "language");
 
 static codeql::Logger& logger() {
   static codeql::Logger ret{"main"};
