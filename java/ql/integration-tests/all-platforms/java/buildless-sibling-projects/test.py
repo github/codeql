042a3b64bbd6785bbf70864ee47bcbfe460717cf
Chris Smowton <smowton@github.com>
2024-01-15 16:09:09 +0000

    Use Java 11 to run old Gradle

--- java/ql/integration-tests/all-platforms/java/buildless-sibling-projects/test.py
+++ java/ql/integration-tests/all-platforms/java/buildless-sibling-projects/test.py
@@ -2,6 +2,9 @@ from create_database_utils import *
 from diagnostics_test_utils import *
 from buildless_test_utils import *
 
+#The version of gradle used doesn't work on java 17
+try_use_java11()
+
 run_codeql_database_create([], lang="java", extra_env={"CODEQL_EXTRACTOR_JAVA_OPTION_BUILDLESS": "true", "CODEQL_EXTRACTOR_JAVA_OPTION_BUILDLESS_CLASSPATH_FROM_BUILD_FILES": "true"})
 
 check_diagnostics()
