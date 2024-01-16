f14b3265ab26622f6be6f2c95a4f0c856928be2d
Michael Nebel <michaelnebel@github.com>
2024-01-16 14:07:45 +0100

    C#: Move static methods in CollectionFlow as these impact result line numbers.

--- csharp/ql/test/library-tests/dataflow/collections/CollectionFlow.cs
+++ csharp/ql/test/library-tests/dataflow/collections/CollectionFlow.cs
@@ -8,6 +8,32 @@ public class CollectionFlow
 
     public A[] As;
 
+    public static void Sink<T>(T t) { }
+
+    public static void SinkElem<T>(T[] ts) => Sink(ts[0]);
+
+    public static void SinkListElem<T>(IList<T> list) => Sink(list[0]);
+
+    public static void SinkDictValue<T>(IDictionary<int, T> dict) => Sink(dict[0]);
+
+    public static void SinkDictKey<T>(IDictionary<T, int> dict) => Sink(dict.Keys.First());
+
+    public static T First<T>(T[] ts) => ts[0];
+
+    public static T ListFirst<T>(IList<T> list) => list[0];
+
+    public static T DictIndexZero<T>(IDictionary<int, T> dict) => dict[0];
+
+    public static T DictFirstValue<T>(IDictionary<int, T> dict) => dict.First().Value;
+
+    public static T DictValuesFirst<T>(IDictionary<int, T> dict) => dict.Values.First();
+
+    public static T DictKeysFirst<T>(IDictionary<T, int> dict) => dict.Keys.First();
+
+    public static T DictFirstKey<T>(IDictionary<T, int> dict) => dict.First().Key;
+
+    public static void SinkParams<T>(params T[] args) => Sink(args[0]);
+
     public void ArrayInitializerFlow()
     {
         var a = new A();
@@ -367,30 +393,4 @@ public class CollectionFlow
         SinkListElem(list); // no flow
         Sink(ListFirst(list)); // no flow
     }
-
-    public static void Sink<T>(T t) { }
-
-    public static void SinkElem<T>(T[] ts) => Sink(ts[0]);
-
-    public static void SinkListElem<T>(IList<T> list) => Sink(list[0]);
-
-    public static void SinkDictValue<T>(IDictionary<int, T> dict) => Sink(dict[0]);
-
-    public static void SinkDictKey<T>(IDictionary<T, int> dict) => Sink(dict.Keys.First());
-
-    public static T First<T>(T[] ts) => ts[0];
-
-    public static T ListFirst<T>(IList<T> list) => list[0];
-
-    public static T DictIndexZero<T>(IDictionary<int, T> dict) => dict[0];
-
-    public static T DictFirstValue<T>(IDictionary<int, T> dict) => dict.First().Value;
-
-    public static T DictValuesFirst<T>(IDictionary<int, T> dict) => dict.Values.First();
-
-    public static T DictKeysFirst<T>(IDictionary<T, int> dict) => dict.Keys.First();
-
-    public static T DictFirstKey<T>(IDictionary<T, int> dict) => dict.First().Key;
-
-    public static void SinkParams<T>(params T[] args) => Sink(args[0]);
 }
