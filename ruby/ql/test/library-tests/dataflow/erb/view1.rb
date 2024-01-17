c26fbab773f479d5ab484e665089b4212e0a94e5
Tom Hvitved <hvitved@github.com>
2024-01-17 09:51:45 +0100

    Add more tests

---
+++ ruby/ql/test/library-tests/dataflow/erb/view1.rb
@@ -0,0 +1,16 @@
+class ViewComponent::Base
+end
+
+class View1 < ViewComponent::Base
+    def initialize(x)
+        @x = x
+    end
+
+    def foo
+        sink(@x) # $ hasValueFlow=1 $ hasValueFlow=2
+    end
+
+    def set(x)
+        @x = x
+    end
+end
