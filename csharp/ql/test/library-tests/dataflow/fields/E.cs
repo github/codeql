4dfeff38f9881aec6f65080043b18668e7ad2c19
Michael Nebel <michaelnebel@github.com>
2024-02-01 13:40:12 +0100

    C#: Add dataflow field test for ref structs with ref and ordinary fields.

--- csharp/ql/test/library-tests/dataflow/fields/E.cs
+++ csharp/ql/test/library-tests/dataflow/fields/E.cs
@@ -17,7 +17,7 @@ public class E
         s.Field = o;
     }
 
-    private void M()
+    private void M1()
     {
         var o = Source<object>(1);
         var s = CreateS(o);
@@ -26,6 +26,35 @@ public class E
         s = new S();
         NotASetter(s, o);
         Sink(s.Field); // no flow
+    }
+
+    ref struct RefS
+    {
+        public object Field;
+        public ref object RefField;
+
+        public RefS(object o1, ref object o2)
+        {
+            Field = o1;
+            RefField = ref o2;
+        }
+    }
+
+    static void PartialSetter(RefS s, object o)
+    {
+        s.Field = o;
+        s.RefField = o;
+    }
+
+    private void M2()
+    {
+        var o1 = new object();
+        var o2 = new object();
+        var refs = new RefS(o1, ref o2);
+        var taint = Source<object>(2);
+        PartialSetter(refs, taint);
+        Sink(refs.Field); // no flow
+        Sink(refs.RefField); // $ hasValueFlow=2
     }
 
     public static void Sink(object o) { }
