f5d4c49b162dfa653a2ff8880898424962ed600f
Michael Nebel <michaelnebel@github.com>
2024-02-02 11:53:43 +0100

    C#: Add some more constructor dataflow tests.

--- csharp/ql/test/library-tests/dataflow/constructors/Constructors.cs
+++ csharp/ql/test/library-tests/dataflow/constructors/Constructors.cs
@@ -34,6 +34,55 @@ public class Constructors
         }
     }
 
+    public class C1
+    {
+        public object Obj;
+
+        public C1(object o) => Obj = o;
+    }
+
+    public class C2(object o21param, object o22param)
+    {
+        public object Obj21 = o21param;
+
+        public object Obj22 => o22param;
+
+        public object Obj23 => Obj21;
+
+        public void SetObj(object o)
+        {
+            o22param = o;
+        }
+    }
+
+    public void M1()
+    {
+        var o = Source<object>(1);
+        var c1 = new C1(o);
+        Sink(c1.Obj); // $ hasValueFlow=1
+    }
+
+    public void M2()
+    {
+        var o21 = Source<object>(2);
+        var o22 = Source<object>(3);
+        var c2 = new C2(o21, o22);
+        Sink(c2.Obj21); // $ hasValueFlow=2
+        Sink(c2.Obj22); // $ hasValueFlow=3
+        Sink(c2.Obj23); // $ hasValueFlow=2
+    }
+
+    public void M3()
+    {
+        var c2 = new C2(new object(), new object());
+        Sink(c2.Obj21); // No flow
+        Sink(c2.Obj22); // No flow
+        Sink(c2.Obj23); // No flow
+        var taint = Source<object>(4);
+        c2.SetObj(taint);
+        Sink(c2.Obj22); // $ hasValueFlow=4
+    }
+
     public static void Sink(object o) { }
 
     public static T Source<T>(object source) => throw null;
