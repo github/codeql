888f47c6c88a5376764c4036d8c5bc4d329be7e0
Tamas Vajk <tamasvajk@github.com>
2024-02-12 12:02:41 +0100

    Remove `cil=false` extractor option from integration tests

--- csharp/ql/integration-tests/linux-only/standalone_dependencies_non_utf8_filename/test.py
+++ csharp/ql/integration-tests/linux-only/standalone_dependencies_non_utf8_filename/test.py
@@ -5,4 +5,4 @@ path = b'\xd2abcd.cs'
 with open(path, 'w') as file:
   file.write('class X { }\n')
 
-run_codeql_database_create([], lang="csharp", extra_args=["--extractor-option=buildless=true", "--extractor-option=cil=false"])
+run_codeql_database_create([], lang="csharp", extra_args=["--extractor-option=buildless=true"])
