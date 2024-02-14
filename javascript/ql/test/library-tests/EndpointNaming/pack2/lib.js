2a91bb8c54b3031f95a2d1e7552040105826007d
Asger F <asgerf@github.com>
2024-02-14 13:27:48 +0100

    JS: Add test showing ambiguous predecessor

--- javascript/ql/test/library-tests/EndpointNaming/pack2/lib.js
+++ javascript/ql/test/library-tests/EndpointNaming/pack2/lib.js
@@ -4,3 +4,5 @@ class AmbiguousClass {
 
 export default AmbiguousClass; // $ alias=(pack2).lib.default==(pack2).lib.LibClass
 export { AmbiguousClass as LibClass }
+
+AmbiguousClass.foo = function() {} // $ method=(pack2).lib.LibClass.foo alias=(pack2).lib.default.foo==(pack2).lib.LibClass.foo
