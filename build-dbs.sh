8e7e5d03a5b4ae096c9f6ad5a8c52684fc214681
Alvaro Muñoz <pwntester@github.com>
2024-02-28 11:15:38 +0100

    fix(test): Add expected files

--- build-dbs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf ql/src/test/test.testproj || true
-rm -rf ql/lib/test/test.testproj || true
-codeql database create ql/src/test/test.testproj -l yaml -s ql/src/test
-codeql database create ql/lib/test/test.testproj -l yaml -s ql/lib/test
