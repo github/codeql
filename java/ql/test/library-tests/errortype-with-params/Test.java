140c3189e348a2d295bc2f5fa2cbeaef55cae5ee
Chris Smowton <smowton@github.com>
2024-02-28 11:00:16 +0000

    Adjust test expectations

--- java/ql/test/library-tests/errortype-with-params/Test.java
+++ java/ql/test/library-tests/errortype-with-params/Test.java
@@ -1,3 +1,4 @@
 public class Test implements Unavailable<String> { }
 
 // Diagnostic Matches: 1 errors during annotation processing
+// Diagnostic Matches: Unknown or erroneous type for expression of kind TypeAccess
