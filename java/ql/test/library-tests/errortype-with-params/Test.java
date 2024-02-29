ef9544cbef5c00e9f839ae610f6c3a605bb7bcac
Chris Smowton <smowton@github.com>
2024-02-29 15:33:29 +0000

    Adjust test expectations now signature type-accesses are named

--- java/ql/test/library-tests/errortype-with-params/Test.java
+++ java/ql/test/library-tests/errortype-with-params/Test.java
@@ -1,4 +1,3 @@
 public class Test implements Unavailable<String> { }
 
 // Diagnostic Matches: 1 errors during annotation processing
-// Diagnostic Matches: Unknown or erroneous type for expression of kind TypeAccess
