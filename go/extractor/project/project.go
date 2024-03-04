acf2f761a6414dd0b7567d443a5b34f6199c1ded
Michael B. Gale <mbg@github.com>
2024-03-04 10:51:58 +0000

    Go: Improve log message for `InitGoModForLegacyProject`

--- go/extractor/project/project.go
+++ go/extractor/project/project.go
@@ -129,7 +129,7 @@ var filesToRemove []string = []string{}
 
 // Try to initialize a go.mod file for projects that do not already have one.
 func InitGoModForLegacyProject(path string) {
-	log.Printf("Project appears to be a legacy Go project, attempting to initialize go.mod in %s\n", path)
+	log.Printf("The code in %s seems to be missing a go.mod file, attempting to initialize one...\n", path)
 
 	modInit := toolchain.InitModule(path)
 
