63a04c056a43ac92e453d1e89a20428e23b8d0b8
Owen Mansel-Chan <owen-mc@github.com>
2024-03-23 21:30:33 +0000

    Add test with `tokenImage` as used in JavaCC

---
+++ java/ql/test/query-tests/security/CWE-532/TokenSequenceParserTest.java
@@ -0,0 +1,17 @@
+import org.apache.logging.log4j.Logger;
+
+interface TokenSequenceParserConstants {
+  /** Literal token values. */
+  String[] tokenImage = {
+    "<EOF>",
+  };
+}
+
+public class TokenSequenceParserTest implements TokenSequenceParserConstants {
+    void test(String password) {
+        Logger logger = null;
+
+        logger.info("When parsing found this: " + tokenImage[0]); // Safe
+    }
+
+}
