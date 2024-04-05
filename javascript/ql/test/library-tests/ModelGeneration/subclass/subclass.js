29a61458e00cb85910179ea433f90c253710baf7
Asger F <asgerf@github.com>
2024-04-05 16:08:57 +0200

    JS: Add test case showing problem with chains going through internal classes

--- javascript/ql/test/library-tests/ModelGeneration/subclass/subclass.js
+++ javascript/ql/test/library-tests/ModelGeneration/subclass/subclass.js
@@ -15,3 +15,11 @@ import * as upstream from "upstream-lib";
 export class D extends upstream.Type {
     d() {}
 }
+
+// Test case where subclass chain goes through an internal class
+// TODO: we miss the subclass chain between ExposedMidSubClass and A
+class InternalMidClass extends A {}
+
+export class ExposedMidSubClass extends InternalMidClass {
+    m() {}
+}
