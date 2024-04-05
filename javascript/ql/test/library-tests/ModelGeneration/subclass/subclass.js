f2ea88aa4ce25dbe073b908621601455aae716b3
Asger F <asgerf@github.com>
2024-04-05 15:11:37 +0200

    JS: Add test showing missing re-export of base class relationship

--- javascript/ql/test/library-tests/ModelGeneration/subclass/subclass.js
+++ javascript/ql/test/library-tests/ModelGeneration/subclass/subclass.js
@@ -9,3 +9,11 @@ export class B extends A {
 export class C extends B {
     c() {}
 }
+
+import * as upstream from "upstream-lib";
+
+// TODO: needs to emit type model: [upstream.Type; (subclass).D.prototype; ""]
+// The getAValueReachableFromSource() logic does not handle the base class -> instance step
+export class D extends upstream.Type {
+    d() {}
+}
