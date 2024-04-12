f08a5988217bf976e0b7b344aa3a696981349b98
Owen Mansel-Chan <owen-mc@github.com>
2024-04-12 10:14:51 +0100

    Add tests for FPs: type switches, type assertions

--- go/ql/test/query-tests/Security/CWE-681/IncorrectIntegerConversion.go
+++ go/ql/test/query-tests/Security/CWE-681/IncorrectIntegerConversion.go
@@ -498,3 +498,55 @@ func dealWithArchSizeCorrectly(s string) uint {
 	}
 	return 0
 }
+
+func typeSwitch1(s string) {
+	i64, _ := strconv.ParseInt(s, 10, 64)
+	var input any = i64
+	switch v := input.(type) {
+	case int16, string:
+		if _, ok := input.(string); ok {
+			return
+		}
+		_ = int16(v.(int16)) // $ SPURIOUS: hasValueFlow="type assertion"
+		_ = int8(v.(int16))  // $ hasValueFlow="type assertion"
+	case int32:
+		_ = int32(v) // $ SPURIOUS: hasValueFlow="v"
+		_ = int8(v)  // $ hasValueFlow="v"
+	case int64:
+		_ = int8(v) // $ hasValueFlow="v"
+	default:
+		_ = int8(v.(int64)) // $ hasValueFlow="type assertion"
+	}
+}
+
+func typeSwitch2(s string) {
+	i64, _ := strconv.ParseInt(s, 10, 64)
+	var input any = i64
+	switch input.(type) {
+	case int16, string:
+		if _, ok := input.(string); ok {
+			return
+		}
+		_ = int16(input.(int16)) // $ SPURIOUS: hasValueFlow="type assertion"
+		_ = int8(input.(int16))  // $ hasValueFlow="type assertion"
+	case int32:
+		_ = int32(input.(int32)) // $ SPURIOUS: hasValueFlow="type assertion"
+		_ = int8(input.(int32))  // $ hasValueFlow="type assertion"
+	case int64:
+		_ = int8(input.(int64)) // $ hasValueFlow="type assertion"
+	default:
+		_ = int8(input.(int64)) // $ hasValueFlow="type assertion"
+	}
+}
+
+func checkedTypeAssertion(s string) {
+	i64, _ := strconv.ParseInt(s, 10, 64)
+	var input any = i64
+	if v, ok := input.(int16); ok {
+		// Need to account for the fact that within this case clause, v is an int16
+		_ = int16(v) // $ SPURIOUS: hasValueFlow="v"
+		_ = int8(v)  // $ hasValueFlow="v"
+	} else if v, ok := input.(int32); ok {
+		_ = int16(v) // $ hasValueFlow="v"
+	}
+}
