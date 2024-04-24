4aa0a8ebae2e6c1bb23f6bd7accf1f284e5b77ce
Paolo Tranquilli <redsun82@github.com>
2024-04-24 13:39:32 +0200

    Kotlin: make wrapper more robust for windows

--- java/kotlin-extractor/current_kotlin_version.py
+++ java/kotlin-extractor/current_kotlin_version.py
@@ -5,9 +5,10 @@ import shutil
 kotlinc = shutil.which('kotlinc')
 if kotlinc is None:
     raise Exception("kotlinc not found")
-output = subprocess.run([kotlinc, "-version"], text=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE,
-                        check=True).stderr
-m = re.match(r'.* kotlinc-jvm ([0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z][a-zA-Z0-9]*)?) .*', output)
+res = subprocess.run([kotlinc, "-version"], text=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+if res.returncode != 0:
+    raise Exception(f"kotlinc -version failed: {res.stderr}")
+m = re.match(r'.* kotlinc-jvm ([0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z][a-zA-Z0-9]*)?) .*', res.stderr)
 if m is None:
-    raise Exception(f'Cannot detect version of kotlinc (got {output})')
+    raise Exception(f'Cannot detect version of kotlinc (got {res.stderr})')
 print(m[1])
