19b2e56d0276518c1c6559536338b61e2451acf6
Paolo Tranquilli <redsun82@github.com>
2024-04-26 13:38:10 +0200

    Go: group BUILD and dbscheme generation target

--- go/gen.py
+++ go/gen.py
@@ -4,13 +4,21 @@ import subprocess
 from python.runfiles import runfiles
 
 this = pathlib.Path(__file__).resolve()
-go_extractor_dir = this.parents[1] / "extractor"
-gazelle = runfiles.Create().Rlocation(sys.argv[1])
+go_extractor_dir = this.parent / "extractor"
+go_dbscheme = this.parent / "ql" / "lib" / "go.dbscheme"
+r = runfiles.Create()
+gazelle, go_gen_dbscheme = map(r.Rlocation, sys.argv[1:])
+
+print("clearing generated BUILD files")
 for build_file in go_extractor_dir.glob("*/**/BUILD.bazel"):
     build_file.unlink()
 
+print("running gazelle")
 subprocess.check_call([gazelle, "go/extractor"])
 
+print("adding header to generated BUILD files")
 for build_file in go_extractor_dir.glob("*/**/BUILD.bazel"):
     contents = build_file.read_text()
     build_file.write_text(f"# generated running `bazel run //go/gazelle`, do not edit\n\n{contents}")
+
+subprocess.check_call([go_gen_dbscheme, go_dbscheme])
