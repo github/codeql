8a04840f933cd6efba82bcba6c3c905344b07abd
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2024-05-02 16:32:45 +0100

    C++: Improve qhelp for IncorrectNotOperatorUsage.ql, including mention of an alternative fix.

--- cpp/ql/src/Likely Bugs/Likely Typos/IncorrectNotOperatorUsage.cpp
+++ cpp/ql/src/Likely Bugs/Likely Typos/IncorrectNotOperatorUsage.cpp
@@ -2,7 +2,7 @@
 
 void f_warning(int i)
 {
-    // The usage of the logical not operator in this case is unlikely to be correct
+    // BAD: the usage of the logical not operator in this case is unlikely to be correct
     // as the output is being used as an operator for a bit-wise and operation
     if (i & !FLAGS)
     {
@@ -12,7 +12,7 @@ void f_warning(int i)
 
 void f_fixed(int i)
 {
-    if (i & ~FLAGS) // Changing the logical not operator for the bit-wise not operator would fix this logic
+    if (i & ~FLAGS) // GOOD: Changing the logical not operator for the bit-wise not operator would fix this logic
     {
         // code
     }
