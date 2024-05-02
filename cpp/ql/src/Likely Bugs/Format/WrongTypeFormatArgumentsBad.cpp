e22159ab5d4e15c6b99ac236303d5b380c1fb06d
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2024-05-02 14:20:02 +0100

    C++: Update WrongTypeFormatArguments.qhelp.

--- cpp/ql/src/Likely Bugs/Format/WrongTypeFormatArgumentsBad.cpp
+++ cpp/ql/src/Likely Bugs/Format/WrongTypeFormatArgumentsBad.cpp
@@ -1,4 +1,4 @@
 int main() {
-  printf("%s\n", 42); //printf will treat 42 as a char*, will most likely segfault
+  printf("%s\n", 42); // BAD: printf will treat 42 as a char*, will most likely segfault
   return 0;
 }
