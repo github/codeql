8a261b7e7a2ac760daa54dd126f13fcf78290a0a
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2024-05-02 14:31:26 +0100

    C++: Update StrncpyFlippedArgs.qhelp.

--- cpp/ql/src/Likely Bugs/Memory Management/StrncpyFlippedArgsBad.cpp
+++ cpp/ql/src/Likely Bugs/Memory Management/StrncpyFlippedArgsBad.cpp
@@ -1,2 +1,9 @@
-strncpy(dest, src, sizeof(src)); //wrong: size of dest should be used
-strncpy(dest, src, strlen(src)); //wrong: size of dest should be used
+char src[256];
+char dest1[128];
+
+...
+
+strncpy(dest1, src, sizeof(src)); // wrong: size of dest should be used
+
+char *dest2 = (char *)malloc(sz1 + sz2 + sz3);
+strncpy(dest2, src, strlen(src)); // wrong: size of dest should be used
