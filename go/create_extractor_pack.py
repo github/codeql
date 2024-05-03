77128de105d35ada1e379a6dea61d3939e6cab6e
Paolo Tranquilli <redsun82@github.com>
2024-05-03 17:44:29 +0200

    Bazel/Go: make installer work from internal repo and on windows
    It turns out everything that is needed for the installer to work on
    windows is enabling runfiles. This also requires symlinks to avoid
    excessive copying of files.

--- go/create_extractor_pack.py
+++ go/create_extractor_pack.py
@@ -1,26 +1,22 @@
+#!/usr/bin/env python3
 import os
 import pathlib
 import shutil
 import sys
 import subprocess
-import zipfile
-from python.runfiles import runfiles
 
 try:
     workspace_dir = pathlib.Path(os.environ['BUILD_WORKSPACE_DIRECTORY'])
 except KeyError:
-    print("this should be run with bazel run", file=sys.stderr)
-    sys.exit(1)
+    res = subprocess.run(["bazel", "run", ":create-extractor-pack"], cwd=pathlib.Path(__file__).parent)
+    sys.exit(res.returncode)
 
-dest_dir = workspace_dir / 'go' / 'build' / 'codeql-extractor-go'
-installer_or_zip = pathlib.Path(runfiles.Create().Rlocation(sys.argv[1]))
+from go._extractor_pack_install_script import main
 
+if os.environ['REPO_NAME'] == 'codeql~':
+    workspace_dir /= 'ql'
+
+dest_dir = workspace_dir / 'go' / 'build' / 'codeql-extractor-pack'
 shutil.rmtree(dest_dir, ignore_errors=True)
-
-if installer_or_zip.suffix == '.zip':
-    dest_dir.mkdir()
-    with zipfile.ZipFile(installer_or_zip) as pack:
-        pack.extractall(dest_dir)
-else:
-    os.environ['DESTDIR'] = str(dest_dir)
-    subprocess.check_call([installer_or_zip])
+os.environ['DESTDIR'] = str(dest_dir)
+main(sys.argv)
