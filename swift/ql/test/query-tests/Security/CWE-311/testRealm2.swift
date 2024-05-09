06dea2d27ff3884109b85b8e7293db65e20c6a45
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2024-05-09 18:09:55 +0100

    Swift: Use sensitive private info regex from the shared library, now that it has that.

--- swift/ql/test/query-tests/Security/CWE-311/testRealm2.swift
+++ swift/ql/test/query-tests/Security/CWE-311/testRealm2.swift
@@ -22,16 +22,16 @@ func test1(o: MyRealmSwiftObject3, myHarmless: String, myPassword: String) {
 
 func test2(o: MyRealmSwiftObject3, ccn: String, socialSecurityNumber: String, ssn: String, ssn_int: Int, userSSN: String, classno: String) {
 	o.data = socialSecurityNumber // BAD
-	o.data = ssn // BAD [NOT DETECTED]
-	o.data = String(ssn_int) // BAD [NOT DETECTED]
+	o.data = ssn // BAD
+	o.data = String(ssn_int) // BAD
 	o.data = userSSN // BAD [NOT DETECTED]
 	o.data = classno // GOOD
 }
 
 func test3(o: MyRealmSwiftObject3, ccn: String, creditCardNumber: String, CCN: String, int_ccn: Int, userCcn: String, succnode: String) {
 	o.data = creditCardNumber // BAD
-	o.data = CCN // BAD [NOT DETECTED]
-	o.data = String(int_ccn) // BAD [NOT DETECTED]
+	o.data = CCN // BAD
+	o.data = String(int_ccn) // BAD
 	o.data = userCcn // BAD [NOT DETECTED]
 	o.data = succnode // GOOD
 }
