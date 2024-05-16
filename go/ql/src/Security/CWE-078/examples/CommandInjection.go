9aeebc6f39b3c8004eaee33faea157404e1aa516
erik-krogh <erik-krogh@github.com>
2024-05-16 10:34:04 +0200

    update the QHelp to add a "--" example

--- go/ql/src/Security/CWE-078/examples/CommandInjection.go
+++ go/ql/src/Security/CWE-078/examples/CommandInjection.go
@@ -7,8 +7,8 @@ import (
 
 func handler(req *http.Request) {
 	imageName := req.URL.Query()["imageName"][0]
-	outputPath = "/tmp/output.svg"
+	outputPath := "/tmp/output.svg"
 	cmd := exec.Command("sh", "-c", fmt.Sprintf("imagetool %s > %s", imageName, outputPath))
 	cmd.Run()
- 	// ...
+	// ...
 }
