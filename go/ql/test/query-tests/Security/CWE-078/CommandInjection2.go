08c0d8ec60eed37c410cb3dc6dee429bfa504187
erik-krogh <erik-krogh@github.com>
2024-05-16 19:59:40 +0200

    autoformat go files

--- go/ql/test/query-tests/Security/CWE-078/CommandInjection2.go
+++ go/ql/test/query-tests/Security/CWE-078/CommandInjection2.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-	"net/http"
-	"os/exec"
-	"log"
-    "os"
-    "regexp"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"regexp"
 )
 
 func handler(req *http.Request) {
 	imageName := req.URL.Query()["imageName"][0]
-  	outputPath = "/tmp/output.svg"
+	outputPath = "/tmp/output.svg"
 	cmd := exec.Command("sh", "-c", fmt.Sprintf("imagetool %s > %s", imageName, outputPath)) // NOT OK - correctly flagged
-  	cmd.Run()
-  // ...
+	cmd.Run()
+	// ...
 }
 
 func handler2(req *http.Request) {
-    imageName := req.URL.Query()["imageName"][0]
-    outputPath := "/tmp/output.svg"
+	imageName := req.URL.Query()["imageName"][0]
+	outputPath := "/tmp/output.svg"
 
-    // Create the output file
-    outfile, err := os.Create(outputPath)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer outfile.Close()
+	// Create the output file
+	outfile, err := os.Create(outputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outfile.Close()
 
-    // Prepare the command
-    cmd := exec.Command("imagetool", imageName) // OK - and not flagged
+	// Prepare the command
+	cmd := exec.Command("imagetool", imageName) // OK - and not flagged
 
-    // Set the output to our file
-    cmd.Stdout = outfile
+	// Set the output to our file
+	cmd.Stdout = outfile
 
-    cmd.Run()
+	cmd.Run()
 }
 
 func handler3(req *http.Request) {
-    imageName := req.URL.Query()["imageName"][0]
-    outputPath := "/tmp/output.svg"
+	imageName := req.URL.Query()["imageName"][0]
+	outputPath := "/tmp/output.svg"
 
-    // Validate the imageName with a regular expression
-    validImageName := regexp.MustCompile(`^[a-zA-Z0-9_\-\.]+$`)
-    if !validImageName.MatchString(imageName) {
-        log.Fatal("Invalid image name")
-        return
-    }
+	// Validate the imageName with a regular expression
+	validImageName := regexp.MustCompile(`^[a-zA-Z0-9_\-\.]+$`)
+	if !validImageName.MatchString(imageName) {
+		log.Fatal("Invalid image name")
+		return
+	}
 
-    cmd := exec.Command("sh", "-c", fmt.Sprintf("imagetool %s > %s", imageName, outputPath)) // OK - but falsely flagged
-    cmd.Run()
+	cmd := exec.Command("sh", "-c", fmt.Sprintf("imagetool %s > %s", imageName, outputPath)) // OK - but falsely flagged
+	cmd.Run()
 }
--- go/ql/test/query-tests/Security/CWE-078/GitSubcommands.go
+++ go/ql/test/query-tests/Security/CWE-078/GitSubcommands.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"net/http"
 	"os/exec"
