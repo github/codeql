deb9e0db60e441f184c169b5a305c4417bdbcae1
Rasmus Lerchedahl Petersen <yoff@github.com>
2024-05-22 11:36:54 +0200

    ModelEditor: more tests

--- python/ql/test/modelling/MyPackage/Foo.py
+++ python/ql/test/modelling/MyPackage/Foo.py
@@ -5,9 +5,17 @@ class C1:
     def m2(self, x):
         return x
     
+    @staticmethod
+    def m3(x):
+        return x
+    
+    @classmethod
+    def m4(cls, x):
+        return x
+    
 class C2(C1):
     def m1(self):
         print("C2.m1()")
 
-    def m2(self, x):
+    def c2only_m1(self, x):
         return x
