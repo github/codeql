0d4cd3e103d6613b26e3e4eea9927123d75cff8c
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2024-05-23 10:03:27 +0100

    Swift: Add more sensitive data test cases.

--- swift/ql/test/query-tests/Security/CWE-311/testRealm2.swift
+++ swift/ql/test/query-tests/Security/CWE-311/testRealm2.swift
@@ -13,9 +13,25 @@ class MyRealmSwiftObject3 : Object {
 	var data: String
 }
 
-func test1(o: MyRealmSwiftObject3, myHarmless: String, myPassword : String) {
+func test1(o: MyRealmSwiftObject3, myHarmless: String, myPassword: String) {
 	// ...
 	o.data = myPassword // BAD
 	o.data = myHarmless
 	// ...
+}
+
+func test2(o: MyRealmSwiftObject3, ccn: String, socialSecurityNumber: String, ssn: String, ssn_int: Int, userSSN: String, classno: String) {
+	o.data = socialSecurityNumber // BAD
+	o.data = ssn // BAD [NOT DETECTED]
+	o.data = String(ssn_int) // BAD [NOT DETECTED]
+	o.data = userSSN // BAD [NOT DETECTED]
+	o.data = classno // GOOD
+}
+
+func test3(o: MyRealmSwiftObject3, ccn: String, creditCardNumber: String, CCN: String, int_ccn: Int, userCcn: String, succnode: String) {
+	o.data = creditCardNumber // BAD
+	o.data = CCN // BAD [NOT DETECTED]
+	o.data = String(int_ccn) // BAD [NOT DETECTED]
+	o.data = userCcn // BAD [NOT DETECTED]
+	o.data = succnode // GOOD
 }
