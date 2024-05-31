8b75bb2ba930fc7a859f485b869b65397dfbbd3d
Michael Nebel <michaelnebel@github.com>
2024-05-31 10:53:35 +0200

    C#: The model generator should consider System.Type and System.DateTime as sanitizers.

--- csharp/ql/test/utils/modelgenerator/dataflow/Summaries.cs
+++ csharp/ql/test/utils/modelgenerator/dataflow/Summaries.cs
@@ -431,13 +431,13 @@ public class SimpleTypes
         return i;
     }
 
-    // SPURIOUS-summary=Models;SimpleTypes;false;M5;(System.DateTime);;Argument[0];ReturnValue;taint;df-generated
+    // neutral=Models;SimpleTypes;M5;(System.DateTime);summary;df-generated
     public DateTime M5(DateTime d)
     {
         return d;
     }
 
-    // SPURIOUS-summary=Models;SimpleTypes;false;M6;(System.Type);;Argument[0];ReturnValue;taint;df-generated
+    // neutral=Models;SimpleTypes;M6;(System.Type);summary;df-generated
     public Type M6(Type t)
     {
         return t;
