b7e16ca55dd1a9544d3b599775e1a1acc5d98e69
Paolo Tranquilli <redsun82@github.com>
2024-06-04 17:59:13 +0200

    Kotlin: fix `custom_plugin` test

--- java/ql/integration-tests/linux-only/kotlin/custom_plugin/test.py
+++ java/ql/integration-tests/linux-only/kotlin/custom_plugin/test.py
@@ -3,17 +3,18 @@ from create_database_utils import *
 import pathlib
 import shutil
 
-root = get_semmle_code_path()
+this_dir = pathlib.Path(__file__).resolve().parent
 cwd = pathlib.Path.cwd()
 builddir = cwd / 'build'
 
 builddir.mkdir(exist_ok=True)
 
 try:
-    runSuccessfully([f'{root}/tools/bazel', f'--output_user_root={builddir}', '--max_idle_secs=1', 'build',
-                    '//java/ql/integration-tests/linux-only/kotlin/custom_plugin/plugin', '--spawn_strategy=local',
-                     '--nouse_action_cache', '--noremote_accept_cached', '--noremote_upload_local_results',
-                     f'--symlink_prefix={cwd / "bazel-"}'], cwd=root)
+    runSuccessfully(
+        [f'{get_semmle_code_path()}/tools/bazel', f'--output_user_root={builddir}', '--max_idle_secs=1', 'build',
+         '//java/ql/integration-tests/linux-only/kotlin/custom_plugin/plugin', '--spawn_strategy=local',
+         '--nouse_action_cache', '--noremote_accept_cached', '--noremote_upload_local_results',
+         f'--symlink_prefix={cwd / "bazel-"}'], cwd=this_dir)
 finally:
     # rules_python creates a read-only directory in bazel's output, this allows cleanup to succeed
     runSuccessfully(['chmod', '-R', '+w', builddir])
