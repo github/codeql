ceea475c450c54832dad377ef81e9c438a4b37d5
am0o0 <77095239+am0o0@users.noreply.github.com>
2024-06-08 01:04:20 +0200

    add new s3 and spring IO path injection sinks

---
+++ java/ql/test/query-tests/security/CWE-022/semmle/tests/NewPathInjection/PathInjection/src/main/java/com/PathInjection/SpringIoPathInjection.java
@@ -0,0 +1,50 @@
+package com.PathInjection;
+
+import java.io.*;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.core.io.*;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.ResourceUtils;
+
+public class SpringIoPathInjection {
+  public void PathInjection(String path) throws IOException {
+    Path fileStorageLocation = Paths.get(path).toAbsolutePath().normalize();
+    Path filePath = fileStorageLocation.resolve(path).normalize();
+    File pathFile = new File(path);
+
+    new UrlResource(filePath.toUri()); // $ PathInjection
+    new UrlResource(filePath.toUri().toURL()); // $ PathInjection
+    new UrlResource("file", path); // $ PathInjection
+    new UrlResource("file", path, "#"); // $ PathInjection
+    new UrlResource(path); // $ PathInjection
+
+    new PathResource(path); // $ PathInjection
+    new PathResource(filePath); // $ PathInjection
+    new PathResource(filePath.toUri()); // $ PathInjection
+
+    new FileUrlResource(filePath.toUri().toURL()); // $ PathInjection
+    new FileUrlResource(path); // $ PathInjection
+
+    new FileSystemResource(pathFile); // $ PathInjection
+    new FileSystemResource(path); // $ PathInjection
+    new FileSystemResource(filePath); // $ PathInjection
+    new FileSystemResource(
+        FileSystems.getFileSystem(URI.create("file:///")), path); // $ PathInjection
+
+    ResourceUtils.getFile(path); // summaryModel
+    ResourceUtils.getFile(path); // summaryModel
+    FileSystemUtils.copyRecursively(filePath, filePath.resolve("/newPath")); // $ PathInjection
+    FileSystemUtils.copyRecursively(pathFile, pathFile); // $ PathInjection
+    FileSystemUtils.deleteRecursively(pathFile); // $ PathInjection
+    FileSystemUtils.deleteRecursively(filePath); // $ PathInjection
+    FileCopyUtils.copy(pathFile, pathFile); // $ PathInjection
+    FileCopyUtils.copyToByteArray(pathFile); // $ PathInjection
+    FileCopyUtils.copyToString(new FileReader("fa"));
+  }
+}
