1ae40c95b15d0e1f8dee8222cd94623627d42910
Tamas Vajk <tamasvajk@github.com>
2024-06-21 10:10:36 +0200

    Change binlog test to have colliding generated file locations

--- csharp/ql/integration-tests/all-platforms/binlog/test.py
+++ csharp/ql/integration-tests/all-platforms/binlog/test.py
@@ -1,5 +1,5 @@
 import subprocess
 from create_database_utils import *
 
-subprocess.check_call(["dotnet", "build", "/bl:test.binlog"])
+subprocess.check_call(["dotnet", "build", "test.sln", "/bl:test.binlog"])
 run_codeql_database_create([], lang="csharp", extra_args=["--build-mode=none", "-Obinlog=test.binlog"])
