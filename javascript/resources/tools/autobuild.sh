fb9db2b244c063c5a433e53184a1b5edf014ce67
Michael B. Gale <mbg@github.com>
2024-06-21 11:50:53 +0100

    JS: Add env vars to allow specifying memory sizes

--- javascript/resources/tools/autobuild.sh
+++ javascript/resources/tools/autobuild.sh
@@ -4,12 +4,33 @@ set -eu
 
 jvm_args=-Xss16m
 
-# If CODEQL_RAM is set, use half for Java and half for TS.
+# If CODEQL_RAM is set, try to automatically calculate how much memory is available for TS and Java
+# if no explicit values are set for them.
 if [ -n "${CODEQL_RAM:-}" ] ; then
     half_ram="$(( CODEQL_RAM / 2 ))"
-    LGTM_TYPESCRIPT_RAM="$half_ram"
+
+    # If either CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_TS_MEMORY or CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_JVM_MAX_MEMORY is set,
+    # calculate the other by subtracting from CODEQL_RAM. If neither is set, then use
+    # half of the available CODEQL_RAM for each.
+    if [ -n "${CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_TS_MEMORY:-}" ] && [ -z "${CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_JVM_MAX_MEMORY}" ] ; then
+        CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_JVM_MAX_MEMORY="$(( $CODEQL_RAM - $CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_TS_MEMORY ))"
+    elif [ -n "${CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_JVM_MAX_MEMORY:-}" ] && [ -z "${CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_TS_MEMORY}" ] ; then
+        CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_TS_MEMORY="$(( $CODEQL_RAM - $CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_JVM_MAX_MEMORY ))"
+    elif [ -z "${CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_JVM_MAX_MEMORY}" ] && [ -z "${CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_TS_MEMORY}" ] ; then
+        CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_TS_MEMORY="$half_ram"
+        CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_JVM_MAX_MEMORY="$half_ram"
+    fi
+fi
+
+# If CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_TS_MEMORY is set, use it for TS by exporting it as LGTM_TYPESCRIPT_RAM.
+if [ -n "${CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_TS_MEMORY:-}" ] ; then
+    LGTM_TYPESCRIPT_RAM="$CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_TS_MEMORY"
     export LGTM_TYPESCRIPT_RAM
-    jvm_args="$jvm_args -Xmx${half_ram}m"
+fi
+
+# If CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_JVM_MAX_MEMORY is set, use it for Java by adding it to the JVM arguments.
+if [ -n "${CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_JVM_MAX_MEMORY:-}" ] ; then
+    jvm_args="$jvm_args -Xmx${CODEQL_EXTRACTOR_JAVASCRIPT_OPTION_JVM_MAX_MEMORY}m"
 fi
 
 # If CODEQL_THREADS is set, propagate via LGTM_THREADS.
