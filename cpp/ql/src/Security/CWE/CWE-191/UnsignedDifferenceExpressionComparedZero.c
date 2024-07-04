7abece46c741cfcb6b2f85e9fa82d9bfa793def2
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2024-07-04 16:33:38 +0100

    C++: Add a 'good' example for cpp/unsigned-difference-expression-compared-zero.

--- cpp/ql/src/Security/CWE/CWE-191/UnsignedDifferenceExpressionComparedZero.c
+++ cpp/ql/src/Security/CWE/CWE-191/UnsignedDifferenceExpressionComparedZero.c
@@ -1,5 +1,10 @@
 unsigned limit = get_limit();
 unsigned total = 0;
-while (limit - total > 0) { // wrong: if `total` is greater than `limit` this will underflow and continue executing the loop.
+
+while (limit - total > 0) { // BAD: if `total` is greater than `limit` this will underflow and continue executing the loop.
+  total += get_data();
+}
+
+while (total < limit) { // GOOD: never underflows.
   total += get_data();
 }
