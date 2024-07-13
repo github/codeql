eb94b9f59b20d383f8b52656d34fca81d8155a90
am0o0 <77095239+am0o0@users.noreply.github.com>
2024-07-13 18:06:02 +0200

    remove redundent stubs

--- java/ql/test/stubs/auth0-jwt-4.4.0/com/auth0/jwt/impl/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Contains parts of the <b>internal</b> implementation of this library.
- *
- * <p><b>Do not use any of the classes in this package.</b> They might be removed
- * or changed at any point without prior warning.
- */
-package com.auth0.jwt.impl;
