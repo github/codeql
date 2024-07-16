bd1f548fbc280c3ee249302026b37eeb685e702a
Paolo Tranquilli <redsun82@gihub.com>
2024-07-16 11:24:01 +0200

    Swift: use `git-lfs` devcontainer feature

--- .devcontainer/swift/user.sh
+++ .devcontainer/swift/user.sh
@@ -1,16 +1,5 @@
 set -xe
 
-# git lfs install may fail because codespaces can install its own hooks
-# let's install it manually
-for script in pre-push post-checkout post-commit post-merge; do
-  if [ -x .git/hooks/$script ]; then
-    sed -i "2i git lfs $script || exit 1" .git/hooks/$script
-  else
-    printf "#!/bin/bash\ngit lfs %s\n" $script > .git/hooks/$script
-    chmod +x .git/hooks/$script
-  fi
-done
-
 # add the workspace to the codeql search path
 mkdir -p /home/vscode/.config/codeql
 echo "--search-path /workspaces/codeql" > /home/vscode/.config/codeql/config
