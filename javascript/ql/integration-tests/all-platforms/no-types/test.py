3badd61a56d00a31804209b27b295d1b0707a021
Cornelius Riemenschneider <cornelius@github.com>
2024-07-18 16:36:11 +0200

    Integration tests: port to pytest.
    Requires an internal PR.

--- javascript/ql/integration-tests/all-platforms/no-types/test.py
+++ javascript/ql/integration-tests/all-platforms/no-types/test.py
@@ -1,3 +1,5 @@
-from create_database_utils import *
+# from create_database_utils import *
 
-run_codeql_database_create([], lang="javascript", extra_args=["-Oskip_types=true"])
+# run_codeql_database_create([], lang="javascript", extra_args=["-Oskip_types=true"])
+def test(codeql, javascript):
+    codeql.database.create(extractor_option="skip_types=true")
