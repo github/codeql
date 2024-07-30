bdff0fdcc5a175a8c0f2fc940cf9bd29cd51f4a4
Owen Mansel-Chan <owen-mc@github.com>
2024-07-30 15:36:35 +0100

    Add test for "tokenizer"

--- java/ql/test/query-tests/security/CWE-532/Test.java
+++ java/ql/test/query-tests/security/CWE-532/Test.java
@@ -1,12 +1,13 @@
 import org.apache.logging.log4j.Logger;
 
 class Test {
-    void test(String password, String authToken, String username, String nullToken) {
+    void test(String password, String authToken, String username, String nullToken, String stringTokenizer) {
         Logger logger = null;
 
         logger.info("User's password is: " + password); // $ hasTaintFlow
         logger.error("Auth failed for: " + authToken); // $ hasTaintFlow
         logger.error("Auth failed for: " + username); // Safe
         logger.error("Auth failed for: " + nullToken); // Safe
+        logger.error("Auth failed for: " + stringTokenizer); // $ hasTaintFlow
     }
 }
