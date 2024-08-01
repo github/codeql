a645e01b4b2a5c73b445e2b0d3494f085cd37ff1
am0o0 <77095239+am0o0@users.noreply.github.com>
2024-08-02 01:03:47 +0200

    delete wrong stubs

---
+++ java/ql/test/stubs/springframework-5.3.8/org/springframework/util/FileCopyUtils.java
@@ -0,0 +1,24 @@
+// Generated automatically from org.springframework.util.FileCopyUtils for testing purposes
+
+package org.springframework.util;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+
+abstract public class FileCopyUtils
+{
+    public FileCopyUtils(){}
+    public static String copyToString(Reader p0){ return null; }
+    public static byte[] copyToByteArray(File p0){ return null; }
+    public static byte[] copyToByteArray(InputStream p0){ return null; }
+    public static int BUFFER_SIZE = 0;
+    public static int copy(File p0, File p1){ return 0; }
+    public static int copy(InputStream p0, OutputStream p1){ return 0; }
+    public static int copy(Reader p0, Writer p1){ return 0; }
+    public static void copy(String p0, Writer p1){}
+    public static void copy(byte[] p0, File p1){}
+    public static void copy(byte[] p0, OutputStream p1){}
+}
