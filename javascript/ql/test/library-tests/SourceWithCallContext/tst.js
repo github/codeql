2e79c26f38c394d74dbd866214819e1b1078cd6d
Asger F <asgerf@github.com>
2024-08-07 14:03:43 +0200

    JS: Add test showing spurious flow into callback when using source with call context

---
+++ javascript/ql/test/library-tests/SourceWithCallContext/tst.js
@@ -0,0 +1,23 @@
+import * as lib from 'lib';
+
+function foo(source_param, callback) {
+    sink(source_param); // $ hasValueFlow=source_param hasValueFlow=explicit_source
+    callback(source_param);
+    return source_param;
+}
+
+lib.something(foo);
+
+function unrelated() {
+    let ret = foo('safe', p => {
+        sink(p); // $ hasValueFlow=source_param [SPURIOUS]
+    });
+    sink(ret);
+}
+
+function related() {
+    let ret = foo(source('explicit_source'), p => {
+        sink(p); // $ hasValueFlow=explicit_source
+    });
+    sink(ret); // $ hasValueFlow=explicit_source
+}
