ae96111848e9f99fdb57a3dfc10774371c1b2f49
Cornelius Riemenschneider <cornelius@github.com>
2024-08-09 09:48:38 +0200

    Revert "Simplify test."
    This reverts commit 2310bd94a48e50f226308996ef063c23ec20645d.

--- go/ql/integration-tests/all-platforms/go/resolve-build-environment/newer-go-needed/test.py
+++ go/ql/integration-tests/all-platforms/go/resolve-build-environment/newer-go-needed/test.py
@@ -1,6 +1,3 @@
-import os
-
-
-def test(check_build_environment, go):
-    # the check for resolve build-environment runs after the test and will pick up this environment variable
-    os.environ["GOTOOLCHAIN"] = "go1.21.0"
+def test(codeql, go, check_build_environment):
+    check_build_environment.env = {"GOTOOLCHAIN": "go1.21.0"}
+    codeql.database.create(source_root="src")
