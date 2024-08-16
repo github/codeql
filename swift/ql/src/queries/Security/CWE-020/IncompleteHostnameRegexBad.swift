2d19d6f61ecc12f8a888f5a5cdd76888e0c01763
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2024-08-16 11:40:57 +0100

    Swift: Fix two of the qhelps by slightly modifying the sample code instead.

--- swift/ql/src/queries/Security/CWE-020/IncompleteHostnameRegexBad.swift
+++ swift/ql/src/queries/Security/CWE-020/IncompleteHostnameRegexBad.swift
@@ -1,11 +1,11 @@
 
-func handleUrl(_ urlString: String) {
+func handleUrl(_ urlString: String) throws {
     // get the 'url=' parameter from the URL
     let components = URLComponents(string: urlString)
     let redirectParam = components?.queryItems?.first(where: { $0.name == "url" })
 
     // check we trust the host
-    let regex = #/^(www|beta).example.com//#  // BAD
+    let regex = try Regex("^(www|beta).example.com/") // BAD
     if let match = redirectParam?.value?.firstMatch(of: regex) {
         // ... trust the URL ...
     }
