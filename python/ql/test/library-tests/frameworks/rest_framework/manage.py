93b50605feebcd9d9a97bdac2740a51c3d42ecfc
Rasmus Wriedt Larsen <rasmuswl@github.com>
2024-08-16 11:51:07 +0200

    Python: Additional threatModelSource annotations

--- python/ql/test/library-tests/frameworks/rest_framework/manage.py
+++ python/ql/test/library-tests/frameworks/rest_framework/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testproj.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testproj.settings') # $ threatModelSource[environment]=os.environ
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,7 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv) # $ threatModelSource[commandargs]=sys.argv
 
 
 if __name__ == '__main__':
