5a392c93482b607feef3157296593eb74639ff6b
Ian Lynagh <igfoo@github.com>
2024-08-21 14:24:16 +0100

    KE2: Uncomment more imports

--- java/kotlin-extractor2/src/main/kotlin/KotlinExtractor.kt
+++ java/kotlin-extractor2/src/main/kotlin/KotlinExtractor.kt
@@ -1,16 +1,16 @@
 import com.intellij.openapi.editor.Document
 import com.intellij.psi.PsiFile
 import java.nio.file.Paths
-// import org.jetbrains.kotlin.analysis.api.KtAnalysisApiInternals
-// import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
-// import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.KtAnalysisApiInternals
+import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
+import org.jetbrains.kotlin.analysis.api.analyze
 // import org.jetbrains.kotlin.analysis.api.lifetime.KtLifetimeTokenProvider
-// import org.jetbrains.kotlin.analysis.api.standalone.buildStandaloneAnalysisAPISession
+import org.jetbrains.kotlin.analysis.api.standalone.buildStandaloneAnalysisAPISession
 // import org.jetbrains.kotlin.analysis.api.standalone.KtAlwaysAccessibleLifetimeTokenProvider
-// import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtLibraryModule
-// import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtSdkModule
-// import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtSourceModule
-// import org.jetbrains.kotlin.analysis.project.structure.KtSourceModule
+import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtLibraryModule
+import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtSdkModule
+import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtSourceModule
+import org.jetbrains.kotlin.analysis.project.structure.KtSourceModule
 import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 import org.jetbrains.kotlin.psi.*
 
