c4bf1c0594aec6bc80f9bbc6ed6f7638129a24a3
Cornelius Riemenschneider <cornelius@github.com>
2024-08-26 17:45:21 +0200

    Fix ripunzip installation.
    Otherwise, the install script fails when being run from the main repo,
    inside the ripunzip fixture.

--- misc/ripunzip/install.sh
+++ misc/ripunzip/install.sh
@@ -2,7 +2,17 @@
 
 set -eu
 
-source misc/bazel/runfiles.sh 2>/dev/null || source external/ql~/misc/bazel/runfiles.sh
+# --- begin runfiles.bash initialization v3 ---
+# Copy-pasted from the Bazel Bash runfiles library v3.
+set -uo pipefail; set +e; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v3 ---
+
 
 dest="${2:-$HOME/.local/bin}"
 
