4b94a83d21b9cf9fc1c7147a522c01f0a02b2107
Paolo Tranquilli <redsun82@gihub.com>
2024-09-02 10:33:10 +0200

    Swift: debug disk space issues

--- swift/ql/integration-tests/posix/hello-world/test.py
+++ swift/ql/integration-tests/posix/hello-world/test.py
@@ -1,6 +1,14 @@
 import runs_on
+import subprocess
 
 
 @runs_on.posix
 def test(codeql, swift):
-    codeql.database.create(command="swift build")
+    try:
+        codeql.database.create(command="swift build")
+    except Exception:
+        if runs_on.macos:
+            subprocess.run("sudo du -h /System/Volumes/Data > dirs_unsorted.list", shell=True)
+            subprocess.run("sort -h dirs_unsorted.list > dirs.log", shell=True)
+            subprocess.run("rm -f dirs_unsorted.list", shell=True)
+        raise
