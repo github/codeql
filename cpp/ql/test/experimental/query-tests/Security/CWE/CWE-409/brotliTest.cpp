e85ca79d770fbee433372c7bfd62d81879079171
am0o0 <77095239+am0o0@users.noreply.github.com>
2024-09-03 12:28:06 +0200

    add tests for brotli

---
+++ cpp/ql/test/experimental/query-tests/Security/CWE/CWE-409/brotliTest.cpp
@@ -0,0 +1,41 @@
+typedef long unsigned int size_t;
+typedef unsigned char uint8_t;
+typedef enum {
+} BrotliDecoderResult;
+
+BrotliDecoderResult BrotliDecoderDecompress(
+        size_t encoded_size,
+        const uint8_t encoded_buffer[],
+        size_t *decoded_size,
+        uint8_t decoded_buffer[]) { return static_cast<BrotliDecoderResult>(0); };
+
+struct {
+} BrotliDecoderStateStruct;
+
+void strncpy(char *string, const char *string1, int i);
+
+typedef struct BrotliDecoderStateStruct BrotliDecoderState;
+
+BrotliDecoderResult BrotliDecoderDecompressStream(
+        BrotliDecoderState *state, size_t *available_in, const uint8_t **next_in,
+        size_t *available_out, uint8_t **next_out, size_t *total_out) { return static_cast<BrotliDecoderResult>(0); };
+
+namespace std {
+    void strncpy(char *string, const char *string1, int i) {
+
+    }
+}
+
+int main(int argc, const char *argv[]) {
+    uint8_t *output = nullptr;
+    BrotliDecoderDecompress(1024 * 1024, (uint8_t *) argv[2],
+                            reinterpret_cast<size_t *>(1024 * 1024 * 1024), output);
+    uint8_t **output2 = nullptr;
+    const uint8_t **input2 = nullptr;
+    std::strncpy(reinterpret_cast<char *>(input2), argv[2], 32);
+    BrotliDecoderDecompressStream(0, reinterpret_cast<size_t *>(1024 * 1024),
+                                  input2, reinterpret_cast<size_t *>(1024 * 1024 * 1024),
+                                  output2,
+                                  reinterpret_cast<size_t *>(1024 * 1024 * 1024));
+    return 0;
+}
