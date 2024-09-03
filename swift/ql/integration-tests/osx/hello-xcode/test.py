7c958660a34058c54fcecf34d0bae3fbbc147e88
Paolo Tranquilli <redsun82@gihub.com>
2024-09-03 12:13:48 +0200

    TEST TEST TEST

--- swift/ql/integration-tests/osx/hello-xcode/test.py
+++ swift/ql/integration-tests/osx/hello-xcode/test.py
@@ -1,13 +1,23 @@
 import runs_on
 import pytest
+import commands
 
 
 @runs_on.macos
 @pytest.mark.ql_test("DB-CHECK", xfail=True)
 def test(codeql, swift):
-    codeql.database.create(
-        command="xcodebuild build "
-        "-project codeql-swift-autobuild-test.xcodeproj "
-        "-target codeql-swift-autobuild-test "
-        "CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO",
+    cmd = ("xcodebuild build "
+           "-project codeql-swift-autobuild-test.xcodeproj "
+           "-target codeql-swift-autobuild-test "
+           "CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO")
+    commands.run(
+        "du -sh /System/Volumes/Data/Library/Developer/CoreSimulator",
+        cmd,
+        "du -sh /System/Volumes/Data/Library/Developer/CoreSimulator",
+        "xcodebuild clean",
     )
+    try:
+        codeql.database.create(command=cmd)
+    finally:
+        commands.run(
+            "du -sh /System/Volumes/Data/Library/Developer/CoreSimulator")
--- swift/ql/integration-tests/posix/hello-world/test.py
+++ swift/ql/integration-tests/posix/hello-world/test.py
@@ -1,6 +1,16 @@
 import runs_on
+import commands
 
 
 @runs_on.posix
 def test(codeql, swift):
-    codeql.database.create(command="swift build")
+    commands.run(
+        "du -sh /System/Volumes/Data/Library/Developer/CoreSimulator",
+        "swift build",
+        "du -sh /System/Volumes/Data/Library/Developer/CoreSimulator",
+        "swift package clean",
+    )
+    try:
+        codeql.database.create(command="swift build")
+    finally:
+        commands.run("du -sh /System/Volumes/Data/Library/Developer/CoreSimulator")
