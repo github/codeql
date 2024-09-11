3ea1134cc15a95439427ef015f2ee4098f3f0dca
Asger F <asgerf@github.com>
2024-09-11 11:04:41 +0200

    JS: Add inline test for .shift() method

---
+++ javascript/ql/test/library-tests/TripleDot/arrays.js
@@ -0,0 +1,15 @@
+import 'dummy';
+
+function shiftKnown() {
+    let array = [source('shift.1'), source('shift.2')];
+    sink(array.shift()); // $ hasValueFlow=shift.1
+    sink(array.shift()); // $ SPURIOUS: hasValueFlow=shift.1 MISSING: hasValueFlow=shift.2
+}
+
+function shiftUnknown() {
+    const array = new Array(Math.floor(Math.random() * 10));
+    array.push(source('shift.unkn'));
+    sink(array.shift()); // $ hasValueFlow=shift.unkn
+    sink(array.shift()); // $ hasValueFlow=shift.unkn
+    sink(array.shift()); // $ hasValueFlow=shift.unkn
+}
