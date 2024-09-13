61ac8d66f545a8f81cb7a5f1fd0331a2fd37b3a0
Arthur Baars <aibaars@github.com>
2024-09-13 16:57:54 +0200

    Rust: make things compile

---
+++ rust/generate-schema/src/main.rs
@@ -0,0 +1,12 @@
+use std::path::PathBuf;
+
+mod codegen;
+mod flags;
+use std::env;
+
+fn project_root() -> PathBuf {
+    let dir =
+        env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| env!("CARGO_MANIFEST_DIR").to_owned());
+    PathBuf::from(dir).parent().unwrap().to_owned()
+}
+fn main() {}
