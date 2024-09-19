0cd4ccb79039de28ecbcad3b0e537302ef435c5d
Michael Nebel <michaelnebel@github.com>
2024-09-19 19:23:53 +0200

    C#/Java: Update model generator expected test output.

--- csharp/ql/test/utils/modelgenerator/dataflow/Summaries.cs
+++ csharp/ql/test/utils/modelgenerator/dataflow/Summaries.cs
@@ -844,12 +844,14 @@ public class SyntheticCollections
     private object[] array;
 
     // summary=Models;SyntheticCollections;false;SyntheticCollections;(System.Object[]);;Argument[0].Element;Argument[this];taint;df-generated
+    // contentbased-summary=Models;SyntheticCollections;false;SyntheticCollections;(System.Object[]);;Argument[0];Argument[this].SyntheticField[Models.SyntheticCollections.array];value;df-generated
     public SyntheticCollections(object[] array)
     {
         this.array = array;
     }
 
     // summary=Models;SyntheticCollections;false;GetElement;(System.Int32);;Argument[this];ReturnValue;taint;df-generated
+    // contentbased-summary=Models;SyntheticCollections;false;GetElement;(System.Int32);;Argument[this].SyntheticField[Models.SyntheticCollections.array].Element;ReturnValue;value;df-generated
     public object GetElement(int index)
     {
         return array[index];
--- java/ql/test/utils/modelgenerator/dataflow/p/SyntheticCollections.java
+++ java/ql/test/utils/modelgenerator/dataflow/p/SyntheticCollections.java
@@ -4,11 +4,13 @@ public class SyntheticCollections {
   private String[] array;
 
   // summary=p;SyntheticCollections;true;SyntheticCollections;(String[]);;Argument[0].ArrayElement;Argument[this];taint;df-generated
+  // contentbased-summary=p;SyntheticCollections;true;SyntheticCollections;(String[]);;Argument[0];Argument[this].SyntheticField[p.SyntheticCollections.array];value;df-generated
   public SyntheticCollections(String[] array) {
     this.array = array;
   }
 
   // summary=p;SyntheticCollections;true;getElement;(Integer);;Argument[this];ReturnValue;taint;df-generated
+  // contentbased-summary=p;SyntheticCollections;true;getElement;(Integer);;Argument[this].SyntheticField[p.SyntheticCollections.array].ArrayElement;ReturnValue;value;df-generated
   public String getElement(Integer index) {
     return array[index];
   }
