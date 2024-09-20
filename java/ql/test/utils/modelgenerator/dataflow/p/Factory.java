9a923d62adba28e87ed8234376cac5686db6cf94
Michael Nebel <michaelnebel@github.com>
2024-09-20 13:27:45 +0200

    C#/Java: Updated expected test output.

--- java/ql/test/utils/modelgenerator/dataflow/p/Factory.java
+++ java/ql/test/utils/modelgenerator/dataflow/p/Factory.java
@@ -7,13 +7,13 @@ public final class Factory {
   private int intValue;
 
   // summary=p;Factory;false;create;(String,int);;Argument[0];ReturnValue;taint;df-generated
-  // contentbased-summary=p;Factory;false;create;(String,int);;Argument[0];ReturnValue.Field[p.Factory.value];value;df-generated
+  // contentbased-summary=p;Factory;false;create;(String,int);;Argument[0];ReturnValue.Field[p.Factory.value];value;dfc-generated
   public static Factory create(String value, int foo) {
     return new Factory(value, foo);
   }
 
   // summary=p;Factory;false;create;(String);;Argument[0];ReturnValue;taint;df-generated
-  // contentbased-summary=p;Factory;false;create;(String);;Argument[0];ReturnValue.Field[p.Factory.value];value;df-generated
+  // contentbased-summary=p;Factory;false;create;(String);;Argument[0];ReturnValue.Field[p.Factory.value];value;dfc-generated
   public static Factory create(String value) {
     return new Factory(value, 0);
   }
@@ -24,7 +24,7 @@ public final class Factory {
   }
 
   // summary=p;Factory;false;getValue;();;Argument[this];ReturnValue;taint;df-generated
-  // contentbased-summary=p;Factory;false;getValue;();;Argument[this].Field[p.Factory.value];ReturnValue;value;df-generated
+  // contentbased-summary=p;Factory;false;getValue;();;Argument[this].Field[p.Factory.value];ReturnValue;value;dfc-generated
   public String getValue() {
     return value;
   }
--- java/ql/test/utils/modelgenerator/dataflow/p/Fanout.java
+++ java/ql/test/utils/modelgenerator/dataflow/p/Fanout.java
@@ -11,7 +11,7 @@ public class Fanout {
     public String v;
 
     // summary=p;Fanout$I1;true;getValue;();;Argument[this];ReturnValue;taint;df-generated
-    // contentbased-summary=p;Fanout$Impl1;true;getValue;();;Argument[this].Field[p.Fanout$Impl1.v];ReturnValue;value;df-generated
+    // contentbased-summary=p;Fanout$Impl1;true;getValue;();;Argument[this].Field[p.Fanout$Impl1.v];ReturnValue;value;dfc-generated
     public String getValue() {
       return v;
     }
@@ -21,7 +21,7 @@ public class Fanout {
     public String v;
 
     // summary=p;Fanout$I1;true;getValue;();;Argument[this];ReturnValue;taint;df-generated
-    // contentbased-summary=p;Fanout$Impl2;true;getValue;();;Argument[this].Field[p.Fanout$Impl2.v];ReturnValue;value;df-generated
+    // contentbased-summary=p;Fanout$Impl2;true;getValue;();;Argument[this].Field[p.Fanout$Impl2.v];ReturnValue;value;dfc-generated
     public String getValue() {
       return v;
     }
@@ -31,7 +31,7 @@ public class Fanout {
     public String v;
 
     // summary=p;Fanout$I1;true;getValue;();;Argument[this];ReturnValue;taint;df-generated
-    // contentbased-summary=p;Fanout$Impl3;true;getValue;();;Argument[this].Field[p.Fanout$Impl3.v];ReturnValue;value;df-generated
+    // contentbased-summary=p;Fanout$Impl3;true;getValue;();;Argument[this].Field[p.Fanout$Impl3.v];ReturnValue;value;dfc-generated
     public String getValue() {
       return v;
     }
@@ -41,7 +41,7 @@ public class Fanout {
     public String v;
 
     // summary=p;Fanout$I1;true;getValue;();;Argument[this];ReturnValue;taint;df-generated
-    // contentbased-summary=p;Fanout$Impl4;true;getValue;();;Argument[this].Field[p.Fanout$Impl4.v];ReturnValue;value;df-generated
+    // contentbased-summary=p;Fanout$Impl4;true;getValue;();;Argument[this].Field[p.Fanout$Impl4.v];ReturnValue;value;dfc-generated
     public String getValue() {
       return v;
     }
@@ -51,17 +51,17 @@ public class Fanout {
   // summary=p;Fanout;true;concatGetValueOnI1;(String,Fanout$I1);;Argument[1];ReturnValue;taint;df-generated
   // No content based summaries are expected for this method on parameter `i`
   // as the fanout (number of content flows) exceeds the limit of 3.
-  // contentbased-summary=p;Fanout;true;concatGetValueOnI1;(String,Fanout$I1);;Argument[0];ReturnValue;taint;df-generated
+  // contentbased-summary=p;Fanout;true;concatGetValueOnI1;(String,Fanout$I1);;Argument[0];ReturnValue;taint;dfc-generated
   public String concatGetValueOnI1(String other, I1 i) {
     return other + i.getValue();
   }
 
   // summary=p;Fanout;true;concatGetValueOnI2;(String,Fanout$I2);;Argument[0];ReturnValue;taint;df-generated
   // summary=p;Fanout;true;concatGetValueOnI2;(String,Fanout$I2);;Argument[1];ReturnValue;taint;df-generated
-  // contentbased-summary=p;Fanout;true;concatGetValueOnI2;(String,Fanout$I2);;Argument[0];ReturnValue;taint;df-generated
-  // contentbased-summary=p;Fanout;true;concatGetValueOnI2;(String,Fanout$I2);;Argument[1].Field[p.Fanout$Impl2.v];ReturnValue;taint;df-generated
-  // contentbased-summary=p;Fanout;true;concatGetValueOnI2;(String,Fanout$I2);;Argument[1].Field[p.Fanout$Impl3.v];ReturnValue;taint;df-generated
-  // contentbased-summary=p;Fanout;true;concatGetValueOnI2;(String,Fanout$I2);;Argument[1].Field[p.Fanout$Impl4.v];ReturnValue;taint;df-generated
+  // contentbased-summary=p;Fanout;true;concatGetValueOnI2;(String,Fanout$I2);;Argument[0];ReturnValue;taint;dfc-generated
+  // contentbased-summary=p;Fanout;true;concatGetValueOnI2;(String,Fanout$I2);;Argument[1].Field[p.Fanout$Impl2.v];ReturnValue;taint;dfc-generated
+  // contentbased-summary=p;Fanout;true;concatGetValueOnI2;(String,Fanout$I2);;Argument[1].Field[p.Fanout$Impl3.v];ReturnValue;taint;dfc-generated
+  // contentbased-summary=p;Fanout;true;concatGetValueOnI2;(String,Fanout$I2);;Argument[1].Field[p.Fanout$Impl4.v];ReturnValue;taint;dfc-generated
   public String concatGetValueOnI2(String other, I2 i) {
     return other + i.getValue();
   }
