57148110715e672c5823e84390fbb8577d2ed5bc
Arthur Baars <aibaars@github.com>
2024-09-24 16:20:50 +0200

    Rust: fix panic when the last character in a range is multi-byte

--- rust/extractor/src/translate/base.rs
+++ rust/extractor/src/translate/base.rs
@@ -3,7 +3,7 @@ use crate::trap::{Label, TrapClass};
 use codeql_extractor::trap::{self};
 use ra_ap_ide_db::line_index::{LineCol, LineIndex};
 use ra_ap_syntax::ast::RangeItem;
-use ra_ap_syntax::{ast, SyntaxError, TextRange, TextSize};
+use ra_ap_syntax::{ast, SyntaxError, TextRange};
 pub trait TextValue {
     fn try_get_text(&self) -> Option<String>;
 }
@@ -70,12 +70,21 @@ impl Translator {
     }
     pub fn location(&self, range: TextRange) -> (LineCol, LineCol) {
         let start = self.line_index.line_col(range.start());
-        let end = self.line_index.line_col(
-            range
-                .end()
-                .checked_sub(TextSize::new(1))
-                .unwrap_or(range.end()),
-        );
+        let range_end = range.end();
+        // QL end positions are inclusive, while TextRange offsets are exclusive and point at the position
+        // right after the last character of the range. We need to shift the end offset one character to the left to
+        // get the right inclusive QL position. Unfortunately, simply subtracting `1` from the end-offset may cause
+        // the offset to point in the middle of a mult-byte character, resulting in a `panic`. Therefore we use `try_line_col`
+        // with decreasing offsets to find the start of the last character included in the range.
+        for i in 1..4 {
+            if let Some(end) = range_end
+                .checked_sub(i.into())
+                .and_then(|x| self.line_index.try_line_col(x))
+            {
+                return (start, end);
+            }
+        }
+        let end = self.line_index.line_col(range_end);
         (start, end)
     }
     pub fn emit_location<T: TrapClass>(&mut self, label: Label<T>, node: impl ast::AstNode) {
