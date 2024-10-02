6df1f5a13c8c9ea9a96bcdd1bd040d041ef03513
Rasmus Lerchedahl Petersen <yoff@github.com>
2024-10-02 12:18:06 +0200

    Python: missing steps for repro - API graph subscript operator to understand comprehensions - captureJumpStep to not require definig value to exist - stdlib modelling: finditer returns list of match objects   - adjust taint output of finditer   - adjust `ReMatchMethodsSummary.getACall`

--- python/ql/test/library-tests/dataflow/tainttracking/DSVW/repro.py
+++ python/ql/test/library-tests/dataflow/tainttracking/DSVW/repro.py
@@ -1,6 +1,7 @@
 import re
 import urllib.parse
 import sys
+import http.client
 
 def generator_dict_re_combo():
     query = TAINTED_STRING
@@ -29,3 +30,16 @@ def parse_qs():
     params = urllib.parse.parse_qs(query)
 
     ensure_tainted(params) # $ tainted
+
+HTML_PREFIX = """<!DOCTYPE html>"""
+
+def flat():
+    self_path = TAINTED_STRING
+
+    path, query = self_path.split('?', 1) if '?' in self_path else (self_path, "")
+    code, content, params, cursor = http.client.OK, HTML_PREFIX, dict((match.group("parameter"), urllib.parse.unquote(','.join(re.findall(r"(?:\A|[?&])%s=([^&]+)" % match.group("parameter"), query)))) for match in re.finditer(r"((\A|[?&])(?P<parameter>[\w\[\]]+)=)([^&]+)", query)), "Cursor"
+    
+    print(code)
+    print(content)
+    ensure_tainted(params) # $ tainted
+    print(cursor)
