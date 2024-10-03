073189ed6b1daae6c254a0b25f0800ce3e9f85c0
Rasmus Lerchedahl Petersen <yoff@github.com>
2024-10-03 14:50:16 +0200

    python: add test for `re.Match` objects returned from `finditer`

--- python/ql/test/library-tests/frameworks/stdlib/test_re.py
+++ python/ql/test/library-tests/frameworks/stdlib/test_re.py
@@ -38,6 +38,12 @@ ensure_tainted(
 
     compiled_pat.match(ts).string, # $ tainted
     re.compile(ts).match("safe").re.pattern, # $ tainted
+
+    list(re.finditer(pat, ts))[0].string, # $ MISSING: tainted
+    [m.string for m in re.finditer(pat, ts)], # $ MISSING: tainted
+
+    list(re.finditer(pat, ts))[0].groups()[0], # $ MISSING: tainted
+    [m.groups()[0] for m in re.finditer(pat, ts)], # $ MISSING: tainted
 )
 ensure_not_tainted(
     safe_match.expand("Hello \1"),
