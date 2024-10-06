30366794c6890e76c0298f585801a43b7fbe7408
Arthur Baars <aibaars@github.com>
2024-10-06 13:13:55 +0200

    Rust: improve QL test script

--- rust/tools/qltest.sh
+++ rust/tools/qltest.sh
@@ -9,7 +9,13 @@ QLTEST_LOG="$CODEQL_EXTRACTOR_RUST_LOG_DIR"/qltest.log
 EXTRACTOR="$CODEQL_EXTRACTOR_RUST_ROOT/tools/$CODEQL_PLATFORM/extractor"
 echo > lib.rs
 for src in *.rs; do
-  echo "mod ${src%.rs};" >> lib.rs
+  if [[ "$src" == "lib.rs" ]]; then
+    continue
+  elif [[ "$src" == "main.rs" ]]; then
+    continue
+  else
+    echo "mod ${src%.rs};" >> lib.rs
+  fi
 done
 cat > Cargo.toml << EOF
 [workspace]
@@ -20,6 +26,13 @@ edition="2021"
 [lib]
 path="lib.rs"
 EOF
+if [[ -f "main.rs" ]]; then
+cat >> Cargo.toml << EOF
+[[bin]]
+name = "main"
+path = "main.rs"
+EOF
+fi
 "$EXTRACTOR" *.rs >> "$QLTEST_LOG"
 if [[ "$?" != 0 ]]; then
   cat "$QLTEST_LOG" # Show compiler errors on extraction failure
