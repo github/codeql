2c970a080d9a57a01b15ea0f3895309d19e18d7a
Paolo Tranquilli <redsun82@github.com>
2024-10-15 16:50:15 +0200

    Rust: remove useless blank line
    Co-authored-by: Simon Friis Vindum <simonfv@gmail.com>

--- rust/codegen/codegen.sh
+++ rust/codegen/codegen.sh
@@ -4,7 +4,6 @@ set -eu
 
 source misc/bazel/runfiles.sh 2>/dev/null || source external/ql+/misc/bazel/runfiles.sh
 
-
 ast_generator="$(rlocation "$1")"
 ast_generator_manifest="$(rlocation "$2")"
 codegen="$(rlocation "$3")"
