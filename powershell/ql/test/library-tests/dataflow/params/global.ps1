6bb859dab0eeb1dc244687102d58b76901924fde
Mathias Vorreiter Pedersen <mapeder@microsoft.com>
2024-10-17 11:44:14 +0100

    PS: Accept test changes.

--- powershell/ql/test/library-tests/dataflow/params/global.ps1
+++ powershell/ql/test/library-tests/dataflow/params/global.ps1
@@ -3,4 +3,4 @@ param([string]$Source1, [string]$Source2, [string]$Source3, [string]$Source4)
 Sink $Source1 # $ hasValueFlow=1
 Sink $Source2 # $ hasValueFlow=2
 Sink $Source3 # $ hasValueFlow=3
-Sink $Source4 # $ MISSING: hasValueFlow=4
+Sink $Source4 # $ hasValueFlow=4
