97bfc5dad7eb38fe73bbab098c010715236fc247
Raul Garcia <42392023+raulgarciamsft@users.noreply.github.com>
2024-10-22 09:20:19 -0700

    Update csharp/ql/src/Security Features/CWE-327/InsecureSQLConnectionInitializerGood.cs
    Co-authored-by: Mathias Vorreiter Pedersen <mathiasvp@github.com>

--- csharp/ql/src/Security Features/CWE-327/InsecureSQLConnectionInitializerGood.cs
+++ csharp/ql/src/Security Features/CWE-327/InsecureSQLConnectionInitializerGood.cs
@@ -3,5 +3,5 @@ using System.Data.SqlClient;
 // BAD, Encrypt not specified
 string connectString =
     "Server=1.2.3.4;Database=Anything;Integrated Security=true;";
-var builder = new SqlConnectionStringBuilder(SetToTrueConnStr) { Encrypt = true }
+var builder = new SqlConnectionStringBuilder(connectString) { Encrypt = true }
 var conn = new SqlConnection(builder.ConnectionString);
