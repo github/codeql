4e8b6dc03832951fc3d0d621106ec43fd7a5dcb5
Paolo Tranquilli <redsun82@gihub.com>
2024-10-25 12:15:15 +0200

    Rust: fix qltest on macOS, and add CI cross-platform testing of it

---
+++ rust/ql/integration-tests/qltest/failing_cargo_check/does_not_compile.rs
@@ -0,0 +1,5 @@
+fn foo() {
+    wat
+    is
+    this?
+}
---
+++ rust/ql/integration-tests/qltest/test.py
@@ -0,0 +1,14 @@
+# these tests are meant to exercise QL test running on multiple platforms
+# therefore they don't rely on integration test built-in QL test running
+# (which skips `qltest.{sh,cmd}`)
+
+def test_lib(codeql, rust, cwd):
+    codeql.test.run("lib", threads=1)
+
+def test_main(codeql, rust):
+    codeql.test.run("main", threads=1)
+
+def test_failing_cargo_check(codeql, rust):
+    out = codeql.test.run("failing_cargo_check", threads=1, show_extractor_output=True,
+                          _assert_failure=True, _capture="stderr")
+    assert "requested cargo check failed" in out
