a36095d85b388053be230002826d03c204b14c84
Simon Friis Vindum <simonfv@gmail.com>
2024-11-01 11:28:10 +0100

    Rust: Add local data flow test with if expression

--- rust/ql/test/library-tests/dataflow/local/main.rs
+++ rust/ql/test/library-tests/dataflow/local/main.rs
@@ -3,6 +3,18 @@ fn variable() {
     println!("{:?} data flow!", s);
 }
 
+fn if_expression(cond: bool) -> i64 {
+    let a = 1;
+    let b = 2;
+    let c = if cond {
+        a
+    } else {
+        b
+    };
+    c
+}
+
 fn main() {
     variable();
+    if_expression(true);
 }
