570393fe2bd6426d435dfaf32f60d39f185bc365
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2024-11-14 16:21:14 +0000

    Swift: Additional test cases.

--- swift/ql/test/library-tests/dataflow/taint/core/conversions.swift
+++ swift/ql/test/library-tests/dataflow/taint/core/conversions.swift
@@ -31,10 +31,21 @@ typealias MyInt = Int
 func testConversions() {
 	sink(arg: sourceInt("conv1-1")) // $ tainted=conv1-1
 	sink(arg: Int(sourceInt("conv1-2"))) // $ tainted=conv1-2
-	sink(arg: UInt8(sourceInt("conv1-3"))) // $ tainted=conv1-3
-	sink(arg: Float(sourceInt("conv1-4"))) // $ tainted=conv1-4
-	sink(arg: String(sourceInt("conv1-5"))) // $ tainted=conv1-5
-	sink(arg: String(sourceInt("conv1-6")).utf8) // $ tainted=conv1-6
+	sink(arg: Int8(sourceInt("conv1-3"))) // $ tainted=conv1-3
+	sink(arg: Int16(sourceInt("conv1-4"))) // $ tainted=conv1-4
+	sink(arg: Int32(sourceInt("conv1-5"))) // $ tainted=conv1-5
+	sink(arg: Int64(sourceInt("conv1-6"))) // $ tainted=conv1-6
+	//sink(arg: Int128(sourceInt("conv1-7"))) --- doesn't build in test (yet)
+	sink(arg: UInt(sourceInt("conv1-8"))) // $ tainted=conv1-8
+	sink(arg: UInt8(sourceInt("conv1-9"))) // $ tainted=conv1-9
+	sink(arg: UInt16(sourceInt("conv1-10"))) // $ tainted=conv1-10
+	sink(arg: UInt32(sourceInt("conv1-11"))) // $ tainted=conv1-11
+	sink(arg: UInt64(sourceInt("conv1-12"))) // $ tainted=conv1-12
+	//sink(arg: UInt128(sourceInt("conv1-13"))) --- doesn't build in test (yet)
+	sink(arg: Float(sourceInt("conv1-14"))) // $ tainted=conv1-14
+	sink(arg: Double(sourceInt("conv1-15"))) // $ tainted=conv1-15
+	sink(arg: String(sourceInt("conv1-16"))) // $ tainted=conv1-16
+	sink(arg: String(sourceInt("conv1-17")).utf8) // $ tainted=conv1-17
 
 	let arr = [1, 2, sourceInt("conv2-1")]
 	sink(arg: arr) // $ tainted=conv2-1
@@ -67,17 +78,26 @@ func testConversions() {
 	sink(arg: v8)
 	sink(arg: v8.advanced(by: 1))
 	sink(arg: v8.advanced(by: sourceInt("conv3-7"))) // $ tainted=conv3-7
+	sink(arg: v8.distance(to: 1))
+	sink(arg: v8.distance(to: sourceUInt64("conv3-8"))) // $ tainted=conv3-8
 
 	sink(arg: Int(exactly: sourceInt("conv4-1"))!) // $ tainted=conv4-1
 	sink(arg: UInt32(exactly: sourceInt("conv4-2"))!) // $ tainted=conv4-2
 	sink(arg: Int(clamping: sourceInt("conv4-3"))) // $ tainted=conv4-3
-	sink(arg: Int(truncatingIfNeeded: sourceInt("conv4-4"))) // $ tainted=conv4-4
-	sink(arg: Int(sourceString("conv4-5"), radix: 10)!) // $ tainted=conv4-5
+	sink(arg: UInt32(clamping: sourceInt("conv4-4"))) // $ tainted=conv4-4
+	sink(arg: Int(truncatingIfNeeded: sourceInt("conv4-5"))) // $ tainted=conv4-5
+	sink(arg: UInt32(truncatingIfNeeded: sourceInt("conv4-6"))) // $ tainted=conv4-6
+	sink(arg: Int(sourceString("conv4-7"), radix: 10)!) // $ tainted=conv4-7
+	sink(arg: UInt32(sourceString("conv4-8"), radix: 10)!) // $ tainted=conv4-8
 
 	sink(arg: Int(littleEndian: sourceInt("conv5-1"))) // $ tainted=conv5-1
-	sink(arg: Int(bigEndian: sourceInt("conv5-2"))) // $ tainted=conv5-2
-	sink(arg: sourceInt("conv5-3").littleEndian) // $ tainted=conv5-3
-	sink(arg: sourceInt("conv5-4").bigEndian) // $ tainted=conv5-4
+	sink(arg: UInt64(littleEndian: sourceUInt64("conv5-2"))) // $ tainted=conv5-2
+	sink(arg: Int(bigEndian: sourceInt("conv5-3"))) // $ tainted=conv5-3
+	sink(arg: UInt64(bigEndian: sourceUInt64("conv5-4"))) // $ tainted=conv5-4
+	sink(arg: sourceInt("conv5-5").littleEndian) // $ tainted=conv5-5
+	sink(arg: sourceUInt64("conv5-6").littleEndian) // $ tainted=conv5-6
+	sink(arg: sourceInt("conv5-7").bigEndian) // $ tainted=conv5-7
+	sink(arg: sourceUInt64("conv5-8").bigEndian) // $ tainted=conv5-8
 
 	let (q1, r1) = 1000.quotientAndRemainder(dividingBy: 2)
 	sink(arg: q1)
@@ -91,17 +111,25 @@ func testConversions() {
 	sink(arg: q3) // $ MISSING: tainted=conv6-2
 	sink(arg: r3) // $ MISSING: tainted=conv6-2
 
+	let (q4, r4) = UInt64(1000).quotientAndRemainder(dividingBy: sourceUInt64("conv6-3"))
+	sink(arg: q4) // $ MISSING: tainted=conv6-3
+	sink(arg: r4) // $ MISSING: tainted=conv6-3
+
 	let pair1 = 1000.addingReportingOverflow(2)
 	sink(arg: pair1.0) // part
 	sink(arg: pair1.1) // overflow
 
-	let pair2 = sourceInt("conv6-3").addingReportingOverflow(2)
-	sink(arg: pair2.0) // $ MISSING: tainted=conv6-3
+	let pair2 = sourceInt("conv6-4").addingReportingOverflow(2)
+	sink(arg: pair2.0) // $ MISSING: tainted=conv6-4
 	sink(arg: pair2.1)
 
-	let pair3 = 1000.addingReportingOverflow(sourceInt("conv6-4"))
-	sink(arg: pair3.0) // $ MISSING: tainted=conv6-4
+	let pair3 = 1000.addingReportingOverflow(sourceInt("conv6-5"))
+	sink(arg: pair3.0) // $ MISSING: tainted=conv6-5
 	sink(arg: pair3.1)
+
+	let pair4 = UInt64(1000).addingReportingOverflow(sourceUInt64("conv6-6"))
+	sink(arg: pair4.0) // $ MISSING: tainted=conv6-6
+	sink(arg: pair4.1)
 
 	// ---
 
@@ -129,6 +157,8 @@ func testConversions() {
 	sink(arg: sourceDouble("conv9-6").significand) // $ tainted=conv9-6
 	sink(arg: sourceUInt("conv9-7").byteSwapped) // $ tainted=conv9-7
 	sink(arg: sourceUInt64("conv9-8").byteSwapped) // $ tainted=conv9-8
+	sink(arg: sourceInt("conv9-9").magnitude) // $ tainted=conv9-9
+	sink(arg: sourceUInt64("conv9-10").magnitude) // $ tainted=conv9-10
 
 	// ---
 
