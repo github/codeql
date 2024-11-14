2b0def1ed3e3aa313b4516001c7e2971cba990e4
Napalys <napalys@github.com>
2024-11-14 15:31:38 +0100

    JS: Add: Test case for checking if regex via using toSpliced

--- javascript/ql/test/library-tests/RegExp/IsRegex/tst.js
+++ javascript/ql/test/library-tests/RegExp/IsRegex/tst.js
@@ -4,3 +4,8 @@ function detectRegexViaSplice(string) {
     let found = getMyThing().search('regex'); // NOT OK
     arr.splice(found, 1);
 };
+
+function detectRegexViaToSpliced(string) {
+    let found = getMyThing().search('regex'); // NOT OK -- Should be marked as regular expression but it is not.
+    arr.toSpliced(found, 1);
+};
