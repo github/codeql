e3b83bb4b4312229ace22fd95f3ed41cfd0db510
Tamas Vajk <tamasvajk@github.com>
2024-11-15 10:19:33 +0100

    Undo accidental comment removal

--- csharp/extractor/Semmle.Extraction.CSharp/Entities/Base/CachedEntityFactory.cs
+++ csharp/extractor/Semmle.Extraction.CSharp/Entities/Base/CachedEntityFactory.cs
@@ -5,6 +5,9 @@ namespace Semmle.Extraction.CSharp
     /// </summary>
     public abstract class CachedEntityFactory<TInit, TEntity> where TEntity : Entities.CachedEntity
     {
+        /// <summary>
+        /// Initializes the entity, but does not generate any trap code.
+        /// </summary>
         public abstract TEntity Create(Context cx, TInit init);
     }
 }
