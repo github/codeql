0f0c1e1609c34c100c05dfe0c452f6a407aff159
Joe Farebrother <joefarebrother@github.com>
2024-11-22 16:32:10 +0000

    Test update

--- python/ql/test/library-tests/frameworks/django-v2-v3/template_test.py
+++ python/ql/test/library-tests/frameworks/django-v2-v3/template_test.py
@@ -1,13 +1,13 @@
 from django.template import Template, engines
 from django.urls import path
-from django.http.response import HttpResponse,
+from django.http.response import HttpResponse
 
 def a(request):  # $requestHandler
     t = Template("abc").render() # $templateConstruction="abc"
     return HttpResponse(t)  # $HttpResponse
 
 def b(request): # $requestHandler
-    # This case is not yet supported
+    # This case is not currently supported
     t = django.template.engines["django"].from_string("abc") # $MISSING:templateConstruction="abc"
     return HttpResponse(t)  # $HttpResponse
 
