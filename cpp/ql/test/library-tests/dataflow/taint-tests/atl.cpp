fe9feb900d8351b380c716c837b7500d9477e808
Mathias Vorreiter Pedersen <mathiasvp@github.com>
2024-11-26 17:29:26 +0000

    C++: We will need all these types.

---
+++ cpp/ql/test/library-tests/dataflow/taint-tests/atl.cpp
@@ -0,0 +1,65 @@
+namespace {
+  template<typename T> T source();
+  template<typename T> T* indirect_source();
+  void sink(...);
+}
+
+typedef unsigned int UINT;
+typedef long LONG;
+typedef void* LPVOID;
+typedef void* PVOID;
+typedef bool BOOL;
+typedef char* PSTR, *LPSTR;
+typedef const char* LPCTSTR;
+typedef unsigned short WORD;
+typedef unsigned long DWORD;
+typedef void* HANDLE;
+typedef LONG HRESULT;
+typedef unsigned long ULONG;
+typedef const char* LPCSTR;
+typedef wchar_t OLECHAR;
+typedef OLECHAR* LPOLESTR;
+typedef const LPOLESTR LPCOLESTR;
+typedef OLECHAR* BSTR;
+typedef wchar_t* LPWSTR, *PWSTR;
+typedef BSTR* LPBSTR;
+typedef unsigned short USHORT;
+typedef char *LPTSTR;
+struct __POSITION { int unused; };typedef __POSITION* POSITION;
+typedef WORD ATL_URL_PORT;
+
+enum ATL_URL_SCHEME{
+   ATL_URL_SCHEME_UNKNOWN = -1,
+   ATL_URL_SCHEME_FTP     = 0,
+   ATL_URL_SCHEME_GOPHER  = 1,
+   ATL_URL_SCHEME_HTTP    = 2,
+   ATL_URL_SCHEME_HTTPS   = 3,
+   ATL_URL_SCHEME_FILE    = 4,
+   ATL_URL_SCHEME_NEWS    = 5,
+   ATL_URL_SCHEME_MAILTO  = 6,
+   ATL_URL_SCHEME_SOCKS   = 7
+};
+
+using HINSTANCE = void*;
+using size_t = decltype(sizeof(int));
+using SIZE_T = size_t;
+
+#define NULL nullptr
+
+typedef struct tagSAFEARRAYBOUND {
+  ULONG cElements;
+  LONG  lLbound;
+} SAFEARRAYBOUND, *LPSAFEARRAYBOUND;
+
+typedef struct tagVARIANT {
+  /* ... */
+} VARIANT;
+
+typedef struct tagSAFEARRAY {
+  USHORT         cDims;
+  USHORT         fFeatures;
+  ULONG          cbElements;
+  ULONG          cLocks;
+  PVOID          pvData;
+  SAFEARRAYBOUND rgsabound[1];
+} SAFEARRAY, *LPSAFEARRAY;
