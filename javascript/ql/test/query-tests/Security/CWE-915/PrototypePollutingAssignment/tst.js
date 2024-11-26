faef9dd8774358c6bdca232bbbb7220cb0556ae4
Napalys <napalys@github.com>
2024-11-26 11:32:15 +0100

    JS: protyte poluting now treats unknownFlags as potentially good sanitization.

--- javascript/ql/test/query-tests/Security/CWE-915/PrototypePollutingAssignment/tst.js
+++ javascript/ql/test/query-tests/Security/CWE-915/PrototypePollutingAssignment/tst.js
@@ -128,5 +128,5 @@ app.get('/foo', (req, res) => {
     let obj = {};
     obj[req.query.x.replace(new RegExp('_', 'g'), '')].x = 'foo'; // OK
     obj[req.query.x.replace(new RegExp('_', ''), '')].x = 'foo'; // NOT OK
-    obj[req.query.x.replace(new RegExp('_', unknownFlags()), '')].x = 'foo'; // OK -- Might be okay but it is currently flagged as a problem
+    obj[req.query.x.replace(new RegExp('_', unknownFlags()), '')].x = 'foo'; // OK
 });
