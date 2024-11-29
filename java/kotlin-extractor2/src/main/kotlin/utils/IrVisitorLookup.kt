0ccf117bf762c08ac01a6df580e53b7c5c3041ec
Ian Lynagh <igfoo@github.com>
2024-11-29 17:49:13 +0000

    KE2: Remove the fakeOverride code
    As far as I can see, the analysis API isn't giving us fake overrides.

--- java/kotlin-extractor2/src/main/kotlin/utils/IrVisitorLookup.kt
+++ java/kotlin-extractor2/src/main/kotlin/utils/IrVisitorLookup.kt
@@ -6,7 +6,6 @@ import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.declarations.IrDeclaration
 import org.jetbrains.kotlin.ir.declarations.IrFile
-import org.jetbrains.kotlin.ir.util.isFakeOverride
 import org.jetbrains.kotlin.ir.visitors.IrElementVisitor
 
 class IrVisitorLookup(
@@ -21,12 +20,6 @@ class IrVisitorLookup(
 
         if (!location.intersects(elementLocation)) {
             // No need to visit children.
-            return
-        }
-
-        if (element is IrDeclaration && element.isFakeOverride) {
-            // These aren't extracted, so we don't expect anything to exist
-            // to which we could ascribe a comment.
             return
         }
 
