6cb0866d0fc53e4bb16c52c2c2684be40cbe71ed
Paolo Tranquilli <redsun82@gihub.com>
2024-11-29 17:41:49 +0100

    Revert "Rust: test running windows flaky test multiple times"
    This reverts commit 90fa3ec4ed358188c1bbd1aee2d9b5ca503ed314.

--- rust/ql/integration-tests/hello-workspace/test_workspace.py
+++ rust/ql/integration-tests/hello-workspace/test_workspace.py
@@ -1,13 +1,4 @@
-import runs_on
-import pytest
-
-@runs_on.posix
 def test_cargo(codeql, rust, cargo, check_source_archive):
-    codeql.database.create()
-
-@runs_on.windows
-@pytest.mark.parametrize("_", range(25))
-def test_cargo_debug(codeql, rust, cargo, check_source_archive, _):
     codeql.database.create()
 
 def test_rust_project(codeql, rust, rust_project, check_source_archive):
