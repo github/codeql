712c69ebc8e04c7aa8122cc3e1a88c8626d6ad19
Asger F <asgerf@github.com>
2024-12-04 13:08:28 +0100

    JS: Fixup the test expectations

--- javascript/ql/test/query-tests/Security/CWE-601/ClientSideUrlRedirect/tst13.js
+++ javascript/ql/test/query-tests/Security/CWE-601/ClientSideUrlRedirect/tst13.js
@@ -1,48 +1,48 @@
 function foo() {
     var payload = document.location.search.substr(1);
     var el = document.createElement("a");
-    el.href = payload;
-    document.body.appendChild(el); // NOT OK
+    el.href = payload; // NOT OK
+    document.body.appendChild(el);
 
     var el = document.createElement("button");
-    el.formaction = payload;
-    document.body.appendChild(el); // NOT OK
+    el.formaction = payload; // NOT OK
+    document.body.appendChild(el);
 
     var el = document.createElement("embed");
-    el.src = payload;
-    document.body.appendChild(el); // NOT OK
+    el.src = payload; // NOT OK
+    document.body.appendChild(el);
 
     var el = document.createElement("form");
-    el.action = payload;
-    document.body.appendChild(el); // NOT OK
+    el.action = payload; // NOT OK
+    document.body.appendChild(el);
 
     var el = document.createElement("frame");
-    el.src = payload;
-    document.body.appendChild(el); // NOT OK
+    el.src = payload; // NOT OK
+    document.body.appendChild(el);
 
     var el = document.createElement("iframe");
-    el.src = payload;
-    document.body.appendChild(el); // NOT OK
+    el.src = payload; // NOT OK
+    document.body.appendChild(el);
 
     var el = document.createElement("input");
-    el.formaction = payload;
-    document.body.appendChild(el); // NOT OK
+    el.formaction = payload; // NOT OK
+    document.body.appendChild(el);
 
     var el = document.createElement("isindex");
-    el.action = payload;
-    document.body.appendChild(el); // NOT OK
+    el.action = payload; // NOT OK
+    document.body.appendChild(el);
 
     var el = document.createElement("isindex");
-    el.formaction = payload;
-    document.body.appendChild(el); // NOT OK
+    el.formaction = payload; // NOT OK
+    document.body.appendChild(el);
 
     var el = document.createElement("object");
-    el.data = payload;
-    document.body.appendChild(el); // NOT OK
+    el.data = payload; // NOT OK
+    document.body.appendChild(el);
 
     var el = document.createElement("script");
-    el.src = payload;
-    document.body.appendChild(el); // NOT OK
+    el.src = payload; // NOT OK
+    document.body.appendChild(el);
 }
 
 (function () {
