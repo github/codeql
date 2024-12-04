70a296be895df6c1e80c8fa2320cfcd4ba11c981
Simon Friis Vindum <simonfv@gmail.com>
2024-12-04 13:24:15 +0100

    Rust: Add string slice taint flow test

--- rust/ql/test/library-tests/dataflow/taint/main.rs
+++ rust/ql/test/library-tests/dataflow/taint/main.rs
@@ -24,8 +24,27 @@ fn cast() {
     sink(b as i64); // $ MISSING: hasTaintFlow=77
 }
 
+mod string {
+    fn source(i: i64) -> String {
+        format!("{}", i)
+    }
+
+    fn sink(s: &str) {
+        println!("{}", s);
+    }
+
+    pub fn string_slice() {
+        let s = source(35);
+        let sliced = &s[1..3];
+        sink(sliced); // $ MISSING: hasTaintFlow=35
+    }
+}
+
+use string::*;
+
 fn main() {
     addition();
     negation();
     cast();
+    string_slice();
 }
