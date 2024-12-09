6c1ec8c3a3a13e16bd8d7cb502b933f4173e349f
Asger F <asgerf@github.com>
2024-12-09 11:16:22 +0100

    JS: Add test for flow from constructor to method

---
+++ javascript/ql/test/library-tests/TripleDot/class-harness.js
@@ -0,0 +1,40 @@
+import 'dummy';
+
+function h1() {
+    class C {
+        constructor(arg) {
+            this.x = source("h1.1")
+        }
+        method() {
+            sink(this.x); // $ MISSING: hasValueFlow=h1.1
+        }
+    }
+}
+
+function h2() {
+    class C {
+        method1() {
+            this.x = source("h2.1")
+        }
+        method2() {
+            sink(this.x); // $ MISSING: hasValueFlow=h2.1
+        }
+    }
+}
+
+function h3() {
+    class C {
+        constructor(arg) {
+            this.x = arg;
+        }
+        method1() {
+            sink(this.x); // $ hasValueFlow=h3.2
+        }
+        method2() {
+            sink(this.x); // $ hasValueFlow=h3.3
+        }
+    }
+    new C(source("h3.1"));
+    new C(source("h3.2")).method1();
+    new C(source("h3.3")).method2();
+}
