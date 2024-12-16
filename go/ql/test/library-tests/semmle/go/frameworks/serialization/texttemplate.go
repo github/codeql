bf34860ad09910864ae9865c460f9347d8d4bb94
Chris Smowton <chris@smowton.net>
2024-12-16 14:21:25 +0000

    Test deeply ntested taint

--- go/ql/test/library-tests/semmle/go/frameworks/serialization/texttemplate.go
+++ go/ql/test/library-tests/semmle/go/frameworks/serialization/texttemplate.go
@@ -17,10 +17,15 @@ type Inner3 struct {
 	Data string
 }
 
+type HasInner3Slice struct {
+	Slice []Inner3
+}
+
 type Outer struct {
 	SliceField []Inner1
 	PtrField   *Inner2
 	MapField   map[string]Inner3
+	DeepField  HasInner3Slice
 }
 
 func source(n int) string { return "dummy" }
@@ -31,8 +36,10 @@ func test() {
 	source1 := source(1)
 	source2 := source(2)
 	source3 := source(3)
+	source4 := source(4)
 
-	toSerialize := Outer{[]Inner1{{source1}}, &Inner2{source2}, map[string]Inner3{"key": {source3}}}
+	toSerialize := Outer{[]Inner1{{source1}}, &Inner2{source2}, map[string]Inner3{"key": {source3}},
+		HasInner3Slice{[]Inner3{{source4}}}}
 	buff1 := new(bytes.Buffer)
 	buff2 := new(bytes.Buffer)
 	bytes1 := make([]byte, 10)
@@ -41,13 +48,13 @@ func test() {
 	tmpl, _ := template.New("test").Parse("Template text goes here (irrelevant for test)")
 	tmpl.ExecuteTemplate(buff1, "test", toSerialize)
 	buff1.Read(bytes1)
-	sink(bytes1) // $ hasTaintFlow=1 hasTaintFlow=2 hasTaintFlow=3
+	sink(bytes1) // $ hasTaintFlow=1 hasTaintFlow=2 hasTaintFlow=3 hasTaintFlow=4
 
 	// Read `buff2` via an `any`-typed variable, to ensure the static type of the argument to tmpl.Execute makes no difference to the result
 	var toSerializeAsAny any
 	toSerializeAsAny = toSerialize
 	tmpl.Execute(buff2, toSerializeAsAny)
 	buff2.Read(bytes2)
-	sink(bytes2) // $ hasTaintFlow=1 hasTaintFlow=2 hasTaintFlow=3
+	sink(bytes2) // $ hasTaintFlow=1 hasTaintFlow=2 hasTaintFlow=3 hasTaintFlow=4
 
 }
