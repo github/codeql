d6b8d42936bae5bc0ce261bc0ebbfc0ce553fe0e
Paolo Tranquilli <redsun82@gihub.com>
2024-12-18 17:20:05 +0100

    Bazel: fix 3rdparty update script

--- misc/bazel/3rdparty/update_cargo_deps.sh
+++ misc/bazel/3rdparty/update_cargo_deps.sh
@@ -2,5 +2,8 @@
 
 set -eu
 
-"${BASH_SOURCE[0]}/update_py_deps.sh"
-"${BASH_SOURCE[0]}/update_tree_sitter_extractors_deps.sh"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
+"$SCRIPT_DIR/update_py_deps.sh"
+"$SCRIPT_DIR/update_tree_sitter_extractors_deps.sh"
