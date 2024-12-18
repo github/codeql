b5b8af3aa2bfa3cd6ca79c6db53c7fa752f9fc6c
Simon Friis Vindum <simonfv@gmail.com>
2024-12-18 13:00:38 +0100

    Rust: Add data flow tests for borrows

--- rust/ql/test/library-tests/dataflow/pointers/main.rs
+++ rust/ql/test/library-tests/dataflow/pointers/main.rs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------------
-// Data flow through pointers.
+// Data flow through borrows and pointers.
 
 fn source(i: i64) -> i64 {
     1000 + i
@@ -32,6 +32,15 @@ fn write_and_read_through_borrow() {
     sink(*b); // $ MISSING: hasValueFlow=37
 }
 
+fn takes_borrowed_value(&n: &i64) {
+    sink(n); // $ MISSING: hasValueFlow=83
+}
+
+fn pass_borrowed_value() {
+    let val = source(83);
+    takes_borrowed_value(&val);
+}
+
 enum MyNumber {
     MyNumber(i64)
 }
@@ -47,8 +56,8 @@ impl MyNumber {
 
     fn get_number(&self) -> i64 {
         match self {
-            MyNumber::MyNumber(number) => {
-                *number
+            &MyNumber::MyNumber(number) => {
+                number
             }
         }
     }
@@ -69,11 +78,33 @@ fn through_self_in_method_explicit_borrow() {
     sink(my_number.get_number()); // $ MISSING: hasValueFlow=40
 }
 
+fn ref_nested_pattern_match() {
+    let a = &(source(23), 1);
+
+    // Match "in order", ref then tuple
+    let b = match a {
+        &(n, _) => n
+    };
+    sink(b); // $ MISSING: hasValueFlow=23
+
+    // Match "out of order", tuple then ref
+    let c = match a {
+        (n, _) => {
+            match n {
+                &i => i
+            }
+        }
+    };
+    sink(c); // $ MISSING: hasValueFlow=23
+}
+
 fn main() {
     read_through_borrow();
     write_through_borrow();
     write_and_read_through_borrow();
+    pass_borrowed_value();
     through_self_in_method_no_borrow();
     through_self_in_method_implicit_borrow();
     through_self_in_method_explicit_borrow();
+    ref_nested_pattern_match();
 }
