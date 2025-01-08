cc54e75235924f656a0db58248ae0db635efdc35
Ed Minnix <egregius313@github.com>
2025-01-07 20:52:17 -0500

    Remove duplicate stubs

--- go/ql/test/library-tests/semmle/go/dataflow/flowsources/local/database/vendor/github.com/jmoiron/sqlx/row.go
+++ go/ql/test/library-tests/semmle/go/dataflow/flowsources/local/database/vendor/github.com/jmoiron/sqlx/row.go
@@ -1,12 +1,8 @@
 package sqlx
 
-import "database/sql"
-
 type Row struct {
 	// Mapper *reflectx.Mapper
 }
-
-// stub Row::MapScan, Row::StructScan, Row::SliceScan, Row::Scan
 
 func (r *Row) MapScan(dest map[string]interface{}) error {
 	return nil
@@ -21,28 +17,5 @@ func (r *Row) SliceScan(dest []interface{}) error {
 }
 
 func (r *Row) Scan(dest ...interface{}) error {
-	return nil
-}
-
-type Rows struct {
-	*sql.Rows
-
-	// Mapper *reflectx.Mapper
-	// contains filtered or unexported fields
-}
-
-func (r *Rows) MapScan(dest map[string]interface{}) error {
-	return nil
-}
-
-func (r *Rows) StructScan(dest interface{}) error {
-	return nil
-}
-
-func (r *Rows) SliceScan(dest []interface{}) error {
-	return nil
-}
-
-func (r *Rows) Scan(dest ...interface{}) error {
 	return nil
 }
--- go/ql/test/library-tests/semmle/go/dataflow/flowsources/local/database/vendor/github.com/jmoiron/sqlx/stub.go
+++ go/ql/test/library-tests/semmle/go/dataflow/flowsources/local/database/vendor/github.com/jmoiron/sqlx/stub.go
@@ -42,51 +42,6 @@ type QueryerContext interface {
 	QueryRowxContext(ctx context.Context, query string, args ...interface{}) *Row
 }
 
-type Row struct {
-	// Mapper *reflectx.Mapper
-}
-
-// stub Row::MapScan, Row::StructScan, Row::SliceScan, Row::Scan
-
-func (r *Row) MapScan(dest map[string]interface{}) error {
-	return nil
-}
-
-func (r *Row) StructScan(dest interface{}) error {
-	return nil
-}
-
-func (r *Row) SliceScan(dest []interface{}) error {
-	return nil
-}
-
-func (r *Row) Scan(dest ...interface{}) error {
-	return nil
-}
-
-type Rows struct {
-	*sql.Rows
-
-	// Mapper *reflectx.Mapper
-	// contains filtered or unexported fields
-}
-
-func (r *Rows) MapScan(dest map[string]interface{}) error {
-	return nil
-}
-
-func (r *Rows) StructScan(dest interface{}) error {
-	return nil
-}
-
-func (r *Rows) SliceScan(dest []interface{}) error {
-	return nil
-}
-
-func (r *Rows) Scan(dest ...interface{}) error {
-	return nil
-}
-
 func NamedQuery(e Ext, query string, arg interface{}) (*Rows, error) {
 	return e.Queryx(query, arg)
 }
@@ -109,24 +64,4 @@ func Select(q Queryer, dest interface{}, query string, args ...interface{}) erro
 
 func SelectContext(ctx context.Context, q QueryerContext, dest interface{}, query string, args ...interface{}) error {
 	return nil
-}
-
-type Conn struct {
-	*sql.Conn
-}
-
-func (c *Conn) GetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error {
-	return nil
-}
-
-func (c *Conn) SelectContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error {
-	return nil
-}
-
-func (c *Conn) QueryRowxContext(ctx context.Context, query string, args ...interface{}) *Row {
-	return nil
-}
-
-func (c *Conn) QueryxContext(ctx context.Context, query string, args ...interface{}) (*Rows, error) {
-	return nil, nil
 }
