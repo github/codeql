8c54f51cc9601b398cff2625b275e381be4f9395
Paolo Tranquilli <redsun82@github.com>
2025-01-17 15:26:58 +0100

    Swift: use xcode fixture

--- swift/ql/integration-tests/osx/hello-ios/test.py
+++ swift/ql/integration-tests/osx/hello-ios/test.py
@@ -4,7 +4,7 @@ import pytest
 
 @runs_on.macos
 @pytest.mark.ql_test("DB-CHECK", xfail=True)
-def test(codeql, swift):
+def test(codeql, swift, xcode_16):
     codeql.database.create(
         command="xcodebuild build "
         "CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO",
