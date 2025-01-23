9b53a390c041588f450b88f8522470275a38baab
Calum Grant <calumgrant@github.com>
2025-01-23 19:22:10 +0000

    C++: Remove potential FPs in cpp/wrong-type-format-argument in BMN

--- cpp/ql/test/query-tests/Likely Bugs/Format/WrongTypeFormatArguments/Buildless/tests.c
+++ cpp/ql/test/query-tests/Likely Bugs/Format/WrongTypeFormatArguments/Buildless/tests.c
@@ -9,7 +9,7 @@ void f(UNKNOWN_CHAR * str) {
     sprintf(0, "%s", ""); // GOOD
     fprintf(0, "%s", ""); // GOOD
     printf("%s", str); // GOOD: erroneous type is ignored
-    printf("%d %d %u %u", 42l, 42ul, 42l, 42ul); // BAD (FP)
+    printf("%d %d %u %u", 42l, 42ul, 42l, 42ul); // GOOD: build mode none
     printf("%d %d %u %u", 42ll, 42ull, 42ll, 42ull); // BAD
     printf("%ld %ld %lu %lu", 42, 42u, 42, 42u); // BAD
 }
