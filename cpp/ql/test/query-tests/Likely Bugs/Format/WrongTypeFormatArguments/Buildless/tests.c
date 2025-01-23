231e3c2949c3ad4ea5a4576bcd9081b9b73b2ec8
Calum Grant <calumgrant@github.com>
2025-01-23 17:12:39 +0000

    C++: Tests for cpp/wrong-type-format-argument

--- cpp/ql/test/query-tests/Likely Bugs/Format/WrongTypeFormatArguments/Buildless/tests.c
+++ cpp/ql/test/query-tests/Likely Bugs/Format/WrongTypeFormatArguments/Buildless/tests.c
@@ -1,12 +1,15 @@
-// semmle-extractor-options: --expect_errors
+// semmle-extractor-options: --expect_errors --build-mode none
 
 int printf(const char * format, ...);
 int fprintf();
 
 void f(UNKNOWN_CHAR * str) {
     printf("%s", 1); // BAD
-    printf("%s", implicit_function()); // GOOD - we should ignore the type
+    printf("%s", implicit_function()); // GOOD: we should ignore the type
     sprintf(0, "%s", ""); // GOOD
     fprintf(0, "%s", ""); // GOOD
-    printf("%s", str); // GOOD - erroneous type is ignored
+    printf("%s", str); // GOOD: erroneous type is ignored
+    printf("%d %d %u %u", 42l, 42ul, 42l, 42ul); // BAD (FP)
+    printf("%d %d %u %u", 42ll, 42ull, 42ll, 42ull); // BAD
+    printf("%ld %ld %lu %lu", 42, 42u, 42, 42u); // BAD
 }
