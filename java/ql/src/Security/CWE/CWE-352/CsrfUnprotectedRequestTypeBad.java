ead224c7b2904194c35be78a8db7df18309af283
Jami Cogswell <jcogs33@Jamis-MacBook-Pro.local>
2025-01-24 12:17:46 -0500

    Java: expand qhelp, include Stapler examples

--- java/ql/src/Security/CWE/CWE-352/CsrfUnprotectedRequestTypeBad.java
+++
@@ -1,5 +0,0 @@
-// BAD - a GET request should not be used for a state-changing action like transfer
-@RequestMapping(value="transfer", method=RequestMethod.GET)
-public boolean transfer(HttpServletRequest request, HttpServletResponse response){
-  return doTransfer(request, response);
-}
--- java/ql/src/Security/CWE/CWE-352/CsrfUnprotectedRequestTypeGood.java
+++
@@ -1,5 +0,0 @@
-// GOOD - use a POST request for a state-changing action
-@RequestMapping(value="transfer", method=RequestMethod.POST)
-public boolean transfer(HttpServletRequest request, HttpServletResponse response){
-  return doTransfer(request, response);
-}
