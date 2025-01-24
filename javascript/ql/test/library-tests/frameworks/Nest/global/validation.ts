34e20d471e91f85cb7eb1973deb7eac687f50c06
Asger F <asgerf@github.com>
2025-01-24 14:24:05 +0100

    JS: Update with inline test expectations

--- javascript/ql/test/library-tests/frameworks/Nest/global/validation.ts
+++ javascript/ql/test/library-tests/frameworks/Nest/global/validation.ts
@@ -4,9 +4,9 @@ import { IsIn } from 'class-validator';
 export class Controller {
   @Get()
   route1(@Query('x') validatedObj: Struct, @Query('y') unvalidated: string) {
-    if (Math.random()) return unvalidated; // NOT OK
-    return validatedObj.key; // OK
-  }
+    if (Math.random()) return unvalidated; // $ Alert responseSendArgument
+    return validatedObj.key;  // $ responseSendArgument
+  } // $ routeHandler
 }
 
 class Struct {
