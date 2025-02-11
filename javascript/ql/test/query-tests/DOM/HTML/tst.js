b715c96258017cc37fdd65325c5bd873f372d06c
Asger F <asgerf@github.com>
2025-02-11 15:34:01 +0100

    JS: Accept changes in DOM tests

--- javascript/ql/test/query-tests/DOM/HTML/tst.js
+++ javascript/ql/test/query-tests/DOM/HTML/tst.js
@@ -2,18 +2,18 @@
 var div1 = <div id="theDiff"></div>;
 var div2 = <div id="theDiff"></div>;
 
-<a href="http://semmle.com" href="https://semmle.com">Semmle</a>; // $ Alert
+<a href="http://semmle.com" href="https://semmle.com">Semmle</a>; // $ Alert[js/conflicting-html-attribute]
 
-<a href="https://semmle.com" href="https://semmle.com">Semmle</a>; // $ Alert
+<a href="https://semmle.com" href="https://semmle.com">Semmle</a>; // $ Alert[js/duplicate-html-attribute]
 
-<div id=""></div>; // $ Alert
-<div id="a b"></div>;
+<div id=""></div>; // $ Alert[js/malformed-html-id]
+<div id="a b"></div>; // $ Alert[js/malformed-html-id]
 
-<a href="http://semmle.com" href={someValue()}>Semmle</a>; // $ Alert
+<a href="http://semmle.com" href={someValue()}>Semmle</a>; // $ Alert[js/conflicting-html-attribute]
 
 
 <div id={someOtherValue()}></div>;
 
-var div3 = <div><div id="theDiff"></div><div id="theDiff"></div></div>; // $ Alert
+var div3 = <div><div id="theDiff"></div><div id="theDiff"></div></div>; // $ Alert[js/duplicate-html-id]
 
-var div4 = <div id="theDiff" id="theDiff"></div>; // $ Alert
+var div4 = <div id="theDiff" id="theDiff"></div>; // $ Alert[js/duplicate-html-attribute]
