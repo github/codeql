b08036ee1f7106721f50e911359c4969c0402765
Asger F <asgerf@github.com>
2025-02-12 15:21:51 +0100

    JS: Accept some alert from PrivateFileExposure

--- javascript/ql/test/query-tests/Security/CWE-200/lib/tst.js
+++ javascript/ql/test/query-tests/Security/CWE-200/lib/tst.js
@@ -4,8 +4,8 @@ var path = require("path");
 
 var app = express();
 
-app.use('basedir', express.static(__dirname)); // $ Alert
+app.use('basedir', express.static(__dirname)); // $ Alert[js/exposure-of-private-files]
 const rootDir = __dirname;
-app.use('basedir', express.static(rootDir)); // $ Alert
+app.use('basedir', express.static(rootDir)); // $ Alert[js/exposure-of-private-files]
 
-app.use('/monthly', express.static(__dirname + '/')); // $ Alert
+app.use('/monthly', express.static(__dirname + '/')); // $ Alert[js/exposure-of-private-files]
