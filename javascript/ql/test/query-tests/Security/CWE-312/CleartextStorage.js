291fea8f42d9e91519d57eede170f72d156fa917
Asger F <asgerf@github.com>
2025-02-12 15:31:44 +0100

    JS: Mark an alert as missing

--- javascript/ql/test/query-tests/Security/CWE-312/CleartextStorage.js
+++ javascript/ql/test/query-tests/Security/CWE-312/CleartextStorage.js
@@ -3,5 +3,5 @@ var express = require('express');
 var app = express();
 app.get('/remember-password', function (req, res) {
   let pw = req.param("current_password");
-  res.cookie("password", pw); // $ Alert - Setting a cookie value with cleartext sensitive data.
+  res.cookie("password", pw); // $ MISSING: Alert - Setting a cookie value with cleartext sensitive data.
 });
