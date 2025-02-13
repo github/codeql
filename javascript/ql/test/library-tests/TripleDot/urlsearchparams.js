26dcbf7a2aafa377b653b1f760d41e22ebb8ae5e
Asger F <asgerf@github.com>
2025-02-13 11:51:00 +0100

    JS: Migrate URLSearchParams model to flow summaries

--- javascript/ql/test/library-tests/TripleDot/urlsearchparams.js
+++ javascript/ql/test/library-tests/TripleDot/urlsearchparams.js
@@ -1,15 +1,15 @@
 function u1() {
     const searchParams = new URLSearchParams(source("u1.1"));
-    sink(searchParams.get("x")); // $ MISSING: hasTaintFlow=u1.1
-    sink(searchParams.get(unknown())); // $ MISSING: hasTaintFlow=u1.1
-    sink(searchParams.getAll("x")); // $ MISSING: hasTaintFlow=u1.1
-    sink(searchParams.getAll(unknown())); // $ MISSING: hasTaintFlow=u1.1
+    sink(searchParams.get("x")); // $ hasTaintFlow=u1.1
+    sink(searchParams.get(unknown())); // $ hasTaintFlow=u1.1
+    sink(searchParams.getAll("x")); // $ hasTaintFlow=u1.1
+    sink(searchParams.getAll(unknown())); // $ hasTaintFlow=u1.1
 }
 
 function u2() {
     const url = new URL(source("u2.1"));
-    sink(url.searchParams.get("x")); // $ MISSING: hasTaintFlow=u2.1
-    sink(url.searchParams.get(unknown())); // $ MISSING: hasTaintFlow=u2.1
-    sink(url.searchParams.getAll("x")); // $ MISSING: hasTaintFlow=u2.1
-    sink(url.searchParams.getAll(unknown())); // $ MISSING: hasTaintFlow=u2.1
+    sink(url.searchParams.get("x")); // $ hasTaintFlow=u2.1
+    sink(url.searchParams.get(unknown())); // $ hasTaintFlow=u2.1
+    sink(url.searchParams.getAll("x")); // $ hasTaintFlow=u2.1
+    sink(url.searchParams.getAll(unknown())); // $ hasTaintFlow=u2.1
 }
