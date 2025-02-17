08b9d934c0a6076a7f65a90fba519ff95fddf3d0
Asger F <asgerf@github.com>
2025-02-17 11:37:44 +0100

    JS: Add a negative test

--- javascript/ql/test/library-tests/TripleDot/arrays.js
+++ javascript/ql/test/library-tests/TripleDot/arrays.js
@@ -42,4 +42,5 @@ function implicitToString() {
     sink(array.toString("utf8")); // $ hasTaintFlow=implicitToString.1 hasTaintFlow=implicitToString.2
 
     sink(Array.prototype.toString.call(array)); // $ hasTaintFlow=implicitToString.1 hasTaintFlow=implicitToString.2
+    sink(Object.prototype.toString.call(array)); // OK - returns "[object Array]"
 }
