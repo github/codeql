48106a1e26bce9cc74b163804a82ac147ee1fa5f
Asger F <asgerf@github.com>
2025-02-21 09:21:07 +0100

    JS: Add query ID

--- javascript/ql/test/query-tests/Security/CWE-400/ReDoS/jsonschema.js
+++ javascript/ql/test/query-tests/Security/CWE-400/ReDoS/jsonschema.js
@@ -2,7 +2,7 @@ import Ajv from 'ajv';
 
 let thing = {
     type: 'string',
-    pattern: '(a?a?)*b' // $ Alert
+    pattern: '(a?a?)*b' // $ Alert[js/redos]
 }
 new Ajv().addSchema(thing, 'thing');
 
@@ -12,12 +12,12 @@ export default {
     properties: {
         foo: {
             type: "string",
-            pattern: "(a?a?)*b" // $ Alert
+            pattern: "(a?a?)*b" // $ Alert[js/redos]
         },
         bar: {
             type: "object",
             patternProperties: {
-                "(a?a?)*b": { // $ Alert
+                "(a?a?)*b": { // $ Alert[js/redos]
                     type: "number"
                 }
             }
