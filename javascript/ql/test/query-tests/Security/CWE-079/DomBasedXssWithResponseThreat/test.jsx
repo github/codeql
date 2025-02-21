05690c21ed8cd1a803c8b5541de94d74f947adc4
Napalys <napalys@github.com>
2025-02-21 12:51:39 +0100

    Added a test for tanstack/react-query useQuery

---
+++ javascript/ql/test/query-tests/Security/CWE-079/DomBasedXssWithResponseThreat/test.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchContent = async () => {
+    const response = await fetch("https://example.com/content"); // $ MISSING: Source[js/xss]
+    const data = await response.json();
+    return data;
+};
+
+const ContentWithDangerousHtml = () => {
+    const { data, error, isLoading } = useQuery(
+        {
+            queryFn: fetchContent
+        }   
+    );
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error fetching content!</div>;
+
+    return (
+        <div>
+            <h1>Content with Dangerous HTML</h1>
+            <div
+                dangerouslySetInnerHTML={{
+                    __html: data, // $ MISSING: Alert[js/xss]
+                }}
+            />
+        </div>
+    );
+};
+
+export default ContentWithDangerousHtml;
