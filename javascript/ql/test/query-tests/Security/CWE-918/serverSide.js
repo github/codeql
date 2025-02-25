4d7cbe6f607e69445a2d2a4911cba20f13389374
Asger F <asgerf@github.com>
2025-02-25 16:52:45 +0100

    JS: Accept to web socket-based SSRF alerts

--- javascript/ql/test/query-tests/Security/CWE-918/serverSide.js
+++ javascript/ql/test/query-tests/Security/CWE-918/serverSide.js
@@ -106,7 +106,7 @@ import * as ws from 'ws';
 new ws.Server({ port: 8080 }).on('connection', function(socket, request) {
   socket.on('message', function(message) {
     const url = request.url;
-    const socket = new ws(url);
+    const socket = new ws(url); // $ Alert[js/request-forgery]
   });
 });
 
@@ -114,7 +114,7 @@ new ws.Server({ port: 8080 }).on('connection', function (socket, request) {
   socket.on('message', function (message) {
     const url = new URL(request.url, base);
     const target = new URL(url.pathname, base);
-    const socket = new ws(url);
+    const socket = new ws(url); // $ Alert[js/request-forgery]
   });
 });
 
