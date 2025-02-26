b4ac2f7d73eb6bc330d13230b7fffd99b71812f9
Asger F <asgerf@github.com>
2025-02-26 13:21:07 +0100

    JS: Add a query ID

--- javascript/ql/test/query-tests/Security/CWE-200/googlecompiler.js
+++ javascript/ql/test/query-tests/Security/CWE-200/googlecompiler.js
@@ -34,7 +34,7 @@ function PostCode(codestring) {
       });
   });
 
-  post_req.write(post_data); // $ Alert - post the data from file to request body
+  post_req.write(post_data); // $ Alert[js/file-access-to-http] - post the data from file to request body
   post_req.end();
 
 }
