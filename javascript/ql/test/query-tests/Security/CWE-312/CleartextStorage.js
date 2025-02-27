193b26e938067937fca2b9d71b47a662c31507d5
Asger F <asgerf@github.com>
2025-02-27 15:37:39 +0100

    JS: Add query IDs

--- javascript/ql/test/query-tests/Security/CWE-312/CleartextStorage.js
+++ javascript/ql/test/query-tests/Security/CWE-312/CleartextStorage.js
@@ -2,6 +2,6 @@ var express = require('express');
 
 var app = express();
 app.get('/remember-password', function (req, res) {
-  let pw = req.param("current_password"); // $ Source
-  res.cookie("password", pw); // $ Alert - Setting a cookie value with cleartext sensitive data.
+  let pw = req.param("current_password"); // $ Source[js/clear-text-storage-of-sensitive-data]
+  res.cookie("password", pw); // $ Alert[js/clear-text-storage-of-sensitive-data] - Setting a cookie value with cleartext sensitive data.
 });
