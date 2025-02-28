c43b2b167f07dba4f9fd118ee4f72a7b442175d9
Chris Smowton <chris@smowton.net>
2025-02-28 14:09:58 +0000

    Fix test on Windows

--- java/ql/integration-tests/java/maven-download-failure/test.py
+++ java/ql/integration-tests/java/maven-download-failure/test.py
@@ -3,8 +3,12 @@ import os.path
 import shutil
 
 def test(codeql, java, check_diagnostics):
+
+    # Avoid shutil resolving mvn to the wrapper script in the test dir:
+    os.environ["NoDefaultCurrentDirectoryInExePath"] = "0"
     runenv = {
         "PATH": os.path.realpath(os.path.dirname(__file__)) + os.pathsep + os.getenv("PATH"),
         "REAL_MVN_PATH": shutil.which("mvn"),
     }
+    del os.environ["NoDefaultCurrentDirectoryInExePath"]
     codeql.database.create(build_mode = "none", _env = runenv)
