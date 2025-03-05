a9d45a2aa23130f28f5ea82ee29e19b6428ed898
Michael Nebel <michaelnebel@github.com>
2025-03-05 14:32:41 +0100

    C#: Add some tests for cs/useless-if-statement.

---
+++ csharp/ql/test/query-tests/Useless Code/FutileConditional/FutileConditional.cs
@@ -0,0 +1,29 @@
+using System;
+
+class FutileConditionalTest
+{
+
+    public void M(string s)
+    {
+        if (s.Length > 0) ; // $ Alert
+
+        if (s.Length > 1)
+        {
+        } // $ Alert
+
+        if (s.Length > 2) // GOOD: because of else-branch
+        {
+        }
+        else
+        {
+            Console.WriteLine("hello");
+        }
+
+        if (s.Length > 3)
+        {
+        }
+        else
+        {
+        } // $ Alert
+    }
+}
