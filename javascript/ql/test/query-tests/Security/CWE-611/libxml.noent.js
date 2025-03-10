75ed0d0b4641e74a97e659730e4491e6f2ab0d13
Asger F <asgerf@github.com>
2025-03-10 14:21:26 +0100

    JS: Remove duplicate '$ Alert' in libxml test

--- javascript/ql/test/query-tests/Security/CWE-611/libxml.noent.js
+++ javascript/ql/test/query-tests/Security/CWE-611/libxml.noent.js
@@ -2,14 +2,14 @@ const express = require('express');
 const libxmljs = require('libxmljs');
 
 express().get('/some/path', function (req) {
-  libxmljs.parseXml(req.param("some-xml"), { noent: true }); // $ Alert // $ Alert - unguarded entity expansion
+  libxmljs.parseXml(req.param("some-xml"), { noent: true }); // $ Alert - unguarded entity expansion
 });
 
 express().post('/some/path', function (req, res) {
-  libxmljs.parseXml(req.param("some-xml"), { noent: true }); // $ Alert // $ Alert - unguarded entity expansion
+  libxmljs.parseXml(req.param("some-xml"), { noent: true }); // $ Alert - unguarded entity expansion
 
-  libxmljs.parseXmlString(req.param("some-xml"), { noent: true }) // $ Alert // $ Alert - unguarded entity expansion
-  libxmljs.parseXmlString(req.files.products.data.toString('utf8'), { noent: true })// $ Alert // $ Alert - unguarded entity expansion
+  libxmljs.parseXmlString(req.param("some-xml"), { noent: true }) // $ Alert - unguarded entity expansion
+  libxmljs.parseXmlString(req.files.products.data.toString('utf8'), { noent: true })// $ Alert - unguarded entity expansion
 
   // OK - no entity expansion
   libxmljs.parseXmlString(req.files.products.data.toString('utf8'), { noent: false })
