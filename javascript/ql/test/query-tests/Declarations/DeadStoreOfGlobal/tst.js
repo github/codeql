0df893e28039e7c6335ee190d729b611ed94e4c3
Asger F <asgerf@github.com>
2025-03-10 14:01:40 +0100

    JS: Remove blank lines and add trailing newline

--- javascript/ql/test/query-tests/Declarations/DeadStoreOfGlobal/tst.js
+++ javascript/ql/test/query-tests/Declarations/DeadStoreOfGlobal/tst.js
@@ -1,30 +1,23 @@
 g = 23; // $ Alert
 
-
 h = 23;
 alert(h);
-
 
 uid = 0;
 function incr() {
 	return uid++;
 }
 
-
 function foo() {
 	var x;
 	x = 0;
 }
 
-
 onload = function() {}
-
 
 global = 42;
 
-
 prop = 42;
-
 
 /*global otherGlobal*/
 otherGlobal = 56;
