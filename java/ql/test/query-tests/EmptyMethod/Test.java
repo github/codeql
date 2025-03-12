17aa3fc4280eb760455a47c41e784c1a4cac3631
Tamas Vajk <tamasvajk@github.com>
2025-03-12 10:50:03 +0100

    Add compliant/non-compliant comments back to the test file

--- java/ql/test/query-tests/EmptyMethod/Test.java
+++ java/ql/test/query-tests/EmptyMethod/Test.java
@@ -2,36 +2,44 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class Test {
 
+  // COMPLIANT
   public void f() {
     int i = 0;
   }
 
+  // COMPLIANT
   public void f1() {
     // intentionally empty
   }
 
+  // NON_COMPLIANT
   public void f2() { } // $ Alert
 
+  // COMPLIANT - exception
   @Pointcut()
   public void f4() {
   }
 
   public abstract class TestInner {
 
-    public abstract void f();
+    public abstract void f(); // COMPLIANT - intentionally empty
+
   }
 
   public class Derived extends TestInner {
 
+    // COMPLIANT: with annotation
     @Override
     public void f() {
     }
 
+    // COMPLIANT: native
     public native int nativeMethod();
   }
 
   public interface TestInterface {
 
+    // NON_COMPLIANT
     default void method() { } // $ Alert
   }
 
