dc2cbf74020f7c80c2f4638f5a2a3c7649f64bec
Owen Mansel-Chan <owen-mc@github.com>
2025-03-13 15:02:26 +0000

    Add tests for always-locked fields

---
+++ java/ql/test/query-tests/security/CWE-367/semmle/tests/FieldAlwaysLocked.java
@@ -0,0 +1,24 @@
+package test.cwe367.semmle.tests;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+class FieldAlwaysLocked {
+
+        Hashtable field;
+
+        public FieldAlwaysLocked() {
+                field = new Hashtable();
+        }
+
+        protected synchronized void checkOut() {
+                Object o;
+                if (field.size() > 0) {
+                        Enumeration e = field.keys(); // $ SPURIOUS: Alert
+                        while (e.hasMoreElements()) {
+                                o = e.nextElement();
+                                field.remove(o); // $ SPURIOUS: Alert
+                        }
+                }
+        }
+}
