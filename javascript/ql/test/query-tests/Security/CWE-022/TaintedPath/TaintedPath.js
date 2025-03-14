dc262236f4840ecffde1e26005087c829d4c9591
Napalys <napalys@github.com>
2025-03-14 11:43:22 +0100

    Enhance taint tracking by including `escape` and `unescape` in TaintedPath customizations.

--- javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/TaintedPath.js
+++ javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/TaintedPath.js
@@ -209,9 +209,9 @@ var server = http.createServer(function(req, res) {
 });
   
 var srv = http.createServer(function(req, res) {
-  let path = url.parse(req.url, true).query.path; // $ MISSING: Source
+  let path = url.parse(req.url, true).query.path; // $ Source
   const improperEscape = escape(path);
-  res.write(fs.readFileSync(improperEscape)); // $ MISSING: Alert
+  res.write(fs.readFileSync(improperEscape)); // $ Alert
   const improperEscape2 = unescape(path);
-  res.write(fs.readFileSync(improperEscape2)); // $ MISSING: Alert
+  res.write(fs.readFileSync(improperEscape2)); // $ Alert
 });
