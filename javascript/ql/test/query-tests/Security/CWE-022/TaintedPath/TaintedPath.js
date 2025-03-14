c4b717b86c2bd82e3f8b0948520a0ac096d8e6f0
Napalys <napalys@github.com>
2025-03-14 08:48:45 +0100

    Added test case for `escape`.

--- javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/TaintedPath.js
+++ javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/TaintedPath.js
@@ -208,3 +208,10 @@ var server = http.createServer(function(req, res) {
   }
 });
   
+var srv = http.createServer(function(req, res) {
+  let path = url.parse(req.url, true).query.path; // $ MISSING: Source
+  const improperEscape = escape(path);
+  res.write(fs.readFileSync(improperEscape)); // $ MISSING: Alert
+  const improperEscape2 = unescape(path);
+  res.write(fs.readFileSync(improperEscape2)); // $ MISSING: Alert
+});
