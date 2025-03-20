056bf4fde7bc6983a0f7cb414213de061dea4911
Napalys <napalys@github.com>
2025-03-20 10:35:51 +0100

    Added test case with inheretence.

--- javascript/ql/test/query-tests/Security/CWE-918/apollo.serverSide.ts
+++ javascript/ql/test/query-tests/Security/CWE-918/apollo.serverSide.ts
@@ -11,4 +11,17 @@ function createApolloServer(typeDefs) {
         },
       };
     const server = new ApolloServer({typeDefs, resolvers});
+
+    const resolvers2 = {
+      Mutation: {
+        downloadFiles: async (_, { files }) => { // $ MISSING: Source[js/request-forgery]
+          files.forEach((file) => { get(file.url, (res) => {}); }); // $ MISSING: Alert[js/request-forgery] Sink[js/request-forgery]
+          return true;
+        },
+      },
+    };
+    
+    class CustomApollo extends ApolloServer {}
+
+    const srv = new CustomApollo({typeDefs, resolvers: resolvers2});
 }
