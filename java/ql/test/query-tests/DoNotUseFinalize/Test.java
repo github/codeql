c689a0e9b718ff14fd5923e25c0b57224badf64b
Jami Cogswell <jcogs33@Jamis-MacBook-Pro.local>
2025-03-20 20:36:21 -0400

    Java: add more test cases

--- java/ql/test/query-tests/DoNotUseFinalize/Test.java
+++ java/ql/test/query-tests/DoNotUseFinalize/Test.java
@@ -7,4 +7,22 @@ public class Test {
     void f1() throws Throwable {
         f(); // COMPLIANT
     }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // COMPLIANT: If a subclass overrides `finalize`
+        // it must invoke the superclass finalizer explicitly.
+        super.finalize();
+    }
+
+    // Overload of `finalize`
+    protected void finalize(String s) throws Throwable {
+        System.out.println(s);
+    }
+
+    // NON_COMPLIANT: call to overload of `finalize`
+    void f2() throws Throwable {
+        this.finalize("overload"); // $ Alert
+    }
+
 }
