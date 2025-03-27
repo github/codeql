e1c5517de71b8f73338c9dcd02582ddb20a03dcb
Owen Mansel-Chan <owen-mc@github.com>
2025-03-27 11:40:53 +0000

    Keep COMPLIANT and NON_COMPLIANT comments in test

--- java/ql/test/query-tests/StringReplaceAllWithNonRegex/Test.java
+++ java/ql/test/query-tests/StringReplaceAllWithNonRegex/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     void f() {
         String s1 = "test";
-        s1 = s1.replaceAll("t", "x"); // $ Alert
-        s1 = s1.replaceAll(".*", "x");
+        s1 = s1.replaceAll("t", "x"); // $ Alert // NON_COMPLIANT
+        s1 = s1.replaceAll(".*", "x"); // COMPLIANT
     }
 }
