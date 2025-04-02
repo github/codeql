b16b407f892d4adaf377f84ea099ad14fa3366e5
Napalys <napalys@github.com>
2025-04-02 12:49:48 +0200

    Add `rimraf` model and update tests for path injection vulnerabilities

--- javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/rimraf.js
+++ javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/rimraf.js
@@ -5,26 +5,26 @@ const app = express();
 app.use(express.json());
 
 app.post('/rmsync', async (req, res) => {
-    const { path } = req.body; // $ MISSING: Source
+    const { path } = req.body; // $ Source
 
-    rimraf.sync(path); // $ MISSING: Alert
-    rimraf.rimrafSync(path); // $ MISSING: Alert
-    rimraf.native(path); // $ MISSING: Alert
-    await rimraf.native(path); // $ MISSING: Alert
-    rimraf.native.sync(path); // $ MISSING: Alert
-    rimraf.nativeSync(path); // $ MISSING: Alert
-    await rimraf.manual(path); // $ MISSING: Alert
-    rimraf.manual(path); // $ MISSING: Alert
-    rimraf.manual.sync(path); // $ MISSING: Alert
-    rimraf.manualSync(path); // $ MISSING: Alert
-    await rimraf.windows(path); // $ MISSING: Alert
-    rimraf.windows(path); // $ MISSING: Alert
-    rimraf.windows.sync(path); // $ MISSING: Alert
-    rimraf.windowsSync(path); // $ MISSING: Alert
-    rimraf.moveRemove(path); // $ MISSING: Alert
-    rimraf.moveRemove.sync(path); // $ MISSING: Alert
-    rimraf.moveRemoveSync(path); // $ MISSING: Alert
-    rimraf.posixSync(path); // $ MISSING: Alert
-    rimraf.posix(path); // $ MISSING: Alert
-    rimraf.posix.sync(path); // $ MISSING: Alert
+    rimraf.sync(path); // $ Alert
+    rimraf.rimrafSync(path); // $ Alert
+    rimraf.native(path); // $ Alert
+    await rimraf.native(path); // $ Alert
+    rimraf.native.sync(path); // $ Alert
+    rimraf.nativeSync(path); // $ Alert
+    await rimraf.manual(path); // $ Alert
+    rimraf.manual(path); // $ Alert
+    rimraf.manual.sync(path); // $ Alert
+    rimraf.manualSync(path); // $ Alert
+    await rimraf.windows(path); // $ Alert
+    rimraf.windows(path); // $ Alert
+    rimraf.windows.sync(path); // $ Alert
+    rimraf.windowsSync(path); // $ Alert
+    rimraf.moveRemove(path); // $ Alert
+    rimraf.moveRemove.sync(path); // $ Alert
+    rimraf.moveRemoveSync(path); // $ Alert
+    rimraf.posixSync(path); // $ Alert
+    rimraf.posix(path); // $ Alert
+    rimraf.posix.sync(path); // $ Alert
 });
