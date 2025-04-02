db2720ea5bad641b76c416e5a11c53193a3e1099
Asger F <asgerf@github.com>
2025-04-02 13:50:41 +0200

    JS: Initial model of Response

--- javascript/ql/test/query-tests/Security/CWE-079/ReflectedXss/response-object.js
+++ javascript/ql/test/query-tests/Security/CWE-079/ReflectedXss/response-object.js
@@ -4,36 +4,36 @@ const express = require('express');
 // in isolation from the more complicated http frameworks.
 
 express().get('/foo', (req) => {
-    const data = req.body; // $ MISSING: Source
+    const data = req.body; // $ Source
 
-    new Response(data); // $ MISSING: Alert
-    new Response(data, {}); // $ MISSING: Alert
-    new Response(data, { headers: null }); // $ MISSING: Alert
+    new Response(data); // $ Alert
+    new Response(data, {}); // $ Alert
+    new Response(data, { headers: null }); // $ Alert
 
-    new Response(data, { headers: { 'content-type': 'text/plain'}});
-    new Response(data, { headers: { 'content-type': 'text/html'}}); // $ MISSING: Alert
+    new Response(data, { headers: { 'content-type': 'text/plain'}}); // $ SPURIOUS: Alert
+    new Response(data, { headers: { 'content-type': 'text/html'}}); // $ Alert
 
-    new Response(data, { headers: { 'Content-Type': 'text/plain'}});
-    new Response(data, { headers: { 'Content-Type': 'text/html'}}); // $ MISSING: Alert
+    new Response(data, { headers: { 'Content-Type': 'text/plain'}}); // $ SPURIOUS: Alert
+    new Response(data, { headers: { 'Content-Type': 'text/html'}}); // $ Alert
 
     const headers1 = new Headers({ 'content-type': 'text/plain'});
-    new Response(data, { headers: headers1 });
+    new Response(data, { headers: headers1 }); // $ SPURIOUS: Alert
 
     const headers2 = new Headers({ 'content-type': 'text/html'});
-    new Response(data, { headers: headers2 }); // $ MISSING: Alert
+    new Response(data, { headers: headers2 }); // $ Alert
 
     const headers3 = new Headers();
-    new Response(data, { headers: headers3 }); // $ MISSING: Alert
+    new Response(data, { headers: headers3 }); // $ Alert
 
     const headers4 = new Headers();
     headers4.set('content-type', 'text/plain');
-    new Response(data, { headers: headers4 });
+    new Response(data, { headers: headers4 }); // $ SPURIOUS: Alert
 
     const headers5 = new Headers();
     headers5.set('content-type', 'text/html');
-    new Response(data, { headers: headers5 }); // $ MISSING: Alert
+    new Response(data, { headers: headers5 }); // $ Alert
 
     const headers6 = new Headers();
     headers6.set('unrelated-header', 'text/plain');
-    new Response(data, { headers: headers6 }); // $ MISSING: Alert
+    new Response(data, { headers: headers6 }); // $ Alert
 });
