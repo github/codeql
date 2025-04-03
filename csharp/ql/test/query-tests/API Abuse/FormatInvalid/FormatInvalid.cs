629817d1d1b1a8f4a4ae7e850a0c3bddef01722f
Michael Nebel <michaelnebel@github.com>
2025-04-03 15:00:04 +0200

    C#: Convert testing of cs/invalid-string-formatting to inline expectations and adjust some of the testcases.

--- csharp/ql/test/query-tests/API Abuse/FormatInvalid/FormatInvalid.cs
+++ csharp/ql/test/query-tests/API Abuse/FormatInvalid/FormatInvalid.cs
@@ -24,38 +24,38 @@ class FormatInvalid
         String.Format("{0, -10 :0.000}", 1);
 
         // BAD: Invalid format string
-        String.Format("{ 0}", 1);
+        String.Format("{ 0}", 1); // $ Alert
 
         // BAD: Invalid format string
-        String.Format("{0,--1}", 1);
+        String.Format("{0,--1}", 1); // $ Alert
 
         // BAD: Invalid format string
-        String.Format("{0:{}}", 1);
+        String.Format("{0:{}}", 1); // $ Alert
 
         // BAD: Invalid format string
-        String.Format("%d", 1);
+        String.Format("%d", 1); // $ Alert Sink
 
         // BAD: } { in the middle.
-        String.Format("{{0}-{1}}", 0, 1);
+        String.Format("{{0}-{1}}", 0, 1); // $ Alert
 
         // BAD: This is invalid
-        String.Format("{0}}", 0, 1);
+        String.Format("{0}}", 0, 1); // $ Alert
 
         // BAD: Invalid
-        string.Format("{foo{0}}", 0);
+        string.Format("{foo{0}}", 0); // $ Alert
 
         // GOOD: {{ is output as {
-        String.Format("{{sdc}}", 0);
+        String.Format("{{sdc}}{0}", 0);
 
         // BAD: Invalid: Stray }
-        String.Format("}", 0);
+        String.Format("}{0}", 0); // $ Alert
 
         // GOOD: {{ output as {
-        String.Format("new {0} ({1} => {{", 0);
+        String.Format("new {0} ({1} => {{", 0, 1);
 
         // GOOD: Literal {{ and }}
-        String.Format("{{", "");
-        String.Format("{{{{}}", "");
+        String.Format("{0}{{", 0);
+        String.Format("{0}{{{{}}", 0);
     }
 
     IFormatProvider fp;
@@ -72,49 +72,49 @@ class FormatInvalid
         Format("}", 0);
 
         // BAD: All of these are format methods with an invalid string.
-        String.Format("}", 0);
-        String.Format("}", ps);
-        String.Format(fp, "}", ps);
-        String.Format("}", 0, 1);
-        String.Format("}", 0, 1, 2);
-        String.Format("}", 0, 1, 2, 3);
+        String.Format("}", 0); // $ Alert
+        String.Format("}", ps); // $ Alert
+        String.Format(fp, "}", ps); // $ Alert
+        String.Format("}", 0, 1); // $ Alert
+        String.Format("}", 0, 1, 2); // $ Alert
+        String.Format("}", 0, 1, 2, 3); // $ Alert
 
-        sb.AppendFormat("}", 0);
-        sb.AppendFormat("}", ps);
-        sb.AppendFormat(fp, "}", ps);
-        sb.AppendFormat("}", 0, 1);
-        sb.AppendFormat("}", 0, 1, 2);
-        sb.AppendFormat("}", 0, 1, 2, 3);
+        sb.AppendFormat("}", 0); // $ Alert
+        sb.AppendFormat("}", ps); // $ Alert
+        sb.AppendFormat(fp, "}", ps); // $ Alert
+        sb.AppendFormat("}", 0, 1); // $ Alert
+        sb.AppendFormat("}", 0, 1, 2); // $ Alert
+        sb.AppendFormat("}", 0, 1, 2, 3); // $ Alert
 
-        Console.WriteLine("}", 0);
-        Console.WriteLine("}", ps);
-        Console.WriteLine("}", 0, 1);
-        Console.WriteLine("}", 0, 1, 2);
-        Console.WriteLine("}", 0, 1, 2, 3);
+        Console.WriteLine("}", 0); // $ Alert
+        Console.WriteLine("}", ps); // $ Alert
+        Console.WriteLine("}", 0, 1); // $ Alert
+        Console.WriteLine("}", 0, 1, 2); // $ Alert
+        Console.WriteLine("}", 0, 1, 2, 3); // $ Alert
 
-        tw.WriteLine("}", 0);
-        tw.WriteLine("}", ps);
-        tw.WriteLine("}", 0, 1);
-        tw.WriteLine("}", 0, 1, 2);
-        tw.WriteLine("}", 0, 1, 2, 3);
+        tw.WriteLine("}", 0); // $ Alert
+        tw.WriteLine("}", ps); // $ Alert
+        tw.WriteLine("}", 0, 1); // $ Alert
+        tw.WriteLine("}", 0, 1, 2); // $ Alert
+        tw.WriteLine("}", 0, 1, 2, 3); // $ Alert
 
-        System.Diagnostics.Debug.WriteLine("}", ps);
-        System.Diagnostics.Trace.TraceError("}", 0);
-        System.Diagnostics.Trace.TraceInformation("}", 0);
-        System.Diagnostics.Trace.TraceWarning("}", 0);
-        ts.TraceInformation("}", 0);
+        System.Diagnostics.Debug.WriteLine("}", ps); // $ Alert
+        System.Diagnostics.Trace.TraceError("}", 0); // $ Alert
+        System.Diagnostics.Trace.TraceInformation("}", 0); // $ Alert
+        System.Diagnostics.Trace.TraceWarning("}", 0); // $ Alert
+        ts.TraceInformation("}", 0); // $ Alert
 
-        Console.Write("}", 0);
-        Console.Write("}", 0, 1);
-        Console.Write("}", 0, 1, 2);
-        Console.Write("}", 0, 1, 2, 3);
+        Console.Write("}", 0); // $ Alert
+        Console.Write("}", 0, 1); // $ Alert
+        Console.Write("}", 0, 1, 2); // $ Alert
+        Console.Write("}", 0, 1, 2, 3); // $ Alert
 
         System.Diagnostics.Debug.WriteLine("}", ""); // GOOD
         System.Diagnostics.Debug.Write("}", "");     // GOOD
 
-        System.Diagnostics.Debug.Assert(true, "Error", "}", ps);
-        sw.Write("}", 0);
-        System.Diagnostics.Debug.Print("}", ps);
+        System.Diagnostics.Debug.Assert(true, "Error", "}", ps); // $ Alert
+        sw.Write("}", 0); // $ Alert
+        System.Diagnostics.Debug.Print("}", ps); // $ Alert
 
         Console.WriteLine("}"); // GOOD
     }
