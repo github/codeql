aa0f7d8ec358eb6a0440d9f69b670d4f288e7551
Asger F <asgerf@github.com>
2025-04-03 12:38:19 +0200

    JS: Add test for baseUrl and path mappings in tsconfig files

---
+++ javascript/ql/test/library-tests/PathResolution/BaseUrl/src/main.ts
@@ -0,0 +1,35 @@
+// Relative import
+import "../lib/file"; // $ importTarget=BaseUrl/lib/file.ts
+import "../lib/file.ts"; // $ importTarget=BaseUrl/lib/file.ts
+import "../lib/file.js"; // $ importTarget=BaseUrl/lib/file.ts
+import "../lib"; // $ importTarget=BaseUrl/lib/index.ts
+import "../lib/index"; // $ importTarget=BaseUrl/lib/index.ts
+import "../lib/index.ts"; // $ importTarget=BaseUrl/lib/index.ts
+import "../lib/index.js"; // $ importTarget=BaseUrl/lib/index.ts
+
+// Import relative to baseUrl
+import "lib/file"; // $ importTarget=BaseUrl/lib/file.ts
+import "lib/file.ts"; // $ importTarget=BaseUrl/lib/file.ts
+import "lib/file.js"; // $ importTarget=BaseUrl/lib/file.ts
+import "lib"; // $ importTarget=BaseUrl/lib/index.ts
+import "lib/index"; // $ importTarget=BaseUrl/lib/index.ts
+import "lib/index.ts"; // $ importTarget=BaseUrl/lib/index.ts
+import "lib/index.js"; // $ importTarget=BaseUrl/lib/index.ts
+
+// Import matching "@/*" path mapping
+import "@/file"; // $ importTarget=BaseUrl/lib/file.ts
+import "@/file.ts"; // $ importTarget=BaseUrl/lib/file.ts
+import "@/file.js"; // $ importTarget=BaseUrl/lib/file.ts
+import "@"; // $ importTarget=BaseUrl/lib/nostar.ts
+import "@/index"; // $ importTarget=BaseUrl/lib/index.ts
+import "@/index.ts"; // $ importTarget=BaseUrl/lib/index.ts
+import "@/index.js"; // $ importTarget=BaseUrl/lib/index.ts
+
+// Import matching "@/*.xyz" path mapping. Note that this is not actually supported by TypeScript.
+import "@/file.xyz";
+import "@/file.ts.xyz";
+import "@/file.js.xyz";
+import "@.xyz";
+import "@/index.xyz";
+import "@/index.ts.xyz";
+import "@/index.js.xyz";
