b4494975035c56261a91bbfb8fde4f5ab60bfe62
Michael Nebel <michaelnebel@github.com>
2025-04-03 15:00:04 +0200

    C#: Convert testing of cs/invalid-string-formatting to inline expectations and adjust some of the testcases.

--- csharp/ql/test/query-tests/API Abuse/FormatInvalid/FormatMissingArgument.cs
+++ csharp/ql/test/query-tests/API Abuse/FormatInvalid/FormatMissingArgument.cs
@@ -8,10 +8,10 @@ class Class1
         String.Format("{0}", 0);
 
         // BAD: Missing {1}
-        String.Format("{1}", 0);
+        String.Format("{1}", 0); // $ Alert Sink
 
         // BAD: Missing {2} and {3}
-        String.Format("{2} {3}", 0, 1);
+        String.Format("{2} {3}", 0, 1); // $ Alert Sink
 
         // GOOD: An array has been supplied.
         String.Format("{0} {1} {2}", args);
@@ -19,16 +19,16 @@ class Class1
         // GOOD: All arguments supplied to params
         String.Format("{0} {1} {2} {3}", 0, 1, 2, 3);
 
-        helper("{1}");
+        helper("{1}"); // $ Source
 
         // BAD: Missing {0}
-        Console.WriteLine("{0}");
+        Console.WriteLine("{0}"); // $ Alert Sink
     }
 
     void helper(string format)
     {
         // BAD: Missing {1}
-        String.Format(format, 0);
+        String.Format(format, 0); // $ Alert Sink
     }
 
     object[] args;
