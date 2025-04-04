63e951b73a7d49c9749c2d393d184b75815b7ed1
Jami Cogswell <jcogs33@Jamis-MacBook-Pro.local>
2025-04-04 18:20:26 -0400

    Java: update existing tests to inline expectations

--- java/ql/test/query-tests/CallsToRunnableRun/CallsToRunnableRun.java
+++ java/ql/test/query-tests/CallsToRunnableRun/CallsToRunnableRun.java
@@ -1,18 +1,18 @@
 import java.lang.Runnable;
 
 public class CallsToRunnableRun extends Thread implements Runnable{
-	
+
 	private Thread wrapped;
 	private Runnable callback;
-	
+
 	@Override
 	public void run() {
-		wrapped.run();
-		callback.run();
+		wrapped.run();  // COMPLIANT: called within a `run` method
+		callback.run(); // COMPLIANT: called within a `run` method
 	}
-	
+
 	public void bad() {
-		wrapped.run();
-		callback.run();
+		wrapped.run();  // $ Alert
+		callback.run(); // COMPLIANT: called on a `Runnable` object
 	}
 }
