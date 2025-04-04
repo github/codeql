7da9eab3a4d84b77903cb8b616e0e5244bf7802f
Óscar San José <oscarsj@github.com>
2025-04-04 14:24:57 +0200

    Use sudo nice with more local testing servers, to avoid timeouts in macos-15

--- java/ql/integration-tests/java/buildless-dependency-different-repository/test.py
+++ java/ql/integration-tests/java/buildless-dependency-different-repository/test.py
@@ -1,11 +1,23 @@
 import subprocess
 import logging
+import runs_on
 
 
 def test(codeql, java):
     # Each of these serves the "repo" and "repo2" directories on http://localhost:924[89]
-    repo_server_process = subprocess.Popen(["python3", "-m", "http.server", "9428"], cwd="repo")
-    repo_server_process2 = subprocess.Popen(["python3", "-m", "http.server", "9429"], cwd="repo2")
+    command1 = ["python3", "-m", "http.server", "9428"]
+    command2 = ["python3", "-m", "http.server", "9429"]
+    if runs_on.github_actions and runs_on.posix:
+        # On GitHub Actions, we try to run the server with higher priority
+        sudo_prefix = ["sudo", "nice", "-n", "10"]
+        command1 = sudo_prefix + command1
+        command2 = sudo_prefix + command2
+    repo_server_process = subprocess.Popen(
+        command1, cwd="repo"
+    )
+    repo_server_process2 = subprocess.Popen(
+        command2, cwd="repo"
+    )
     try:
         codeql.database.create(
             extractor_option="buildless=true",
