971193ae44f0871825be86ef5e7527de55794e08
Asger F <asgerf@github.com>
2025-04-07 21:35:59 +0200

    More tests

---
+++ javascript/ql/test/library-tests/PathResolution/import-packages.ts
@@ -0,0 +1,24 @@
+import './PackageWithMain/main'; // $ importTarget=PackageWithMain/main.js
+import '@example/package-with-main'; // $ importTarget=PackageWithMain/main.js
+
+import './PackageWithModuleMain'; // $ importTarget=PackageWithModuleMain/main.js
+import '@example/package-with-module-main'; // $ importTarget=PackageWithModuleMain/main.js
+
+import './PackageWithExports'; // Not a valid import
+import './PackageWithExports/fake-file'; // Not a valid import
+import './PackageWithExports/star/foo'; // Not a valid import
+import '@example/package-with-exports'; // $ importTarget=PackageWithExports/main.js
+import '@example/package-with-exports/fake-file'; // $ importTarget=PackageWithExports/fake-file-impl.js
+import '@example/package-with-exports/star/foo'; // $ importTarget=PackageWithExports/star-impl/foo.js
+
+import './PackageIndexFile'; // $ importTarget=PackageIndexFile/index.js
+import '@example/package-with-index-file'; // $ importTarget=PackageIndexFile/index.js
+
+import './PackageGuess1'; // $ importTarget=PackageGuess1/src/index.ts
+import '@example/package-guess1'; // $ importTarget=PackageGuess1/src/index.ts
+
+import './PackageGuess2'; // $ MISSING: importTarget=PackageGuess2/blah/stuff.ts
+import '@example/package-guess2'; // $ importTarget=PackageGuess2/blah/stuff.ts
+
+import './PackageWithModuleMainExt'; // $ MISSING: importTarget=PackageWithModuleMainExt/lib/main.ts
+import '@example/package-with-module-main-ext'; // $ MISSING: importTarget=PackageWithModuleMainExt/lib/main.ts
