95ea1b25f4220f3a06b973156673ddadf9166bee
Asger F <asgerf@github.com>
2025-04-07 09:50:31 +0200

    JS: Add package.json with 'exports'

--- javascript/ql/test/library-tests/PathResolution/Basic/import-packages.ts
+++ javascript/ql/test/library-tests/PathResolution/Basic/import-packages.ts
@@ -1,5 +1,7 @@
 import '../PackageWithMain'; // $ MISSING: importTarget=PackageWithMain/main.js
 import '../PackageWithModuleMain'; // $ MISSING: importTarget=PackageWithModuleMain/main.js
+import '../PackageWithExports'; // $ MISSING: importTarget=PackageWithExports/main.js
 
 import '@example/package-with-main'; // $ importTarget=PackageWithMain/main.js
 import '@example/package-with-module-main'; // $ importTarget=PackageWithModuleMain/main.js
+import '@example/package-with-exports'; // $ importTarget=PackageWithExports/main.js
