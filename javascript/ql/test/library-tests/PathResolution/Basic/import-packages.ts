c8076645786909ca715f5d88bc8cf30f6c5fc6f5
Asger F <asgerf@github.com>
2025-04-07 09:28:56 +0200

    JS: Add test for importing package.json with main

---
+++ javascript/ql/test/library-tests/PathResolution/Basic/import-packages.ts
@@ -0,0 +1,5 @@
+import '../PackageWithMain'; // $ MISSING: importTarget=PackageWithMain/main.js
+import '../PackageWithModuleMain'; // $ MISSING: importTarget=PackageWithModuleMain/main.js
+
+import '@example/package-with-main'; // $ importTarget=PackageWithMain/main.js
+import '@example/package-with-module-main'; // $ importTarget=PackageWithModuleMain/main.js
