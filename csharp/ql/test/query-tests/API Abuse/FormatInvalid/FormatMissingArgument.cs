523fff39dc8d3cabc9820c50436b10b40d04ff81
Michael Nebel <michaelnebel@github.com>
2025-04-09 14:41:21 +0200

    C#: Add more invalid-string-formatting testcases.

--- csharp/ql/test/query-tests/API Abuse/FormatInvalid/FormatMissingArgument.cs
+++ csharp/ql/test/query-tests/API Abuse/FormatInvalid/FormatMissingArgument.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 class Class1
 {
@@ -28,5 +29,59 @@ class Class1
         String.Format(format, 0); // $ Alert Sink
     }
 
+    void TestCompositeFormatMissingArgument()
+    {
+        var format0 = CompositeFormat.Parse("{0}");
+        var format1 = CompositeFormat.Parse("{1}"); // $ Source
+        var format01 = CompositeFormat.Parse("{0}{1}");
+        var format23 = CompositeFormat.Parse("{2}{3}"); // $ Source
+
+        // GOOD: All args supplied
+        String.Format<string>(null, format0, "");
+
+        // BAD: Missing {1}
+        String.Format<string>(null, format1, ""); // $ Alert Sink
+
+        // GOOD: All args supplied
+        String.Format<string, string>(null, format01, "", "");
+
+        // BAD: Missing {2} and {3}
+        String.Format<string, string>(null, format23, "", ""); // $ Alert Sink
+
+
+        // GOOD: All arguments supplied
+        sb.AppendFormat(null, format0, "");
+        sb.AppendFormat<string>(null, format0, "");
+
+        // BAD: Missing {1}
+        sb.AppendFormat(null, format1, ""); // $ Alert Sink
+        sb.AppendFormat<string>(null, format1, ""); // $ Alert Sink
+
+        // GOOD: All args supplied
+        sb.AppendFormat<string, string>(null, format01, "", "");
+
+        // BAD: Missing {2} and {3}
+        sb.AppendFormat<string, string>(null, format23, "", ""); // $ Alert Sink
+
+
+        var span = new Span<char>();
+
+        // GOOD: All args supplied
+        span.TryWrite(null, format0, out _, "");
+        span.TryWrite<string>(null, format0, out _, "");
+
+        // BAD: Missing {1}
+        span.TryWrite(null, format1, out _, ""); // $ Alert Sink
+        span.TryWrite<string>(null, format1, out _, ""); // $ Alert Sink
+
+        // GOOD: All args supplied
+        span.TryWrite<string, string>(null, format01, out _, "", "");
+
+        // BAD: Missing {2} and {3}
+        span.TryWrite<string, string>(null, format23, out _, "", ""); // $ Alert Sink
+    }
+
     object[] args;
+
+    StringBuilder sb;
 }
