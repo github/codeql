171a84609e4ad54ae6cb6dc66fc680ee33883a59
Napalys <napalys@github.com>
2025-04-10 14:13:48 +0200

    Applied copilot suggestion.

--- javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/make-dir.js
+++ javascript/ql/test/query-tests/Security/CWE-022/TaintedPath/make-dir.js
@@ -3,9 +3,9 @@ import { makeDirectory, makeDirectorySync } from 'make-dir';
 const express = require('express');
 const app = express();
 
-app.get('/makedir', (req, res) => {
+app.get('/makedir', async (req, res) => {
     const file = req.query.file; // $ Source
 
-    makeDirectory(file); // $ Alert
+    await makeDirectory(file); // $ Alert
     makeDirectorySync(file); // $ Alert
 });
