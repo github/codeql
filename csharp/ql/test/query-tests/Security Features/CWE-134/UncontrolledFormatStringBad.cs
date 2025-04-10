197f91175ee455d7c066370a4bc7d4f598d3605d
Michael Nebel <michaelnebel@github.com>
2025-04-10 14:13:45 +0200

    C#: Convert cs/uncontrolled-format-string tests to use test inline expectations.

--- csharp/ql/test/query-tests/Security Features/CWE-134/UncontrolledFormatStringBad.cs
+++ csharp/ql/test/query-tests/Security Features/CWE-134/UncontrolledFormatStringBad.cs
@@ -6,9 +6,9 @@ public class HttpHandler : IHttpHandler
 
     public void ProcessRequest(HttpContext ctx)
     {
-        string format = ctx.Request.QueryString["nameformat"];
+        string format = ctx.Request.QueryString["nameformat"]; // $ Source
 
         // BAD: Uncontrolled format string.
-        FormattedName = string.Format(format, Surname, Forenames);
+        FormattedName = string.Format(format, Surname, Forenames); // $ Alert
     }
 }
