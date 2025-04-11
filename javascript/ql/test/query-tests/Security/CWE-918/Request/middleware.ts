8674b61e5a886af7bd8f4d1c91cd5ed4670291ca
Napalys <napalys@github.com>
2025-04-11 09:26:16 +0200

    Added SSRF test case with `searchParams` for `NextRequest`

--- javascript/ql/test/query-tests/Security/CWE-918/Request/middleware.ts
+++ javascript/ql/test/query-tests/Security/CWE-918/Request/middleware.ts
@@ -2,10 +2,16 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
     const target = req.nextUrl // $ Source[js/request-forgery]
+    const target2 = target.searchParams.get('target'); // $ MISSING: Source[js/request-forgery]
     if (target) {
       const res = await fetch(target) // $ Alert[js/request-forgery] Sink[js/request-forgery]
       const data = await res.text()
       return new NextResponse(data)
+    }
+    if (target2) {
+        const res = await fetch(target2); // $ MISSING: Alert[js/request-forgery] Sink[js/request-forgery]
+        const data = await res.text();
+        return new NextResponse(data);
     }
     return NextResponse.next()
 }
