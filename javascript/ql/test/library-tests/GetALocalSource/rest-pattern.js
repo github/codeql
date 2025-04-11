7703b1fab5211538d70ae2e4c994cab3c00a49a0
Asger F <asgerf@github.com>
2025-04-11 08:26:04 +0200

    JS: Add test for missing getALocalSource flow for rest pattern

---
+++ javascript/ql/test/library-tests/GetALocalSource/rest-pattern.js
@@ -0,0 +1,15 @@
+function t1() {
+    const { ...rest } = source('t1.1');
+    rest; // $ MISSING: getALocalSource=rest
+}
+
+function t2() {
+    const [ ...rest ] = source('t2.1');
+    rest; // $ MISSING: getALocalSource=rest
+}
+
+function t3() {
+    const { p1, ...rest } = source('t3.1');
+    p1; // $ getALocalSource=p1
+    rest; // $ MISSING: getALocalSource=rest
+}
