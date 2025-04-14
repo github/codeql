bdda2a7773c2257c29f34b3a36bdd76bbce71b62
Óscar San José <oscarsj@github.com>
2025-04-14 14:44:59 +0200

    Fix indentation in DoubleFreeBad.cpp comments

--- cpp/ql/src/Critical/DoubleFreeBad.cpp
+++ cpp/ql/src/Critical/DoubleFreeBad.cpp
@@ -4,9 +4,9 @@ int* f() {
 	do_stuff(buff);
 	free(buff);
 	int *new_buffer = malloc(SIZE*sizeof(int));
-	free(buff); // BAD: If new_buffer is assigned the same address as buff,
-              // the memory allocator will free the new buffer memory region,
-              // leading to use-after-free problems and memory corruption.
-			  
+	free(buff);
+	// BAD: If new_buffer is assigned the same address as buff,
+    // the memory allocator will free the new buffer memory region,
+    // leading to use-after-free problems and memory corruption.
 	return new_buffer;
 }
