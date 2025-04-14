60e250c3ae8434e0e5b591f4f1bd328b96094734
Sam Robson <sam-robson@github.com>
2025-04-14 15:01:46 +0100

    trigger checks

--- cpp/ql/src/Critical/DoubleFreeBad.cpp
+++ cpp/ql/src/Critical/DoubleFreeBad.cpp
@@ -5,6 +5,7 @@ int* f() {
 	free(buff);
 	int *new_buffer = malloc(SIZE*sizeof(int));
 	free(buff);
+
 	// BAD: If new_buffer is assigned the same address as buff,
     // the memory allocator will free the new buffer memory region,
     // leading to use-after-free problems and memory corruption.
