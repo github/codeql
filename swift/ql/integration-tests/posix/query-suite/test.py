4c9aee2cc7bc889cec3cba996bc05d214142f9c4
Tamas Vajk <tamasvajk@github.com>
2025-04-23 13:28:20 +0200

    Add query suite tests for swift with shared logic

--- swift/ql/integration-tests/posix/query-suite/test.py
+++ swift/ql/integration-tests/posix/query-suite/test.py
@@ -10,14 +10,14 @@ def get_test_module(semmle_code_dir):
     spec.loader.exec_module(mod)
     return mod
 
-well_known_query_suites = ['java-code-quality.qls', 'java-security-and-quality.qls', 'java-security-extended.qls', 'java-code-scanning.qls']
+
+well_known_query_suites = ['swift-code-quality.qls', 'swift-security-and-quality.qls', 'swift-security-extended.qls', 'swift-code-scanning.qls']
 
 @runs_on.posix
 @pytest.mark.parametrize("query_suite", well_known_query_suites)
-def test(codeql, java, cwd, expected_files, semmle_code_dir, query_suite):
+def test(codeql, swift, cwd, expected_files, semmle_code_dir, query_suite):
     get_test_module(semmle_code_dir).test(codeql, cwd, expected_files, semmle_code_dir, query_suite)
 
-
 @runs_on.posix
-def test_not_included_queries(codeql, java, cwd, expected_files, semmle_code_dir):
-    get_test_module(semmle_code_dir).test_not_included_queries(codeql, 'java', cwd, expected_files, semmle_code_dir, well_known_query_suites)
+def test_not_included_queries(codeql, swift, cwd, expected_files, semmle_code_dir):
+    get_test_module(semmle_code_dir).test_not_included_queries(codeql, 'swift', cwd, expected_files, semmle_code_dir, well_known_query_suites)
