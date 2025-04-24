522dd51416ec33b5be635a864d1c84e287c6fed3
Tamas Vajk <tamasvajk@github.com>
2025-04-24 08:35:48 +0200

    Improve query suite test based on feedback

--- swift/ql/integration-tests/posix/query-suite/test.py
+++ swift/ql/integration-tests/posix/query-suite/test.py
@@ -1,23 +1,14 @@
 import runs_on
 import pytest
-import sys
-
-def get_test_module(semmle_code_dir):
-    import importlib.util
-    spec = importlib.util.spec_from_file_location('test-module', semmle_code_dir / 'ql' / 'misc' / 'pytest' / 'lib' / 'query-suite-test.py')
-    mod = importlib.util.module_from_spec(spec)
-    sys.modules["test-module"] = mod
-    spec.loader.exec_module(mod)
-    return mod
-
+from query_suites import *
 
 well_known_query_suites = ['swift-code-quality.qls', 'swift-security-and-quality.qls', 'swift-security-extended.qls', 'swift-code-scanning.qls']
 
 @runs_on.posix
 @pytest.mark.parametrize("query_suite", well_known_query_suites)
-def test(codeql, swift, cwd, expected_files, semmle_code_dir, query_suite):
-    get_test_module(semmle_code_dir).test(codeql, cwd, expected_files, semmle_code_dir, query_suite)
+def test(codeql, swift, check_query_suite, query_suite):
+    check_query_suite(query_suite)
 
 @runs_on.posix
-def test_not_included_queries(codeql, swift, cwd, expected_files, semmle_code_dir):
-    get_test_module(semmle_code_dir).test_not_included_queries(codeql, 'swift', cwd, expected_files, semmle_code_dir, well_known_query_suites)
+def test_not_included_queries(codeql, swift, check_queries_not_included):
+    check_queries_not_included('swift', well_known_query_suites)
