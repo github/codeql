c6678949b9bd4f6bb77ce4a148713d7864f3ba5f
Mathias Vorreiter Pedersen <mapeder@microsoft.com>
2025-04-29 17:57:59 +0100

    PS: Add query test for the new SQL injection query.

---
+++ powershell/ql/test/query-tests/security/cwe-089/test.ps1
@@ -0,0 +1,56 @@
+$userinput = Read-Host "Please enter a value"
+
+# Example using Invoke-Sqlcmd with string interpolation
+$query = "SELECT * FROM MyTable WHERE MyColumn = '$userinput'"
+Invoke-Sqlcmd -ServerInstance "MyServer" -Database "MyDatabase" -Query $query # BAD
+
+# Example using Invoke-Sqlcmd with string concatenation
+$query = "SELECT * FROM MyTable WHERE " + $userinput
+Invoke-Sqlcmd -ServerInstance "MyServer" -Database "MyDatabase" -Query $query # BAD
+
+#Example using System.Data.SqlClient
+$connection = New-Object System.Data.SqlClient.SqlConnection
+$connection.ConnectionString = "Server=MyServer;Database=MyDatabase;"
+$connection.Open()
+
+$command = $connection.CreateCommand()
+$command.CommandText = "SELECT * FROM MyTable WHERE MyColumn = '$userinput'" # BAD
+$reader = $command.ExecuteReader()
+$reader.Close()
+$connection.Close()
+
+# Example using System.Data.OleDb
+$connection = New-Object System.Data.OleDb.OleDbConnection
+$connection.ConnectionString = "Provider=SQLOLEDB;Data Source=MyServer;Initial Catalog=MyDatabase;"
+$connection.Open()
+
+$command = $connection.CreateCommand()
+$command.CommandText = "SELECT * FROM MyTable WHERE MyColumn = '$userinput'" # BAD
+$reader = $command.ExecuteReader()
+$reader.Close()
+$connection.Close()
+
+# Example using System.Data.SqlClient with parameters
+$connection = New-Object System.Data.SqlClient.SqlConnection
+$connection.ConnectionString = "Server=MyServer;Database=MyDatabase;"
+$connection.Open()
+
+$command = $connection.CreateCommand()
+$command.CommandText = "SELECT * FROM MyTable WHERE MyColumn = @userinput"
+$parameter = $command.Parameters.Add("@userinput", [System.Data.SqlDbType]::NVarChar)
+$parameter.Value = $userinput # GOOD
+$reader = $command.ExecuteReader()
+$reader.Close()
+$connection.Close()
+
+# Example using System.Data.OleDb with parameters
+$connection = New-Object System.Data.OleDb.OleDbConnection
+$connection.ConnectionString = "Provider=SQLOLEDB;Data Source=MyServer;Initial Catalog=MyDatabase;" 
+$connection.Open()
+$command = $connection.CreateCommand()
+$command.CommandText = "SELECT * FROM MyTable WHERE MyColumn = ?"
+$parameter = $command.Parameters.Add("?", [System.Data.OleDb.OleDbType]::VarChar)
+$parameter.Value = $userinput # GOOD
+$reader = $command.ExecuteReader()
+$reader.Close()
+$connection.Close()
