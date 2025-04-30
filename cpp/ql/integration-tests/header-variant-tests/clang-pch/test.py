edd18dc0521aa972fcee2a66b1aebf7fc44348a7
Jeroen Ketema <jketema@github.com>
2025-04-30 16:23:06 +0200

    C++: Address review comment

--- cpp/ql/integration-tests/header-variant-tests/clang-pch/test.py
+++ cpp/ql/integration-tests/header-variant-tests/clang-pch/test.py
@@ -3,14 +3,15 @@ import os
 
 def test(codeql, cpp):
     os.mkdir("pch")
+    extractor = f"{cpp.get_tool("extractor")}" 
     codeql.database.create(command=[
-        f'"{cpp.get_tool("extractor")}" --mimic-clang -emit-pch -o pch/a.pch a.c',
-        f'"{cpp.get_tool("extractor")}" --mimic-clang -include-pch pch/a.pch -Iextra_dummy_path b.c',
-        f'"{cpp.get_tool("extractor")}" --mimic-clang -include pch/a -Iextra_dummy_path c.c',
-        f'"{cpp.get_tool("extractor")}" --mimic-clang -emit-pch -o pch/d.pch d.c',
-        f'"{cpp.get_tool("extractor")}" --mimic-clang -include-pch pch/d.pch e.c',
-        f'"{cpp.get_tool("extractor")}" --mimic-clang -emit-pch -o pch/f.pch f.c',
-        f'"{cpp.get_tool("extractor")}" --mimic-clang -include-pch pch/f.pch g.c',
-        f'"{cpp.get_tool("extractor")}" --mimic-clang -emit-pch -o pch/h.pch h.c',
-        f'"{cpp.get_tool("extractor")}" --mimic-clang -include-pch pch/h.pch i.c',
+        f'"{extractor}" --mimic-clang -emit-pch -o pch/a.pch a.c',
+        f'"{extractor}" --mimic-clang -include-pch pch/a.pch -Iextra_dummy_path b.c',
+        f'"{extractor}" --mimic-clang -include pch/a -Iextra_dummy_path c.c',
+        f'"{extractor}" --mimic-clang -emit-pch -o pch/d.pch d.c',
+        f'"{extractor}" --mimic-clang -include-pch pch/d.pch e.c',
+        f'"{extractor}" --mimic-clang -emit-pch -o pch/f.pch f.c',
+        f'"{extractor}" --mimic-clang -include-pch pch/f.pch g.c',
+        f'"{extractor}" --mimic-clang -emit-pch -o pch/h.pch h.c',
+        f'"{extractor}" --mimic-clang -include-pch pch/h.pch i.c',
     ])
--- cpp/ql/integration-tests/header-variant-tests/microsoft-pch/test.py
+++ cpp/ql/integration-tests/header-variant-tests/microsoft-pch/test.py
@@ -3,8 +3,9 @@ import os
 
 def test(codeql, cpp):
     os.mkdir("pch")
+    extractor = f"{cpp.get_tool("extractor")}" 
     codeql.database.create(command=[
-        f'"{cpp.get_tool("extractor")}" --mimic-cl /Yca.h /Fppch/a.pch a.c',
-        f'"{cpp.get_tool("extractor")}" --mimic-cl /Yub.h /Fppch/a.pch b.c',
-        f'"{cpp.get_tool("extractor")}" --mimic-cl /Yuc.h /Fppch/a.pch c.c',
+        f'"{extractor}" --mimic-cl /Yca.h /Fppch/a.pch a.c',
+        f'"{extractor}" --mimic-cl /Yub.h /Fppch/a.pch b.c',
+        f'"{extractor}" --mimic-cl /Yuc.h /Fppch/a.pch c.c',
     ])
