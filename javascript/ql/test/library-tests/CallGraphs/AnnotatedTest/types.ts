167f75230184edca81bc1d6392c5a3ef1f3b2b55
Asger F <asgerf@github.com>
2025-04-30 14:15:07 +0200

    JS: Also propagate through promise types

--- javascript/ql/test/library-tests/CallGraphs/AnnotatedTest/types.ts
+++ javascript/ql/test/library-tests/CallGraphs/AnnotatedTest/types.ts
@@ -14,3 +14,14 @@ function t1(c: NS.C, d: NS.D) {
   /** calls:NS.C.m */
   d.m();
 }
+
+async function t2(cp: Promise<NS.C>) {
+  const c = await cp;
+  /** calls:NS.C.m */
+  c.m();
+
+  cp.then(c2 => {
+    /** calls:NS.C.m */
+    c2.m();
+  })
+}
