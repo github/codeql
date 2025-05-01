e1fc0ca051d43b46aef2fd1adeeb3a5b3c25ed33
Napalys Klicius <napalys@github.com>
2025-05-01 14:01:33 +0200

    Added implementation `hdbcli` as part of `PEP249::PEP249ModuleApiNode`

--- python/ql/test/library-tests/frameworks/hdbcli/pep249.py
+++ python/ql/test/library-tests/frameworks/hdbcli/pep249.py
@@ -3,7 +3,7 @@ from hdbcli import dbapi
 conn = dbapi.connect(address="hostname", port=300, user="username", password="password")
 cursor = conn.cursor()
 
-cursor.execute("some sql", (42,))  # $ MISSING: getSql="some sql"
-cursor.executemany("some sql", (42,))  # $ MISSING: getSql="some sql"
+cursor.execute("some sql", (42,))  # $ getSql="some sql"
+cursor.executemany("some sql", (42,))  # $ getSql="some sql"
     
 cursor.close()
