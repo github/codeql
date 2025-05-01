a0c849139c1d8ec29b4d5f67c03da44c6cdda554
Anders Schack-Mulligen <aschackmull@github.com>
2025-05-01 13:51:13 +0200

    Java: Add guards-logic qltest with inline expectation.

---
+++ java/ql/test/library-tests/guards/Guards.java
@@ -0,0 +1,130 @@
+public class Guards {
+  static void chk() { }
+
+  static boolean g(Object lbl) { return lbl.hashCode() > 10; }
+
+  static void checkTrue(boolean b, String msg) {
+    if (!b) throw new Error(msg);
+  }
+
+  static void checkFalse(boolean b, String msg) {
+    checkTrue(!b, msg);
+  }
+
+  void t1(int[] a, String s) {
+    if (g("A")) {
+      chk(); // $ guarded=g(A):true
+    } else {
+      chk(); // $ guarded=g(A):false
+    }
+
+    boolean b = g(1) ? g(2) : true;
+    if (b != false) {
+      chk(); // $ guarded=...?...:...:true guarded='b != false:true' guarded=b:true
+    } else {
+      chk(); // $ guarded=...?...:...:false guarded='b != false:false' guarded=b:false guarded=g(1):true guarded=g(2):false
+    }
+    int sz = a != null ? a.length : 0;
+    for (int i = 0; i < sz; i++) {
+      chk(); // $ guarded='a != null:true' guarded='i < sz:true'
+      int e = a[i];
+      if (e > 2) break;
+    }
+    chk(); // nothing guards here
+
+    if (g(3))
+      s = "bar";
+    switch (s) {
+      case "bar":
+        chk(); // $ guarded='s:match "bar"'
+        break;
+      case "foo":
+        chk(); // $ guarded='s:match "foo"' guarded=g(3):false
+        break;
+      default:
+        chk(); // $ guarded='s:non-match "bar"' guarded='s:non-match "foo"' guarded='s:match default' guarded=g(3):false
+        break;
+    }
+
+    Object o = g(4) ? null : s;
+    if (o instanceof String) {
+      chk(); // $ guarded=...instanceof...:true guarded=g(4):false
+    }
+  }
+
+  void t2() {
+    checkTrue(g(1), "A");
+    checkFalse(g(2), "B");
+    chk(); // $ guarded=checkTrue(A):true guarded=g(1):true guarded=checkFalse(B):false guarded=g(2):false
+  }
+
+  void t3() {
+    boolean b = g(1) && (g(2) || g(3));
+    if (b) {
+      chk(); // $ guarded=b:true guarded='g(...) && ... \|\| ...:true' guarded=g(1):true guarded='g(...) \|\| g(...):true'
+    } else {
+      chk(); // $ guarded=b:false guarded='g(...) && ... \|\| ...:false'
+    }
+    b = g(4) || !g(5);
+    if (b) {
+      chk(); // $ guarded=b:true guarded='g(...) \|\| !...:true'
+    } else {
+      chk(); // $ guarded=b:false guarded='g(...) \|\| !...:false' guarded=g(4):false guarded=!...:false guarded=g(5):true
+    }
+  }
+
+  enum Val {
+    E1,
+    E2,
+    E3
+  }
+
+  void t4() {
+    Val x = null; // unique value
+    if (g(1)) x = Val.E1; // unique value
+    if (g(2)) x = Val.E2;
+    if (g("Alt2")) x = Val.E2;
+    if (g(3)) x = Val.E3; // unique value
+    if (x == null)
+      chk(); // $ guarded='x == null:true' guarded=g(3):false
+    switch (x) {
+      case E1:
+        chk(); // $ guarded='x:match E1' guarded=g(1):true guarded=g(3):false
+        break;
+      case E2:
+        chk(); // $ guarded='x:match E2' guarded=g(3):false
+        break;
+      case E3:
+        chk(); // $ guarded='x:match E3' guarded=g(3):true
+        break;
+    }
+    Object o = g(4) ? new Object() : null;
+    if (o == null) {
+      chk(); // $ guarded='o == null:true' guarded=g(4):false
+    } else {
+      chk(); // $ guarded='o == null:false' guarded=g(4):true
+    }
+  }
+
+  void t5(String foo) {
+    String base = foo;
+    if (base == null) {
+      base = "/user";
+    }
+    if (base.equals("/"))
+      chk(); // $ guarded=equals(/):true guarded='base == null:false'
+  }
+
+  void t6() {
+    Object o = null;
+    if (g(1)) {
+      o = new Object();
+      if (g(2)) { }
+    }
+    if (o != null) {
+      chk(); // $ guarded='o != null:true'
+    } else {
+      chk(); // $ guarded='o != null:false' guarded=g(1):false
+    }
+  }
+}
