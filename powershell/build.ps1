3dc74e9fdb1dadb10af4405f3620d51025a6d19a
Mathias Vorreiter Pedersen <mapeder@microsoft.com>
2025-05-01 12:36:11 +0100

    PS: Harden the build script so that you can run it from any folder.

--- powershell/build.ps1
+++ powershell/build.ps1
@@ -3,15 +3,15 @@ param (
 )
 
 $toolsWin64Folder = Join-Path (Join-Path (Join-Path $cliFolder "powershell") "tools") "win64"
-dotnet publish (Join-Path "extractor" "powershell.sln" | Resolve-Path) -o $toolsWin64Folder
+dotnet publish (Join-Path "$PSScriptRoot/extractor" "powershell.sln" | Resolve-Path) -o $toolsWin64Folder
 if ($LASTEXITCODE -ne 0) {
     Write-Host "Build failed"
     exit 1
 }
 
 $powershellFolder = Join-Path -Path $cliFolder -ChildPath "powershell"
-Copy-Item -Path codeql-extractor.yml -Destination $powershellFolder -Force
-$qlLibFolder = Join-Path -Path "ql" -ChildPath "lib"
+Copy-Item -Path "$PSScriptRoot/codeql-extractor.yml" -Destination $powershellFolder -Force
+$qlLibFolder = Join-Path -Path "$PSScriptRoot/ql" -ChildPath "lib"
 Copy-Item -Path (Join-Path $qlLibFolder "semmlecode.powershell.dbscheme") -Destination $powershellFolder -Force
 Copy-Item -Path (Join-Path $qlLibFolder "semmlecode.powershell.dbscheme.stats") -Destination $powershellFolder -Force
-Copy-Item -Path "tools" -Destination $powershellFolder -Recurse -Force
+Copy-Item -Path "$PSScriptRoot/tools" -Destination $powershellFolder -Recurse -Force
