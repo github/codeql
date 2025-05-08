0066f74d3fe7f31665f78ae43faaf5b6a20d12c8
Nicolas Will <nicolaswill@github.com>
2025-05-08 02:40:56 +0200

    Delete development scripts

--- misc/scripts/cryptography/cbom.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CODEQL_PATH="/Users/nicolaswill/.local/share/gh/extensions/gh-codeql/dist/release/v2.20.4/codeql"
-DATABASE_PATH="/Users/nicolaswill/pqc/gpt-crypto-test-cases/gpt_ai_gen_jca_test_cases_db"
-QUERY_FILE="/Users/nicolaswill/pqc/codeql/java/ql/src/experimental/Quantum/PrintCBOMGraph.ql"
-OUTPUT_DIR="graph_output"
-
-python3 generate_cbom.py -c "$CODEQL_PATH" -d "$DATABASE_PATH" -q "$QUERY_FILE" -o "$OUTPUT_DIR"
