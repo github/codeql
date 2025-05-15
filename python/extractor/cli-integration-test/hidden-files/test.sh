96558b53b89fb8a72d087db5d55b6ce64848953d
Taus <tausbn@github.com>
2025-05-15 14:53:15 +0000

    Python: Update test
    The second test case now sets the `paths-ignore` setting in the config
    file in order to skip files in hidden directories.

--- python/extractor/cli-integration-test/hidden-files/test.sh
+++ python/extractor/cli-integration-test/hidden-files/test.sh
@@ -16,8 +16,8 @@ $CODEQL database create db --language python --source-root repo_dir/
 $CODEQL query run --database db query.ql > query-default.actual
 diff query-default.expected query-default.actual
 
-# Test 2: Setting the relevant extractor option to true skips files in hidden directories
-$CODEQL database create db-skipped --language python --source-root repo_dir/ --extractor-option python.skip_hidden_directories=true
+# Test 2: The default behavior can be overridden by setting `paths-ignore` in the config file
+$CODEQL database create db-skipped --language python --source-root repo_dir/ --codescanning-config=config.yml
 $CODEQL query run --database db-skipped query.ql > query-skipped.actual
 diff query-skipped.expected query-skipped.actual
 
