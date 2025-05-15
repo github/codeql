4b2d323cb6028209e48faf9bc484f7cf2e6e3a13
Michael Nebel <michaelnebel@github.com>
2025-05-15 13:44:12 +0200

    C#: Add some more test cases.

--- csharp/ql/test/query-tests/Likely Bugs/HashedButNoHash/HashedButNoHash.cs
+++ csharp/ql/test/query-tests/Likely Bugs/HashedButNoHash/HashedButNoHash.cs
@@ -7,9 +7,26 @@ public class Test
     {
         var h = new Hashtable();
         h.Add(this, null); // $ Alert
+        h.Contains(this); // $ Alert
+        h.ContainsKey(this); // $ Alert
+        h[this] = null; // $ Alert
+        h.Remove(this); // $ Alert
+
+        var l = new List<Test>();
+        l.Add(this); // Good
 
         var d = new Dictionary<Test, bool>();
         d.Add(this, false); // $ Alert
+        d.ContainsKey(this); // $ Alert
+        d[this] = false; // $ Alert
+        d.Remove(this); // $ Alert
+        d.TryAdd(this, false); // $ Alert
+        d.TryGetValue(this, out bool _); // $ Alert
+
+        var hs = new HashSet<Test>();
+        hs.Add(this); // $ Alert
+        hs.Contains(this); // $ Alert
+        hs.Remove(this); // $ Alert
     }
 
     public override bool Equals(object other)
