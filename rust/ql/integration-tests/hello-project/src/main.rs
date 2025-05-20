a3e32023c587d3b5faf1ce5be4e1708c64b930a9
Paolo Tranquilli <redsun82@gihub.com>
2025-05-20 11:16:31 +0200

    Rust: show unexpanded macro

--- rust/ql/integration-tests/hello-project/src/main.rs
+++ rust/ql/integration-tests/hello-project/src/main.rs
@@ -1,6 +1,8 @@
+#![doc = include_str!("./README.md")]  // <- rust-analyzer 0.0.274 is not expanding this
+
 mod file_module;
 mod directory_module;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello world!");
 }
