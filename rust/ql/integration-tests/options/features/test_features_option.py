ca661c7877470bd7d4acc7f18834da31f226e75e
Paolo Tranquilli <redsun82@gihub.com>
2025-05-21 15:15:03 +0200

    Rust: use all features by default

--- rust/ql/integration-tests/options/features/test_features_option.py
+++ rust/ql/integration-tests/options/features/test_features_option.py
@@ -1,5 +1,6 @@
 import pytest
 
+@pytest.mark.ql_test(expected=".all.expected")
 def test_default(codeql, rust):
     codeql.database.create()
 
@@ -8,10 +9,33 @@ def test_default(codeql, rust):
                              pytest.param(p,
                                           marks=pytest.mark.ql_test(expected=f".{e}.expected"))
                              for p, e in (
+                                 ("default", "none"),
                                  ("foo", "foo"),
                                  ("bar", "bar"),
                                  ("*", "all"),
-                                 ("foo,bar", "all"))
+                                 ("foo,bar", "all"),
+                                 ("default,foo", "foo"),
+                                 ("default,bar", "bar"),
+                             )
                          ])
 def test_features(codeql, rust, features):
+    codeql.database.create(extractor_option=f"cargo_features={features}")
+
+@pytest.mark.parametrize("features",
+                         [
+                             pytest.param(p,
+                                          marks=pytest.mark.ql_test(expected=f".{e}.expected"))
+                             for p, e in (
+                                 ("default", "foo"),
+                                 ("foo", "foo"),
+                                 ("bar", "bar"),
+                                 ("*", "all"),
+                                 ("foo,bar", "all"),
+                                 ("default,foo", "foo"),
+                                 ("default,bar", "all"),
+                             )
+                         ])
+def test_features_with_deault(codeql, rust, features):
+    with open("Cargo.toml", "a") as f:
+        print('default = ["foo"]', file=f)
     codeql.database.create(extractor_option=f"cargo_features={features}")
