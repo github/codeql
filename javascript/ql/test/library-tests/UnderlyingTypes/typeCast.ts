79101fd1210aae83ce0ed97d39feeb7f0c5432a7
Asger F <asgerf@github.com>
2025-06-04 22:07:03 +0200

    JS: Add test with type casts

---
+++ javascript/ql/test/library-tests/UnderlyingTypes/typeCast.ts
@@ -0,0 +1,7 @@
+import * as express from 'express';
+
+function t1(e) {
+    var req1 = e as express.Request; // $ hasUnderlyingType='express'.Request
+    var req2 = <express.Request>e; // $ hasUnderlyingType='express'.Request
+    var req3 = e satisfies express.Request;
+}
