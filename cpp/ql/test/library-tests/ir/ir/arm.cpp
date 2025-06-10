a4be27659bc708f210d76cb1280f659433122b7f
Jeroen Ketema <jketema@github.com>
2025-06-10 15:55:18 +0200

    C++: Minor test case reformatting

--- cpp/ql/test/library-tests/ir/ir/arm.cpp
+++ cpp/ql/test/library-tests/ir/ir/arm.cpp
@@ -3,21 +3,19 @@
 typedef __Uint8x8_t uint8x8_t;
 typedef __Uint16x8_t uint16x8_t;
 
-uint8x8_t vadd_u8 (uint8x8_t __a, uint8x8_t __b)
-{
-  return __a + __b;
+uint8x8_t vadd_u8(uint8x8_t a, uint8x8_t b) {
+  return a + b;
 }
 
 // Workaround: the frontend only exposes this when the arm_neon.h
 // header is encountered.
 uint16x8_t __builtin_aarch64_uaddlv8qi_uuu(uint8x8_t, uint8x8_t);
 
-uint16x8_t vaddl_u8 (uint8x8_t __a, uint8x8_t __b)
-{
-  return __builtin_aarch64_uaddlv8qi_uuu (__a, __b);
+uint16x8_t vaddl_u8(uint8x8_t a, uint8x8_t b) {
+  return __builtin_aarch64_uaddlv8qi_uuu (a, b);
 }
 
 uint16x8_t arm_add(uint8x8_t a, uint8x8_t b) {
-    uint8x8_t c = vadd_u8(a, b);
-    return vaddl_u8(a, c);
+  uint8x8_t c = vadd_u8(a, b);
+  return vaddl_u8(a, c);
 }
