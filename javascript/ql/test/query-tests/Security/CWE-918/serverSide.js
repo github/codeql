deb715a5177976063033534966d48fee0475597b
Napalys Klicius <napalys@github.com>
2025-06-13 11:15:16 +0200

    JS: Add test case with `encodeURI` for request forgery

--- javascript/ql/test/query-tests/Security/CWE-918/serverSide.js
+++ javascript/ql/test/query-tests/Security/CWE-918/serverSide.js
@@ -141,4 +141,8 @@ var server2 = http.createServer(function(req, res) {
   axios.get(target.toString()); // $Alert[js/request-forgery]
   axios.get(target); // $Alert[js/request-forgery]
   axios.get(target.href); // $Alert[js/request-forgery]
+  const encodedUrl = encodeURI(input);
+  axios.get(encodedUrl); // $MISSING:Alert[js/request-forgery]
+  const escapedUrl = escape(input);
+  axios.get(escapedUrl); // $MISSING:Alert[js/request-forgery]
 });
