060b98d36cecc0ad9eea1423452753bdc19e1ce3
Napalys Klicius <napalys@github.com>
2025-06-16 16:31:17 +0200

    JS: enchance middleware taint tracking via local source

--- javascript/ql/test/query-tests/Security/CWE-918/serverSide2.js
+++ javascript/ql/test/query-tests/Security/CWE-918/serverSide2.js
@@ -7,14 +7,14 @@ const PORT = 3000;
 
 app.use((req, res, next) => {
   req.parsedQueryFromParsedUrl = qs.parse(req._parsedUrl.query);  // $Source[js/request-forgery]
-  req.parsedQuery.url = req.url || {}; // $MISSING:Source[js/request-forgery]
+  req.parsedQuery.url = req.url || {}; // $Source[js/request-forgery]
   req.SomeObject.url = req.url; // $Source[js/request-forgery]
   next();
 });
 
 app.get('/proxy', async (req, res) => {
     const targetUrl = req.parsedQuery.url;  
-    const response = await axios.get(targetUrl); // $MISSING:Alert[js/request-forgery]
+    const response = await axios.get(targetUrl); // $Alert[js/request-forgery]
 
     const targetUrl1 = req.parsedQueryFromParsedUrl.url;  
     const response1 = await axios.get(targetUrl1); // $Alert[js/request-forgery]
