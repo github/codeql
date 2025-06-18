5cd729545d5e3dbd7e8360dd4c9b23b5771bb854
Tom Hvitved <hvitved@github.com>
2025-06-18 09:22:32 +0200

    Rust: Extend jump-to-def query with method calls

--- rust/ql/test/library-tests/definitions/main.rs
+++ rust/ql/test/library-tests/definitions/main.rs
@@ -2,6 +2,18 @@ struct S;
 
 mod lib;
 
+mod M1 {
+    pub mod M2 {
+        pub struct S;
+
+        impl S {
+            pub fn method(self) -> Self {
+                S
+            }
+        }
+    }
+}
+
 fn main() {
     let width = 4;
     let precision = 2;
@@ -14,4 +26,6 @@ fn main() {
     println!("{1} {} {0} {}", 1, 2);
     assert_eq!(format!("Hello {:<5}!", "x"), "Hello x    !");
     let x = S;
+    let s = M1::M2::S;
+    s.method();
 }
