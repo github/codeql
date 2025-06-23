162b1c51a9db15fa7d4c1a2ae792bb0eb1feb14e
Nora Dimitrijević <d10c@users.noreply.github.com>
2025-06-23 17:59:18 +0200

    Java: convert XXE test to .qlref

--- java/ql/test/query-tests/security/CWE-611/SimpleXMLTests.java
+++ java/ql/test/query-tests/security/CWE-611/SimpleXMLTests.java
@@ -11,145 +11,145 @@ public class SimpleXMLTests {
 
   public void persisterValidate1(Socket sock) throws Exception {
     Persister persister = new Persister();
-    persister.validate(this.getClass(), sock.getInputStream()); // $ hasTaintFlow
+    persister.validate(this.getClass(), sock.getInputStream()); // $ Alert
   }
 
   public void persisterValidate2(Socket sock) throws Exception {
     Persister persister = new Persister();
-    persister.validate(this.getClass(), sock.getInputStream(), true); // $ hasTaintFlow
+    persister.validate(this.getClass(), sock.getInputStream(), true); // $ Alert
   }
 
   public void persisterValidate3(Socket sock) throws Exception {
     Persister persister = new Persister();
-    persister.validate(this.getClass(), new InputStreamReader(sock.getInputStream())); // $ hasTaintFlow
+    persister.validate(this.getClass(), new InputStreamReader(sock.getInputStream())); // $ Alert
   }
 
   public void persisterValidate4(Socket sock) throws Exception {
     Persister persister = new Persister();
     byte[] b = new byte[] {};
-    sock.getInputStream().read(b);
-    persister.validate(this.getClass(), new String(b)); // $ hasTaintFlow
+    sock.getInputStream().read(b); // $ Source
+    persister.validate(this.getClass(), new String(b)); // $ Alert
   }
 
   public void persisterValidate5(Socket sock) throws Exception {
     Persister persister = new Persister();
     byte[] b = new byte[] {};
-    sock.getInputStream().read(b);
-    persister.validate(this.getClass(), new String(b), true); // $ hasTaintFlow
+    sock.getInputStream().read(b); // $ Source
+    persister.validate(this.getClass(), new String(b), true); // $ Alert
   }
 
   public void persisterValidate6(Socket sock) throws Exception {
     Persister persister = new Persister();
-    persister.validate(this.getClass(), new InputStreamReader(sock.getInputStream()), true); // $ hasTaintFlow
+    persister.validate(this.getClass(), new InputStreamReader(sock.getInputStream()), true); // $ Alert
   }
 
   public void persisterRead1(Socket sock) throws Exception {
     Persister persister = new Persister();
-    persister.read(this.getClass(), sock.getInputStream()); // $ hasTaintFlow
+    persister.read(this.getClass(), sock.getInputStream()); // $ Alert
   }
 
   public void persisterRead2(Socket sock) throws Exception {
     Persister persister = new Persister();
-    persister.read(this.getClass(), sock.getInputStream(), true); // $ hasTaintFlow
+    persister.read(this.getClass(), sock.getInputStream(), true); // $ Alert
   }
 
   public void persisterRead3(Socket sock) throws Exception {
     Persister persister = new Persister();
-    persister.read(this, sock.getInputStream()); // $ hasTaintFlow
+    persister.read(this, sock.getInputStream()); // $ Alert
   }
 
   public void persisterRead4(Socket sock) throws Exception {
     Persister persister = new Persister();
-    persister.read(this, sock.getInputStream(), true); // $ hasTaintFlow
+    persister.read(this, sock.getInputStream(), true); // $ Alert
   }
 
   public void persisterRead5(Socket sock) throws Exception {
     Persister persister = new Persister();
-    persister.read(this.getClass(), new InputStreamReader(sock.getInputStream())); // $ hasTaintFlow
+    persister.read(this.getClass(), new InputStreamReader(sock.getInputStream())); // $ Alert
   }
 
   public void persisterRead6(Socket sock) throws Exception {
     Persister persister = new Persister();
-    persister.read(this.getClass(), new InputStreamReader(sock.getInputStream()), true); // $ hasTaintFlow
+    persister.read(this.getClass(), new InputStreamReader(sock.getInputStream()), true); // $ Alert
   }
 
   public void persisterRead7(Socket sock) throws Exception {
     Persister persister = new Persister();
-    persister.read(this, new InputStreamReader(sock.getInputStream())); // $ hasTaintFlow
+    persister.read(this, new InputStreamReader(sock.getInputStream())); // $ Alert
   }
 
   public void persisterRead8(Socket sock) throws Exception {
     Persister persister = new Persister();
-    persister.read(this, new InputStreamReader(sock.getInputStream()), true); // $ hasTaintFlow
+    persister.read(this, new InputStreamReader(sock.getInputStream()), true); // $ Alert
   }
 
   public void persisterRead9(Socket sock) throws Exception {
     Persister persister = new Persister();
     byte[] b = new byte[] {};
-    sock.getInputStream().read(b);
-    persister.read(this.getClass(), new String(b)); // $ hasTaintFlow
+    sock.getInputStream().read(b); // $ Source
+    persister.read(this.getClass(), new String(b)); // $ Alert
   }
 
   public void persisterRead10(Socket sock) throws Exception {
     Persister persister = new Persister();
     byte[] b = new byte[] {};
-    sock.getInputStream().read(b);
-    persister.read(this.getClass(), new String(b), true); // $ hasTaintFlow
+    sock.getInputStream().read(b); // $ Source
+    persister.read(this.getClass(), new String(b), true); // $ Alert
   }
 
   public void persisterRead11(Socket sock) throws Exception {
     Persister persister = new Persister();
     byte[] b = new byte[] {};
-    sock.getInputStream().read(b);
-    persister.read(this, new String(b)); // $ hasTaintFlow
+    sock.getInputStream().read(b); // $ Source
+    persister.read(this, new String(b)); // $ Alert
   }
 
   public void persisterRead12(Socket sock) throws Exception {
     Persister persister = new Persister();
     byte[] b = new byte[] {};
-    sock.getInputStream().read(b);
-    persister.read(this, new String(b), true); // $ hasTaintFlow
+    sock.getInputStream().read(b); // $ Source
+    persister.read(this, new String(b), true); // $ Alert
   }
 
   public void nodeBuilderRead1(Socket sock) throws Exception {
-    NodeBuilder.read(sock.getInputStream()); // $ hasTaintFlow
+    NodeBuilder.read(sock.getInputStream()); // $ Alert
   }
 
   public void nodeBuilderRead2(Socket sock) throws Exception {
-    NodeBuilder.read(new InputStreamReader(sock.getInputStream())); // $ hasTaintFlow
+    NodeBuilder.read(new InputStreamReader(sock.getInputStream())); // $ Alert
   }
 
   public void documentProviderProvide1(Socket sock) throws Exception {
     DocumentProvider provider = new DocumentProvider();
-    provider.provide(sock.getInputStream()); // $ hasTaintFlow
+    provider.provide(sock.getInputStream()); // $ Alert
   }
 
   public void documentProviderProvide2(Socket sock) throws Exception {
     DocumentProvider provider = new DocumentProvider();
-    provider.provide(new InputStreamReader(sock.getInputStream())); // $ hasTaintFlow
+    provider.provide(new InputStreamReader(sock.getInputStream())); // $ Alert
   }
 
   public void streamProviderProvide1(Socket sock) throws Exception {
     StreamProvider provider = new StreamProvider();
-    provider.provide(sock.getInputStream()); // $ hasTaintFlow
+    provider.provide(sock.getInputStream()); // $ Alert
   }
 
   public void streamProviderProvide2(Socket sock) throws Exception {
     StreamProvider provider = new StreamProvider();
-    provider.provide(new InputStreamReader(sock.getInputStream())); // $ hasTaintFlow
+    provider.provide(new InputStreamReader(sock.getInputStream())); // $ Alert
   }
 
   public void formatterFormat1(Socket sock) throws Exception {
     Formatter formatter = new Formatter();
     byte[] b = new byte[] {};
-    sock.getInputStream().read(b);
-    formatter.format(new String(b), null); // $ hasTaintFlow
+    sock.getInputStream().read(b); // $ Source
+    formatter.format(new String(b), null); // $ Alert
   }
 
   public void formatterFormat2(Socket sock) throws Exception {
     Formatter formatter = new Formatter();
     byte[] b = new byte[] {};
-    sock.getInputStream().read(b);
-    formatter.format(new String(b)); // $ hasTaintFlow
+    sock.getInputStream().read(b); // $ Source
+    formatter.format(new String(b)); // $ Alert
   }
 }
