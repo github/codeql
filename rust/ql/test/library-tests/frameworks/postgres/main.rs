ffaaa5d9508c850189395b7e84981e07e25c118a
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2025-07-01 17:31:15 +0100

    Rust: Accept test changes.

--- rust/ql/test/library-tests/frameworks/postgres/main.rs
+++ rust/ql/test/library-tests/frameworks/postgres/main.rs
@@ -16,7 +16,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         )",
         &[],
     )?;
-    
+
     let query = format!("INSERT INTO person (name, age) VALUES ('{}', '{}')", name, age);
 
     conn.execute(query.as_str(), &[])?;     // $ sql-sink
@@ -33,9 +33,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // conn.query_typed_raw(query.as_str(), &[])?;
 
     for row in &conn.query("SELECT id, name, age FROM person", &[])? {  // $ sql-sink
-        let id: i32 = row.get("id");        // $ database-read
-        let name: &str = row.try_get("name")?;   // $ database-read
-        let age: i32 = row.try_get("age").unwrap();      // $ database-read
+        let id: i32 = row.get("id");        // $ MISSING: database-read
+        let name: &str = row.try_get("name")?;   // $ MISSING: database-read
+        let age: i32 = row.try_get("age").unwrap();      // $ MISSING: database-read
         println!("found person: {} {} {}", id, name, age);
     }
 
