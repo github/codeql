8ba7efd4556a7b33fd8631b013cbb174fe5fc8ae
Paolo Tranquilli <redsun82@github.com>
2025-07-08 15:41:18 +0200

    Just: fix mono-argument case for argumentless recipes

--- misc/just/forward-command.ts
+++ misc/just/forward-command.ts
@@ -6,8 +6,8 @@ function commonJustfile(paths: string[]): string {
     if (paths.length === 0) return "";
     const splitPaths = paths.map((p) => p.split(path.sep));
     let justfile: string | undefined = undefined;
-    for (let i = 0; i < splitPaths[0].length; i++) {
-        let candidate = path.resolve(path.join(splitPaths[0].slice(0, i).join(path.sep), "justfile"));
+    for (let i = 0; i <= splitPaths[0].length; i++) {
+        let candidate = path.join(splitPaths[0].slice(0, i).join(path.sep), "justfile");
         if (fs.existsSync(candidate)) {
             justfile = candidate;
         }
@@ -42,25 +42,34 @@ function forwardCommand(args: string[]): number {
         (arg) => !is_non_positional.test(arg),
     );
 
-    if (positionalArgs.length === 0) {
-        console.error("No positional arguments provided");
-        return 1;
+    const justfile = commonJustfile(positionalArgs.length !== 0 ? positionalArgs : ["."]);
+    let cwd: string | undefined;
+    let invocation = [process.env["JUST_EXECUTABLE"] || "just"];
+    if (positionalArgs.length === 1 && justfile == path.join(positionalArgs[0], "justfile")) {
+        // If there's only one positional argument and it matches the justfile path, suppress arguments
+        // so for example `just build ql/rust` becomes `just build` in the `ql/rust` directory
+        cwd = positionalArgs[0];
+        invocation.push(
+            cmd,
+            ...flags,
+        );
+        console.log(`-> cd ${cwd}; just ${invocation.slice(1).join(" ")}`);
+    } else {
+        cwd = undefined;
+        invocation.push(
+            "--justfile",
+            justfile,
+            cmd,
+            ...flags,
+            ...positionalArgs,
+        );
+        console.log(`-> just ${invocation.slice(1).join(" ")}`);
     }
 
-    const justfile = commonJustfile(positionalArgs);
-
-    const invocation = [
-        process.env["JUST_EXECUTABLE"] || "just",
-        "--justfile",
-        justfile,
-        cmd,
-        ...flags,
-        ...positionalArgs,
-    ];
-    console.log(`-> just ${invocation.slice(1).join(" ")}`);
     try {
         child_process.execFileSync(invocation[0], invocation.slice(1), {
             stdio: "inherit",
+            cwd,
         });
     } catch (error) {
         return 1;
