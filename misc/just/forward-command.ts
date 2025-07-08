c4305151c3274f63e17c7e8ac8177fcd0adfb0ce
Paolo Tranquilli <redsun82@github.com>
2025-07-08 10:52:15 +0200

    Just: simplify forwarder using `--justfile`

--- misc/just/forward-command.ts
+++ misc/just/forward-command.ts
@@ -2,24 +2,23 @@ import * as child_process from "child_process";
 import * as path from "path";
 import * as fs from "fs";
 
-function commonDir(paths: string[]): string {
+function commonJustfile(paths: string[]): string {
     if (paths.length === 0) return "";
     const splitPaths = paths.map((p) => p.split(path.sep));
-    let i;
-    for (i = 0; i < splitPaths[0].length; i++) {
+    let justfile: string | undefined = undefined;
+    for (let i = 0; i < splitPaths[0].length; i++) {
+        let candidate = path.join(...splitPaths[0].slice(0, i), "justfile");
+        if (fs.existsSync(candidate)) {
+            justfile = candidate;
+        }
         if (!splitPaths.every((parts) => parts[i] === splitPaths[0][i])) {
             break;
         }
     }
-    const commonParts = splitPaths[0].slice(0, i);
-    let ret = commonParts.join(path.sep);
-    if (!fs.existsSync(ret)) {
-        throw new Error(`Common directory does not exist: ${ret}`);
+    if (justfile === undefined) {
+        throw new Error("No common justfile found");
     }
-    if (!fs.lstatSync(ret).isDirectory()) {
-        ret = path.dirname(ret);
-    }
-    return ret;
+    return justfile;
 }
 
 function forwardCommand(args: string[]): number {
@@ -48,40 +47,20 @@ function forwardCommand(args: string[]): number {
         return 1;
     }
 
-    const commonPath = commonDir(positionalArgs);
-    let relativeArgs = positionalArgs.map(
-        (arg) => path.relative(commonPath, arg) || ".",
-    );
-    if (relativeArgs.length === 1 && relativeArgs[0] === ".") {
-        relativeArgs = [];
-    }
-    let relativeFlags = flags.map((arg) => {
-        // this might break in specific corner cases, but is good enough for most uses
-        // workaround if this doesn't work is to not use the forwarder (call just directly in the relevant directory)
-        if (arg.includes("=") && arg.includes(path.sep)) {
-            let [flags, flag_arg] = arg.split("=", 2);
-            flag_arg = flag_arg
-                .split(path.delimiter)
-                .map((p) =>
-                    path.isAbsolute(p) ? p : path.relative(commonPath, p),
-                )
-                .join(path.delimiter);
-            return `${flags}=${flag_arg}`;
-        }
-        return arg;
-    });
+    const justfile = commonJustfile(positionalArgs);
 
     const invocation = [
         process.env["JUST_EXECUTABLE"] || "just",
+        "--justfile",
+        justfile,
         cmd,
-        ...relativeFlags,
-        ...relativeArgs,
+        ...flags,
+        ...positionalArgs,
     ];
-    console.log(`-> ${commonPath}: just ${invocation.slice(1).join(" ")}`);
+    console.log(`-> ${justfile}: ${invocation.slice(1).join(" ")}`);
     try {
         child_process.execFileSync(invocation[0], invocation.slice(1), {
             stdio: "inherit",
-            cwd: commonPath,
         });
     } catch (error) {
         return 1;
