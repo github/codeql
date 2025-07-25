c0da9c407e12535984b98370f84a4f176bb17b34
Joe Farebrother <joefarebrother@github.com>
2025-07-25 13:15:46 +0100

    Fix typo in test dir name + update examples

--- python/ql/src/Functions/examples/IncorrectRaiseInSpecialMethod.py
+++ python/ql/src/Functions/examples/IncorrectRaiseInSpecialMethod.py
@@ -5,7 +5,7 @@ class A:
     def __add__(self, other):
         # BAD: Should return NotImplemented instead of raising
         if not isinstance(other,A):
-            raise TypeError(f"Cannot add A to {other.__type__}")
+            raise TypeError(f"Cannot add A to {other.__class__}")
         return A(self.a + other.a)
 
 class B:
@@ -13,7 +13,7 @@ class B:
         self.a = a 
 
     def __add__(self, other):
-        # GOOD: Returning NotImplemented allows for other classes to support adding do B.
+        # GOOD: Returning NotImplemented allows for the operation to fallback to other implementations to allow other classes to support adding to B.
         if not isinstance(other,B):
             return NotImplemented
         return B(self.a + other.a)
--- python/ql/src/Functions/examples/IncorrectRaiseInSpecialMethod3.py
+++ python/ql/src/Functions/examples/IncorrectRaiseInSpecialMethod3.py
@@ -1,4 +1,4 @@
 class D:
     def __hash__(self):
         # BAD: Use `__hash__ = None` instead.
-        raise NotImplementedError(f"{self.__type__} is unhashable.")
+        raise NotImplementedError(f"{self.__class__} is unhashable.")
