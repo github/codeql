ee1af432fe2a62a99f91918faa95a2c28836023b
Napalys Klicius <napalys@github.com>
2025-07-29 09:52:49 +0200

    Added modeling of client-s3 v2 and v3

--- javascript/ql/test/query-tests/Security/CWE-089/untyped/clients3.js
+++ javascript/ql/test/query-tests/Security/CWE-089/untyped/clients3.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/client/v3/execute', async (req, res) => {
-    let maliciousInput = req.body.filter; // $ MISSING: Source
+    let maliciousInput = req.body.filter; // $ Source
     const client = new S3Client({ region: "us-east-1" });
     const params = {
         Bucket: "my-bucket",
@@ -15,18 +15,18 @@ app.post('/client/v3/execute', async (req, res) => {
         ExpressionType: "SQL",
         Expression: "SELECT * FROM S3Object WHERE " + maliciousInput,
     };
-    await client.send(new SelectObjectContentCommand(params)); // $ MISSING: Alert
+    await client.send(new SelectObjectContentCommand(params)); // $ Alert
     res.end();
 });
 
 app.post('/client/v2/execute', async (req, res) => {
-    let maliciousInput = req.body.filter; // $ MISSING: Source
+    let maliciousInput = req.body.filter; // $ Source
     const s3 = new AWS.S3({ region: "us-east-1" });
     const params = {
         Bucket: "my-bucket",
         Key: "data.csv",
         ExpressionType: "SQL",
-        Expression: "SELECT * FROM S3Object WHERE " + maliciousInput, // $ MISSING: Alert
+        Expression: "SELECT * FROM S3Object WHERE " + maliciousInput, // $ Alert
     };
     await s3.selectObjectContent(params).promise();
     res.end();
@@ -35,7 +35,7 @@ app.post('/client/v2/execute', async (req, res) => {
         Bucket: "my-bucket",
         Key: "data.csv",
         ExpressionType: "SQL",
-        Expression: "SELECT * FROM S3Object WHERE " + maliciousInput, // $ MISSING: Alert
+        Expression: "SELECT * FROM S3Object WHERE " + maliciousInput, // $ Alert
     };
 
     s3.selectObjectContent(params1, (err, data) => {
