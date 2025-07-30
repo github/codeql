34317d2d4ad66e2f5cd33a10a92389116ef5a2f1
Joe Farebrother <joefarebrother@github.com>
2025-07-30 13:24:43 +0100

    Update documentation

--- python/ql/src/Classes/SubclassShadowing.py
+++ python/ql/src/Classes/SubclassShadowing.py
@@ -1,17 +1,9 @@
-class Mammal(object):
-
-    def __init__(self, milk = 0):
-        self.milk = milk
-
-
-class Cow(Mammal):
-
+class A:
     def __init__(self):
-        Mammal.__init__(self)
+        self._foo = 3
 
-    def milk(self):
-        return "Milk"
-
-#Cow().milk() will raise an error as Cow().milk is the 'milk' attribute
-#set in Mammal.__init__, not the 'milk' method defined on Cow.
+class B:
+    # BAD: _foo is shadowed by attribute A._foo
+    def _foo(self):
+        return 2
 
