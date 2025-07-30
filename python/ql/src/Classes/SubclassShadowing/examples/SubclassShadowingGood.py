63577f0cca1f9346390c46a34893468172f6c4d5
Joe Farebrother <joefarebrother@github.com>
2025-07-30 15:52:26 +0100

    Add extra example

--- python/ql/src/Classes/SubclassShadowing/examples/SubclassShadowingGood.py
+++ python/ql/src/Classes/SubclassShadowing/examples/SubclassShadowingGood.py
@@ -1,9 +1,15 @@
 class A:
-    def __init__(self):
-        self._foo = 3
+    def __init__(self, default_func=None):
+        if default_func is not None:
+            self.default = default_func 
 
-class B:
-    # BAD: _foo is shadowed by attribute A._foo
-    def _foo(self):
-        return 2
-
+    # GOOD: The shadowing behavior is explicitly intended in the superclass.
+    def default(self):
+        return []
+    
+class B(A):
+    
+    # Subclasses may override the method `default`, which will still be shadowed by the attribute `default` if it is set.
+    # As this is part of the expected behavior of the superclass, this is fine. 
+    def default(self):
+        return {}
