652e9cba3d8a1fcb3a54a69b63826e2a2715f134
Napalys Klicius <napalys@github.com>
2025-08-06 11:23:02 +0200

    Java: Added inline test expectations for `java/visible-for-testing-abuse`

--- java/ql/test/query-tests/VisibleForTestingAbuse/packageone/SourcePackage.java
+++ java/ql/test/query-tests/VisibleForTestingAbuse/packageone/SourcePackage.java
@@ -5,6 +5,6 @@ import packagetwo.Annotated;
 public class SourcePackage extends Annotated {
     void f() {
         AnnotatedClass a = new AnnotatedClass(); // COMPLIANT - same package
-        String s1 = Annotated.m1; // NON_COMPLIANT
+        String s1 = Annotated.m1; // $ Alert
     }
 }
--- java/ql/test/query-tests/VisibleForTestingAbuse/packagetwo/Source.java
+++ java/ql/test/query-tests/VisibleForTestingAbuse/packagetwo/Source.java
@@ -4,9 +4,9 @@ import packageone.*;
 
 public class Source {
     void f() {
-        int i = Annotated.f(); // NON_COMPLIANT
-        String s = Annotated.m; // NON_COMPLIANT
-        AnnotatedClass a = new AnnotatedClass(); // NON_COMPLIANT
+        int i = Annotated.f(); // $ Alert
+        String s = Annotated.m; // $ Alert
+        AnnotatedClass a = new AnnotatedClass(); // $ Alert
         String s1 = Annotated.m1; // COMPLIANT - same package
     }
 }
