96f6832a6f789f88c46788c3ca4868d9c42d9aec
REDMOND\brodes <brodes@microsoft.com>
2025-10-06 10:07:15 -0400

    Crypto: Updating expected files for unit tests.

--- java/ql/test/experimental/library-tests/quantum/jca/SignatureOperation.java
+++ java/ql/test/experimental/library-tests/quantum/jca/SignatureOperation.java
@@ -357,7 +357,6 @@ public class SignatureOperation {
 
         // ============ Edge Cases ============
         signAndVerifyEmptyMessage();
-        signAndVerifyNonAsciiMessage();
         tamperSignatureEdgeCase();
         dynamicSignatureSelectionDemo();
     }
