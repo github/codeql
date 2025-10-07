5c6d187ef2342c4f2997d59612d68523c168c302
idrissrio <idrissrio@github.com>
2025-10-07 09:19:58 +0200

    Java: Fix buildless test HTTP server binding on macOS26

--- java/ql/integration-tests/java/buildless-dependency-different-repository/test.py
+++ java/ql/integration-tests/java/buildless-dependency-different-repository/test.py
@@ -4,8 +4,8 @@ import logging
 
 def test(codeql, java):
     # Each of these serves the "repo" and "repo2" directories on http://localhost:924[89]
-    repo_server_process = subprocess.Popen(["python3", "-m", "http.server", "9428"], cwd="repo")
-    repo_server_process2 = subprocess.Popen(["python3", "-m", "http.server", "9429"], cwd="repo2")
+    repo_server_process = subprocess.Popen(["python3", "-m", "http.server", "9428", "-b", "localhost"], cwd="repo")
+    repo_server_process2 = subprocess.Popen(["python3", "-m", "http.server", "9429", "-b", "localhost"], cwd="repo2")
     try:
         codeql.database.create(
             extractor_option="buildless=true",
