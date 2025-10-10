f16742bf743c1f91a7c6c04763f04d9f81a51c7e
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2025-10-10 16:45:12 +0100

    Rust: Add models.

--- rust/ql/test/query-tests/security/CWE-089/mysql.rs
+++ rust/ql/test/query-tests/security/CWE-089/mysql.rs
@@ -10,29 +10,29 @@ mod sync_test
         let mut conn2: Conn = pool.get_conn()?.unwrap();
 
         // construct queries
-        let mut remote_string = reqwest::blocking::get("http://example.com/").unwrap().text().unwrap_or(String::from("")); // $ MISSING: Source=remote10
+        let mut remote_string = reqwest::blocking::get("http://example.com/").unwrap().text().unwrap_or(String::from("")); // $ Source=remote10
         let safe_query = String::from("SELECT * FROM people WHERE firstname='Alice'");
         let unsafe_query = String::from("SELECT * FROM people WHERE firstname='") + &remote_string + "'";
         let prepared_query = String::from("SELECT * FROM people WHERE firstname=?"); // (prepared arguments are safe)
 
         // direct execution (safe)
-        let _ : Vec<i64> = conn.query(safe_query.as_str())?;
+        let _ : Vec<i64> = conn.query(safe_query.as_str())?; // $ sql-sink
 
         // direct execution (unsafe)
-        let _ : Vec<i64> = conn.query(unsafe_query.as_str())?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote10
-        let _ : Vec<Result<i64, FromRowError>> = conn.query_opt(unsafe_query.as_str())?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote10
-        conn.query_drop(unsafe_query.as_str()); // $ MISSING: sql-sink Alert[rust/sql-injection]=remote10
-        let _ : i64 = conn.query_first(unsafe_query.as_str())?.unwrap(); // $ MISSING: sql-sink Alert[rust/sql-injection]=remote10
-        let _ : Result<i64, FromRowError>= conn.query_first_opt(unsafe_query.as_str())?.unwrap(); // $ MISSING: sql-sink Alert[rust/sql-injection]=remote10
-        let _ = conn.query_fold(unsafe_query.as_str(), 0, |_: i64, _: i64| -> i64 { 0 })?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote10
-        let _ = conn.query_fold_opt(unsafe_query.as_str(), 0, |_: i64, _: Result<i64, FromRowError>| -> i64 { 0 })?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote10
-        let _ = conn.query_iter(unsafe_query.as_str())?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote10
-        let _ = conn.query_map(unsafe_query.as_str(), |_: i64| -> () {})?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote10
-        let _ = conn.query_map_opt(unsafe_query.as_str(), |_: Result<i64, FromRowError>| -> () {})?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote10
-        let _ : Vec<i64> = conn2.query(unsafe_query.as_str())?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote10
+        let _ : Vec<i64> = conn.query(unsafe_query.as_str())?; // $ sql-sink Alert[rust/sql-injection]=remote10
+        let _ : Vec<Result<i64, FromRowError>> = conn.query_opt(unsafe_query.as_str())?; // $ sql-sink Alert[rust/sql-injection]=remote10
+        conn.query_drop(unsafe_query.as_str()); // $ sql-sink Alert[rust/sql-injection]=remote10
+        let _ : i64 = conn.query_first(unsafe_query.as_str())?.unwrap(); // $ sql-sink Alert[rust/sql-injection]=remote10
+        let _ : Result<i64, FromRowError>= conn.query_first_opt(unsafe_query.as_str())?.unwrap(); // $ sql-sink Alert[rust/sql-injection]=remote10
+        let _ = conn.query_fold(unsafe_query.as_str(), 0, |_: i64, _: i64| -> i64 { 0 })?; // $ sql-sink Alert[rust/sql-injection]=remote10
+        let _ = conn.query_fold_opt(unsafe_query.as_str(), 0, |_: i64, _: Result<i64, FromRowError>| -> i64 { 0 })?; // $ sql-sink Alert[rust/sql-injection]=remote10
+        let _ = conn.query_iter(unsafe_query.as_str())?; // $ sql-sink Alert[rust/sql-injection]=remote10
+        let _ = conn.query_map(unsafe_query.as_str(), |_: i64| -> () {})?; // $ sql-sink Alert[rust/sql-injection]=remote10
+        let _ = conn.query_map_opt(unsafe_query.as_str(), |_: Result<i64, FromRowError>| -> () {})?; // $ sql-sink Alert[rust/sql-injection]=remote10
+        let _ : Vec<i64> = conn2.query(unsafe_query.as_str())?; // $ sql-sink Alert[rust/sql-injection]=remote10
 
         // prepared queries (safe)
-        let stmt = conn.prep(prepared_query.as_str())?;
+        let stmt = conn.prep(prepared_query.as_str())?; // $ sql-sink
         let _ : Vec<i64> = conn.exec(&stmt, (remote_string.as_str(),))?;
         let _ : Vec<Result<i64, FromRowError>> = conn.exec_opt(&stmt, (remote_string.as_str(),))?;
         let _ = conn.exec_batch(&stmt, vec![(remote_string.as_str(),)])?;
@@ -47,7 +47,7 @@ mod sync_test
 
         // transactions
         let mut trans = conn.start_transaction(TxOpts::default())?;
-        trans.query_drop(unsafe_query.as_str()); // $ MISSING: sql-sink Alert[rust/sql-injection]=remote11
+        trans.query_drop(unsafe_query.as_str()); // $ sql-sink Alert[rust/sql-injection]=remote10
         trans.commit()?;
 
         Ok(())
@@ -65,25 +65,25 @@ mod async_test
         let mut conn = pool.get_conn().await?;
 
         // construct queries
-        let mut remote_string = reqwest::blocking::get("http://example.com/").unwrap().text().unwrap_or(String::from("")); // $ MISSING: Source=remote11
+        let mut remote_string = reqwest::blocking::get("http://example.com/").unwrap().text().unwrap_or(String::from("")); // $ Source=remote11
         let safe_query = String::from("SELECT * FROM people WHERE firstname='Alice'");
         let unsafe_query = String::from("SELECT * FROM people WHERE firstname='") + &remote_string + "'";
         let prepared_query = String::from("SELECT * FROM people WHERE firstname=?"); // (prepared arguments are safe)
 
         // direct execution (safe)
-        let _ : Vec<i64> = conn.query(safe_query.as_str()).await?;
+        let _ : Vec<i64> = conn.query(safe_query.as_str()).await?; // $ sql-sink
 
         // direct execution (unsafe)
-        let _ : Vec<i64> = conn.query(unsafe_query.as_str()).await?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote11
-        conn.query_drop(unsafe_query.as_str()); // $ MISSING: sql-sink Alert[rust/sql-injection]=remote11
-        let _ : Option<i64> = conn.query_first(unsafe_query.as_str()).await?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote11
-        let _ = conn.query_fold(unsafe_query.as_str(), 0, |_: i64, _: i64| -> i64 { 0 }).await?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote11
-        let _ = conn.query_iter(unsafe_query.as_str()).await?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote11
-        let _ = conn.query_stream::<i64, &str>(unsafe_query.as_str()).await?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote11
-        let _ = conn.query_map(unsafe_query.as_str(), |_: i64| -> () {}).await?; // $ MISSING: sql-sink Alert[rust/sql-injection]=remote11
+        let _ : Vec<i64> = conn.query(unsafe_query.as_str()).await?; // $ sql-sink Alert[rust/sql-injection]=remote11
+        conn.query_drop(unsafe_query.as_str()); // $ sql-sink Alert[rust/sql-injection]=remote11
+        let _ : Option<i64> = conn.query_first(unsafe_query.as_str()).await?; // $ sql-sink Alert[rust/sql-injection]=remote11
+        let _ = conn.query_fold(unsafe_query.as_str(), 0, |_: i64, _: i64| -> i64 { 0 }).await?; // $ sql-sink Alert[rust/sql-injection]=remote11
+        let _ = conn.query_iter(unsafe_query.as_str()).await?; // $ sql-sink Alert[rust/sql-injection]=remote11
+        let _ = conn.query_stream::<i64, &str>(unsafe_query.as_str()).await?; // $ sql-sink Alert[rust/sql-injection]=remote11
+        let _ = conn.query_map(unsafe_query.as_str(), |_: i64| -> () {}).await?; // $ sql-sink Alert[rust/sql-injection]=remote11
 
         // prepared queries (safe)
-        let stmt = conn.prep(prepared_query.as_str()).await?;
+        let stmt = conn.prep(prepared_query.as_str()).await?; // $ sql-sink
         let _ : Vec<i64> = conn.exec(&stmt, (remote_string.as_str(),)).await?;
         let _ = conn.exec_batch(&stmt, vec![(remote_string.as_str(),)]).await?;
         conn.exec_drop(&stmt, (&remote_string.as_str(),));
@@ -95,7 +95,7 @@ mod async_test
 
         // transactions
         let mut trans = conn.start_transaction(TxOpts::default()).await?;
-        trans.query_drop(unsafe_query.as_str()); // $ MISSING: sql-sink Alert[rust/sql-injection]=remote11
+        trans.query_drop(unsafe_query.as_str()); // $ sql-sink Alert[rust/sql-injection]=remote11
         trans.commit().await?;
 
         Ok(())
