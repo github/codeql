b1af95b3a74046bda93675f782cd5664097a9234
Tom Hvitved <hvitved@github.com>
2025-11-04 11:51:36 +0100

    Rust: Reintroduce extractor resolution

--- rust/extractor/src/translate/mappings.rs
+++ rust/extractor/src/translate/mappings.rs
@@ -1,4 +1,6 @@
 use crate::trap::{Label, TrapClass};
+use ra_ap_hir::{Enum, Function, HasContainer, Module, Semantics, Struct, Trait, Union};
+use ra_ap_ide_db::RootDatabase;
 use ra_ap_syntax::{AstNode, ast, ast::RangeItem};
 
 pub(crate) trait HasTrapClass: AstNode {
@@ -61,5 +63,104 @@ impl TextValue for ast::RangeExpr {
 impl TextValue for ast::RangePat {
     fn try_get_text(&self) -> Option<String> {
         self.op_token().map(|x| x.text().to_string())
+    }
+}
+
+pub(crate) trait AddressableHir<Ast: AstNode>: HasContainer + Copy {
+    fn name(self, sema: &Semantics<'_, RootDatabase>) -> Option<String>;
+    fn module(self, sema: &Semantics<'_, RootDatabase>) -> Module;
+}
+
+impl AddressableHir<ast::Fn> for Function {
+    fn name(self, sema: &Semantics<'_, RootDatabase>) -> Option<String> {
+        Some(self.name(sema.db).as_str().to_owned())
+    }
+
+    fn module(self, sema: &Semantics<'_, RootDatabase>) -> Module {
+        self.module(sema.db)
+    }
+}
+
+impl AddressableHir<ast::Trait> for Trait {
+    fn name(self, sema: &Semantics<'_, RootDatabase>) -> Option<String> {
+        Some(self.name(sema.db).as_str().to_owned())
+    }
+
+    fn module(self, sema: &Semantics<'_, RootDatabase>) -> Module {
+        self.module(sema.db)
+    }
+}
+
+impl AddressableHir<ast::Module> for Module {
+    fn name(self, sema: &Semantics<'_, RootDatabase>) -> Option<String> {
+        self.name(sema.db).map(|s| s.as_str().to_owned())
+    }
+
+    fn module(self, _sema: &Semantics<'_, RootDatabase>) -> Module {
+        self
+    }
+}
+
+impl AddressableHir<ast::Struct> for Struct {
+    fn name(self, sema: &Semantics<'_, RootDatabase>) -> Option<String> {
+        Some(self.name(sema.db).as_str().to_owned())
+    }
+
+    fn module(self, sema: &Semantics<'_, RootDatabase>) -> Module {
+        self.module(sema.db)
+    }
+}
+
+impl AddressableHir<ast::Enum> for Enum {
+    fn name(self, sema: &Semantics<'_, RootDatabase>) -> Option<String> {
+        Some(self.name(sema.db).as_str().to_owned())
+    }
+
+    fn module(self, sema: &Semantics<'_, RootDatabase>) -> Module {
+        self.module(sema.db)
+    }
+}
+
+impl AddressableHir<ast::Union> for Union {
+    fn name(self, sema: &Semantics<'_, RootDatabase>) -> Option<String> {
+        Some(self.name(sema.db).as_str().to_owned())
+    }
+
+    fn module(self, sema: &Semantics<'_, RootDatabase>) -> Module {
+        self.module(sema.db)
+    }
+}
+
+pub trait PathAst: AstNode {
+    fn path(&self) -> Option<ast::Path>;
+}
+
+impl PathAst for ast::PathExpr {
+    fn path(&self) -> Option<ast::Path> {
+        self.path()
+    }
+}
+
+impl PathAst for ast::RecordExpr {
+    fn path(&self) -> Option<ast::Path> {
+        self.path()
+    }
+}
+
+impl PathAst for ast::PathPat {
+    fn path(&self) -> Option<ast::Path> {
+        self.path()
+    }
+}
+
+impl PathAst for ast::RecordPat {
+    fn path(&self) -> Option<ast::Path> {
+        self.path()
+    }
+}
+
+impl PathAst for ast::TupleStructPat {
+    fn path(&self) -> Option<ast::Path> {
+        self.path()
     }
 }
