a69e78185b34cea6c895a566bd9f05936fad4fcd
Michael Nebel <michaelnebel@github.com>
2025-11-28 13:59:30 +0100

    C#: Pin tests and mark .NET 10 tests as flaky.

--- csharp/ql/integration-tests/all-platforms/dotnet_10/test.py
+++ csharp/ql/integration-tests/all-platforms/dotnet_10/test.py
@@ -1,5 +1,9 @@
+import pytest
+
+@pytest.mark.flaky(rerun_filter=lambda args*: runs_on.macos)
 def test1(codeql, csharp):
     codeql.database.create()
 
+@pytest.mark.flaky(rerun_filter=lambda args*: runs_on.macos)
 def test2(codeql, csharp):
     codeql.database.create(build_mode="none")
