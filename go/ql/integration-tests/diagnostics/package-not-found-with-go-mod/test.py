f837d90060d7f2be58fea9e3092ee5757ec3406c
Michael B. Gale <mbg@github.com>
2026-01-25 13:10:26 +0000

    Improve `go/autobuilder/package-not-found` diagnostic message

--- go/ql/integration-tests/diagnostics/package-not-found-with-go-mod/test.py
+++ go/ql/integration-tests/diagnostics/package-not-found-with-go-mod/test.py
@@ -4,4 +4,10 @@ import os
 def test(codeql, go, check_build_environment):
     check_build_environment.source_root = "work"
     os.environ["LGTM_INDEX_IMPORT_PATH"] = "test"
+
+    # The diagnostic message depends on the environment we are running in. To ensure consistent
+    # output, we set `GITHUB_ACTIONS` to `true` if we are not actually running in a workflow.
+    if (os.environ.get("GITHUB_ACTIONS", "") != "true"):
+        os.environ["GITHUB_ACTIONS"] = "true"
+
     codeql.database.create(source_root="work")
