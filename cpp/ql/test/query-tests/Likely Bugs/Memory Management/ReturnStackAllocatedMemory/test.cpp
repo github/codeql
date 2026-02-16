366ebcad834dc2a8b93e314fa80624f7ed7b5124
Jeroen Ketema <jketema@github.com>
2026-02-16 18:58:39 +0100

    C++: Add `cpp/return-stack-allocated-memory` test case

--- cpp/ql/test/query-tests/Likely Bugs/Memory Management/ReturnStackAllocatedMemory/test.cpp
+++ cpp/ql/test/query-tests/Likely Bugs/Memory Management/ReturnStackAllocatedMemory/test.cpp
@@ -250,3 +250,8 @@ void* test_strndupa(const char* s, size_t size) {
 	return s2; // BAD
 }
 
+int* f_rec(int *p, bool b) {
+  int x;
+  int* px = f_rec(&x, b); // GOOD [FALSE POSITIVE]
+  return p;
+}
