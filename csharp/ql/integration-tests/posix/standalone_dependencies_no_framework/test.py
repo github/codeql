fa73cd5d5c8233a1a1633f0f8fcc5147627df1e4
Óscar San José <oscarsj@github.com>
2026-02-17 18:49:51 +0100

    Remove unnecessary blank line in test.py

--- csharp/ql/integration-tests/posix/standalone_dependencies_no_framework/test.py
+++ csharp/ql/integration-tests/posix/standalone_dependencies_no_framework/test.py
@@ -3,7 +3,6 @@ import os
 from ..conftest import _supports_mono_nuget
 
 
-
 @pytest.mark.only_if(_supports_mono_nuget())
 def test(codeql, csharp):
     os.environ["CODEQL_EXTRACTOR_CSHARP_BUILDLESS_DOTNET_FRAMEWORK_REFERENCES"] = (
