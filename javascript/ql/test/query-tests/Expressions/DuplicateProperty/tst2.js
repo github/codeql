c673bd9151dab23dcf7ea9e743b753a04f39abec
Asger F <asgerf@github.com>
2026-02-27 13:34:55 +0100

    JS: Document a missing alert due to limitation in structural comparison

--- javascript/ql/test/query-tests/Expressions/DuplicateProperty/tst2.js
+++ javascript/ql/test/query-tests/Expressions/DuplicateProperty/tst2.js
@@ -7,7 +7,7 @@ var o = {
 
   setX: function(x) {
     this.x = x;
-  },
+  }, // $ MISSING: Alert // The structural comparison fails to treat the two 'x' variables as the same
 
   getX: function() {
     return this.x;
