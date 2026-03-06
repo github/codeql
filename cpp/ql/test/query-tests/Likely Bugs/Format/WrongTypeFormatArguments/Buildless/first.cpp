da99d3660d409775ea1d61646b630c831bc4f2cd
Geoffrey White <40627776+geoffw0@users.noreply.github.com>
2026-03-06 11:53:43 +0000

    C++: Turns out we can simplify.

--- cpp/ql/test/query-tests/Likely Bugs/Format/WrongTypeFormatArguments/Buildless/first.cpp
+++ cpp/ql/test/query-tests/Likely Bugs/Format/WrongTypeFormatArguments/Buildless/first.cpp
@@ -1,8 +1,3 @@
-// semmle-extractor-options: --expect_errors
-
-int printf(const char * format, ...);
 
 // defines type size_t plausibly
 typedef unsigned long size_t;
-
-#include "include_twice.h"
--- cpp/ql/test/query-tests/Likely Bugs/Format/WrongTypeFormatArguments/Buildless/second.cpp
+++ cpp/ql/test/query-tests/Likely Bugs/Format/WrongTypeFormatArguments/Buildless/second.cpp
@@ -5,4 +5,26 @@ int printf(const char * format, ...);
 // defines type `myFunctionPointerType`, referencing `size_t`
 typedef size_t (*myFunctionPointerType) ();
 
-#include "include_twice.h"
+void test_size_t() {
+    size_t s = 0;
+
+    printf("%zd", s); // GOOD
+    printf("%zi", s); // GOOD
+    printf("%zu", s); // GOOD [FALSE POSITIVE]
+    printf("%zx", s); // GOOD [FALSE POSITIVE]
+    printf("%d", s); // BAD
+    printf("%ld", s); // BAD
+    printf("%lld", s); // BAD
+    printf("%u", s); // BAD
+
+    char buffer[1024];
+
+    printf("%zd", &buffer[1023] - buffer); // GOOD
+    printf("%zi", &buffer[1023] - buffer); // GOOD
+    printf("%zu", &buffer[1023] - buffer); // GOOD
+    printf("%zx", &buffer[1023] - buffer); // GOOD
+    printf("%d", &buffer[1023] - buffer); // BAD
+    printf("%ld", &buffer[1023] - buffer); // BAD [NOT DETECTED]
+    printf("%lld", &buffer[1023] - buffer); // BAD [NOT DETECTED]
+    printf("%u", &buffer[1023] - buffer); // BAD
+}
