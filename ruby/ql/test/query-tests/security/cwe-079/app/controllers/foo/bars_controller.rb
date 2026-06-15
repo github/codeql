5e606b7befcea5a393a2c20ed9ca3650932b9dbd
Owen Mansel-Chan <owen-mc@github.com>
2026-06-15 22:33:29 +0100

    Don't use inline expectations when alerts in erb files

--- ruby/ql/test/query-tests/security/cwe-079/app/controllers/foo/bars_controller.rb
+++ ruby/ql/test/query-tests/security/cwe-079/app/controllers/foo/bars_controller.rb
@@ -6,34 +6,34 @@ class BarsController < ApplicationController
   end
 
   def user_name
-    return params[:user_name] # $ Source[rb/reflected-xss]
+    return params[:user_name]
   end
 
   def user_name_memo
-    @user_name ||= params[:user_name] # $ Source[rb/reflected-xss]
+    @user_name ||= params[:user_name]
   end
 
   def show
-    @user_website = params[:website] # $ Source[rb/reflected-xss]
-    dt = params[:text] # $ Source[rb/reflected-xss]
+    @user_website = params[:website]
+    dt = params[:text]
     @instance_text = dt
     @safe_foo = params[:text]
     @safe_foo = "safe_foo"
     @html_escaped = ERB::Util.html_escape(params[:text])
     @header_escaped = ERB::Util.html_escape(cookies[:foo]) # OK - cookies not controllable by 3rd party
-    response.header["content-type"] = params[:content_type] # $ Alert[rb/reflected-xss]
+    response.header["content-type"] = params[:content_type]
     response.header["x-customer-header"] = params[:bar] # OK - header not relevant to XSS
     render "foo/bars/show", locals: { display_text: dt, safe_text: "hello" }
   end
 
   def make_safe_html
-    str = params[:user_name] # $ Source[rb/reflected-xss]
-    str.html_safe # $ Alert[rb/reflected-xss]
+    str = params[:user_name]
+    str.html_safe
 
-    translate("welcome", name: params[:user_name]).html_safe # $ Alert[rb/reflected-xss] // NOT OK - translate preserves taint
-    t("welcome", name: params[:user_name]).html_safe # $ Alert[rb/reflected-xss] // NOT OK - t is an alias of translate
+    translate("welcome", name: params[:user_name]).html_safe # NOT OK - translate preserves taint
+    t("welcome", name: params[:user_name]).html_safe # NOT OK - t is an alias of translate
     t("welcome_html", name: params[:user_name]).html_safe # OK - t escapes html when key ends in _html
-    I18n.t("welcome_html", name: params[:user_name]).html_safe # $ Alert[rb/reflected-xss] // NOT OK - I18n.t does not escape html
-    I18n.translate("welcome_html", name: params[:user_name]).html_safe # $ Alert[rb/reflected-xss] // NOT OK - alias
+    I18n.t("welcome_html", name: params[:user_name]).html_safe # NOT OK - I18n.t does not escape html
+    I18n.translate("welcome_html", name: params[:user_name]).html_safe # NOT OK - alias
   end
 end
