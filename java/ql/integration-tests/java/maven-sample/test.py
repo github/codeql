af11f6e6185cf555a243130a935a2a523a9952a2
Mario Campos <mario-campos@github.com>
2026-06-23 21:04:05 +0000

    Use Maven Central mirror in Java Maven integration tests

--- java/ql/integration-tests/java/maven-sample/test.py
+++ java/ql/integration-tests/java/maven-sample/test.py
@@ -1,2 +1,8 @@
+import os
+
 def test(codeql, java):
-    codeql.database.create()
+    codeql.database.create(
+        _env={
+            "LGTM_INDEX_MAVEN_SETTINGS_FILE": os.path.join(os.path.dirname(os.path.realpath(__file__)), "settings.xml")
+        },
+    )
