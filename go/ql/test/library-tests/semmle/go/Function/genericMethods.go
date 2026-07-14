48c57e35c38d2d95c649f632e6fab5d29379105e
Jeroen Ketema <jketema@github.com>
2026-07-14 11:35:28 +0200

    Go: Fix crash in `getTypeParamParentLabel`

--- go/ql/test/library-tests/semmle/go/Function/genericMethods.go
+++ go/ql/test/library-tests/semmle/go/Function/genericMethods.go
@@ -11,9 +11,9 @@ func (*StructForGenericMethod2[P3]) GenericMethod2[P4 any](x P4) {}
 func generic_methods(s1 StructForGenericMethod1, s2 StructForGenericMethod2[int]) {
 	// Call the generic method specifying the type
 	s1.GenericMethod1[int](1)
-	//s2.GenericMethod2[string]("hello")
+	s2.GenericMethod2[string]("hello")
 
 	// Call the generic method without specifying the type
 	s1.GenericMethod1("hello")
-	//s2.GenericMethod2(42)
+	s2.GenericMethod2(42)
 }
