5ba941e465430b0dd6984152da4fdb71d1e85d73
Asger F <asgerf@github.com>
2026-07-29 13:35:50 +0200

    unified: Handle ConditionalPattern in local scoping

--- unified/ql/test/library-tests/variables/test.swift
+++ unified/ql/test/library-tests/variables/test.swift
@@ -80,8 +80,8 @@ func t10(value: Int) { // name=value1
 // Switch with multiple cases
 func t11(value: Int) { // name=value1
     switch value { // $ access=value1
-    case let x where x > 0: // $ MISSING: access=x1 // name=x1
-        print(x) // $ MISSING: access=x1
+    case let x where x > 0: // $ access=x1 // name=x1
+        print(x) // $ access=x1
     case let x: // name=x2
         print(x) // $ access=x2
     }
