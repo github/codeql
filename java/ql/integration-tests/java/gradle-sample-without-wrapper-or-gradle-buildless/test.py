08bc837b96a531228e0acd8f5ef982aa1581fc1a
Mads Navntoft <navntoft@github.com>
2026-08-10 13:35:46 +0200

    Java: Redact java_version in gradle-buildless integration test

--- java/ql/integration-tests/java/gradle-sample-without-wrapper-or-gradle-buildless/test.py
+++ java/ql/integration-tests/java/gradle-sample-without-wrapper-or-gradle-buildless/test.py
@@ -5,7 +5,7 @@ import pathlib
 
 # The version of gradle used doesn't work on java 17
 def test(codeql, use_java_11, java, environment, check_diagnostics):
-    check_diagnostics.redact += ["attributes.java_vendor"]
+    check_diagnostics.redact += ["attributes.java_vendor", "attributes.java_version"]
     gradle_override_dir = pathlib.Path(tempfile.mkdtemp())
     if runs_on.windows:
         (gradle_override_dir / "gradle.bat").write_text("@echo off\nexit /b 2\n")
