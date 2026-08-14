03eb9969780b3de7fdc26e5ad7f46e8eb937bfc4
Asger F <asgerf@github.com>
2026-08-14 13:30:45 +0200

    unified: Add AST mappings for string interpolation

--- unified/extractor/tests/corpus/swift/literals/string-with-interpolation.swift
+++ unified/extractor/tests/corpus/swift/literals/string-with-interpolation.swift
@@ -9,3 +9,9 @@
 
 // Plain string before and after interpolation
 "prefix \(value) suffix"
+
+// Calls to custom DefaultStringInterpolation.appendInterpolation impls
+"foo \(x, y)"
+"foo \(x, y, z)"
+"foo \(arg: x)"
+"foo \(arg: x, arg2: y)"
