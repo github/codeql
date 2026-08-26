0c05a19907b75b686d41be083b5d23488d79fff1
Asger F <asgerf@github.com>
2026-08-26 15:39:17 +0200

    unified: Add corpus with with 'case .foo, T.foo, [T].foo'

--- unified/extractor/tests/corpus/swift/control-flow/switch-with-binding-pattern.swift
+++ unified/extractor/tests/corpus/swift/control-flow/switch-with-binding-pattern.swift
@@ -3,4 +3,12 @@ case .circle(let r):
   print(r)
 case .square(let s):
   print(s)
+case .foo:
+  print("foo")
+case T.foo:
+  print("foo")
+// Ambiguous reference to a static property on the Array<T> type, or an instance property on Array<S> where S is the type of the expression T.
+// The parser can't tell the difference.
+case [T].foo:
+  print("foo")
 }
