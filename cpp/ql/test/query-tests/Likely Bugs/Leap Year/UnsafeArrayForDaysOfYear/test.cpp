f0de9c3913ba381d56832ddf3f083283d7ac3695
Mathias Vorreiter Pedersen <mathiasvp@github.com>
2026-08-28 13:39:54 +0100

    C++: Add a crazy false positive from a Microsoft internal issue.

--- cpp/ql/test/query-tests/Likely Bugs/Leap Year/UnsafeArrayForDaysOfYear/test.cpp
+++ cpp/ql/test/query-tests/Likely Bugs/Leap Year/UnsafeArrayForDaysOfYear/test.cpp
@@ -68,3 +68,12 @@ void VectorOfDays_FalsePositive(int dayOfYear, int x)
 
 	items[dayOfYear - 1] = x;
 }
+
+void f_______________________________________________________this_name_must_be_exactly_357_chars__________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________() {
+  // Using this magic compiler variable results in a `const char` array being
+  // initialized with the function name which (including `void`, a space,
+  // and `()`). in this case, this adds up to exactly 364 characters.
+  // The initializer for `__PRETTY_FUNCTION__` thus initializes an array of
+  // length 365 (because the null-terminator adds another character).
+  auto x = __PRETTY_FUNCTION__; // $ SPURIOUS: Alert
+}
