75d215d6adf5af3eff3aa8287d30dbd2095fe6eb
Asger F <asgerf@github.com>
2026-09-08 11:34:56 +0200

    unified: Add basic test with tuples

--- unified/ql/test/library-tests/dataflow/test.swift
+++ unified/ql/test/library-tests/dataflow/test.swift
@@ -6,3 +6,10 @@ func t2() {
     sink(source("t2.1") + "blah"); // $ hasTaintFlow=t2.1
     sink("blah" + source("t2.2")); // $ hasTaintFlow=t2.2
 }
+
+func t3() {
+    sink((source("t3.1"), "safe").0); // $ MISSING: hasValueFlow=t3.1
+    sink((source("t3.2"), "safe").1); // no flow
+    sink(("safe", source("t3.3")).0); // no flow
+    sink(("safe", source("t3.4")).1); // $ MISSING: hasValueFlow=t3.4
+}
