e7c8469cf76e5511d75fbb8c5bf57d0f042194ff
Tom Hvitved <hvitved@github.com>
2026-09-11 14:06:51 +0200

    Unified: Add tests for explicit `self`/`Self` qualifiers

---
+++ unified/ql/test/library-tests/static-name-binding/explicit-instance-field-access.swift
@@ -0,0 +1,47 @@
+private class A {
+    let x = 123 // name=A.instance.x
+
+    func getX() {
+        return self.x // not handled by static name binding
+    }
+
+    static let y = 456 // name=A.type.y
+
+    func getY1() {
+        return Self.y // $ MISSING: access=A access=A.type.y
+    }
+
+    static func getY2() {
+        return self.y // $ not handled by static name binding
+    }
+
+    class func z() -> Int { // name=A.type.z
+        return 789
+    }
+
+    class func getZ() {
+        return self.z // $ not handled by static name binding
+    }
+}
+
+private class B : A { // $ access=A
+    func getX2() {
+        return self.x // not handled by static name binding
+    }
+
+    func getY3() {
+        return Self.y // $ MISSING: access=B access=A.type.y
+    }
+
+    static func getY4() {
+        return self.y // $ not handled by static name binding
+    }
+
+    class func z() -> Int { // name=B.type.z
+        return 789
+    }
+
+    class func getZ2() {
+        return self.z // $ not handled by static name binding
+    }
+}
