6fe1db031d99de6b27acd56d81a69b743c492f65
Asger F <asgerf@github.com>
2026-09-15 11:25:55 +0200

    unified: Add sink calls without flow

--- unified/ql/test/library-tests/dataflow/implicit-self.swift
+++ unified/ql/test/library-tests/dataflow/implicit-self.swift
@@ -7,41 +7,49 @@ class C {
     var box = Box()
 
     func t1() {
+        sink(self.x); // no flow
         self.x = source("t1.1");
         sink(self.x); // $ hasValueFlow=t1.1
     }
 
     func t2() {
+        sink(x); // no flow
         x = source("t2.1");
         sink(x); // $ hasValueFlow=t2.1
     }
 
     func t3() {
+        sink(self.x); // no flow
         x = source("t3.1");
         sink(self.x); // $ hasValueFlow=t3.1
     }
 
     func t4() {
+        sink(x); // no flow
         self.x = source("t4.1");
         sink(x); // $ hasValueFlow=t4.1
     }
 
     func t5() {
+        sink(self.box.x); // no flow
         self.box.x = source("t5.1");
         sink(self.box.x); // $ hasValueFlow=t5.1
     }
 
     func t6() {
+        sink(box.x); // no flow
         box.x = source("t6.1");
         sink(box.x); // $ hasValueFlow=t6.1
     }
 
     func t7() {
+        sink(self.box.x); // no flow
         box.x = source("t7.1");
         sink(self.box.x); // $ hasValueFlow=t7.1
     }
 
     func t8() {
+        sink(box.x); // no flow
         self.box.x = source("t8.1");
         sink(box.x); // $ hasValueFlow=t8.1
     }
